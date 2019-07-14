1ebfdbdcfa9ba42f728dad5be5d603931da62a50
Anouer Hermassi <ahermass@stevens.edu>
2019-07-14 16:36:41 +0100

    Same solution but in constant space

--- Arrays/832. Flipping an Image.py
+++ Arrays/832. Flipping an Image.py
@@ -10,8 +10,21 @@ def flip_and_invert_image(A):
     inverted = [list(map(invert, row)) for row in flipped]
     return inverted
 
+
+def flip_and_invert_image_v2(A):  # Constant space
+    """
+    :type A: List[List[int]]
+    :rtype: List[List[int]]
+    Time complexity: O(N) where N is the number of elements in A (number of lists)
+    Space complexity: O(1) since the transformation is in-place
+    """
+    invert = lambda x: 0 if x == 1 else 1
+    A = [list(map(invert, row[::-1])) for row in A]
+    return A
+
+
 if __name__ == '__main__':
     A = [[1,1,0],[1,0,1],[0,0,0]]
-    inverted = flip_and_invert_image(A)
+    inverted = flip_and_invert_image_v2(A)
     print('Array before inversion: ', A)
     print('Array After inversion: ', inverted)
