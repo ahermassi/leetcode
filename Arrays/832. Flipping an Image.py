b05e2dd15aed65c19043b82ea1a2ccd8b8d63f88
Anouer Hermassi <ahermass@stevens.edu>
2019-07-14 16:27:11 +0100

    Added space complexity analysis

--- Arrays/832. Flipping an Image.py
+++ Arrays/832. Flipping an Image.py
@@ -3,12 +3,12 @@ def flip_and_invert_image(A):
     :type A: List[List[int]]
     :rtype: List[List[int]]
     Time complexity: O(N) where N is the number of elements in A (number of lists)
+    Space complexity: O(N) where N is the number of A elements, since a new array is introduced to hold the result
     """
     flipped = [lst[::-1] for lst in A]
     invert = lambda x: 0 if x == 1 else 1
     inverted = [list(map(invert, row)) for row in flipped]
     return inverted
-
 
 if __name__ == '__main__':
     A = [[1,1,0],[1,0,1],[0,0,0]]
