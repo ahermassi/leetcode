997a187417e68c07514525c0d0deae81354b5b33
Anouer Hermassi <ahermass@stevens.edu>
2019-07-16 18:49:16 +0100

    Finding the contiguous subarray which has the largest sum

---
+++ Arrays/53_Maximum_Subarray.py
@@ -0,0 +1,34 @@
+""" Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest
+sum and return its sum. """
+
+import unittest2 as unittest
+
+
+def maximum_subarray_v1(nums):
+    """ Greedy approach. The problem to find maximum (or minimum) element (or sum) with a single array as the input
+    is a good candidate to be solved by the greedy approach in linear time. Assume first element is the maximum (
+    local and global) sum. Iterate over the array, and at each step ask yourself: which is greater, the current
+    element x or (last sum + x) ? Update the local sum and global sum accordingly.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+    curr_sum = max_sum = nums[0]
+    for i in range(1, len(nums)):
+        curr_sum = max(nums[i], curr_sum + nums[i])  # Continue summing up or start over (greedy)
+        max_sum = max(max_sum, curr_sum)  # Update global max
+    return max_sum
+
+
+class Test(unittest.TestCase):
+    data = [
+        ([-2,1,-3,4,-1,2,1,-5,4], 6),
+        ([0, -1, 5], 5)
+    ]
+
+    def test_two_sum(self):
+        for test_array, result in self.data:
+            self.assertEqual(result, maximum_subarray_v1(test_array))
+
+
+if __name__ == '__main__':
+    unittest.main()
