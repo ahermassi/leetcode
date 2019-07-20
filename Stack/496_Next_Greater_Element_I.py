5de27e1d067f489f8e9f1c75e8f21b3f5281bfee
Anouer Hermassi <ahermass@stevens.edu>
2019-07-20 13:11:15 +0100

    Find next greater element in an array using a stack

--- Stack/496_Next_Greater_Element_I.py
+++ Stack/496_Next_Greater_Element_I.py
@@ -7,7 +7,7 @@ Output: [-1,3,-1] """
 import unittest2 as unittest
 
 
-def next_greater_element(nums1, nums2):
+def next_greater_element_v1(nums1, nums2):
     """ Brute force approach.
     Time complexity: O(N * M) where N is the length of nums1 and M is the length of nums2
     Space complexity: O(N)
@@ -23,6 +23,23 @@ def next_greater_element(nums1, nums2):
     return res
 
 
+def next_greater_element_v2(nums1, nums2):
+    """ We use a stack to keep a decreasing sub-sequence. Whenever we see a number x greater than stack.peek() we pop
+    all elements less than x and for all the popped ones, their next greater element is x.
+    Time complexity: O(max(N, M)) where N is the length of nums1 and M is the length of nums2
+    Space complexity: O(max(N, M))
+    """
+    cache, stack, res = {}, [], []
+    for i in nums2:
+        while len(stack) and i > stack[-1]:
+            cache[stack.pop()] = i
+        else:
+            stack.append(i)
+    for i in nums1:
+        res.append(cache.get(i, -1))
+    return res
+
+
 class Test(unittest.TestCase):
     data = [([4, 1, 2], [1, 3, 4, 2], [-1, 3, -1]),
             ([2, 4], [1, 2, 3, 4], [3, -1])
@@ -30,7 +47,8 @@ class Test(unittest.TestCase):
 
     def test_move_zeroes(self):
         for nums1, nums2, result in self.data:
-            self.assertEqual(result, next_greater_element(nums1, nums2))
+            self.assertEqual(result, next_greater_element_v1(nums1, nums2))
+            self.assertEqual(result, next_greater_element_v2(nums1, nums2))
 
 
 if __name__ == '__main__':
