ba871aedadd011a778ccee5cef61c09e32d1c94e
Anouer Hermassi <ahermass@stevens.edu>
2019-07-25 12:01:04 +0100

    Converting a sorted array to a BST

---
+++ DFS/108_Convert_Sorted_Array_to_BST.py
@@ -0,0 +1,48 @@
+""" Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of
+every node never differ by more than 1. """
+
+import unittest2 as unittest
+
+
+# Definition for a  binary tree node
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def sorted_array_to_bst_v1(nums):
+    """ For a sorted array, the left half will be in the left subtree, middle value as the root, right half in the
+    right subtree. This holds true for every node.
+    Time complexity: since Python slicing takes O(N), this algorithm is actually O(N log N)
+    Space complexity: O(log N) (draw recursion tree)
+    """
+    if not nums:
+        return
+    if len(nums) == 1:
+        return TreeNode(nums[0])
+    mid = len(nums) // 2
+    root = TreeNode(nums[mid])
+    root.left = sorted_array_to_bst_v1(nums[:mid])
+    root.right = sorted_array_to_bst_v1(nums[mid + 1:])
+    return root
+
+
+class Test(unittest.TestCase):
+    data = [-10, -3, 0, 5, 9]
+
+    def test_is_alien_sorted1(self):
+        root = sorted_array_to_bst_v1(self.data)
+        self.assertEqual(0, root.val)
+        self.assertEqual(-3, root.left.val)
+        self.assertEqual(9, root.right.val)
+        self.assertEqual(-10, root.left.left.val)
+        self.assertEqual(5, root.right.left.val)
+
+
+if __name__ == '__main__':
+    unittest.main()
