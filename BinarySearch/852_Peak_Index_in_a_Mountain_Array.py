94fe7847f78bb39b2141383bedd0486ca9e1b7e2
Anouer Hermassi <ahermass@stevens.edu>
2019-07-28 22:09:32 +0100

    Finding peak index in a mountain array - Recursive binary search

--- BinarySearch/852_Peak_Index_in_a_Mountain_Array.py
+++ BinarySearch/852_Peak_Index_in_a_Mountain_Array.py
@@ -11,14 +11,35 @@ def peak_index_in_mountain_array_v1(A):
             return i
 
 
+def peak_index_in_mountain_array_v2(A):
+    """ Recursive binary search. The idea is that 2 conditions invalidate a mountain array:
+        A[peak_index - 1] > A[peak_index]
+        A[peak_index + 1] > A[peak_index]
+    Time complexity: O(log N)
+    Space complexity: O(log N)
+    """
+
+    def find_peak_index(A, left, right):
+        if left == right:
+            return left
+        mid = (left + right) // 2
+        if A[mid - 1] > A[mid]:
+            return find_peak_index(A, left, mid - 1)
+        elif A[mid + 1] > A[mid]:
+            return find_peak_index(A, mid + 1, right)
+        return mid
+
+    return find_peak_index(A, 0, len(A) - 1)
+
+
 class Test(unittest.TestCase):
     data = [([0, 1, 0], 1),
             ([0, 2, 1, 0], 1)
             ]
 
-    def test_two_sum(self):
+    def test_peak_index_in_mountain_array_(self):
         for test_array, result in self.data:
-            self.assertEqual(result, peak_index_in_mountain_array_v1(test_array))
+            self.assertEqual(result, peak_index_in_mountain_array_v2(test_array))
 
 
 if __name__ == '__main__':
