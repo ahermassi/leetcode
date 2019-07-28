8ecfe3ac1b3b87be0cf8eedf7f739196fadf787a
Anouer Hermassi <ahermass@stevens.edu>
2019-07-28 12:00:39 +0100

    Merging two sorted lists - Iterative

--- LinkedList/21_Merge_Two_Sorted_Lists.py
+++ LinkedList/21_Merge_Two_Sorted_Lists.py
@@ -15,7 +15,7 @@ class ListNode:
         self.next = None
 
 
-def merge_two_lists(l1, l2):
+def merge_two_lists_v1(l1, l2):
     """ We can recursively define the result of a merge operation on two lists as the following:
         list1[0] + merge(list1[1:], list2)  if  list1[0]<list2[0]
         list2[0] + merge(list1, list2[1:])  otherwise
@@ -28,11 +28,33 @@ def merge_two_lists(l1, l2):
     if not l1 or not l2:
         return l1 or l2
     if l1.val < l2.val:
-        l1.next = merge_two_lists(l1.next, l2)  # This is the case list1[0] + merge(list1[1:], list2)
+        l1.next = merge_two_lists_v1(l1.next, l2)  # This is the case list1[0] + merge(list1[1:], list2)
         return l1
     else:
-        l2.next = merge_two_lists(l1, l2.next)  # This is the case list2[0] + merge(list1, list2[1:])
+        l2.next = merge_two_lists_v1(l1, l2.next)  # This is the case list2[0] + merge(list1, list2[1:])
         return l2
+
+
+def merge_two_lists_v2(l1, l2):
+    """ We have a dummy head, and a tail indicating last node in this growing List. At each step we append either l1
+        or l2 to tail, and update tail, l1 or l2 accordingly. After the loop terminates, at most one of l1 and l2 is
+        non-null. Therefore (because the input lists were in SORTED order), if either list is non-null, it contains only
+        elements greater than all of the previously-merged elements.
+    Time complexity: O(N + M), the while loop runs for a number of iterations equal to sum of lengths of the two lists.
+    Space complexity: O(1), the iterative approach only allocates a few pointers, so it has a constant overall memory
+    footprint.
+    """
+    head = tail = ListNode(0)  # Dummy head and tail, starting at same position
+    while l1 and l2:
+        if l1.val <= l2.val:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
+    tail.next = l1 or l2
+    return head.next
 
 
 class Test(unittest.TestCase):
@@ -42,7 +64,7 @@ class Test(unittest.TestCase):
     head2 = ListNode(1)
     head2.next = ListNode(3)
     head2.next.next = ListNode(4)
-    head = merge_two_lists(head1, head2)
+    head = merge_two_lists_v2(head1, head2)
 
     def test_merge_two_lists(self):
         self.assertEqual(1, self.head.val)
