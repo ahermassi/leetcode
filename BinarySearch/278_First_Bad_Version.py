543c65f3fdeaf1dd41c812a009e9006da5689ce9
Anouer Hermassi <ahermass@stevens.edu>
2019-07-29 14:01:07 +0100

    Find first bad version

---
+++ BinarySearch/278_First_Bad_Version.py
@@ -0,0 +1,22 @@
+def is_bad_version(version):
+    pass
+
+
+def first_bad_version(n):
+    """ Classic binary search. However:
+        When is_bad_version(mid) is False, we know that all versions preceding and including mid are all good. So we set
+        left = mid + 1 to indicate that the new search space is the interval [mid + 1, right] (inclusive).
+        When is_bad_version(mid) is True, mid may or may not be the first bad version, but we can tell for sure that
+        all versions after mid can be discarded. Therefore we set right = mid - 1 as the new search space of interval
+        [left, mid - 1] (inclusive).
+    Time complexity: O(log n), the search space is halved each time
+    Space complexity: O(1)
+    """
+    left, right = 1, n
+    while left <= right:
+        mid = (left + right) // 2
+        if not is_bad_version(mid):
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
