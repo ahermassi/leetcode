f8bc0fbcc52369721b5d9483b2d9345f964ed2c3
Anouer Hermassi <ahermass@stevens.edu>
2019-08-01 20:43:00 +0100

    Merge two sorted arrays - Constant space

--- Arrays/88_Merge_Sorted_Array.py
+++ Arrays/88_Merge_Sorted_Array.py
@@ -30,12 +30,31 @@ def merge_v1(nums1, m, nums2, n):
         nums1[i + j:] = nums2[j:]
 
 
+def merge_v2(nums1, m, nums2, n):
+    """ If we start to overwrite nums1 from the end, where there is no information yet, then no additional space
+    is needed.
+    Time complexity: O(n + m)
+    Space complexity: O(1)
+    """
+    first_zero = len(nums1) - 1  # This is the first slot where a new element can be moved
+    while m > 0 and n > 0:
+        if nums1[m - 1] >= nums2[n - 1]:  # If the last non-zero in nums1 is >= last non-zero in nums2
+            nums1[first_zero] = nums1[m - 1]  # Move that last non-zero of nums1 to the end of nums1
+            m -= 1
+        else:
+            nums1[first_zero] = nums2[n - 1]
+            n -= 1
+        first_zero -= 1
+    if n > 0:
+        nums1[:n] = nums2[:n]
+
+
 class Test(unittest.TestCase):
     data = [([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3, [1, 2, 2, 3, 5, 6])]
 
     def test_merge(self):
         for test_array1, m, test_array2, n, result in self.data:
-            merge_v1(test_array1, m, test_array2, n)
+            merge_v2(test_array1, m, test_array2, n)
             self.assertEqual(result, test_array1)
 
 
