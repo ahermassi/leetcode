4bfde7511850f8688d9cfcb61208116f7f51126b
Anouer Hermassi <ahermass@stevens.edu>
2019-08-03 14:52:17 +0100

    Read N characters using read4()

---
+++ Strings/157_Read_N_Characters_Given_Read4.py
@@ -0,0 +1,26 @@
+""" Given a file and assume that you can only read the file using a given method read4, implement a method to read n
+characters. """
+
+
+def read4(buf):
+    pass
+
+
+def read(buf, n):
+    """ This function is to read n characters into buf only 4 characters at a time using read4() function.
+    """
+    i = 0
+    while i < n:
+        buf4 = [''] * 4  # Temporary buffer to read characters into
+        count = read4(buf)
+        if not count:  # This check is used for cases when we attempt to read more characters than the actual length
+            # of the file, in which case read4 returns count = 0
+            break
+        count = min(count, n - i)  # This is for cases when the remaining number of characters in the file is greater
+        # than the remaining number of characters we want to read. Example: 'leetcode', n = 5. First pass we read 4
+        # characters 'leet', second pass read4 reads 'code' and returns count = 4, but we want to read only one
+        # character more: 5 - 4 = 1
+        buf[i:] = buf4[:count]  # Read that exact number of characters from buf4 and append it to buf
+        i += count
+    return i
+
