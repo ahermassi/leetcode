94fce4103b71af9b9632189985f09ec3ff625d76
Anouer Hermassi <ahermass@stevens.edu>
2019-08-04 18:12:39 +0100

    Find a tree path that adds up to a sum - DFS

--- Tree/112_Path_Sum.py
+++ Tree/112_Path_Sum.py
@@ -26,6 +26,29 @@ def has_path_sum_v1(root, sum):
     return has_path_sum_v1(root.left, sum) or has_path_sum_v1(root.right, sum)
 
 
+def has_path_sum_v2(root, sum):
+    """ We start from a stack which contains the root node and the corresponding remaining sum which is
+        (sum - root.val). Then we proceed to the iterations: pop the current node out of the stack and return True if
+        the remaining sum is 0 and we're on the leaf node. If the remaining sum is not zero or we're not on the leaf yet
+        then we push the child nodes and corresponding remaining sums into stack.
+    Time complexity: O(N)
+    Space complexity: in the worst case, the tree is completely unbalanced and we would keep all N nodes in the stack
+    so O(N); in the best case (the tree is completely balanced), it is O(log N) which is the height of the tree
+    """
+    if not root:
+        return False
+    stack = [(root, sum - root.val)]
+    while stack:
+        node, curr_sum = stack.pop()
+        if not node.left and not node.right and curr_sum == 0:
+            return True
+        if node.left:
+            stack.append((node.left, curr_sum - node.left.val))
+        if node.right:
+            stack.append((node.right, curr_sum - node.right.val))
+    return False
+
+
 class Test(unittest.TestCase):
     root = TreeNode(5)
     root.left = TreeNode(4)
@@ -40,6 +63,7 @@ class Test(unittest.TestCase):
 
     def test_find_target(self):
         self.assertEqual(True, has_path_sum_v1(self.root, 22))
+        self.assertEqual(True, has_path_sum_v2(self.root, 22))
 
 
 if __name__ == '__main__':
