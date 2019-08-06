2bf3d49ac9470637f7909640a302622e2662dcbc
Anouer Hermassi <ahermass@stevens.edu>
2019-08-06 15:26:25 +0100

    Design a logger rate limiter

---
+++ HashTable/359_Logger_Rate_Limiter.py
@@ -0,0 +1,59 @@
+""" Design a logger system that receive stream of messages along with its timestamps, each message should be printed
+if and only if it is not printed in the last 10 seconds.
+Given a message and a timestamp (in seconds granularity), return true if the message should be printed in the given
+timestamp, otherwise returns false. """
+
+import unittest2 as unittest
+
+
+class Logger(object):
+    """ Possible optimization (follow-up):
+        for a Logger, probably the solution is not that practical since the hash table soon will blow up.
+        We can have another thread running cron job to evict timeout entries from the hash map who existed for more
+        than 10 seconds.
+    """
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.log = {}
+
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        :type timestamp: int
+        :type message: str
+        :rtype: bool
+        """
+        if message not in self.log:
+            self.log[message] = timestamp
+            return True
+        if timestamp - self.log[message] >= 10:
+            self.log[message] = timestamp
+            return True
+        return False
+
+
+class Test(unittest.TestCase):
+    logger = Logger()
+    foo1 = logger.shouldPrintMessage(1, "foo")
+    bar1 = logger.shouldPrintMessage(2, "bar")
+    foo2 = logger.shouldPrintMessage(3, "foo")
+    bar2 = logger.shouldPrintMessage(8, "bar")
+    foo3 = logger.shouldPrintMessage(10, "foo")
+    foo4 = logger.shouldPrintMessage(11, "foo")
+
+    def test_logger(self):
+        self.assertTrue(self.foo1)
+        self.assertTrue(self.bar1)
+        self.assertFalse(self.foo2)
+        self.assertFalse(self.bar2)
+        self.assertFalse(self.foo3)
+        self.assertTrue(self.foo4)
+
+
+if __name__ == '__main__':
+    unittest.main()
