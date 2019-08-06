b89faa7121ebd6e410e5fc0fa77ac85b7843b54c
Anouer Hermassi <ahermass@stevens.edu>
2019-08-06 10:01:49 +0100

    Converting BST into a greater tree - Recursive

---
+++ Tree/538_Convert_BST_to_Greater_Tree.py
@@ -0,0 +1,63 @@
+""" Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed
+to the original key plus sum of all keys greater than the original key in BST.
+"""
+
+import unittest2 as unittest
+
+
+# Definition for a binary tree node.
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def convert_bst_v1(root):
+    """ This technique is known as reverse in-order traversal. The basic idea of such a traversal is that before
+        visiting any node in the tree, we must first visit all nodes with greater value, keeping sum of all values that
+        we have already visited and adding that sum to the node's values as we traverse the tree. Where are all of these
+        nodes conveniently located? In the right subtree.
+    Time complexity: O(N), function gets called on each node no more than once
+    Space complexity: O(N) in the worst case
+    """
+
+    def reverse_inorder(root, total):
+        if root:
+            total = reverse_inorder(root.right, total)
+            total += root.val
+            root.val = total
+            total = reverse_inorder(root.left, total)
+        return total
+
+    reverse_inorder(root, 0)
+    return root
+
+
+class Test(unittest.TestCase):
+    root = TreeNode(5)
+    root.left = TreeNode(2)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(0)
+    root.left.right = TreeNode(4)
+    root.right.left = TreeNode(7)
+    root.right.right = TreeNode(9)
+    root = convert_bst_v1(root)
+
+    def test_convert_bst(self):
+        self.assertEqual(29, self.root.val)
+        self.assertEqual(35, self.root.left.val)
+        self.assertEqual(17, self.root.right.val)
+        self.assertEqual(35, self.root.left.left.val)
+        self.assertEqual(33, self.root.left.right.val)
+        self.assertEqual(24, self.root.right.left.val)
+        self.assertEqual(9, self.root.right.right.val)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
