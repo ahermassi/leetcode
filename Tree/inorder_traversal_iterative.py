9258d904c09c0a753863b1d2ab2d7bcaea4004e0
Anouer Hermassi <ahermass@stevens.edu>
2019-08-25 14:30:24 -0400

    Inorder traversal utility function - Documenting V2

--- Tree/inorder_traversal_iterative.py
+++ Tree/inorder_traversal_iterative.py
@@ -21,6 +21,18 @@ def inorder(root):
 
 
 def inorder_v2(root):
+    """ For a given node root, the next smallest element will always be the leftmost element in its tree. So,
+        for a given root node, we keep on following the leftmost branch until we reach a node which doesn't have a left
+        child and that will be the next smallest element. For the root of our BST, this leftmost node would be the
+        smallest node in the tree. Rest of the nodes are added to the stack because they are pending processing.
+        Now, there are two possibilities that we have to deal with:
+        One is where the node at the top of the stack is actually a leaf node. This is the best case and here we don't
+        have to do anything. Simply pop the node off the stack and return its value. So, this would be a constant time
+        operation.
+        Second is where the node has a right child. We don't need to check for the left child because of the way we
+        have added nodes onto the stack. The topmost node either won't have a left child or would already have the left
+        subtree processed. If it has a right child, then we call our helper function on the node's right child.
+    """
 
     def process_leftmost(root):
         while root:
