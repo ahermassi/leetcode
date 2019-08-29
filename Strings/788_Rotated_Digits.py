53854d5a5ac305e1326e244855ca4b732dd7fe9b
Anouer Hermassi <ahermass@stevens.edu>
2019-08-29 09:43:24 -0400

    Rotated digits - v2

--- Strings/788_Rotated_Digits.py
+++ Strings/788_Rotated_Digits.py
@@ -6,7 +6,7 @@ Now given a positive number N, how many numbers X from 1 to N are good?
 import unittest2 as unittest
 
 
-def rotated_digits(N):
+def rotated_digits_v1(N):
     """ If at least one of the digits is in {3, 4, 7} then the number is not good (these digits do not rotate)
      If at least one of the digits is in {2, 5, 6, 9} then the number is good (these digits rotate to different digits)
      Time complexity: O(N)
@@ -20,10 +20,26 @@ def rotated_digits(N):
     return count
 
 
+def rotated_digits_v2(N):
+    """ Much simpler version.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+    good = 0
+    for i in range(1, N + 1):
+        digits = str(i)
+        if '3' in digits or '4' in digits or '7' in digits:
+            continue
+        if '2' in digits or '5' in digits or '6' in digits or '9' in digits:
+            good += 1
+    return good
+
+
 class Test(unittest.TestCase):
 
     def test_rotated_digits(self):
-        self.assertEqual(4, rotated_digits(10))
+        self.assertEqual(4, rotated_digits_v1(10))
+        self.assertEqual(4, rotated_digits_v2(10))
 
 
 if __name__ == '__main__':
