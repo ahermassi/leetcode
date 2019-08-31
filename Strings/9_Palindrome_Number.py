d895862e66e49bb608fc48bbb131a59b07c15b45
Anouer Hermassi <ahermass@stevens.edu>
2019-08-31 15:33:33 -0400

    Determine if an integer is a palindrome - math v1

--- Strings/9_Palindrome_Number.py
+++ Strings/9_Palindrome_Number.py
@@ -4,7 +4,7 @@
 import unittest2 as unittest
 
 
-def is_palindrome(x):
+def is_palindrome_v1(x):
     """ Convert x to a string and verify if it's a palindrome. Pretty straightforward.
     Time complexity: O(N)
     Space complexity: O(N)
@@ -19,12 +19,27 @@ def is_palindrome(x):
     return True
 
 
+def is_palindrome_v2(x):
+    """ Reverse the number and see if it is equal to the original number.
+    Time complexity: O(log10 x), since we divide the input by 10 in every iteration
+    Space complexity: O(1)
+    """
+    if x < 0:
+        return False
+    p, reverse = x, 0
+    while p:
+        reverse = reverse * 10 + p % 10
+        p = p // 10
+    return reverse == x
+
+
 class Test(unittest.TestCase):
     data = [(121, True), (-123, False), (10, False)]
 
     def test_is_palindrome(self):
         for test_number, result in self.data:
-            self.assertEqual(result, is_palindrome(test_number))
+            self.assertEqual(result, is_palindrome_v1(test_number))
+            self.assertEqual(result, is_palindrome_v2(test_number))
 
 
 if __name__ == '__main__':
