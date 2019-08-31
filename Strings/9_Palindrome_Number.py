8275ebb6cb16d22b513bd849fa0950514d51fee9
Anouer Hermassi <ahermass@stevens.edu>
2019-08-31 15:45:31 -0400

    Determine if an integer is a palindrome - math v2

--- Strings/9_Palindrome_Number.py
+++ Strings/9_Palindrome_Number.py
@@ -33,6 +33,28 @@ def is_palindrome_v2(x):
     return reverse == x
 
 
+def is_palindrome_v3(x):
+    """ What if we only revert half of the int number? After all, the reverse of the last half of the palindrome should
+        be the same as the first half of the number, if the number is a palindrome.
+        Now the question is, how do we know that we've reached the half of the number?
+        Since we divided the number by 10, and multiplied the reversed number by 10, when the original number is less
+        than the reversed number, it means we've processed half of the number digits.
+    Time complexity: O(log10 x)
+    Space complexity: O(1)
+    """
+    if x < 0 or (x % 10 == 0 and x):  # When x < 0, x is not a palindrome. Also if the last digit of the number is 0,
+        # in order to be a palindrome, the first digit of the number also needs to be 0. Only 0 satisfies this property.
+        return False
+    p, reverse = x, 0
+    while reverse < p:
+        reverse = reverse * 10 + p % 10
+        p = p // 10
+    # When the length is an odd number, we can get rid of the middle digit by reverse // 10. For example, when the
+    # input is 12321, at the end of the while loop we get x = 12, reverse = 123, since the middle digit doesn't matter
+    # in palindrome(it will always equal to itself), we can simply get rid of it.
+    return reverse == p or p == reverse // 10
+
+
 class Test(unittest.TestCase):
     data = [(121, True), (-123, False), (10, False)]
 
@@ -40,6 +62,7 @@ class Test(unittest.TestCase):
         for test_number, result in self.data:
             self.assertEqual(result, is_palindrome_v1(test_number))
             self.assertEqual(result, is_palindrome_v2(test_number))
+            self.assertEqual(result, is_palindrome_v3(test_number))
 
 
 if __name__ == '__main__':
