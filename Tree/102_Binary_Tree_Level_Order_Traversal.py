dfff3b7633cb0c6f2d812f5056521c9be6cc1f69
Anouer Hermassi <ahermass@stevens.edu>
2019-09-03 10:22:34 -0400

    Level order traversal of binary tree - queue

---
+++ Tree/102_Binary_Tree_Level_Order_Traversal.py
@@ -0,0 +1,62 @@
+""" Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+"""
+
+from collections import deque
+
+import unittest2 as unittest
+
+
+# Definition for a binary tree node.
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def level_order_v1(root):
+    """ Let's keep nodes of each tree level in the queue structure.
+        Initiate queue with a root. While queue is not empty :
+            Compute how many elements should be on the current level : it's a queue length.
+            Pop out all these elements from the queue and add them into the current level.
+            Push their child nodes into the queue for the next level.
+    Time complexity: O(N) where N is the number of nodes
+    Space complexity: O(N)
+    """
+    if not root:
+        return None
+    queue, level, ans = deque(), [], []
+    queue.append(root)
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.pop()
+            level.append(node.val)
+            if node.left:
+                queue.appendleft(node.left)
+            if node.right:
+                queue.appendleft(node.right)
+        ans.append(level)
+    return ans
+
+
+class Test(unittest.TestCase):
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    result = [
+        [3],
+        [9, 20],
+        [15, 7]
+    ]
+
+    def test_level_order(self):
+        self.assertEqual(self.result, level_order_v1(self.root))
+
+
+if __name__ == '__main__':
+    unittest.main()
