217ff963d915d10537bf5876a8985fe0fc1cc655
Anouer Hermassi <ahermass@stevens.edu>
2019-09-10 15:03:29 -0400

    Find number of unique paths - bottom-up memoization

--- DynamicProgramming/62_Unique_Paths.py
+++ DynamicProgramming/62_Unique_Paths.py
@@ -46,12 +46,28 @@ def unique_paths_v1(m, n):
     return count(0, 0)
 
 
+def unique_paths_v2(m, n):
+    """ Bottom up + memoization.
+        dp[i][j] equals to how many ways I can reach the current cell (i, j) moving only right and/or down, expect the
+        first row and the first column, since there are only one way to get to those cells (straight right or straight
+        down, respectively).
+    Time complexity: O(n * m)
+    Space complexity: O(n * m)
+    """
+    dp = [[1 for _ in range(m)] for _ in range(n)]
+    for i in range(1, n):
+        for j in range(1, m):
+            dp[i][j] = dp[i][j - 1] + dp[i - 1][j]  # This is our 'right' and 'down' from previous solution
+    return dp[-1][-1]  # This is the bottom right corner where we want to stop
+
+
 class Test(unittest.TestCase):
     data = [(3, 2, 3), (7, 3, 28)]
 
     def test_unique_paths(self):
         for test_m, test_n, result in self.data:
             self.assertEqual(result, unique_paths_v1(test_m, test_n))
+            self.assertEqual(result, unique_paths_v2(test_m, test_n))
 
 
 if __name__ == '__main__':
