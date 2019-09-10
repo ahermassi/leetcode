9763081b019bc312d8e4aceb6e3b4085d589e56a
Anouer Hermassi <ahermass@stevens.edu>
2019-09-10 12:18:31 -0400

    Count number of islands - v2

--- DFS/200_Number_of_Islands.py
+++ DFS/200_Number_of_Islands.py
@@ -5,22 +5,23 @@ grid are all surrounded by water. '''
 import unittest2 as unittest
 
 
-def num_islands(grid):
+def num_islands_v1(grid):
     """ Iterate through each of the cell and if it is an island, do dfs to mark all adjacent islands, then increase
         the counter by 1.
         This solution uses a 'seen' set in order to avoid an infinite recursion.
+        This is DFS because the recursive call happens before finishing the current execution.
     Time complexity: O(N * M) where N is the number of rows in the given grid and M is the number of columns. We visit
     every square once.
     Space complexity: O(N * M) for both visited set and recursion call stack
     """
-    def process(i, j):
+    def dfs(i, j):
         if i < 0 or i >= row or j < 0 or j >= col or grid[i][j] == '0' or (i, j) in seen:
             return
         seen.add((i, j))
-        process(i - 1, j)
-        process(i + 1, j)
-        process(i, j - 1)
-        process(i, j + 1)
+        dfs(i - 1, j)
+        dfs(i + 1, j)
+        dfs(i, j - 1)
+        dfs(i, j + 1)
 
     if not grid:
         return 0
@@ -29,7 +30,33 @@ def num_islands(grid):
     for i in range(row):
         for j in range(col):
             if grid[i][j] == '1' and (i, j) not in seen:
-                process(i, j)
+                dfs(i, j)
+                count += 1
+    return count
+
+
+def num_islands_v2(grid):
+    """ Same solution as above, but without using a 'seen' set. Instead, mark every visited cell as '0'.
+    Time complexity: O(N * M)
+    Space complexity: O(N * M)
+    """
+    def dfs(i, j):
+        if i < 0 or i >= row or j < 0 or j >= col or grid[i][j] == '0':
+            return
+        grid[i][j] = '0'
+        dfs(i - 1, j)
+        dfs(i + 1, j)
+        dfs(i, j - 1)
+        dfs(i, j + 1)
+
+    if not grid:
+        return 0
+    count, seen = 0, set()
+    row, col = len(grid), len(grid[0])
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == '1':
+                dfs(i, j)
                 count += 1
     return count
 
@@ -40,7 +67,8 @@ class Test(unittest.TestCase):
 
     def test_num_islands(self):
         for test_island, result in self.data:
-            self.assertEqual(result, num_islands(test_island))
+            self.assertEqual(result, num_islands_v1(test_island))
+            self.assertEqual(result, num_islands_v2(test_island))
 
 
 if __name__ == '__main__':
