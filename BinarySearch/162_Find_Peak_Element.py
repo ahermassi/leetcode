5e083ac24faf805288f318b80663c76369516b80
Anouer Hermassi <ahermass@stevens.edu>
2019-10-01 16:25:07 -0400

    Find peak element in array

---
+++ BinarySearch/162_Find_Peak_Element.py
@@ -0,0 +1,51 @@
+""" A peak element is an element that is greater than its neighbors.
+Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine. """
+
+import unittest2 as unittest
+
+
+def find_peak_element_v1(nums):
+    """ We can view any given sequence in numsnums array as alternating ascending and descending sequences. By making
+        use of this, and the fact that we can return any peak as the result, we can make use of Binary Search to find
+        the required peak element.
+        In case of simple Binary Search, we work on a sorted sequence of numbers and try to find out the required
+        number by reducing the search space at every step. In this case, we use a modification of this simple Binary
+        Search to our advantage. We start off by finding the middle element, mid from the given nums array. If
+        this element happens to be lying in a descending sequence of numbers. or a local falling slope(found by
+        comparing nums[i]nums[i] to its right neighbour), it means that the peak will always lie towards the left of
+        this element. Thus, we reduce the search space to the left of midmid(including itself) and perform the same
+        process on left subarray.
+        If the middle element, mid lies in an ascending sequence of numbers, or a rising slope(found by comparing
+        nums[i] to its right neighbour), it obviously implies that the peak lies towards the right of this element.
+        Thus, we reduce the search space to the right of mid and perform the same process on the right subarray.
+        In this way, we keep on reducing the search space till we eventually reach a state where only one element is
+        remaining in the search space. This single element is the peak element.
+    Time complexity: (logN)
+    Space complexity: O(1)
+    """
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return left
+
+
+def find_peak_element_v2(nums):
+    """ Recursive version of previous algorithm.
+    Time complexity: (logN)
+    Space complexity: O(logN), the depth of recursion tree
+    """
+    def helper(nums, left, right):
+        if left == right:
+            return left
+        mid = (left + right) // 2
+        if nums[mid] > nums[mid + 1]:
+            return helper(nums, left, mid)
+        else:
+            return helper(nums, mid + 1, right)
+
+    return helper(nums, 0, len(nums) - 1)
