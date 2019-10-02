9a76da667a12a822dd5df57fb6c83d52d84c6a32
Anouer Hermassi <ahermass@stevens.edu>
2019-10-02 10:52:36 -0400

    Minimum number of meeting rooms required - v2

--- Heap/253_Meeting_Rooms_II.py
+++ Heap/253_Meeting_Rooms_II.py
@@ -36,8 +36,40 @@ def min_meeting_rooms_v1(intervals):
             heappush(heap, interval[1])  # If a new room is to be assigned, then also we add to the heap
     return len(heap)
 
-def min_meeting_rooms_v2(intervals):
 
+def min_meeting_rooms_v2(intervals):
+    """ Separate out the start times and the end times in their separate arrays.
+        Sort the start times and the end times separately. Note that this will mess up the original correspondence of
+        start times and end times. They will be treated individually now.
+        We consider two pointers: s_ptr and e_ptr which refer to start pointer and end pointer. The start pointer
+        simply iterates over all the meetings and the end pointer helps us track if a meeting has ended and if we can
+        reuse a room.
+        When considering a specific meeting pointed to by s_ptr, we check if this start timing is greater than the
+        meeting pointed to by e_ptr. If this is the case then that would mean some meeting has ended by the time the
+        meeting at s_ptr had to start. So we can reuse one of the rooms. Otherwise, we have to allocate a new room.
+        If a meeting has indeed ended i.e. if start[s_ptr] >= end[e_ptr], then we increment e_ptr.
+        Repeat this process until s_ptr processes all of the meetings.
+    Time complexity: O(N log N) for Timsort
+    Space complexity: O(N) because we create two separate arrays of size NN, one for recording the start times and one
+    for the end times
+    """
+    if not intervals:
+        return 0
+    used_rooms = 0
+    start_timings = sorted([i[0] for i in intervals])
+    end_timings = sorted(i[1] for i in intervals)
+    start_pointer = end_pointer = 0  # The two pointers in the algorithm: e_ptr and s_ptr
+    while start_pointer < len(intervals):  # Until all the meetings have been processed
+        if start_timings[start_pointer] >= end_timings[end_pointer]:  # If there is a meeting that has ended by the
+            # time the meeting at `start_pointer` starts
+            used_rooms -= 1  # Free up a room and increment the end_pointer
+            end_pointer += 1
+        # We do this irrespective of whether a room frees up or not.
+        # If a room got free, then this used_rooms += 1 wouldn't have any effect. used_rooms would
+        # remain the same in that case. If no room was free, then this would increase used_rooms
+        used_rooms += 1
+        start_pointer += 1
+    return used_rooms
 
 
 class Test(unittest.TestCase):
@@ -46,6 +78,7 @@ class Test(unittest.TestCase):
     def test_min_meeting_rooms(self):
         for test_intervals, result in self.data:
             self.assertEqual(result, min_meeting_rooms_v1(test_intervals))
+            self.assertEqual(result, min_meeting_rooms_v2(test_intervals))
 
 
 if __name__ == '__main__':
