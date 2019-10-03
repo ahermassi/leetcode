fcb7f8a75322c742d3d7d8a42cc8a403159faa46
Anouer Hermassi <ahermass@stevens.edu>
2019-10-03 13:01:07 -0400

    Find top k most frequent elements in array - v2

--- Heap/347_Top_K_Frequent_Elements.py
+++ Heap/347_Top_K_Frequent_Elements.py
@@ -19,12 +19,35 @@ def top_k_frequent_v1(nums, k):
     return nlargest(k, d.keys(), d.get)
 
 
+def top_k_frequent_v2(nums, k):
+    """ Build a frequency hash map, 'counter'. Then build another hash map that maps frequencies to the elements that
+        appear with that frequency, 'freq'. Now, in a reversed range [len(nums) + 1 .. 0], if any index in that range
+        is in 'frequency' map, append the corresponding elements in final output list 'res'. Return when 'res' has
+        already k elements.
+        Note: we use a reversed range because we want the top k or most frequent k, so it makes sense to start with
+        the max index.
+    Time complexity: O(N)
+    Space complexity: O(N) for the hash maps
+    """
+    counter, freq, res = defaultdict(int), defaultdict(list), []
+    for num in nums:
+        counter[num] += 1
+    for key, v in counter.items():
+        freq[v].append(key)
+    for i in reversed(range(len(nums) + 1)):
+        if i in freq:
+            res.extend(freq[i])
+            if len(res) >= k:
+                return res[:k]
+
+
 class Test(unittest.TestCase):
     data = [([1, 1, 1, 2, 2, 3], 2, [1, 2]), ([1], 1, [1])]
 
     def test_top_k_frequent(self):
         for test_array, test_k, result in self.data:
             self.assertEqual(result, top_k_frequent_v1(test_array, test_k))
+            self.assertEqual(result, top_k_frequent_v2(test_array, test_k))
 
 
 if __name__ == '__main__':
