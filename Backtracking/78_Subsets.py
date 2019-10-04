1953b28c5b8dc892f36e79ed6b57d739ba552024
Anouer Hermassi <ahermass@stevens.edu>
2019-10-04 12:30:38 -0400

    Generating all possible subsets - v1

---
+++ Backtracking/78_Subsets.py
@@ -0,0 +1,47 @@
+""" Given a set of distinct integers, nums, return all possible subsets (the power set).
+Note: The solution set must not contain duplicate subsets. """
+
+import unittest2 as unittest
+
+
+def subsets_v1(nums):
+    """ While iterating through all numbers, for each new number, we can either pick it or not pick it.
+            1- if pick, just add current number to every existing subset.
+            2- if not pick, just leave all existing subsets as they are.
+        We just combine both into our result.
+        Here's an example to help understand the code:
+        The set to iterate over/generate the power set for:
+        input_set = [1, 2, 3]
+        Subset initially only has the empty set (empty list), []
+        subset = [[]]
+        In each iteration, concatenate each element/list in subset with the list [n], then extend the results into
+        subset
+        element -> subset list after each iteration
+        In the first iteration, subset's only element is []
+            [ []  + [1] ] = [ [1] ]
+            num = 1 -> [[], [1]]
+        In the second iteration, we concatenate [] with [2] and [1] with [2]
+            [ [] + [2] ] = [ [2] ], [ [1] + [2] ] = [ [1, 2] ]
+            num = 2 -> [[], [1], [2], [1, 2]]
+        In the third iteration, we concatenate [3] with each element in subset
+            [ [] + [3] ] = [ [3] ], [ [1] + [3] ] = [ [1, 3] ], [ [2] + [3] ] = [ [2, 3] ], [ [1, 2] + [3] ] = [ [1, 2, 3] ]
+            num = 3 -> [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+    Time complexity: O(2 ** N)
+    Space complexity: O(1)
+    """
+    res = [[]]
+    for num in nums:
+        res += [lst + [num] for lst in res]
+    return res
+
+
+class Test(unittest.TestCase):
+    data = [([1, 2, 3], [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]])]
+
+    def test_subsets(self):
+        for test_array, result in self.data:
+            self.assertEqual(result, sorted(subsets_v1(test_array)))
+
+
+if __name__ == '__main__':
+    unittest.main()
