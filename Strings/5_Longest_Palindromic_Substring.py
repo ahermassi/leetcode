03fe7f76e17e1bc58e66dd54c746f3b32f3618ef
Anouer Hermassi <ahermass@stevens.edu>
2019-10-06 12:21:12 -0400

    Find the longest palindromic substring - DP

--- Strings/5_Longest_Palindromic_Substring.py
+++ Strings/5_Longest_Palindromic_Substring.py
@@ -23,3 +23,34 @@ def longest_palindrome_v1(s):
         res = max(res, odd_palindrome, even_palindrome, key=len)
     return res
 
+
+def longest_palindrome_v2(s):
+    """ Bottom-up Dynamic Programming.
+        We observe how we can avoid unnecessary re-computation while validating palindromes. Consider the case "ababa".
+        If we already knew that "bab" is a palindrome, it is obvious that "ababa" must be a palindrome since the two
+        left and right end letters are the same.
+        We define dp[i][j] as following:
+        dp[i][j] is True is substring s[i:j + 1] is palindrome
+        Therefore:
+        dp[i][j] = s[i] == s[j] and dp[i+1][j-1]
+        This yields a straight forward DP solution, which we first initialize the one letter palindromes, and work our
+        way up finding all two letters palindromes, and so on...
+    Time complexity: O(N ** 2)
+    Space complexity: O(N ** 2) to store dp array
+    """
+    res = ''
+    dp = [[False] * len(s) for _ in range(len(s))]
+    for i in range(len(s)):
+        dp[i][i] = True
+        res = s[i]
+    for i in reversed(range(len(s))):
+        for j in range(i + 1, len(s)):
+            if s[i] == s[j] and (j - i < 2 or dp[i + 1][j - 1]):  # dp[i+1][j-1] represents the middle of the current
+                # considered substring
+                dp[i][j] = True  # If the middle is a palindrome and the endpoints equal each other, it follows that
+                # s[i:j+1] is a palindrome
+                if j - i + 1 > len(res):
+                    res = s[i: j + 1]
+    return res
+
+
