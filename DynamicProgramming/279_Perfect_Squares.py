f7ff16a8ba08d3c33c2cab6864eed66ba58a59f0
Anouer Hermassi <ahermass@stevens.edu>
2019-10-08 12:24:05 -0400

    Finding least number of perfect numbers which sum to n - v1

---
+++ DynamicProgramming/279_Perfect_Squares.py
@@ -0,0 +1,42 @@
+""" Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which
+sum to n. """
+
+import unittest as unittest
+
+
+def num_squares_v1(n):
+    """ Suppose that min_squares[i] records to least number of perfect square numbers that sum up to i. And there are
+        multiple ways for perfect square numbers to sum up to i. The candidate way is to add a perfect square number
+        j**2 to a sum of perfect square numbers that equals to i - j**2. And it can be generalized as:
+         i = (i- j**2) + j**2.
+         So the least number of perfect square numbers that sum up to (i - j**2) is dp[i - j**2]. So candidate answer
+         is dp[i - j**2] + 1 because j**2 itself is a perfect square
+        So for dp[i], we just pick the minimum of all candidates.
+        So for each i, we try to find the least number of perfect squares that make up (i - j**2), and then add +1 to
+        the count because j**2 itself is a perfect square (i = (i - j**2) + j**2)
+        At each iteration, i - j**2 has to be >= 0 because we're dealing only with positive integers.
+        i - j**2 >= 0
+        --> j**2 <= i
+        --> j <= sqrt(i) = i ** 0.5
+        For this reason, the second loop goes from 1 to sqrt(i)
+    Time complexity: O(n sqrt(n)) which is approximated by O(n log n)
+    Space complexity: O(n)
+    """
+    min_squares = [float('inf')] * (n + 1)
+    min_squares[0] = 0
+    for i in range(1, n + 1):
+        for j in range(1, int(i ** 0.5) + 1):
+            min_squares[i] = min(min_squares[i], min_squares[i - j ** 2] + 1)
+    return min_squares[n]
+
+
+class Test(unittest.TestCase):
+    data = [(12, 3), (13, 2)]
+
+    def test_num_squares(self):
+        for test_number, result in self.data:
+            self.assertEqual(result, num_squares_v1(test_number))
+
+
+if __name__ == '__main__':
+    unittest.main()
