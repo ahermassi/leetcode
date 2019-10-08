9e5d8eb44d081b7e974741798fdcb2341f64539c
Anouer Hermassi <ahermass@stevens.edu>
2019-10-08 15:57:52 -0400

    Finding least number of perfect numbers which sum to n - v2

--- DynamicProgramming/279_Perfect_Squares.py
+++ DynamicProgramming/279_Perfect_Squares.py
@@ -30,12 +30,41 @@ def num_squares_v1(n):
     return min_squares[n]
 
 
+def num_squares_v2(n):
+    """ The basic idea of this solution is a BSF search for shortest path.
+        The root node is n, and we are trying to keep reducing a perfect square number from it at each layer. So the
+        next layer nodes are {n - i**2 for i in range(1, int(n**0.5)+1)}. And target leaf node is 0, indicates n is
+        made up of a number of perfect square numbers and depth is the least number of perfect square numbers.
+    Time complexity: O(n), sqrt(n) elements in the queue checked sqrt(n) times (length of perfect_squares list)
+    Space complexity: O(sqrt(n))
+    """
+    perfect_squares = [i * i for i in range(1, int(n ** 0.5) + 1)]  # These are the candidate perfect squares that n can
+    # be made of: all squares less than or equal to n
+    queue, count = {n}, 0  # queue holds the intermediate integers that result from reducing n by (n - perfect square).
+    # The same process is applied to those intermediate results until 0 is found. We use a set data structure to
+    # eliminate the duplicate integers. Example: n = 12, perfect_squares = [1, 4, 9]. In second iteration, 11 - 4 = 7
+    # and 8 - 1 = 7 as well (see execution tree in notes)
+    while queue:
+        count += 1  # A new depth: count + 1
+        new_queue = set()
+        for i in queue:
+            for j in perfect_squares:
+                if i == j:  # 0 is found: i - j == 0
+                    return count
+                if j > i:  # (i - j) would result in a negative integer
+                    break
+                new_queue.add(i - j)
+        queue = new_queue
+    return count
+
+
 class Test(unittest.TestCase):
     data = [(12, 3), (13, 2)]
 
     def test_num_squares(self):
         for test_number, result in self.data:
             self.assertEqual(result, num_squares_v1(test_number))
+            self.assertEqual(result, num_squares_v2(test_number))
 
 
 if __name__ == '__main__':
