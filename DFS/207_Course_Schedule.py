89ae5525dff9d440d9a002df3e22a53afb6e93bc
Anouer Hermassi <ahermass@stevens.edu>
2019-10-09 15:55:09 -0400

    Course schedule - fixed typo

--- DFS/207_Course_Schedule.py
+++ DFS/207_Course_Schedule.py
@@ -2,6 +2,7 @@
 Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed
 as a pair: [0,1]
 Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses? """
+
 import collections
 
 import unittest2 as unittest
@@ -31,7 +32,7 @@ def can_finish_dfs(numCourses, prerequisites):
             return False
         if visited[i] == 1:  # If it is done visited, then do not visit again
             return True
-        visited[i] = -1  # Mark as being visited
+        visited[i] = -1  # Mark as being visited during current recursion
         for j in graph[i]:  # Visit all the neighbours
             if not dfs(j):
                 return False
@@ -46,7 +47,7 @@ def can_finish_dfs(numCourses, prerequisites):
 
 
 def can_finish_bfs(numCourses, prerequisites):
-    """ Same as above but in DFS fashion.
+    """ Same as above but in BFS fashion.
     Time complexity: O(|V| + |E|)
     Space complexity: O(|V| + |E|)
     """
@@ -55,7 +56,8 @@ def can_finish_bfs(numCourses, prerequisites):
     for to_, from_ in prerequisites:
         graph[from_].append(to_)
         indegree[to_] += 1
-    queue = collections.deque(v for v in range(numCourses) if indegree[v] == 0)
+    queue = collections.deque(v for v in range(numCourses) if indegree[v] == 0)  # Iterate the inDegree map, find the
+    # Node has 0 inDegree. (If none, there must be a circle)
     n = len(queue)
     while queue and n != numCourses:  # adding n != numCourses to terminate loop earlier
         v = queue.popleft()
@@ -68,7 +70,7 @@ def can_finish_bfs(numCourses, prerequisites):
 
 
 class Test(unittest.TestCase):
-    data = [(2, [[1, 0]], True), (2, [[1, 0],[0, 1]], False)]
+    data = [(2, [[1, 0]], True), (2, [[1, 0], [0, 1]], False)]
 
     def test_can_finish(self):
         for test_num_courses, test_prerequisites, result in self.data:
