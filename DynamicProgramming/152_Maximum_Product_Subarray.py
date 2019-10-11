c1ba04840ed4e4dac954c5c28cc97d6a8c7248da
Anouer Hermassi <ahermass@stevens.edu>
2019-10-11 10:00:25 -0400

    Finding max product of subarray - v1 more comments

--- DynamicProgramming/152_Maximum_Product_Subarray.py
+++ DynamicProgramming/152_Maximum_Product_Subarray.py
@@ -5,10 +5,15 @@ import unittest2 as unittest
 
 
 def max_product_v1(nums):
-    """ This is very similar to the " 53- Maximum Sub array problem. Here we keep 2 values: the max cumulative
+    """ This is very similar to the 53- Maximum Sub array problem. Here we keep 2 values: the max cumulative
         product UP TO current element starting from SOMEWHERE in the past, and the minimum cumulative product UP TO
         current element. At each new element, we could either add the new element to the existing product, or start
         fresh the product from current index (wipe out previous results), hence the 2 max() lines.
+        The max result from A[0] to A[i] can only come from:
+            Decision 1. discard previous result, restart at A[i]
+            Decision 2. take A[i], MAX[i] = MAX[i-1] * A[i]
+            Decision 3. this is the most tricky part: A[i] can be negative, then MAX[i-1] * A[i] is negative (suppose
+            MAX[i-1] is positive).
     Time complexity: O(N)
     Space complexity: O(1)
     """
