a85b6ee6982d18369081b3b1b977bd2851ebc470
Anouer Hermassi <ahermass@stevens.edu>
2019-10-13 14:58:25 -0400

    Jump game - v2

--- DynamicProgramming/55_Jump_Game.py
+++ DynamicProgramming/55_Jump_Game.py
@@ -19,12 +19,28 @@ def can_jump_v1(nums):
     return last_pos == 0
 
 
+def can_jump_v2(nums):
+    """ Looking from the start and selecting the locally optimum in the hope of reaching global optimum.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+    furthest_jump, n = 0, len(nums) - 1
+    for i, v in enumerate(nums):
+        if furthest_jump < i:  # If previous furthest_jump is smaller than i, it means we cannot reach location i,
+            # thus return false.
+            return False
+        if furthest_jump >= n:
+            return True
+        furthest_jump = max(furthest_jump, i + v)
+
+
 class Test(unittest.TestCase):
     data = [([2, 3, 1, 1, 4], True), ([3, 2, 1, 0, 4], False)]
 
     def test_can_jump(self):
         for test_array, result in self.data:
             self.assertEqual(result, can_jump_v1(test_array))
+            self.assertEqual(result, can_jump_v2(test_array))
 
 
 if __name__ == '__main__':
