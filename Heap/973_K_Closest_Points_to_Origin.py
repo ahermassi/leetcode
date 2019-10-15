e4d214da3c974f183993e8a22325bdf9a89b934f
Anouer Hermassi <ahermass@stevens.edu>
2019-10-15 10:59:40 -0400

    Fidning K closest points to origin - v2

--- Heap/973_K_Closest_Points_to_Origin.py
+++ Heap/973_K_Closest_Points_to_Origin.py
@@ -23,6 +23,49 @@ def k_closest_v1(points, K):
             heappushpop(heap, (-d, x, y))
         else:
             heappush(heap, (-d, x, y))
-    return [[b, c] for a, b, c in heap]
+    return [[x, y] for dist, x, y in heap]
+
+# Review the algorithm and fix a bug
+
+
+def k_closest_v2(points, K):
+    """ This solution is based on quick sort, we can also call it quick select. In the quick sort, we will always
+        choose a pivot to compare with other elements.
+        Partition in quick select picks a pivot (either randomly or first/last element). Then it rearranges the list
+        in a way that all elements less than pivot are on left side of pivot and others on right. It then returns
+        index of the pivot element.
+        Now here we are finding kth smallest element. After partition cases are:
+            1- k == pivot. Then you have already found kth smallest. This is because the way partition is working.
+                There are exactly k - 1 elements that are smaller than the kth element.
+            2- k < pivot. Then kth smallest is on the left side of pivot.
+            3- k > pivot. Then kth smallest is on the right side of pivot. And to find it you actually have to find
+                k-pivot smallest number on right.
+    Time complexity: the average time complexity is O(N) , but just like quick sort, in the worst case, this solution
+    would be degenerated to O(N^2)
+    Space complexity: O(N) where N is the length of points
+    """
+
+    def partition(left, right):
+        pivot = left
+        temp = points[pivot]
+        while left < right and dist(pivot) <= dist(right):
+            right -= 1
+        points[left] = points[right]
+        while left < right and dist(left) <= dist(pivot):
+            left += 1
+        points[right] = points[left]
+        points[left] = temp
+        return left
+
+    dist = lambda i: points[i][0] ** 2 + points[i][1] ** 2
+    left, right = 0, len(points) - 1
+    while left <= right:
+        mid = partition(left, right)
+        if mid == K - 1:
+            return points[:K]
+        if mid < K - 1:
+            left = mid + 1
+        else:
+            right = mid - 1
 
 
