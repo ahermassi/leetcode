0d29f4e09d087e0584e863b48ceda361fbed0234
Anouer Hermassi <ahermass@stevens.edu>
2019-10-18 14:37:10 -0400

    Finding the minimum number of rotations to make row of domino match - v1

---
+++ Arrays/1007_Minimum_Domino_Rotations_For_Equal_Row.py
@@ -0,0 +1,51 @@
+""" In a row of dominoes, A[i] and B[i] represent the top and bottom halves of the i-th domino.  (A domino is a tile
+with two numbers from 1 to 6 - one on each half of the tile.)
+We may rotate the i-th domino, so that A[i] and B[i] swap values.
+Return the minimum number of rotations so that all the values in A are the same, or all the values in B are the same.
+If it cannot be done, return -1. """
+
+import unittest2 as unittest
+
+
+def min_domino_rotations_v1(A, B):
+    """ Pick up the first element. It has two sides: A[0] and B[0].
+        Check if we could make all elements in A row or B row to be equal to A[0]. If yes, return the minimum number
+        of rotations needed.
+        Otherwise, check if we could make all elements in A row or B row to be equal to B[0]. If yes, return the
+        minimum number of rotations needed.
+        Otherwise return -1.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+
+    def rotate(val):
+        rotate_a = rotate_b = 0
+        for i in range(n):
+            if A[i] != val and B[i] != val:
+                return -1
+            if A[i] != val:
+                rotate_a += 1
+            elif B[i] != val:
+                rotate_b += 1
+        return min(rotate_a, rotate_b)
+
+    n = len(A)
+    rotations = rotate(A[0])
+    if rotations != -1 or A[0] == B[0]:  # Second condition prevents the second check from running, since B[0] is
+        # passed in for the second check
+        return rotations  # If A[0] works, no need to check B[0]. Because if both A[0] and B[0] exist in all
+        # dominoes, when we swap A[0] in a whole row, we will swap B[0] in a whole at the same time. The result of
+        # trying A[0] and B[0] will be the same.
+    return rotate(B[0])
+
+
+class Test(unittest.TestCase):
+    data = [([2, 1, 2, 4, 2, 2], [5, 2, 6, 2, 3, 2], 2), ([3, 5, 1, 2, 3], [3, 6, 3, 3, 4], -1)]
+
+    def test_min_domino_rotations(self):
+        for test_a, test_b, result in self.data:
+            self.assertEqual(result, min_domino_rotations_v1(test_a, test_b))
+
+
+if __name__ == '__main__':
+    unittest.main()
