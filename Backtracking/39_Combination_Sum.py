e9cd2e018ace5d8b6e63723af31e1ce6a76a3862
Anouer Hermassi <ahermass@stevens.edu>
2019-10-19 10:02:19 -0400

    Finding all unique combinations that sum to a target - v1

---
+++ Backtracking/39_Combination_Sum.py
@@ -0,0 +1,51 @@
+""" Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique
+combinations in candidates where the candidate numbers sums to target.
+The same repeated number may be chosen from candidates unlimited number of times.
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations. """
+
+import unittest2 as unittest
+
+
+def combination_sum_v1(candidates, target):
+    """ Good old DFS. Explore all the possible paths that reduce the target to 0. Sorting is not necessary here. The
+        only help with sorting is that we can stop searching earlier by breaking the for loop when 'candidate' is
+        larger than 'remaining' target.
+        Sorting is not for correctness but for speed. What we do by sorting is we limit the range of numbers on which
+        we call DFS recursively, as we know the numbers outside the range cannot be in our solution. For small inputs
+        this speed up may not be substantial but for larger inputs, sorting will definitely give a faster solution.
+    Time complexity: O(#candidates ^ target)
+    Space complexity: Space complexity: O(target) for call stack
+    """
+
+    def dfs(remaining, path):
+        if remaining == 0:
+            res.append(path)
+            return
+        for candidate in candidates:
+            if candidate > remaining:  # If one 'candidate' in bigger than 'remaining', the remaining items must
+                # bigger than 'remaining', so break early
+                break
+            if path and candidate < path[-1]:  # This check is for the purpose of avoiding duplicate paths and
+                # maintaining the answer in increasing order. We do this by avoiding appending values that are
+                # smaller than the last item in path (if the path is not empty).
+                continue
+            dfs(remaining - candidate, path + [candidate])
+
+    res = []
+    candidates.sort()
+    dfs(target, [])
+    return res
+
+
+class Test(unittest.TestCase):
+    data = [([2, 3, 6, 7], 7, [[7], [2, 2, 3]]), ([2, 3, 5], 8, [[2, 2, 2, 2], [2, 3, 3], [3, 5]])]
+
+    def test_combination_sum(self):
+        for test_candidates, test_target, result in self.data:
+            self.assertEqual(sorted(result), combination_sum_v1(test_candidates, test_target))
+
+
+if __name__ == '__main__':
+    unittest.main()
