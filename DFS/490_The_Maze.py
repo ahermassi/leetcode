6aa4813be71b9951b87412c01c4ff4799c34ccf6
Anouer Hermassi <ahermass@stevens.edu>
2019-10-21 10:24:38 -0400

    Searching a maze - v2

--- DFS/490_The_Maze.py
+++ DFS/490_The_Maze.py
@@ -2,7 +2,7 @@
 left or right, but it won't stop rolling until hitting a wall. When the ball stops, it could choose the next direction.
 Given the ball's start position, the destination and the maze, determine whether the ball could stop at the destination.
 """
-
+from collections import deque
 import unittest2 as unittest
 
 
@@ -40,6 +40,35 @@ def has_path_v1(maze, start, destination):
     return dfs(x, y)
 
 
+def has_path_v2(maze, start, destination):
+    """ BFS solution.
+        The same search space tree can also be explored in a Depth First Search manner. In this case, we try to explore
+        the search space on a level by level basis. i.e. We try to move in all the directions at every step. When all
+        the directions have been explored and we still don't reach the destination, then only we proceed to the new set
+        of traversals from the new positions obtained.
+    Time complexity: O(N * M), complete traversal of maze will be done in the worst case
+    Space complexity: O(N * M), 'visited' set of size N * M is used and queue size can grow up to N * M in worst case
+    """
+    n, m, visited = len(maze), len(maze[0]), set()
+    queue = deque([(start[0], start[1])])
+    visited.add((start[0], start[1]))
+    while queue:
+        i, j = queue.popleft()
+        if [i, j] == destination:
+            return True
+        for x, y in (-1, 0), (1, 0), (0, -1), (0, 1):
+            new_i, new_j = i + x, j + y
+            while 0 <= new_i < n and 0 <= new_j < m and maze[new_i][new_j] == 0:
+                new_i += x
+                new_j += y
+            new_i -= x
+            new_j -= y
+            if (new_i, new_j) not in visited:
+                visited.add((new_i, new_j))
+                queue.append((new_i, new_j))
+    return False
+
+
 class Test(unittest.TestCase):
     data = [
         ([[0, 0, 1, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 1, 0], [1, 1, 0, 1, 1], [0, 0, 0, 0, 0]], [0, 4], [4, 4], True)]
@@ -47,6 +76,7 @@ class Test(unittest.TestCase):
     def test_has_path(self):
         for test_maze, test_start, test_destination, result in self.data:
             self.assertEqual(result, has_path_v1(test_maze, test_start, test_destination))
+            self.assertEqual(result, has_path_v2(test_maze, test_start, test_destination))
 
 
 if __name__ == '__main__':
