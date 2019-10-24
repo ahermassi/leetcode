baacf86346d700f3619764651371f9bbe71197ec
Anouer Hermassi <ahermass@stevens.edu>
2019-10-24 10:54:03 -0400

    Constructing a quad tree - v2

--- Tree/427_Construct_Quad_Tree.py
+++ Tree/427_Construct_Quad_Tree.py
@@ -12,7 +12,7 @@ class Node(object):
         self.bottomRight = bottomRight
 
 
-def construct(grid):
+def construct_v1(grid):
     """ Construct the tree recursively.
     Time complexity: O(N * 2 logN), where log is base 4
     Space complexity: O(logN), where log is base 4
@@ -21,9 +21,37 @@ def construct(grid):
     if all(grid[i][j] == grid[0][0] for i in range(n) for j in range(m)):
         return Node(grid[0][0] == 1, True, None, None, None, None)
     root = Node(grid[0][0] == 1, False, None, None, None, None)
-    root.topLeft = construct([row[:n // 2] for row in grid[:n // 2]])
-    root.topRight = construct([row[n // 2:] for row in grid[:n // 2]])
-    root.bottomLeft = construct([row[:n // 2] for row in grid[n // 2:]])
-    root.bottomRight = construct([row[n // 2:] for row in grid[n // 2:]])
+    root.topLeft = construct_v1([row[:n // 2] for row in grid[:n // 2]])
+    root.topRight = construct_v1([row[n // 2:] for row in grid[:n // 2]])
+    root.bottomLeft = construct_v1([row[:n // 2] for row in grid[n // 2:]])
+    root.bottomRight = construct_v1([row[n // 2:] for row in grid[n // 2:]])
     return root
+
+
+def construct_v2(grid):
+    """ Use the results of recursive calls to improve time complexity. Each cell is examined only once.
+    Time complexity: O(N ** 2)
+    Space complexity: O(log N), where log is base 4
+    """
+
+    def helper(x, y, length):
+        if length == 1:
+            return Node(grid[0][0] == 1, True, None, None, None, None)
+        root = Node(grid[0][0] == 1, False, None, None, None, None)
+        topLeft = helper(x, y, length // 2)
+        topRight = helper(x, y + length // 2, length // 2)
+        bottomLeft = helper(x + length // 2, y, length // 2)
+        bottomRight = helper(x + length // 2, y + length // 2, length // 2)
+        if topLeft.isLeaf and topRight.isLeaf and bottomLeft.isLeaf and bottomRight.isLeaf and topLeft.val == \
+                topRight.val and topRight.val == bottomLeft.val and bottomLeft.val == bottomRight.val:
+            root.isLeaf = True
+            root.val = topLeft.val
+        else:
+            root.topLeft = topLeft
+            root.topRight = topRight
+            root.bottomLeft = bottomLeft
+            root.bottomRight = bottomRight
+        return root
+
+    return helper(0, 0, len(grid))
 
