415e39685baf2a50b0a82b0db6bc18c216e413e4
Anouer Hermassi <ahermass@stevens.edu>
2019-10-24 15:12:23 -0400

    Flattenning a multilevel doubly linked list

---
+++ LinkedList/430_Flatten_a_Multilevel_Doubly_Linked_List.py
@@ -0,0 +1,31 @@
+""" You are given a doubly linked list which in addition to the next and previous pointers, it could have a child
+pointer, which may or may not point to a separate doubly linked list. These child lists may have one or more children
+of their own, and so on, to produce a multilevel data structure, as shown in the example below.
+Flatten the list so that all the nodes appear in a single-level, doubly linked list. You are given the head of the
+first level of the list. """
+
+
+def flatten_v1(head):
+    """ Start form the head , move one step each time to the next node.
+        When a node with child is met, say node p, follow its child chain to the end and connect the tail node with
+        p.next, by doing this we merge the child chain back to the main list.
+        Return to p and proceed until finding next node with child.
+        Repeat until reaching the end of list.
+    Time complexity: O(N)
+    Space complexity: O(N) for the call stack
+    """
+    temp = head
+    while temp:
+        if temp.child:
+            nxt = temp.next
+            temp.next = flatten_v1(temp.child)
+            temp.next.prev = temp
+            temp.child = None
+            while temp.next:
+                temp = temp.next
+            if nxt:
+                temp.next = nxt
+                temp.next.prev = temp
+        temp = temp.next
+    return head
+
