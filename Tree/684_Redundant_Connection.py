924627b01f133825bb5c8cea101577d93d99ab8a
Anouer Hermassi <ahermass@stevens.edu>
2019-10-28 11:59:21 -0400

    Finding the redundant connection in an undirected graph - v2

--- Tree/684_Redundant_Connection.py
+++ Tree/684_Redundant_Connection.py
@@ -36,12 +36,42 @@ def find_redundant_connection_v1(edges):
         graph[v].add(u)
 
 
+# https://leetcode.com/articles/redundant-connection/
+# https://leetcode.com/problems/redundant-connection/discuss/123819/Union-Find-with-Explanations-(Java-Python)
+
+def find_redundant_connection_v2(edges):
+    """ Union-find solution. We simply find the first edge occurring in the graph that is already connected.
+    Time complexity: O(N), where N is the number of vertices (and also the number of edges) in the graph
+    Space complexity: O(N)
+    """
+
+    def find(x):
+        if parent[x] == 0:
+            return x
+        parent[x] = find(parent[x])
+        return parent[x]
+
+    def union(x, y):
+        rootX = find(x)
+        rootY = find(y)
+        if rootX == rootY:
+            return False
+        parent[rootX] = rootY
+        return True
+
+    parent = [0] * len(edges)
+    for x, y in edges:
+        if not union(x - 1, y - 1):
+            return [x, y]
+
+
 class Test(unittest.TestCase):
     data = [([[1, 2], [1, 3], [2, 3]], [2, 3]), ([[1, 2], [2, 3], [3, 4], [1, 4], [1, 5]], [1, 4])]
 
     def test_find_redundant_connection(self):
         for test_edges, result in self.data:
             self.assertEqual(result, find_redundant_connection_v1(test_edges))
+            self.assertEqual(result, find_redundant_connection_v2(test_edges))
 
 
 if __name__ == '__main__':
