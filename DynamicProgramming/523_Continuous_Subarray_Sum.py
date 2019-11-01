581c1082138932f297de414b9741b9dad96c73d0
Anouer Hermassi <ahermass@stevens.edu>
2019-11-01 14:45:14 -0400

    Finding continuous subarray of sum multiple of k - v2

--- DynamicProgramming/523_Continuous_Subarray_Sum.py
+++ DynamicProgramming/523_Continuous_Subarray_Sum.py
@@ -15,15 +15,44 @@ def check_subarray_sum_v1(nums, k):
     """
     n = len(nums)
     for i in range(n):
-        sum = nums[i]
+        cur_sum = nums[i]
         for j in range(i + 1, n):
-            sum += nums[j]
+            cur_sum += nums[j]
             if k == 0:
-                if sum == 0:
+                if cur_sum == 0:
                     return True
                 continue
-            elif sum % k == 0:
+            elif cur_sum % k == 0:
                 return True
+    return False
+
+
+def check_subarray_sum_v2(nums, k):
+    """ Similar to 560- Subarray Sum Equals K. The idea behind this approach is as follows: If the cumulative sum
+        up to two indices is the same, the sum of the elements lying in between those indices is zero.
+        Extending the same thought further, if the cumulative sum up to two indices, say i and j is at a difference of
+        k, i.e. if sum[i] - sum[j] = k, the sum of elements lying between indices i and j is k.
+        We iterate through the input array exactly once, keeping track of the running sum mod k of the elements in the
+        process. If we find that a running sum value at index j has been previously seen before in some earlier index i
+        in the array, then we know that the sub-array (i,j] contains a desired sum.
+        This is one of those magics of remainder theorem:
+            ((a + (n*x)) % x) is same as (a % x)
+        For e.g. in case of the array [23,2,6,4,7] the running sum is [23,25,31,35,42] and the remainders are
+        [5,1,1,5,0]. We got remainder 5 at index 0 and at index 3. That means, in between these two indexes we must
+        have added a number which is multiple of the k.
+    Time complexity: O(N)
+    Space complexity: O(min(N,k)), hash map can contain up to min(N,k) different pairings
+    """
+    indices = {0: -1}  # This initialization avoids the case when the first element of the array is multiple of k,
+    # since 0-(-1)= 1 is not greater than 1, while we want sub arrays of size at least 2
+    cur_sum = 0
+    for i, num in enumerate(nums):
+        cur_sum += num
+        cur_sum = cur_sum % k if k else cur_sum
+        if cur_sum not in indices:
+            indices[cur_sum] = i
+        elif i - indices[cur_sum] > 1:  # Difference of sub array's start/end indices to ensure it is of size at least 2
+            return True
     return False
 
 
@@ -33,6 +62,7 @@ class Test(unittest.TestCase):
     def test_check_subarray_sum(self):
         for test_array, test_k, result in self.data:
             self.assertEqual(result, check_subarray_sum_v1(test_array, test_k))
+            self.assertEqual(result, check_subarray_sum_v2(test_array, test_k))
 
 
 if __name__ == '__main__':
