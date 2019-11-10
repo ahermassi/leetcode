1af974839014dd4743370374d61fcf46630cc5e6
Anouer Hermassi <ahermass@stevens.edu>
2019-11-10 12:58:24 -0500

    Max area of island - v2 update

--- Arrays/695_Max_Area_of_Island.py
+++ Arrays/695_Max_Area_of_Island.py
@@ -37,31 +37,31 @@ def max_area_of_island_v1(grid):
 
 
 def max_area_of_island_v2(grid):
-    """ We can try the same approach using a stack based, (or "iterative") depth-first search.
+    """ We can try the same approach using a stack-based, (or iterative) DFS.
         Here, 'visited' set will represent squares that have either been visited or are added to our list of squares
         to visit (stack). For every starting land square that hasn't been visited, we will explore 4-directionally
-        around it, adding land squares that haven't been added to 'visited to our stack.
+        around it, adding land squares that haven't been added to 'visited' to our stack.
         On the side, we'll keep a count 'area' of the total number of squares seen during the exploration of this shape.
         We'll want the running max of these counts.
     Time complexity: O(N * M)
     Space complexity: O(N * M), the space used by 'visited' to keep track of visited squares and the space used by stack
     """
-    rows, cols, max_area, visited = len(grid), len(grid[0]), 0, set()
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] and (i, j) not in visited:
-                stack = [(i, j)]
-                visited.add((i, j))
+    n, m = len(grid), len(grid[0])
+    visited, res = set(), 0
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 1 and (i, j) not in visited:
                 area = 0
+                stack = [(i, j)]
                 while stack:
-                    area += 1
                     x, y = stack.pop()
-                    for a, b in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
-                        if 0 <= a < rows and 0 <= b < cols and grid[a][b] and (a, b) not in visited:
-                            visited.add((a, b))
-                            stack.append((a, b))
-                max_area = max(area, max_area)
-    return max_area
+                    if not 0 <= x < n or not 0 <= y < m or (x, y) in visited or grid[x][y] == 0:
+                        continue
+                    area += 1
+                    visited.add((x, y))
+                    stack.extend([(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)])
+                res = max(res, area)
+    return res
 
 
 class Test(unittest.TestCase):
