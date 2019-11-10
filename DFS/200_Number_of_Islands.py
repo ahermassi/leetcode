0145c20714526740fff8f40c64b57135e57f486f
Anouer Hermassi <ahermass@stevens.edu>
2019-11-10 13:13:02 -0500

    Number of islands - v1 update

--- DFS/200_Number_of_Islands.py
+++ DFS/200_Number_of_Islands.py
@@ -1,6 +1,6 @@
-''' Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by
+""" Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by
 water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the
-grid are all surrounded by water. '''
+grid are all surrounded by water. """
 
 import unittest2 as unittest
 
@@ -8,31 +8,29 @@ import unittest2 as unittest
 def num_islands_v1(grid):
     """ Iterate through each of the cell and if it is an island, do dfs to mark all adjacent islands, then increase
         the counter by 1.
-        This solution uses a 'seen' set in order to avoid an infinite recursion.
+        This solution uses a 'visited' set in order to avoid an infinite recursion.
         This is DFS because the recursive call happens before finishing the current execution.
     Time complexity: O(N * M) where N is the number of rows in the given grid and M is the number of columns. We visit
     every square once.
-    Space complexity: O(N * M) for both visited set and recursion call stack
+    Space complexity: O(N * M) for both 'visited' set and recursion call stack
     """
     def dfs(i, j):
-        if i < 0 or i >= row or j < 0 or j >= col or grid[i][j] == '0' or (i, j) in seen:
+        if not 0 <= i < n or not 0 <= j < m or (i, j) in visited or grid[i][j] == '0':
             return
-        seen.add((i, j))
-        dfs(i - 1, j)
-        dfs(i + 1, j)
-        dfs(i, j - 1)
-        dfs(i, j + 1)
+        visited.add((i, j))
+        for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+            dfs(x, y)
 
     if not grid:
         return 0
-    count, seen = 0, set()
-    row, col = len(grid), len(grid[0])
-    for i in range(row):
-        for j in range(col):
-            if grid[i][j] == '1' and (i, j) not in seen:
+    n, m, res = len(grid), len(grid[0]), 0
+    visited = set()
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == '1' and (i, j) not in visited:
                 dfs(i, j)
-                count += 1
-    return count
+                res += 1
+    return res
 
 
 def num_islands_v2(grid):
