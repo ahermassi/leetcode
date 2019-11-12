04cfd0cb41d380d4dabd82164543c32d0f293a5c
Anouer Hermassi <ahermass@stevens.edu>
2019-11-12 11:50:02 -0500

    Add strings - update

--- Strings/415_Add_Strings.py
+++ Strings/415_Add_Strings.py
@@ -14,9 +14,8 @@ def add_strings(num1, num2):
     num1, num2 = list(num1), list(num2)
     carry, res = 0, []
     while num1 or num2:
-        n1 = n2 = 0
-        if num1: n1 = ord(num1.pop()) - ord('0')
-        if num2: n2 = ord(num2.pop()) - ord('0')
+        n1 = ord(num1.pop()) - ord('0') if num1 else 0
+        n2 = ord(num2.pop()) - ord('0') if num2 else 0
         temp = n1 + n2 + carry
         res.append(temp % 10)
         carry = temp // 10
