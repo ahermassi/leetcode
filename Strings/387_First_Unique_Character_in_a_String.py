5ea764c6895fa93d1dcac939362ea571947d974d
Anouer Hermassi <ahermass@stevens.edu>
2019-11-12 10:52:19 -0500

    First unique character in a string - v2

--- Strings/387_First_Unique_Character_in_a_String.py
+++ Strings/387_First_Unique_Character_in_a_String.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 import unittest2 as unittest
 
 
-def first_uniq_char(s):
+def first_uniq_char_v1(s):
     """ The idea is to go through the string and save in a hash map the number of times each character appears in the
     string. And then we go through the string the second time, this time we use the hash map as a reference to check
     if a character is unique or not.
@@ -22,6 +22,26 @@ def first_uniq_char(s):
     return -1
 
 
+def first_uniq_char_v2(s):
+    """ Single pass over the string. Construct a character to index mapping, and when a character is encountered for
+        a second time set its map value to 1. We end up with a hash map where all unique characters have values
+        different from -1.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+    map = defaultdict(int)
+    for i, c in enumerate(s):
+        if c in map:
+            map[c] = -1
+        else:
+            map[c] = i
+    res = float('inf')
+    for v in map.values():
+        if v != -1:
+            res = min(res, v)
+    return res if res != float('inf') else -1
+
+
 class Test(unittest.TestCase):
     data = [('leetcode', 0),
             ('loveleetcode', 2)
@@ -29,7 +49,8 @@ class Test(unittest.TestCase):
 
     def test_first_uniq_char(self):
         for test_string, result in self.data:
-            self.assertEqual(result, first_uniq_char(test_string))
+            self.assertEqual(result, first_uniq_char_v1(test_string))
+            self.assertEqual(result, first_uniq_char_v2(test_string))
 
 
 if __name__ == '__main__':
