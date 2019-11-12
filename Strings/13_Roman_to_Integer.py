378d176acc11cf1c4700a1877fef5e99ada7ca07
Anouer Hermassi <ahermass@stevens.edu>
2019-11-12 10:00:40 -0500

    Roman to integer - update

--- Strings/13_Roman_to_Integer.py
+++ Strings/13_Roman_to_Integer.py
@@ -3,42 +3,21 @@
 import unittest2 as unittest
 
 
-def roman_to_int_v1(s):
-    """ Most intuitive approach.
-    Time complexity: O(N)
-    Space complexity: O(1)
-    """
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    sum = 0
-    i = 0
-    while i < len(s):
-        c = s[i]
-        if i + 1 < len(s) and (c == 'I' and s[i + 1] in {'V', 'X'} or
-                               c == 'X' and s[i + 1] in {'L', 'C'} or
-                               c == 'C' and s[i + 1] in {'D', 'M'}):
-                sum = sum + roman[s[i + 1]] - roman[c]
-                i += 2
-        else:
-            sum += roman[c]
-            i += 1
-    return sum
-
-
-def roman_to_int_v2(s):
+def roman_to_int(s):
     """ If one letter is less than its latter one, this letter is subtracted.
     Time complexity: O(N)
     Space complexity: O(1)
     """
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    sum = 0
-    for i in range(len(s)):
-        c = s[i]
-        next = s[i + 1: i + 2]
-        if next and roman[next] > roman[c]:
-            sum -= roman[c]
+    roman_to_integer = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    for i in range(len(s) - 1):
+        c, nxt = s[i], s[i + 1]
+        if roman_to_integer[c] < roman_to_integer[nxt]:
+            res -= roman_to_integer[c]
         else:
-            sum += roman[c]
-    return sum
+            res += roman_to_integer[c]
+    res += roman_to_integer[s[-1]]  # Don't forget to add the last letter anyway as the loop stops at index len(s)-1
+    return res
 
 
 class Test(unittest.TestCase):
@@ -52,8 +31,8 @@ class Test(unittest.TestCase):
 
     def test_roman_to_int(self):
         for result, test_string in self.data:
-            self.assertEqual(result, roman_to_int_v1(test_string))
-            self.assertEqual(result, roman_to_int_v2(test_string))
+            self.assertEqual(result, roman_to_int(test_string))
+            self.assertEqual(result, roman_to_int(test_string))
 
 
 if __name__ == '__main__':
