ad97186e0abe3ce27566524d29b79046104bd9b6
Anouer Hermassi <ahermass@stevens.edu>
2019-11-12 15:23:20 -0500

    Add binary - update

--- Strings/67_Add_Binary.py
+++ Strings/67_Add_Binary.py
@@ -4,25 +4,22 @@ The input strings are both non-empty and contains only characters 1 or 0. """
 import unittest2 as unittest
 
 
-def add_binary_v1(a, b):
+def add_binary(a, b):
     """ Check out 415_Add_Strings. Same logic.
     Time complexity: O(max(N, M)) where N is the length of a and M is the length of b
     Space complexity: O(max(N, M))
     """
     a, b = list(a), list(b)
-    carry, res = 0, []
+    res, carry = [], 0
     while a or b:
-        n1 = n2 = 0
-        if a:
-            n1 = ord(a.pop()) - ord('0')
-        if b:
-            n2 = ord(b.pop()) - ord('0')
-        temp = n1 + n2 + carry
-        res.append(temp % 2)
-        carry = temp // 2
+        val1 = ord(a.pop()) - ord('0') if a else 0
+        val2 = ord(b.pop()) - ord('0') if b else 0
+        val = val1 + val2 + carry
+        res.append(str(val % 2))
+        carry = val // 2
     if carry:
-        res.append(carry)
-    return ''.join(str(i) for i in reversed(res))
+        res.append(str(carry))
+    return ''.join(res)[::-1]
 
 
 class Test(unittest.TestCase):
@@ -30,9 +27,9 @@ class Test(unittest.TestCase):
             ('1010', '1011', '10101')
             ]
 
-    def test_add_binary_v1(self):
+    def test_add_binary(self):
         for test_num1, test_num2, result in self.data:
-            self.assertEqual(result, add_binary_v1(test_num1, test_num2))
+            self.assertEqual(result, add_binary(test_num1, test_num2))
 
 
 if __name__ == '__main__':
