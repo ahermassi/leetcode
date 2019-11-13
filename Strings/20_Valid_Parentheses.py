59c23375e9e238cff885bc99c8452cac5dc1105e
Anouer Hermassi <ahermass@stevens.edu>
2019-11-13 09:08:04 -0500

    Valid parentheses - update

--- Strings/20_Valid_Parentheses.py
+++ Strings/20_Valid_Parentheses.py
@@ -9,44 +9,19 @@ import unittest2 as unittest
 
 
 def is_valid_v1(s):
-    """ Let's do this recursively.
-        1- An empty string is valid
-        2 - A string s of length 2 is valid if and only if s[1] is the closing bracket of s[0] (Condition 2 below)
-        3- A string s of the length > 2 is:
-            * Not valid if s[0] is a closing bracket. (Condition 3 below)
-            * Candidate valid if s[0] is an opening bracket but s[1] is not its matching closing bracket, in which case
-            recursively investigate s[1:] and go to 1- 2- 3- (Condition 4 below)
-            * Candidate valid if s[1] is the closing bracket of s[0], in which case remove s[0:2] and recursively
-            investigate the rest of the string. (Condition 5 below)
-        The idea is to examine pairs of same type of bracket, when a matching pair is found delete it, rinse and repeat.
-    Time complexity: O(N) where N is the length of s
-    Space complexity: O(N), in the worst case s could be a sequence of only opening brackets ( '(((((((((((' )
-    """
-
-    s, d = list(s), {'(': ')', '{': '}', '[': ']'}
-
-    def process(s, index):
-        if not s:  # Condition 1
-            return True
-        if len(s) == 2:
-            try:
-                return s[index + 1] == d[s[index]]  # Condition 2
-            except KeyError:
-                return False
-        if len(s) > 2:
-            if s[index] not in d:  # Condition 3
-                return False
-            if s[index + 1] != d[s[index]]:  # Condition 4
-                return process(s, index + 1)
-            else:  # Condition 5
-                s[index:index + 2] = []
-                return process(s, index - 1 if index >= 1 else 0)
-
-    return process(s, 0)
-
-
-def is_valid_v2(s):
-    """ Iterative approach. Pretty straightforward.
+    """ An interesting property about a valid parenthesis expression is that a sub-expression of a valid expression
+        should also be a valid expression. (Not every sub-expression)
+        What if whenever we encounter a matching pair of parenthesis in the expression, we simply remove it from the
+        expression? If it is a valid expression, we would be left with an empty string in the end.
+        The stack data structure can come in handy here in representing this recursive structure of the problem. We
+        can't really process this from the inside out because we don't have an idea about the overall structure. But,
+        the stack can help us process this recursively i.e. from outside to inwards.
+        If we encounter an opening bracket, we simply push it onto the stack. This means we will process it later, let
+        us simply move onto the sub-expression ahead.
+        If we encounter a closing bracket, then we check the element on top of the stack. If the element at the top of
+        the stack is an opening bracket of the same type, then we pop it off the stack and continue processing. Else,
+        this implies an invalid expression.
+        In the end, if we are left with a stack still having elements, then this implies an invalid expression.
     Time complexity: O(N)
     Space complexity: O(N)
     """
@@ -70,7 +45,6 @@ class Test(unittest.TestCase):
     def test_is_valid(self):
         for test_string, result in self.data:
             self.assertEqual(result, is_valid_v1(test_string))
-            self.assertEqual(result, is_valid_v2(test_string))
 
 
 if __name__ == '__main__':
