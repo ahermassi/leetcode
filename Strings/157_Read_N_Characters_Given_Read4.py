747f164a978e319466643fe20c0d10d10d5fe83b
Anouer Hermassi <ahermass@stevens.edu>
2019-11-13 15:03:35 -0500

    Read N characters given read4 - update

--- Strings/157_Read_N_Characters_Given_Read4.py
+++ Strings/157_Read_N_Characters_Given_Read4.py
@@ -9,18 +9,19 @@ def read4(buf):
 def read(buf, n):
     """ This function is to read n characters into buf only 4 characters at a time using read4() function.
     """
-    i = 0
-    while i < n:
-        buf4 = [''] * 4  # Temporary buffer to read characters into
-        count = read4(buf)
+    total = 0
+    while total < n:
+        temp = [''] * 4  # Temporary buffer to read characters into
+        count = read4(temp)
         if not count:  # This check is used for cases when we attempt to read more characters than the actual length
             # of the file, in which case read4 returns count = 0
             break
-        count = min(count, n - i)  # This is for cases when the remaining number of characters in the file is greater
-        # than the remaining number of characters we want to read. Example: 'leetcode', n = 5. First pass we read 4
-        # characters 'leet', second pass read4 reads 'code' and returns count = 4, but we want to read only one
+        count = min(count, n - total)  # This is for cases when the remaining number of characters in the file is
+        # greater than the remaining number of characters we want to read. Example: 'leetcode', n = 5. First pass we
+        # read 4 characters 'leet', second pass read4 reads 'code' and returns count = 4, but we want to read only one
         # character more: 5 - 4 = 1
-        buf[i:] = buf4[:count]  # Read that exact number of characters from buf4 and append it to buf
-        i += count
-    return i
+        # Even if we read 4 chars from read4, we don't want to exceed n and only want to read chars till n
+        buf[total:] = temp[:count]  # Read that exact number of characters from temp and append it to buf
+        total += count
+    return total
 
