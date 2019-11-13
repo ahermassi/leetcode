45bfd34cdda9a373da281e7f0f4c07ee0ba8b81e
Anouer Hermassi <ahermass@stevens.edu>
2019-11-13 16:35:44 -0500

    Palindromic substrings - v2 update

--- DynamicProgramming/647_Palindromic_Substrings.py
+++ DynamicProgramming/647_Palindromic_Substrings.py
@@ -34,20 +34,30 @@ def count_substrings_v2(s):
         If we already knew that "bab" is a palindrome, it is obvious that "ababa" must be a palindrome since the two
         left and right end letters are the same.
         We define dp[i][j] as following:
-        dp[i][j] is True is substring s[i:j + 1] is palindrome
+            dp[i][j] is True if substring s[i:j + 1] is palindrome
         Therefore:
-        dp[i][j] = (s[i] == s[j]) AND (dp[i+1][j-1])
-        This yields a straight forward DP solution, which we first initialize the one letter palindromes, and work our
-        way up finding all two letters palindromes, and so on...
+            dp[i][j] = (s[i] == s[j]) AND (dp[i+1][j-1])
+        But here, we should explain why we use dp[i+1][j-1] to calculate dp[i][j]. The reason is that i is in
+        descending order and j is in ascending order. Then we know that before d[i][j] the value of d[i+1][j-1] is
+        already known and calculated in a previous iteration.
+        Build a table with all possible string[start:end] combinations, storing which are palindromes and which are
+        not (True or False). At any given moment, when we're checking if string[i:j] is a palindrome, we only need to
+        know two things:
+            1- Is string[i] equal to string[j] ?
+            2- Is string[i+1:j-1] a palindrome?
+        For condition (1), a simple check will do. For condition (2), we use the table. If both conditions are met,
+        mark dp[i][j] as True and increase the count.
     Time complexity: O(N ** 2)
     Space complexity: O(N ** 2) to store dp array
     """
     n, res = len(s), 0
     dp = [[False] * n for _ in range(n)]
-    for i in range(n):
+    for i in range(n):  # Every isolated character is a palindrome
         dp[i][i] = True
         res += 1
-    for i in reversed(range(n)):
+    for i in reversed(range(n)):  # We reverse the range because the sub-problem is not populated yet. For example, if
+        # we want to know if substring s[0-5] (i.e. dp[0][5]) is palindrome, we'd look up the table for dp[1][4].
+        # However, if i goes from left to right, dp[1] has not been calculated yet because dp[1] comes after dp[0].
         for j in range(i + 1, n):
             if s[i] == s[j] and (j - i < 2 or dp[i + 1][j - 1]):
                 dp[i][j] = True
