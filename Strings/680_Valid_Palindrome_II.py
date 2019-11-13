8eaa4149233267d5d694cb32059f52816e1f5541
Anouer Hermassi <ahermass@stevens.edu>
2019-11-13 09:47:40 -0500

    Valid palindrome II - update

--- Strings/680_Valid_Palindrome_II.py
+++ Strings/680_Valid_Palindrome_II.py
@@ -7,17 +7,17 @@ def valid_palindrome(s):
     """ We can use the standard two-pointer approach that starts at the left and right of the string and move
         inwards. Whenever there is a mismatch, we can either exclude the character at the left or the right pointer. We
         then check if either substring is a palindrome.
+        In other words: check from left and right at the same time until the first different pair. Now we have
+        something like a****b, where a and b are different. We need to delete either a or b to make it a palindrome.
     Time complexity: O(N)
     Space complexity: O(N) for the creation of reversed strings using [::-1] notation
     """
     left, right = 0, len(s) - 1
-    while left < right:
-        if s[left] != s[right]:
-            left_str, right_str = s[left:right], s[left + 1: right + 1]
-            return left_str == left_str[::-1] or right_str == right_str[::-1]
+    while left < right and s[left] == s[right]:
         left += 1
         right -= 1
-    return True
+    left_subs, right_subs = s[left:right], s[left + 1:right + 1]  # Deleting right and left characters respectively
+    return left_subs == left_subs[::-1] or right_subs == right_subs[::-1]
 
 
 class Test(unittest.TestCase):
