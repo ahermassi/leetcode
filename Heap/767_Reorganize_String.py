fa927c920130279f3a78decdf208dafafcd8556b
Anouer Hermassi <ahermass@stevens.edu>
2019-11-14 14:29:02 -0500

    Reorganize string - v2

--- Heap/767_Reorganize_String.py
+++ Heap/767_Reorganize_String.py
@@ -7,7 +7,7 @@ from heapq import heappush, heappop
 import unittest2 as unittest
 
 
-def reorganize_string(S):
+def reorganize_string_v1(S):
     """ This is similar to 621- Task Scheduler problem, with the cooling interval is equal to 2.
         This is a greedy approach that tries to write the most common letter followed by the second most common letter
         and so on.
@@ -39,12 +39,50 @@ def reorganize_string(S):
     return res if not heap else res + heap[0][1]
 
 
+def reorganize_string_v2(S):
+    """ The idea is inspired from counting sort.
+        Count letter appearance and store it in chars[i]
+        Find the letter with largest occurrence.
+        Put that letter into even indices (0, 2, 4, ...) of 'res' array
+        Alternately place the rest of letters
+    Time complexity: O(N)
+    Space complexity: O(N)
+    """
+    chars = [0] * 26
+    for c in S:
+        chars[ord(c) - ord('a')] += 1
+    most_frequent, max_count = 0, 0
+    for i, val in enumerate(chars):
+        if val > max_count:
+            max_count = val
+            most_frequent = i
+    if max_count > (len(S) + 1) / 2:
+        return ''
+    res = [None] * len(S)
+    idx = 0
+    while chars[most_frequent]:
+        res[idx] = chr(most_frequent + ord('a'))
+        chars[most_frequent] -= 1
+        idx += 2
+    for i in range(len(chars)):
+        while chars[i]:
+            if idx >= len(res):  # This check makes sure that after exhausting all the empty indices we go back to
+                # the beginning of array and start from the first ODD index since the first even indices are occupied
+                # by the most frequent character
+                idx = 1
+            res[idx] = chr(i + ord('a'))
+            chars[i] -= 1
+            idx += 2
+    return ''.join(res)
+
+
 class Test(unittest.TestCase):
     data = [('aab', 'aba'), ('aaab', '')]
 
     def test_reorganize_string(self):
         for test_string, result in self.data:
-            self.assertEqual(result, reorganize_string(test_string))
+            self.assertEqual(result, reorganize_string_v1(test_string))
+            self.assertEqual(result, reorganize_string_v2(test_string))
 
 
 if __name__ == '__main__':
