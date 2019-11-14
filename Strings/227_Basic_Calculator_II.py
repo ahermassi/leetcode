5ba1933c8a151e92c811aeaaa40f5ff158c5b85e
Anouer Hermassi <ahermass@stevens.edu>
2019-11-14 15:35:23 -0500

    Basic calculator II - update

--- Strings/227_Basic_Calculator_II.py
+++ Strings/227_Basic_Calculator_II.py
@@ -9,11 +9,16 @@ def calculate(s):
     """ This solution is stack based. The stack is used to hold temporary results for partial expressions with lower
         precedence levels, which are + and -. This helps delaying calculations until an operator with a higher
         precedence is met.
+        Every time an operation sign is met, 'last_operator' variable tells us what type of a PREVIOUS operation the
+        last integer we read is part of: 11 - 2 * 5: say we read 11 and then encounter '-', we look at 'last_operator'
+        which is '+' by default, so we know that 11 is part of an addition operation (0 + 11). Then we read 2 and meet
+        '*', we look at 'last_operator' which was assigned to '-', so we know that 2 is part of a subtraction operation
+        (11 - 2).
         For example, let's look at '22 - 31 * 52 + 22'.
         First, num variable is used to accumulate the integer value of operands. Here, the first value of num will hold
         22. When '-' is encountered, we look at the last operator which is '+' by default. Since '+' is a lower
         precedence operator, push num (22) and let it wait, and change last operator to '-'. Again, num holds the value
-        of next operand 31, and '*' is encountered. last operator is '-', so push -num (-31) to the stack and change
+        of next operand 31, and '*' is encountered. Last operator is '-', so push -num (-31) to the stack and change
         last operator to *. Now num = 52 is processed and '+' is encountered. Since last operator is '*' which has
         higher precedence than '+', push the result of num = 52 multiplied by last element in the stack = -31 (after
         popping it), and change last operator to '+'. Now last operand is read i.e. num = 22, and we are at the end of
@@ -22,21 +27,27 @@ def calculate(s):
     Time complexity: O(N) where N is the length of s
     Space complexity: O(N)
      """
-    num, stack, last_operator = 0, [], '+'
+    num, stack, last_operation = 0, [], '+'
     for i in range(len(s)):
         if s[i].isdigit():
             num = num * 10 + int(s[i])
         if s[i] in '+-*/' or i == len(s) - 1:
-            if last_operator == '+':
+            if last_operation == '+':  # I have a new operation sign in hand and the accumulated integer 'num' is
+                # part of an PREVIOUS addition operation
                 stack.append(num)
-            elif last_operator == '-':
+            elif last_operation == '-':
                 stack.append(-num)
-            elif last_operator == '*':
-                stack.append(stack.pop() * num)
-            else:
-                stack.append(int(float(stack.pop()) / num))
+            elif last_operation == '*':  # I have a new operation sign in hand and the accumulated integer 'num' is
+                # part of a PREVIOUS multiplication operation
+                stack.append(stack.pop() * num)  # Multiply 'num' with the previously accumulated integer that's been
+                # waiting in the stack since multiplication is a higher level operation
+            else:  # I have a new operation sign in hand and the accumulated integer 'num' is part of a PREVIOUS
+                # division operation
+                stack.append(int(stack.pop() / num))  # Perform the division of the previously accumulated integer
+                # that's been waiting in the stack by the accumulated integer 'num' since division is a higher level
+                # operation
             num = 0
-            last_operator = s[i]
+            last_operation = s[i]
     return sum(stack)
 
 
