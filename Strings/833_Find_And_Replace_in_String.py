335e545c09abb0bf834037726edd4771b3318bb1
Anouer Hermassi <ahermass@stevens.edu>
2019-11-14 12:08:08 -0500

    Find and replace in string - v1 update

--- Strings/833_Find_And_Replace_in_String.py
+++ Strings/833_Find_And_Replace_in_String.py
@@ -3,7 +3,7 @@
 import unittest2 as unittest
 
 
-def find_replace_string(S, indexes, sources, targets):
+def find_replace_string_v1(S, indexes, sources, targets):
     """ Pretty straightforward. Scan S from left to right and run some verifications.
     Time complexity: O(N + Q), where N is the length of S and Q is the length of indexes (or sources, or targets)
     Space complexity: O(N + Q)
@@ -14,8 +14,8 @@ def find_replace_string(S, indexes, sources, targets):
         c = S[i]
         if i in d and S[i:i + len(d[i][0])] == d[i][0]:
             res.append(d[i][1])
-            i += len(d[i][0])
-        else:
+            i += len(d[i][0])  # We skip as many characters we replaced because that entire substring no longer exists
+        else:  # No characters to replace at the current index, so append the original character
             res.append(c)
             i += 1
     return ''.join(res)
@@ -27,7 +27,7 @@ class Test(unittest.TestCase):
 
     def test_find_replace_string(self):
         for test_string, indexes, sources, targets, result in self.data:
-            self.assertEqual(result, find_replace_string(test_string, indexes, sources, targets))
+            self.assertEqual(result, find_replace_string_v1(test_string, indexes, sources, targets))
 
 
 if __name__ == '__main__':
