a506b212b03c869512f90b2291534eb8c47178b6
Anouer Hermassi <ahermass@stevens.edu>
2019-11-14 12:15:24 -0500

    Find and replace in string - v2

--- Strings/833_Find_And_Replace_in_String.py
+++ Strings/833_Find_And_Replace_in_String.py
@@ -21,6 +21,22 @@ def find_replace_string_v1(S, indexes, sources, targets):
     return ''.join(res)
 
 
+def find_replace_string_v2(S, indexes, sources, targets):
+    """ Make a list of characters out of the string. When an index and a source match in the original string, replace
+        the character at that list index with the ENTIRE target string and remove the following characters that occur
+        in the original string from the list (up to the source length).
+    Time complexity: O(N + Q), where N is the length of S and Q is the length of indexes (or sources, or targets)
+    Space complexity: O(N + Q)
+    """
+    res = list(S)
+    for index, source, target in zip(indexes, sources, targets):
+        if S[index:index + len(source)] == source:
+            res[index] = target
+            for i in range(index + 1, index + len(source)):
+                res[i] = ''
+    return ''.join(res)
+
+
 class Test(unittest.TestCase):
     data = [('abcd', [0, 2], ['a', 'cd'], ['eee', 'ffff'], 'eeebffff'),
             ('abcd', [0, 2], ['ab', 'ec'], ['eee', 'ffff'], 'eeecd')]
@@ -28,6 +44,7 @@ class Test(unittest.TestCase):
     def test_find_replace_string(self):
         for test_string, indexes, sources, targets, result in self.data:
             self.assertEqual(result, find_replace_string_v1(test_string, indexes, sources, targets))
+            self.assertEqual(result, find_replace_string_v2(test_string, indexes, sources, targets))
 
 
 if __name__ == '__main__':
