0d66ad3c2562b28a95ff039261c6456643e2b02a
Anouer Hermassi <ahermass@stevens.edu>
2019-11-17 14:39:04 -0500

    Maximum subarray - v1 update

--- Arrays/53_Maximum_Subarray.py
+++ Arrays/53_Maximum_Subarray.py
@@ -5,19 +5,18 @@ import unittest2 as unittest
 
 
 def maximum_subarray_v1(nums):
-    """
-    This algorithm is called Kadane's algorithm. Assume first element is the maximum (local and global) sum. Iterate
-    over the array, and at each step ask yourself: which is greater, the current element x or (last sum + x) ? Update
-    the local sum and global sum accordingly.
+    """ This is called Kadane's algorithm. Assume first element is the maximum (local and global) sum. Iterate over the
+        array, and at each step ask yourself: which is greater, the current element x or (last sum + x) ? Update the
+        local sum and global sum accordingly.
     Time complexity: O(N)
     Space complexity: O(1)
     """
-    curr_sum = max_sum = nums[0]
+    max_so_far = max_ending_here = nums[0]
     for i in range(1, len(nums)):
-        curr_sum = max(nums[i], curr_sum + nums[i])  # What's the maximum subarray ending here ? Either [current
-        # element] or [previous subarray, current]
-        max_sum = max(max_sum, curr_sum)  # Update global max
-    return max_sum
+        max_ending_here = max(nums[i], max_ending_here + nums[i])  # What's the maximum sub array ending here ? Either
+        # [current element] or [previous sub array, current]
+        max_so_far = max(max_so_far, max_ending_here)  # Update global max
+    return max_so_far
 
 
 def maximum_subarray_v2(nums):
