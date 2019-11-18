f7b9e7ab2aa2039943c2858aca41a913e29f8586
Anouer Hermassi <ahermass@stevens.edu>
2019-11-18 13:51:40 -0500

    Valid mountain array - v2

--- Arrays/941_Valid_Mountain_Array.py
+++ Arrays/941_Valid_Mountain_Array.py
@@ -3,10 +3,10 @@
 import unittest2 as unittest
 
 
-def valid_mountain_array(A):
+def valid_mountain_array_v1(A):
     """ If we walk along the mountain from left to right, we have to move strictly up, then strictly down.
         Walk up from left to right until we can't: that has to be the peak. We should ensure the peak is not the first
-        or last element. Then, we walk down. If we reach the end, the array is valid, otherwise its not.
+        or last element. Then, we walk down. If we reach the end, the array is valid, otherwise it is not.
     Time complexity: O(N)
     Space complexity: O(1)
     """
@@ -20,6 +20,21 @@ def valid_mountain_array(A):
     return i == len(A) - 1
 
 
+def valid_mountain_array_v2(A):
+    """ Two people climb from left and from right separately. If they are climbing the same mountain, they will meet at
+        the same point.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+    n = len(A)
+    left, right = 0, n - 1
+    while left < n - 1 and A[left] < A[left + 1]:
+        left += 1
+    while right > 0 and A[right - 1] > A[right]:
+        right -= 1
+    return 0 < left == right < n - 1
+
+
 class Test(unittest.TestCase):
     data = [([2, 1], False),
             ([3, 5, 5], False),
@@ -28,7 +43,8 @@ class Test(unittest.TestCase):
 
     def test_valid_mountain_array(self):
         for test_array, result in self.data:
-            self.assertEqual(result, valid_mountain_array(test_array))
+            self.assertEqual(result, valid_mountain_array_v1(test_array))
+            self.assertEqual(result, valid_mountain_array_v2(test_array))
 
 
 if __name__ == '__main__':
