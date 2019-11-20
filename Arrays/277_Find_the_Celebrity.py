dc41d61ba6b6b18a385b6932f4a1efeb5babe605
Anouer Hermassi <ahermass@stevens.edu>
2019-11-20 12:15:12 -0500

    Find the celebrity - v1 update

--- Arrays/277_Find_the_Celebrity.py
+++ Arrays/277_Find_the_Celebrity.py
@@ -16,21 +16,27 @@ def find_celebrity_v1(n):
         The moment you realize a call to knows(i,j) eliminates either i or j the problem is solved. knows(i,j) == true
         then i can't be a celebrity. since a celebrity knows nobody and knows(i,j) == false then j can't be a celebrity
         since everyone must know the celebrity.
+        Notice this interesting property: for any two persons A and B, the possible candidate is always mutually
+        exclusive. i.e. only one person of A and B can be candidate. Because if A knows B, A cannot be celebrity; if A
+        doesn't know B, B cannot be be celebrity.
+        With the above property, traversing and checking every person with a current candidate will filter out all
+        'bad guys'.
     Time complexity: O(n)
     Space complexity: O(1)
     """
     candidate = 0
     for i in range(1, n):
-        if knows(candidate, i):
+        if knows(candidate, i):  # If true, then 'candidate' cannot be the real celebrity because real celebrity should
+            # know nobody. If false, then i cannot be the celebrity because celebrity should be known by everyone.
             candidate = i
-    # We've established that candidate does not know anyone AFTER it
-    # Let's establish that candidate is known by, but does not know everyone BEFORE it
-    for i in range(candidate):
-        if knows(candidate, i):
+    # We've established that candidate does not know anyone AFTER him.
+    # Let's establish that candidate doesn't know anyone BEFORE him and is known by EVERYONE.
+    for i in range(n):
+        if i < candidate and (knows(candidate, i) or not knows(i, candidate)):
             return -1
-    # Last thing we don't know: Does everyone after candidate know candidate ?
-    for i in range(candidate + 1, n):
-        if not knows(i, candidate):
+        elif i > candidate and not knows(i, candidate):  # From the first loop we already know that the candidate does
+            # not know people ahead of them, so we can just check that everyone ahead of the candidate knows the
+            # candidate
             return -1
     return candidate
 
