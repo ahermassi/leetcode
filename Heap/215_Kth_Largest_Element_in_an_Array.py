3d99dfcd3456cc12a63ac13d1384d69e5efef141
Anouer Hermassi <ahermass@stevens.edu>
2019-11-21 16:39:47 -0500

    Kth largest element in an array - removed v3

--- Heap/215_Kth_Largest_Element_in_an_Array.py
+++ Heap/215_Kth_Largest_Element_in_an_Array.py
@@ -1,9 +1,8 @@
 """ Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order,
 not the kth distinct element. """
 
-from heapq import heappush, heappop, nlargest
+from heapq import heappush, heappop
 from random import randint
-
 import unittest2 as unittest
 
 
@@ -30,21 +29,12 @@ def find_kth_largest_v2(nums, k):
 
 
 def find_kth_largest_v3(nums, k):
-    """ Using the built-in nlargest.
-    Time complexity: The time complexity of adding an element in a heap is O(log(k)) (binary tree of k elements) and
-    we do it N times, that means O(N log(k))). Hence the overall complexity of the algorithm is O(N log(k))
-    Space complexity: O(k) to store the heap elements
-    """
-    return nlargest(k, nums)[-1]
-
-
-def find_kth_largest_v4(nums, k):
-    """ This approach is basically the same as for quick sort. For simplicity let's notice that kth largest element is
+    """ This approach is basically the same as for quick sort. For simplicity, notice that kth largest element is
         the same as (N - k)th smallest element, hence we could implement kth smallest algorithm for this problem.
         First we choose a pivot, and define its position in a sorted array in a linear time. This could be done with
         the help of partition algorithm. As an output we have an array where pivot is on its perfect position in the
         ascending sorted array, all elements on the left of the pivot are smaller than pivot, and all elements on the
-        right of the pivot are larger or equal to pivot.
+        right of the pivot are larger than or equal to pivot.
         If the pivot's rank is smaller than the rank we want to find, we are sure all elements before pivot actually
         have even smaller rank, we search the target at the part after pivot.
         If the pivot's rank is larger than the rank we want to find, we search the target at the left part before pivot.
@@ -81,7 +71,6 @@ class Test(unittest.TestCase):
             self.assertEqual(result, find_kth_largest_v1(test_array, test_k))
             self.assertEqual(result, find_kth_largest_v2(test_array, test_k))
             self.assertEqual(result, find_kth_largest_v3(test_array, test_k))
-            self.assertEqual(result, find_kth_largest_v4(test_array, test_k))
 
 
 if __name__ == '__main__':
