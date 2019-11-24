f32b68cdb33fedac426849a4739b6d800b653013
Anouer Hermassi <ahermass@stevens.edu>
2019-11-24 12:05:06 -0500

    Swap nodes in pairs - v2

--- LinkedList/24_Swap_Nodes_in_Pairs.py
+++ LinkedList/24_Swap_Nodes_in_Pairs.py
@@ -14,19 +14,36 @@ class ListNode(object):
         self.next = None
 
 
-def swap_pairs(head):
+def swap_pairs_v1(head):
     """ Do the swapping going backwards and return the new head of every 2 adjacent pairs at each call. This ensures
         the last node returned is the head of the new list.
     Time complexity: O(N)
-    Space complexity: O(1)
+    Space complexity: O(N) for the call stack
     """
     if not head or not head.next:
         return head
-    p = swap_pairs(head.next.next)  # Navigate to the last/before-last node (depends on even/odd length of the list)
+    p = swap_pairs_v1(head.next.next)  # Navigate to the last/before-last node (depends on even/odd length of the list)
     nxt = head.next
     nxt.next = head
     head.next = p
     return nxt  # After swapping, return the 'local' new head. The last one returned is the head of the entire new list
+
+
+def swap_pairs_v2(head):
+    """ Iterative solution.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+    dummy = ListNode(0)
+    dummy.next = head
+    cur = dummy
+    while cur.next and cur.next.next:
+        first, second = cur.next, cur.next.next
+        first.next = second.next
+        cur.next = second
+        second.next = first
+        cur = second.next
+    return dummy.next
 
 
 class Test(unittest.TestCase):
@@ -36,7 +53,7 @@ class Test(unittest.TestCase):
     head.next.next.next = ListNode(4)
 
     def test_swap_pairs(self):
-        head = swap_pairs(self.head)
+        head = swap_pairs_v1(self.head)
         self.assertEqual(2, head.val)
         self.assertEqual(1, head.next.val)
         self.assertEqual(4, head.next.next.val)
