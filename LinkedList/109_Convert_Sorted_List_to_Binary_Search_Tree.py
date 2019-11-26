f763789636cea8c5be803e7543d9369c8014a4fa
Anouer Hermassi <ahermass@stevens.edu>
2019-11-26 11:13:02 -0500

    Convert sorted list to BST - v3

--- LinkedList/109_Convert_Sorted_List_to_Binary_Search_Tree.py
+++ LinkedList/109_Convert_Sorted_List_to_Binary_Search_Tree.py
@@ -89,6 +89,48 @@ def sorted_list_to_bst_v2(head):
     return helper(0, len(vals) - 1)
 
 
+def sorted_list_to_bst_v3(head):
+    """ The critical idea based on the in-order traversal that we will exploit to solve this problem is: We know that
+        the leftmost element in the in-order traversal has to be the head of our given linked list. Similarly, the next
+        element in the in-order traversal will be the second element in the linked list and so on. This is made
+        possible because the initial list given to us is sorted in ascending order.
+        Iterate over the linked list to find out its length.
+        We will make use of two different pointer variables here to mark the beginning and the end of the list. Let's
+        call them 'left' and 'right' with their initial values being 0 and length - 1 respectively.
+        Remember, we have to simulate the in-order traversal here. We can find out the middle element by using
+        (left + right) / 2.
+        Recurse on the left half by using (left, mid - 1) as the starting and ending points.
+        The invariance that we maintain in this algorithm is that whenever we are done building the left half of the
+        BST, the head pointer in the linked list will point to the root node. So, we simply use the current value
+        pointed to by head as the root node and progress the head node by once i.e. head = head.next.
+        We recurse on the right hand side using (mid + 1, right) as the starting and ending points.
+    Time complexity: O(N), since we still have to process each of the nodes in the linked list once and form
+    corresponding BST nodes.
+    Space complexity: O(logN) since now the only extra space is used by the recursion stack and since we are building a
+    height balanced BST, the height is bounded by logN.
+    """
+
+    def in_order(left, right):
+        if left > right:
+            return None
+        nonlocal head
+        mid = (left + right) // 2
+        left = in_order(left, mid - 1)  # First step of simulated in-order traversal. Recursively form the left half.
+        root = TreeNode(head.val)  # Once left half is traversed, process the current node.
+        root.left = left
+        head = head.next  # Maintain the invariance mentioned in the algorithm.
+        root.right = in_order(mid + 1, right)  # Recurse on the right hand side and form BST out of them.
+        return root
+
+    if not head:
+        return None
+    size, temp = 0, head
+    while temp:
+        size += 1
+        temp = temp.next
+    return in_order(0, size - 1)
+
+
 
 
 
