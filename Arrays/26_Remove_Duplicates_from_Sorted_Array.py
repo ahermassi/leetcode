d492ace140db774b0ebcc84731c41e2c1080ba3c
Anouer Hermassi <ahermass@stevens.edu>
2019-11-25 21:38:25 -0500

    Remove duplicates from sorted array - comments

--- Arrays/26_Remove_Duplicates_from_Sorted_Array.py
+++ Arrays/26_Remove_Duplicates_from_Sorted_Array.py
@@ -5,17 +5,19 @@ memory. """
 
 
 def remove_duplicates(nums):
-    """ The goal is not to remove the elements, but to swap to the end. Since the array is already sorted,
-        we can keep two pointers i and write_index, where write_index is the slow-runner and i is the fast-runner.
+    """ The goal is not to remove the elements, but to swap to the end. Since the array is already sorted, we can keep
+        two pointers i and write_index, where write_index is the slow-runner and i is the fast-runner.
         As long as nums[i] == nums[write_index-1] , we increment i to skip the duplicates. When we encounter
         nums[i] != nums[write_index-1], the duplicate run has ended so we must copy its value to nums[write_index].
         write_index is then incremented and we repeat the same process again until i reaches the end of array.
+        The invariant of this algorithm is: all elements before write_index are unique. All elements between write_index
+        and i are duplicate of nums[write_index - 1].
     Time complexity: O(N) where N is the length of array nums
     Space complexity: O(1)
     """
     if not nums:
         return 0
-    write_index = 1  # This variable is is the index where we'll write the next non-duplicate element
+    write_index = 1  # This variable is the index where we'll write the next non-duplicate element
     for i in range(1, len(nums)):
         if nums[i] != nums[write_index - 1]:
             nums[write_index] = nums[i]
