7ba12e49d753a250cbeb55af800e901ddac85dbf
Anouer Hermassi <ahermass@stevens.edu>
2019-11-27 16:09:19 -0500

    Combination sum - v1 update

--- Backtracking/39_Combination_Sum.py
+++ Backtracking/39_Combination_Sum.py
@@ -9,6 +9,28 @@ import unittest2 as unittest
 
 
 def combination_sum_v1(candidates, target):
+    """ Explore all the possible paths that reduce the target to 0, passing an index parameter to DFS call to indicate
+        where to start adding candidates. At every step that is taken to complete a path, only elements explored from
+        that point onwards are considered. As a result, extending the path always involves adding a larger or equal
+        number to what was previously present. That is why it stays unique.
+    Time complexity: O(#candidates ^ target)
+    Space complexity: Space complexity: O(target) for call stack
+    """
+
+    def dfs(index, path, remaining):
+        if remaining == 0:
+            res.append(path)
+            return
+        for i in range(index, n):  # We include 'index' because we're allowed to choose the same number multiple times
+            if candidates[i] <= remaining:  # There is no use in exploring a combination that sums to beyond target
+                dfs(i, path + [candidates[i]], remaining - candidates[i])
+
+    n, res = len(candidates), []
+    dfs(0, [], target)
+    return res
+
+
+def combination_sum_v2(candidates, target):
     """ Good old DFS. Explore all the possible paths that reduce the target to 0. Sorting is not necessary here. The
         only help with sorting is that we can stop searching earlier by breaking the for loop when 'candidate' is
         larger than 'remaining' target.
@@ -39,30 +61,6 @@ def combination_sum_v1(candidates, target):
     res = []
     candidates.sort()
     dfs(target, [])
-    return res
-
-
-def combination_sum_v2(candidates, target):
-    """ Same as above, but passing an extra index parameter to DFS call to indicate where to start adding candidates.
-        The original candidates array is sorted. And at every step that is taken to complete a path, only elements
-        explored from that points onwards are considered. As a result extending the path always involves adding a
-        larger or equal number to what was previously present. That is why it stays unique.
-    Time complexity: O(#candidates ^ target)
-    Space complexity: Space complexity: O(target) for call stack
-    """
-
-    def dfs(remaining, index, path):
-        if remaining == 0:
-            res.append(path)
-            return
-        for i in range(index, len(candidates)):
-            if candidates[i] > remaining:
-                break
-            dfs(remaining - candidates[i], i, path + [candidates[i]])
-
-    res = []
-    candidates.sort()
-    dfs(target, 0, [])
     return res
 
 
