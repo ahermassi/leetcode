6cdcafa224d4cadcab1b247726e48ab27524ed70
Anouer Hermassi <ahermass@stevens.edu>
2019-11-29 20:29:56 -0500

    Find all anagrams in a string - v2

--- HashTable/438_Find_All_Anagrams_in_a_String.py
+++ HashTable/438_Find_All_Anagrams_in_a_String.py
@@ -7,7 +7,7 @@ from collections import Counter
 import unittest2 as unittest
 
 
-def find_anagrams(s, p):
+def find_anagrams_v1(s, p):
     """ Maintain a window of len(p) in s, and slide to right until finish.
     Time complexity: O(N), the comparison of the two hash maps is still O(1) because every hash map can hold at most
     26 characters.
@@ -27,12 +27,32 @@ def find_anagrams(s, p):
     return ans
 
 
+def find_anagrams_v2(s, p):
+    n, m = len(s), len(p)
+    need = Counter(p)
+    left, right, missing, res = 0, 0, len(p), []
+    while right < n:
+        if need[s[right]] > 0:
+            missing -= 1
+        need[s[right]] -= 1
+        if missing == 0:
+            res.append(left)
+        if right - left == m - 1:
+            need[s[left]] += 1
+            if need[s[left]] > 0:
+                missing += 1
+            left += 1
+        right += 1
+    return res
+
+
 class Test(unittest.TestCase):
     data = [('cbaebabacd', 'abc', [0, 6]), ('abab', 'ab', [0, 1, 2])]
 
     def test_find_anagrams(self):
         for test_string, test_pattern, result in self.data:
-            self.assertEqual(result, find_anagrams(test_string, test_pattern))
+            self.assertEqual(result, find_anagrams_v1(test_string, test_pattern))
+            self.assertEqual(result, find_anagrams_v2(test_string, test_pattern))
 
 
 if __name__ == '__main__':
