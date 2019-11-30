cf7443fee0cec15b08cdb607445d82c4c31ead4f
Anouer Hermassi <ahermass@stevens.edu>
2019-11-30 11:37:26 -0500

    Intersection of two arrays - v2 update

--- HashTable/349_Intersection_of_Two_Arrays.py
+++ HashTable/349_Intersection_of_Two_Arrays.py
@@ -18,19 +18,17 @@ def intersection_v1(nums1, nums2):
 
 
 def intersection_v2(nums1, nums2):
-    """ Use a hash table to record all nums that appeared in the first list, and then check if there are nums in the
-    second list have appeared in the hash table.
+    """ Same as previous solution, but instead of appending common elements to a set to avoid redundancy, append them
+        to the final result list instead and remove that element from the nums set.
     Time complexity: O(N + M) where N is the length of nums1 and M is the length of nums2
     Space complexity: O(N + M)
     """
-    count, result = defaultdict(int), []
-    for i in nums1:
-        if i not in count:  # Avoid duplicate numbers
-            count[i] = 1
-    for i in nums2:
-        if count[i] == 1 and i not in result:  # If number exists in hash table and not in the result list
-            result.append(i)
-    return result
+    res, nums1 = [], set(nums1)
+    for num in nums2:
+        if num in nums1:
+            res.append(num)
+            nums1.remove(num)
+    return res
 
 
 def intersection_v3(nums1, nums2):
