e7ca2c3319c6a1b1a1eb231b1894bbce9b3630f7
Anouer Hermassi <ahermass@stevens.edu>
2019-12-01 09:46:24 -0500

    First bad version - update

--- BinarySearch/278_First_Bad_Version.py
+++ BinarySearch/278_First_Bad_Version.py
@@ -7,8 +7,18 @@ def first_bad_version(n):
         When is_bad_version(mid) is False, we know that all versions preceding and including mid are all good. So we set
         left = mid + 1 to indicate that the new search space is the interval [mid + 1, right] (inclusive).
         When is_bad_version(mid) is True, mid may or may not be the first bad version, but we can tell for sure that
-        all versions after mid can be discarded. Therefore we set right = mid - 1 as the new search space of interval
-        [left, mid - 1] (inclusive).
+        all versions after mid can be discarded. Therefore we set right = mid as the new search space of interval
+        [left, mid] (inclusive).
+        Scenario #1: isBadVersion(mid) => false
+        1 2 3 4 5 6 7 8 9
+        G G G G G G B B B       G = Good, B = Bad
+        |       |       |
+        left    mid    right
+        Scenario #2: isBadVersion(mid) => true
+        1 2 3 4 5 6 7 8 9
+        G G G B B B B B B       G = Good, B = Bad
+        |       |       |
+        left    mid    right
         Note that binary search can be used because if we model the problem as an array [good, good, good, bad, bad]
         where 'good' means is_bad_version(arr[i]) == False and 'bad' means is_bad_version(arr[i]) == True, the array is
         sorted. (every version after a bad version is bad)
@@ -16,10 +26,10 @@ def first_bad_version(n):
     Space complexity: O(1)
     """
     left, right = 1, n
-    while left <= right:
+    while left < right:
         mid = (left + right) // 2
         if not is_bad_version(mid):
             left = mid + 1
         else:
-            right = mid - 1
+            right = mid
     return left
