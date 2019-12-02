70b21668650a153eae986094cc67b405934eb03d
Anouer Hermassi <ahermass@stevens.edu>
2019-12-02 09:51:44 -0500

    Kth smallest element in a sorted matrix - update

--- Heap/378_Kth_Smallest_Element_in_a_Sorted_Matrix.py
+++ Heap/378_Kth_Smallest_Element_in_a_Sorted_Matrix.py
@@ -2,34 +2,40 @@
 element in the matrix.
 Note that it is the kth smallest element in the sorted order, not the kth distinct element. """
 
-from heapq import heapify, heappop, heappush
+from heapq import heappop, heappush
 import unittest2 as unittest
 
 
 def kth_smallest(matrix, k):
     """ As each row (or column) of the given matrix can be seen as a sorted list, we essentially need to find the Kth
         smallest number in ‘N’ sorted lists.
-        Build a minHeap of elements from the first column (every row/column is a sorted array) (could be done with
+        Build a min heap of elements from the first column (every row/column is a sorted array) (could be done with
         first row as well, same logic)
         Do the following operations k times :
-        Every time when you poll out the root (top Element in Heap), you need to know the row number and column number
+        Every time when we poll out the root (top element in Heap), we need to know the row number and column number
         of that element. Replace that root with the next element from the same row (which is a sorted array).
+        Think of the first element of each column (or row) we initially push to the heap as a representative of each
+        column (or row). Each row (or column) keeps bringing the next greater element and the heap adjusts accordingly
+        to keep the smallest in the front. As we know that the smallest number of the matrix is at the top left corner,
+        so no matter what our choice was (pushing first element of each row or column), the very first element in the
+        heap will be always the absolute smallest.
     Time complexity: O(k logN), First, we inserted N elements from each of the ‘N’ rows, which will take O(N). Then we
     went through at most K elements in the matrix and removed/added one element in the heap in each step. As we can’t
     have more than N elements in the heap in any condition, therefore, the overall time complexity of the above
     algorithm will be O(N + k logN)
     Space complexity: O(N) for the heap
     """
-    heap = [(row[0], i, 0) for i, row in enumerate(matrix)]  # Put the 1st element of each column in the min heap
-    heapify(heap)
-    n, res = len(matrix[0]), 0
+    heap = []
+    for i, row in enumerate(matrix):  # Put the 1st element of each column in the min heap
+        heappush(heap, (row[0], i, 0))
+    n, number = len(matrix[0]), 0
     for _ in range(k):
-        res, row, col = heappop(heap)  # Take the smallest (top) element form the min heap, if the running count is
+        number, row, col = heappop(heap)  # Take the smallest (top) element form the min heap, if the running count is
         # equal to k return the number. If the row of the top element has more elements, add the next element to the
         # heap
         if col + 1 < n:
             heappush(heap, (matrix[row][col + 1], row, col + 1))
-    return res
+    return number
 
 
 class Test(unittest.TestCase):
