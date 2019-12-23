e2b8dc7728cfdcc8d683347ec8b508d83ea86cd2
Anouer Hermassi <ahermass@stevens.edu>
2019-12-23 09:58:50 -0500

    Reverse string - v2

--- Strings/344_Reverse_String.py
+++ Strings/344_Reverse_String.py
@@ -5,8 +5,8 @@ memory. """
 import unittest2 as unittest
 
 
-def reverse_string(s):
-    """
+def reverse_string_v1(s):
+    """ Two pointers are used to process two array elements at the same time.
     Time complexity: O(N)
     Space complexity: O(1)
     """
@@ -18,13 +18,31 @@ def reverse_string(s):
     return s
 
 
+def reverse_string_v2(s):
+    """ Implement recursive function helper() which receives two pointers, left and right, as arguments.
+        If left >= right, do nothing.
+        Otherwise, swap s[left] and s[right] and call helper(left + 1, right - 1).
+    Time complexity: O(N)
+    Space complexity: O(N) for recursion stack
+    """
+
+    def helper(left, right):
+        if left < right:
+            s[left], s[right] = s[right], s[left]
+            helper(left + 1, right - 1)
+
+    helper(0, len(s) - 1)
+    return s
+
+
 class Test(unittest.TestCase):
     data = [(['h', 'e', 'l', 'l', 'o'], ['o', 'l', 'l', 'e', 'h']),
             (['H', 'a', 'n', 'n', 'a', 'h'], ['h', 'a', 'n', 'n', 'a', 'H'])]
 
     def test_reverse_string(self):
         for test_string, result in self.data:
-            self.assertEqual(result, reverse_string(test_string))
+            # self.assertEqual(result, reverse_string_v1(test_string))
+            self.assertEqual(result, reverse_string_v2(test_string))
 
 
 if __name__ == '__main__':
