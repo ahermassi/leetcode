a8c3769c995dfc1b0f0ceb5d712adc2732284c67
Anouer Hermassi <ahermass@stevens.edu>
2019-12-26 15:11:05 -0500

    Climbing stairs - v2 update

--- DynamicProgramming/70_Climbing_Stairs.py
+++ DynamicProgramming/70_Climbing_Stairs.py
@@ -20,10 +20,13 @@ def climb_stairs_v2(n):
             if n == 1, then there is only one NEW way to climb the stair.
             if n == 2, then there are two NEW ways to climb the stairs.
         The key intuition to solve the problem is that given a number of stairs n, if we know the number of ways to get
-        to the points [n-1] and [n-2] respectively, denoted as n1 and n2 , then the total ways to get to the point [n]
-        is n1 + n2. Because from the [n-1] point, we can take one single step to reach [n]. And from the [n-2] point,
-        we could take two steps to get there.
-        Now given the above intuition, one can construct an array where each node stores the solution for each number
+        to the points (n-1) and (n-2) respectively, denoted as n1 and n2 , then the total ways to get to the point n
+        is n1 + n2. Because from the (n-1)th point, we can take one single step to reach n. And from the (n-2)th point,
+        we can take two steps to get there.
+        In other words: show me how many distinct ways you can climb to the (n-1)th and (n-2)th steps, because when
+        you reach those points you can climb 1 or 2 steps, respectively, to reach the top. So at the end it is the sum
+        of how many distinct ways you can climb to points (n-1) and (n-2).
+        Now given the above intuition, we can construct a hash map where each node stores the solution for each number
         n. Or if we look at it closer, it is clear that this is basically a fibonacci number, with the starting numbers
         as 1 and 2, instead of 1 and 1.
     Time complexity: O(n)
@@ -33,7 +36,7 @@ def climb_stairs_v2(n):
     def climb(n):
         if n in memo:
             return memo[n]
-        res = climb(n - 1) + climb(n - 2)
+        res = climb(n-1) + climb(n-2)
         memo[n] = res
         return res
 
