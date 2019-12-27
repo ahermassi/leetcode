53e5fb51cb29a432020bbc08b70f98601591a292
Anouer Hermassi <ahermass@stevens.edu>
2019-12-27 10:09:41 -0500

    Remove duplicates from sorted array - update

--- Arrays/26_Remove_Duplicates_from_Sorted_Array.py
+++ Arrays/26_Remove_Duplicates_from_Sorted_Array.py
@@ -7,21 +7,21 @@ memory. """
 def remove_duplicates(nums):
     """ The goal is not to remove the elements, but to swap to the end. Since the array is already sorted, we can keep
         two pointers i and write_index, where write_index is the slow-runner and i is the fast-runner.
-        As long as nums[i] == nums[write_index-1] , we increment i to skip the duplicates. When we encounter
-        nums[i] != nums[write_index-1], the duplicate run has ended so we must copy its value to nums[write_index].
+        As long as nums[i] == nums[write_index] , we increment i to skip the duplicates. When we encounter the first
+        nums[i] != nums[write_index], the duplicate run has ended so we must copy its value to nums[write_index+1].
         write_index is then incremented and we repeat the same process again until i reaches the end of array.
-        The invariant of this algorithm is: all elements before write_index are unique. All elements between write_index
-        and i are duplicate of nums[write_index - 1].
+        The invariant of this algorithm is:
+            All elements up to write_index (included) are unique. All elements between write_index and i are duplicate
+            of nums[write_index].
     Time complexity: O(N) where N is the length of array nums
     Space complexity: O(1)
     """
-    if not nums:
-        return 0
-    write_index = 1  # This variable is the index where we'll write the next non-duplicate element
-    for i in range(1, len(nums)):
-        if nums[i] != nums[write_index - 1]:
-            nums[write_index] = nums[i]
+    write_index = 0  # This variable is the index where we'll write the next non-duplicate element
+    n = len(nums)
+    for i in range(n):
+        if nums[i] != nums[write_index]:
             write_index += 1
-    return write_index
+            nums[write_index] = nums[i]
+    return write_index + 1
 
 
