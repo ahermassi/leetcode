256808401a4d8c50358eb34f06a61b1bfa5eca9b
Anouer Hermassi <ahermass@stevens.edu>
2020-01-03 10:32:51 -0500

    Unique paths - v1/2 update

--- DynamicProgramming/62_Unique_Paths.py
+++ DynamicProgramming/62_Unique_Paths.py
@@ -10,55 +10,48 @@ import unittest2 as unittest
 def unique_paths_v1(m, n):
     """ Top down + memoization
         The idea is to notice that:
-        - When we are in cells (n - 2, m - 1) or (n - 1, m - 2), there is exactly only one way to reach bottom right
-          corner, which is to either move down or right, respectively
-        - When we are at cell (n - 2, m - 2), there are exactly 2 ways to reach bottom right corner, which are to either
-          right - down or down - right
-        These are the base cases.
-        So if we know the number of ways to get to these points, denoted as right and down , then the total ways to get
-        to the bottom right corner is right + down
+            When we are at cells (n - 2, m - 1) or (n - 1, m - 2), there is exactly only one way to reach bottom right
+            corner, which is to either move down or right, respectively
+        This is the base case.
+        So if we know the number of ways to get to these points, then the total ways to get to the bottom right corner
+        is the sum.
     Time complexity: O(n * m)
     Space complexity: O(n * m)
     """
 
-    def count(i, j):
-        if i >= n or j >= m:
+    def helper(i, j):
+        if not 0 <= i < n or not 0 <= j < m:
             return 0
-        if (i == n - 2 and j == m - 1) or (i == n - 1 and j == m - 2):
+        if (i, j) in {(n - 1, m - 2), (n - 2, m - 1), (n - 1, m - 1)}:
             return 1
-        if i == n - 2 and j == m - 2:
-            return 2
-        if (i, j + 1) in memo:
-            right = memo[i, j + 1]
-        else:
-            right = count(i, j + 1)
-            memo[(i, j + 1)] = right
-        if (i + 1, j) in memo:
-            down = memo[i + 1, j]
-        else:
-            down = count(i + 1, j)
-            memo[(i + 1, j)] = down
-        return right + down
+        if (i, j) in memo:
+            return memo[(i, j)]
+        res = helper(i + 1, j) + helper(i, j + 1)
+        memo[(i, j)] = res
+        return res
 
     memo = {}
-    if n == m == 1:
-        return 1
-    return count(0, 0)
+    return helper(0, 0)
 
 
 def unique_paths_v2(m, n):
-    """ Bottom up + memoization.
-        dp[i][j] equals to how many ways I can reach the current cell (i, j) moving only right and/or down, expect the
-        first row and the first column, since there are only one way to get to those cells (straight right or straight
-        down, respectively).
+    """ Bottom-up dynamic programming.
+        Let dp[i][j] be the number of unique ways ways we can reach the current cell (i, j) moving only right and/or
+        down and starting from top-left corner cell (0, 0). Therefore, dp[i][j] is the sum of unique ways we can reach
+        the left cell (i, j-1) and top cell (i-1, j) (since starting from left and top, we can move right and down,
+        respectively. to arrive at (i,j)).
+            dp[i][j] = dp[i][j-1] + dp[i-1][j]
+        The first column and first row must be 1's since there is only one path to get there (i.e. to get anywhere in
+        the first row we must have just done all right moves, and similarly for the first column we must have just done
+        all down moves).
     Time complexity: O(n * m)
     Space complexity: O(n * m)
     """
     dp = [[1 for _ in range(m)] for _ in range(n)]
     for i in range(1, n):
         for j in range(1, m):
-            dp[i][j] = dp[i][j - 1] + dp[i - 1][j]  # This is our 'right' and 'down' from previous solution
-    return dp[-1][-1]  # This is the bottom right corner where we want to stop
+            dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+    return dp[n-1][m-1]  # This is the bottom-right corner where we want to stop
 
 
 def unique_paths_v3(m, n):
