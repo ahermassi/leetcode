3ff317087a5fb160e1f18e32b7aaeea1ff0a4e2d
Anouer Hermassi <ahermass@stevens.edu>
2020-01-03 14:53:01 -0500

    Valid sudoku - update

--- HashTable/36_Valid_Sudoku.py
+++ HashTable/36_Valid_Sudoku.py
@@ -9,70 +9,72 @@ import unittest2 as unittest
 
 def is_valid_sudoku_v1(board):
     """ Iterate three times over the board to ensure that :
-            There is no rows with duplicates.
-            There is no columns with duplicates.
-            There is no sub-boxes with duplicates.
+            1- There is no rows with duplicates.
+            2- There is no columns with duplicates.
+            3- There is no sub-boxes with duplicates.
     Time complexity: O(1) as the number of iterations is known in advance (9 X 9 board)
     Space complexity: O(1)
     """
 
-    def valid_rows(board):
-        for row in board:
-            if not is_valid(row):
+    def is_valid(lst):
+        for l in lst:
+            if not is_valid_unit(l):
                 return False
         return True
 
-    def valid_cols(board):
-        for col in zip(*board):
-            if not is_valid(col):
+    def is_valid_unit(unit):
+        vals = set()
+        for cell in unit:
+            if cell != '.' and cell in vals:
                 return False
+            vals.add(cell)
         return True
 
-    def valid_squares(board):
+    def is_valid_squares():
+        # Row 0: i = 0; j takes values 0,3,6 to create: box0(x=0..2, y=0..2), box1(x=0..2, y=3..5), box2(x=0..2, y=6..8)
+        # -> x = 0..2 = i..i+3, y = j..j+3
+        # Row 1: i = 3; j takes values 0,3,6 to create: box0(x=3..5, y=0..2), box1(x=3..5, y=3..5), box2(x=3..5, y=6..8)
+        # -> x = 3..5 = i..i+3, y = j..j+3
+        # Row 2: i = 6; j takes values 0,3,6 to create: box0(x=6..8, y=0..2), box1(x=6..8, y=3..5), box2(x=6..8, y=6..8)
+        # -> x = 6..8 = i..i+3, y = j..j+3
         for i in (0, 3, 6):
             for j in (0, 3, 6):
                 square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
-                if not is_valid(square):
+                if not is_valid_unit(square):
                     return False
         return True
 
-    def is_valid(row):
-        d = {}
-        for cell in row:
-            if cell != '.' and cell in d:
-                return False
-            d[cell] = 1
-        return True
-
-    return valid_rows(board) and valid_cols(board) and valid_squares(board)
+    rows = [row for row in board]
+    cols = [col for col in zip(*board)]
+    return is_valid(rows) and is_valid(cols) and is_valid_squares()
 
 
 def is_valid_sudoku_v2(board):
     """ Actually, all this could be done in just one iteration.
         We could use box_index = (row / 3) * 3 + col / 3 where / is an integer division, row is a row number, and col
         is a column number.
-        Move along the board. Check for each cell value if it was seen already in the current row / column / box
+        Move along the board. Check for each cell value if it was seen already in the current row / column / box.
     Time complexity: O(1)
     Space complexity: O(1)
     """
-    rows = [{} for _ in range(9)]
-    cols = [{} for _ in range(9)]
-    boxes = [{} for _ in range(9)]
-
+    rows = [set() for _ in range(9)]
+    cols = [set() for _ in range(9)]
+    boxes = [set() for _ in range(9)]
     for i in range(9):
         for j in range(9):
             cell = board[i][j]
-            if cell != '.':
-                if cell in rows[i]:
-                    return False
-                rows[i][cell] = 1
-                if cell in cols[j]:
-                    return False
-                cols[j][cell] = 1
-                box_index = (i // 3) * 3 + j // 3
-                if cell in boxes[box_index]:
-                    return False
-                boxes[box_index][cell] = 1
+            if cell == '.':
+                continue
+            if cell in rows[i]:
+                return False
+            rows[i].add(cell)
+            if cell in cols[j]:
+                return False
+            cols[j].add(cell)
+            box_index = (i // 3) * 3 + j // 3
+            if cell in boxes[box_index]:
+                return False
+            boxes[box_index].add(cell)
     return True
 
 
@@ -100,7 +102,7 @@ class Test(unittest.TestCase):
              [".", ".", ".", ".", "8", ".", ".", "7", "9"]
          ], False)]
 
-    def test_find_duplicate(self):
+    def test_is_valid_sudoku(self):
         for test_board, result in self.data:
             self.assertEqual(result, is_valid_sudoku_v1(test_board))
             self.assertEqual(result, is_valid_sudoku_v2(test_board))
