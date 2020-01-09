6975565898029ad116049f195646a89b34b7938f
Anouer Hermassi <ahermass@stevens.edu>
2020-01-09 10:29:54 -0500

    Maximum product subarray - v1 update

--- DynamicProgramming/152_Maximum_Product_Subarray.py
+++ DynamicProgramming/152_Maximum_Product_Subarray.py
@@ -10,17 +10,19 @@ def max_product_v1(nums):
         current element. At each new element, we could either add the new element to the existing product, or start
         fresh the product from current index (wipe out previous results), hence the 2 max() lines.
         The max result from A[0] to A[i] can only come from:
-            Decision 1. discard previous result, restart at A[i]
-            Decision 2. take A[i], MAX[i] = MAX[i-1] * A[i]
-            Decision 3. this is the most tricky part: A[i] can be negative, then MAX[i-1] * A[i] is negative (suppose
-            MAX[i-1] is positive).
+            Decision 1: discard previous result, restart at A[i]
+            Decision 2: take A[i], MAX[i] = MAX[i-1] * A[i]
+            Decision 3: this is the trickiest part: A[i] can be negative, then MAX[i-1] * A[i] can be smaller than
+            MAX[i-1]
     Time complexity: O(N)
     Space complexity: O(1)
     """
-    max_so_far = min_so_far = global_max = nums[0]
+    max_so_far = min_so_far = global_max = nums[0]  # max_so_far / min_so_far store the max/min product of sub-array
+    # that ends with the current element nums[i]
     for i in range(1, len(nums)):
         candidates = (nums[i], max_so_far * nums[i], min_so_far * nums[i])  # These values are the candidates of
-        # maximum product and minimum product up to ith index
+        # maximum product and minimum product up to ith index: start a new product with nums[i], multiply the previous
+        # num product with nums[i], or multiply the previous max product with nums[i]
         max_so_far = max(candidates)
         min_so_far = min(candidates)
         global_max = max(global_max, max_so_far)
