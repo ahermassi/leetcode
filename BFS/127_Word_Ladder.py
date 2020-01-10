9afe16536e94d9e5a3f2bc4a50203e29faf73e5e
Anouer Hermassi <ahermass@stevens.edu>
2020-01-10 10:21:34 -0500

    Word ladder - v1 update

--- BFS/127_Word_Ladder.py
+++ BFS/127_Word_Ladder.py
@@ -10,35 +10,51 @@ import unittest2 as unittest
 
 
 def ladder_length_v1(beginWord, endWord, wordList):
-    """ BFS. One of the most important steps here is to figure out how to find adjacent nodes i.e. words which differ
-        by one letter. To efficiently find the neighboring nodes for any given word we do some pre-processing on the
-        words of the given wordList. The pre-processing involves replacing the letter of a word by a non-alphabet
-        say, *. This pre-processing helps to form generic states to represent a single letter change.
+    """ We are given a beginWord and an endWord. Let these two represent start node and end node of a graph. We have to
+        reach from the start node to the end node using some intermediate nodes/words. The intermediate nodes are
+        determined by the wordList given to us. The only condition for every step we take on this ladder of words is
+        the current word should change by just one letter.
+        We will essentially be working with an undirected and unweighted graph with words as nodes and edges between
+        words which differ by just one letter. The problem boils down to finding the shortest path from a start node to
+        a destination node, if there exists one. Hence it can be solved using Breadth First Search approach.
+        One of the most important steps here is to figure out how to find adjacent nodes i.e. words which differ
+        by one letter. To efficiently find the neighboring nodes for any given word, we do some pre-processing on the
+        words of the given wordList. The pre-processing involves replacing the letter of a word by a non-alphabet, say,
+        *. This pre-processing helps to form generic states to represent a single letter change.
         For e.g. Dog ----> D*g <---- Dig
         Both Dog and Dig map to the same intermediate or generic state D*g.
-        Start from beginWord and search the endWord using BFS.
-    Time complexity: O(N * M**2) where N is the number of words and M is the length of each word (same length)
-    Space complexity: O(N * M), O(N * M) for patterns dictionary, O(N) for queue and visited set, so overall O(N * M)
+        The pre-processing step helps us find out the generic one letter away nodes for any word of the word list and
+        hence making it easier and quicker to get the adjacent nodes. Otherwise, for every word we will have to iterate
+        over the entire word list and find words that differ by one letter. That would take a lot of time. This
+        pre-processing step essentially builds the adjacency list first before beginning the breadth first search
+        algorithm.
+        Start from beginWord and search the endWord using BFS. To prevent cycles, use a visited set. Eventually, if we
+        reach the desired word, its level would represent the shortest transformation sequence length.
+    Time complexity: O(N * M), where M is the length of words (all words have same length) and N is the total number of
+    words in the input word list. Finding out all the transformations takes M iterations for each of the NN words.
+    Also, breadth first search in the worst case might go to each of the N words.
+    Space complexity: O(N * M), to store all M transformations for each of the N words, in the 'transformations'
+    dictionary, 'visited' set is of size N, queue for BFS in worst case would need space for all N words.
     """
     if endWord not in wordList:
         return 0
-    queue = deque([(beginWord, 1)])
-    patterns = defaultdict(list)  # Dictionary to hold combination of words that can be formed, from any given word,
-    # by changing one letter at a time.
+    transformations = defaultdict(list)  # Dictionary to hold patterns of words that can be formed, from any given
+    # word, by changing one letter at a time.
     for word in wordList:
         for i in range(len(word)):
-            patterns[word[:i] + '*' + word[i + 1:]].append(word)
+            transformations[word[:i] + '*' + word[i + 1:]].append(word)
+    queue = deque([(beginWord, 1)])
     visited = set()  # Visited to make sure we don't repeat processing same word.
     while queue:
         word, depth = queue.popleft()
         for i in range(len(word)):
-            pattern = word[:i] + '*' + word[i + 1:]
-            for w in patterns[pattern]:  # The words which share the same intermediate state/pattern
+            t = word[:i] + '*' + word[i + 1:]
+            for w in transformations[t]:  # The words which share the same intermediate state/pattern
                 if w == endWord:
                     return depth + 1
                 if w not in visited:
-                    queue.append((w, depth + 1))
                     visited.add(w)
+                    queue.append((w, depth + 1))
     return 0
 
 
