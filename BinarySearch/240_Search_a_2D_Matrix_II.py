8770bfb5f128af4719cd47c3c5735d3f809ca574
Anouer Hermassi <ahermass@stevens.edu>
2020-01-11 14:07:43 -0500

    Search a 2D matrix II - v1 update

--- BinarySearch/240_Search_a_2D_Matrix_II.py
+++ BinarySearch/240_Search_a_2D_Matrix_II.py
@@ -21,26 +21,27 @@ def search_matrix_v1(matrix, target):
             immediately return true). The existing matrix can be partitioned into four sub matrices around this index;
             the top-left and bottom-right sub matrices cannot contain target (via the argument outlined in Base Case
             section), so we can prune them from the search space. Additionally, the bottom-left and top-right
-            submatrice are sorted two-dimensional matrices, so we can recursively apply this algorithm to them.
-    Time complexity: O(N log N)
-    Space complexity: O(log N), use of recursion means that we will use memory proportional to the height of its
+            sub matrices are sorted two-dimensional matrices, so we can recursively apply this algorithm on them.
+    Time complexity: O(N logN)
+    Space complexity: O(logN), use of recursion means that we will use memory proportional to the height of its
     recursion tree. Because this approach discards half of matrix on each level of recursion (and makes two recursive
     calls), the height of the tree is bounded by logN.
     """
 
-    def search_submatrix(left, right, up, down):
-        if left > right or up > down:
+    def search_submatrix(left, right, top, bottom):
+        if left > right or top > bottom:
             return False
-        if target < matrix[up][left] or target > matrix[down][right]:  # Top-left corner element is always the
+        if target < matrix[top][left] or target > matrix[bottom][right]:  # Top-left corner element is always the
             # smallest of the matrix, and the bottom-right element is always the biggest
             return False
         mid = (left + right) // 2
-        row = up
-        while row <= down and matrix[row][mid] <= target:
+        row = top
+        # Locate a row such that matrix[row-1][mid] < target < matrix[row][mid]
+        while row <= bottom and matrix[row][mid] <= target:
             if matrix[row][mid] == target:
                 return True
             row += 1
-        return search_submatrix(left, mid - 1, row, down) or search_submatrix(mid + 1, right, up, row - 1)
+        return search_submatrix(left, mid - 1, row, bottom) or search_submatrix(mid + 1, right, top, row - 1)
 
     if not matrix:
         return False
