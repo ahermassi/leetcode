ae71aa1065f1d55ed563b123488d81f3cc643cca
Anouer Hermassi <ahermass@stevens.edu>
2020-01-11 10:13:27 -0500

    Missing ranges - v2 update

--- Arrays/163_Missing_Ranges.py
+++ Arrays/163_Missing_Ranges.py
@@ -5,40 +5,36 @@ import unittest2 as unittest
 
 
 def find_missing_ranges_v1(nums, lower, upper):
-    """ Just simply Add lower-1 and upper+1 into the list. The missing range should be num[i]+1 ~> num[i+1]-1
+    """ Just simply add lower-1 and upper+1 to the list. The missing range should be num[i-1]+1 ~> num[i]-1
     Time complexity: O(N)
     Space complexity: O(N) for the new nums array
     """
-    res = []
     nums = [lower - 1] + nums + [upper + 1]
-    for i in range(len(nums) - 1):
-        gap = nums[i + 1] - nums[i]
+    n, res = len(nums), []
+    for i in range(1, n):
+        gap = nums[i] - nums[i - 1]
         if gap == 2:
-            res.append(str(nums[i] + 1))
+            res.append(str(nums[i - 1] + 1))
         elif gap > 2:
-            res.append(str(nums[i] + 1) + '->' + str(nums[i + 1] - 1))
+            res.append(str(nums[i - 1] + 1) + '->' + str(nums[i] - 1))
     return res
 
 
 def find_missing_ranges_v2(nums, lower, upper):
-    """ Same as above, but without using any extra space. The edge cases are handled manually.
+    """ Same as above, but using constant space.
     Time complexity: O(N)
     Space complexity: O(1)
     """
-    if not nums:
-        return [str(lower) + '->' + str(upper) if lower != upper else str(lower)]
-    res = []
-    if nums[0] > lower:  # First edge case of left boundary
-        res.append(str(lower) + '->' + str(nums[0] - 1) if lower != nums[0] - 1 else str(lower))
-    n, i = len(nums), 0
-    for i in range(n - 1):
-        gap = nums[i + 1] - nums[i]
+    n, res = len(nums), []
+    pre = lower - 1
+    for i in range(n + 1):
+        curr = upper + 1 if i == n else nums[i]  # If we go out of bound, assign upper+1 to curr
+        gap = curr - pre
         if gap == 2:
-            res.append(str(nums[i] + 1))
+            res.append(str(pre + 1))
         elif gap > 2:
-            res.append(str(nums[i] + 1) + '->' + str(nums[i + 1] - 1))
-    if nums[-1] < upper:  # Second edge case of right boundary
-        res.append(str(nums[-1] + 1) + '->' + str(upper) if nums[-1] + 1 != upper else str(upper))
+            res.append(str(pre + 1) + '->' + str(curr - 1))
+        pre = curr
     return res
 
 
