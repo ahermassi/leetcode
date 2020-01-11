a82fe9d17e0b930e95802d53338858f46f89120d
Anouer Hermassi <ahermass@stevens.edu>
2020-01-11 14:22:06 -0500

    Search a 2D matrix II - v update

--- BinarySearch/240_Search_a_2D_Matrix_II.py
+++ BinarySearch/240_Search_a_2D_Matrix_II.py
@@ -48,13 +48,17 @@ def search_matrix_v1(matrix, target):
     return search_submatrix(0, len(matrix[0]) - 1, 0, len(matrix) - 1)
 
 
+# Watch: https://youtu.be/FOa55B9Ikfg?t=945
+
 def search_matrix_v2(matrix, target):
-    """ Because the rows and columns of the matrix are sorted (from left-to-right and top-to-bottom, respectively), we
-        can prune O(m) or O(n) elements when looking at any particular value.
-        First, we initialize a (row,col) pointer to the bottom-left of the matrix. Then, until we find target and
-        return True (or the pointer points to a (row,col) that lies outside of the dimensions of the matrix), we do the
-        following: if the currently-pointed-to value is larger than target we can move one row "up". Otherwise, if the
-        currently-pointed-to value is smaller than target, we can move one column "right". It is not too tricky to see
+    """ If we stand on the bottom-left (or top-right) corner of the matrix and look diagonally, it's kind of like a
+        BST. We can go through this matrix to find the target like how we traverse the BST.
+        Because the rows and columns of the matrix are sorted (from left-to-right and top-to-bottom, respectively), we
+        can prune O(M) or O(N) elements when looking at any particular value.
+        First, we initialize a (row, col) pointer to the bottom-left of the matrix. Then, until we find target and
+        return True (or the pointer points to a (row, col) that lies outside of the dimensions of the matrix), we do the
+        following: if the currently-pointed-to value is larger than target, we can move one row up. Otherwise, if the
+        currently-pointed-to value is smaller than target, we can move one column right. It is not too tricky to see
         why doing this will never prune the correct answer; because the rows are sorted from left-to-right, we know
         that every value to the right of the current value is larger. Therefore, if the current value is already larger
         than target, we know that every value to its right will also be too large. A very similar argument can be made
