3d298916b17dee04b3a483e06e00165856e76ac8
Anouer Hermassi <ahermass@stevens.edu>
2020-01-12 10:29:33 -0500

    Longest increasing subsequence - v1 update

--- DynamicProgramming/300_Longest_Increasing_Subsequence.py
+++ DynamicProgramming/300_Longest_Increasing_Subsequence.py
@@ -3,26 +3,45 @@
 import unittest as unittest
 
 
-def length_of_LIS_v1(nums):
-    """ We use dp array to store our sub problems, default answer is 1. A single item is neither increasing or
-        decreasing. Each index i holds the answer to "what is the longest increasing sub sequence ending at index i of
-        the original nums array ?". Test every possible end index of a longest increasing sub sequence
-    Time complexity: O(N ** 2)
+# Watch: https://www.youtube.com/watch?v=fV-TF4OvZpk
+
+def length_of_lis_v1(nums):
+    """ Let dp[i] be the length of the longest increasing sub-sequence ending at and including index i of the original
+        nums array.
+        Default answer is 1. A single item is neither increasing or decreasing.
+        In order to find out dp[i], we need to try to append the current element nums[i] in every possible increasing
+        sub-sequence upto the (i−1)th index(including the (i−1)th index), such that the new sequence formed by adding
+        the current element is also an increasing sub-sequence. Therefore:
+            dp[i] = max(dp[j] + 1 for 0 <= j < i)
+        At the end, the maximum out of all the dp[i]'s determines the final result:
+            LIS_length = max(dp[i] for 0 <= i < n)
+        Example: nums = [-1, 3, 4, 5, 2, 8]
+        At index 0 we always know that we can have a sub-sequence of length 1. In fact, at all positions the LIS can be
+        at least length 1.
+        We then look at index 1. We need to ask ourselves if the item at index 1 can lengthen the LIS found at index 0.
+        We check if 3 is greater than or equal to -1... it is. Great. index 1 can be tacked on, but should we?
+        dp[1] = 1, dp[0] = 1. It makes sense because if we tack 3 onto the LIS we found for the sub-problem of just
+        [-1] then at index 1 we will also have a LIS. So what we basically do is build a table and ask ourselves these
+        questions all along the way.
+        Each cell represents the answer to the sub-problem asked against the sub-sequence from index 0 to index i
+        (including the element at index i).
+    Time complexity: O(N^2)
     Space complexity: O(N)
     """
     if not nums:
         return 0
-    dp = [1] * len(nums)
-    max_len = 1
-    for i in range(1, len(nums)):
-        # We aim to see if we can append the item at nums[i] to extend the Longest Increasing Subsequence achieved
+    n = len(nums)
+    dp, max_len = [1] * n, 1
+    for i in range(1, n):
+        # We aim to see if we can append the item at nums[i] to extend the Longest Increasing Sub-sequence achieved
         # from index 0...j. We want to solve for dp[i] if the value at i is greater than the value at j
         for j in range(i):
             if nums[i] > nums[j]:
                 dp[i] = max(dp[i], dp[j] + 1)  # The value of dp[j] is the length of the LIS from 0...j, we conceptually
-                # "append" this item to that LIS by adding 1 to that sub problem answer, yielding a otentially new
+                # 'append' this item to that LIS by adding 1 to that sub-problem answer, yielding a potentially new
                 # answer for LIS[0..i]
-        max_len = max(max_len, dp[i])
+        max_len = max(max_len, dp[i])  # We now have an answer for LIS[0...i]. Compete it against the best LIS length
+        # found so far.
     return max_len
 
 
