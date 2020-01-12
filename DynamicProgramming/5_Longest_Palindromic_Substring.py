98d0d46579bdf13d70ea7d56acb55cd519dc21a8
Anouer Hermassi <ahermass@stevens.edu>
2020-01-12 12:11:36 -0500

    Longest palindromic substring - v2 update

--- DynamicProgramming/5_Longest_Palindromic_Substring.py
+++ DynamicProgramming/5_Longest_Palindromic_Substring.py
@@ -26,26 +26,26 @@ def longest_palindrome_v1(s):
 
 def longest_palindrome_v2(s):
     """ Bottom-up Dynamic Programming.
-        We observe how we can avoid unnecessary re-computation while validating palindromes. Consider the case "ababa".
-        If we already knew that "bab" is a palindrome, it is obvious that "ababa" must be a palindrome since the two
-        left and right end letters are the same.
+        We observe that we can avoid unnecessary re-computation while validating palindromes.
+        Consider the case 'ababa'. If we already knew that 'bab' is a palindrome, it is obvious that 'ababa' must be a
+        palindrome since the two left and right end letters are the same.
         We define dp[i][j] as following:
-        dp[i][j] is True is substring s[i:j + 1] is palindrome
+            dp[i][j] = True if substring s[i:j + 1] is palindrome
         Therefore:
-        dp[i][j] = (s[i] == s[j]) AND (dp[i+1][j-1])
-        This yields a straight forward DP solution, which we first initialize the one letter palindromes, and work our
-        way up finding all two letters palindromes, and so on...
+            dp[i][j] = (s[i] == s[j]) AND (dp[i+1][j-1])
+        This yields a straightforward DP solution, in which we first initialize the 1-letter palindromes, and work our
+        way up finding all 2-letter palindromes, and so on.
         Why are we counting down for i, but counting up for j? Each sub-problem dp[i][j] depends on dp[i+1][j-1].
-    Time complexity: O(N ** 2)
-    Space complexity: O(N ** 2) to store dp array
+    Time complexity: O(N^2)
+    Space complexity: O(N^2) to store dp array
     """
-    res = ''
-    dp = [[False] * len(s) for _ in range(len(s))]
-    for i in range(len(s)):
+    n, res = len(s), ''
+    dp = [[False] * n for _ in range(n)]
+    for i in range(n):
         dp[i][i] = True
         res = s[i]
-    for i in reversed(range(len(s))):
-        for j in range(i + 1, len(s)):
+    for i in reversed(range(n)):
+        for j in range(i + 1, n):
             if s[i] == s[j] and (j - i < 2 or dp[i + 1][j - 1]):  # dp[i+1][j-1] represents the middle of the current
                 # considered substring
                 dp[i][j] = True  # If the middle is a palindrome and the endpoints equal each other, it follows that
