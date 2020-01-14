74625f36cbd433ffd7f0b549fed189c6554e1c0f
Anouer Hermassi <ahermass@stevens.edu>
2020-01-14 15:22:01 -0500

    Find minimum in rotated sorted array - v1 update

--- BinarySearch/153_Find_Minimum_in_Rotated_Sorted_Array.py
+++ BinarySearch/153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -12,15 +12,15 @@ def find_min_v1(nums):
     """ There is a point in the array at which we would notice a change. This is the point which would help us in this
         question. We call this the Inflection Point.
         In this modified version of binary search algorithm, we are looking for this point. We notice that:
-            All the elements to the left of inflection point > first element of the array.
-            All the elements to the right of inflection point < first element of the array.
+            All the elements to the left of inflection point >= first element of the rotated array
+            All the elements to the right of inflection point < first element of the rotated array
         Find the mid element of the array.
-        If mid element > first element of array this means that we need to look for the inflection point on the right
+        If mid element > first element of array, this means that we need to look for the inflection point on the right
         of mid.
-        If mid element < first element of array this that we need to look for the inflection point on the left of mid.
+        If mid element < first element of array, this that we need to look for the inflection point on the left of mid.
         We stop our search when we find the inflection point, when either of the two conditions is satisfied:
-            nums[mid] > nums[mid + 1] Hence, mid+1 is the smallest.
-            nums[mid - 1] > nums[mid] Hence, mid is the smallest.
+            nums[mid] > nums[mid + 1] --> nums[mid + 1] is the smallest
+            nums[mid - 1] > nums[mid] --> nums[mid] is the smallest
     Time complexity: O(logN)
     Space complexity: O(1)
     """
@@ -31,10 +31,10 @@ def find_min_v1(nums):
     left, right = 0, len(nums) - 1
     while left <= right:
         mid = (left + right) // 2
-        if nums[mid] > nums[mid + 1]:  # If the mid element is greater than its next element, then mid+1 element is
+        if nums[mid] > nums[mid + 1]:  # If the mid element is greater than its next element, then nums[mid + 1] is
             # the smallest. This point would be the point of change from higher to lower values.
             return nums[mid + 1]
-        if nums[mid] < nums[mid - 1]:  # If the mid element is less than its previous element, then mid element is
+        if nums[mid] < nums[mid - 1]:  # If the mid element is less than its previous element, then nums[mid] is
             # the smallest
             return nums[mid]
         if nums[0] < nums[mid]:  # If the mid element's value is greater than the 0th element, this means the smallest
