0f3b8338bf13f7ab1bf8e676ed830301746f4825
Anouer Hermassi <ahermass@stevens.edu>
2020-01-14 11:39:07 -0500

    Number of connected components in an undirected graph - todo

--- DFS/323_Number_of_Connected Components_in_an_Undirected_Graph.py
+++ DFS/323_Number_of_Connected Components_in_an_Undirected_Graph.py
@@ -60,6 +60,8 @@ def count_components_v2(n, edges):
     return res
 
 
+# TODO: Union-Find
+
 class Test(unittest.TestCase):
     data = [(5, [[0, 1], [1, 2], [3, 4]], 2), (5, [[0, 1], [1, 2], [2, 3], [3, 4]], 1)]
 
