c07c5dced4da10a130bd6fca1916523723263690
Anouer Hermassi <ahermass@stevens.edu>
2020-01-16 16:38:07 -0500

    Finding the minimum number of intervals to remove to avoid overlap - v2

--- Arrays/435_Non-overlapping_Intervals.py
+++ Arrays/435_Non-overlapping_Intervals.py
@@ -32,12 +32,40 @@ def erase_overlap_intervals_v1(intervals):
     return removed
 
 
+def erase_overlap_intervals_v2(intervals):
+    """ Dynamic programming. TLE.
+        Let dp[i] be the maximum number of valid intervals that can be included in the final list if only the intervals
+        up to and including the ith interval are considered. To find dp[i+1], we can't consider the value of dp[i-1]
+        only, because it could be possible that the (i-1)th or any previous interval is overlapping with the ith
+        interval. Thus, we need to consider the maximum of all dp[j]'s such that j ≤ i and jth and ith intervals don't
+        Therefore:
+            dp[i] = max(dp[j] for 0 <= j < i such that jth and ith intervals don't overlap) + 1
+        In the end, to obtain the maximum number of intervals that can be included in the final list, let's call it
+        'res', we need to find the maximum value in the dp array. The final result will be the total number of input
+        intervals given minus the result just obtained: len(intervals) - result
+    Time complexity: O(N^2)
+    Space complexity: O(N)
+    """
+    if not intervals:
+        return 0
+    n = len(intervals)
+    intervals.sort()
+    dp = [0] * n
+    dp[0] = 1
+    for i in range(1, n):
+        for j in range(i):
+            if intervals[i][0] >= intervals[j][1]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return n - max(dp)
+
+
 class Test(unittest.TestCase):
     data = [([[1, 2], [2, 3], [3, 4], [1, 3]], 1), ([[1, 2], [1, 2], [1, 2]], 2), ([[1, 2], [2, 3]], 0)]
 
     def test_erase_overlap_intervals(self):
         for test_intervals, result in self.data:
             self.assertEqual(result, erase_overlap_intervals_v1(test_intervals))
+            self.assertEqual(result, erase_overlap_intervals_v2(test_intervals))
 
 
 if __name__ == '__main__':
