6f7acac1436858f807fc766060ddab8ef2e1a718
Anouer Hermassi <ahermass@stevens.edu>
2020-01-15 20:13:14 -0500

    Encoding a decoding a list of strings - v2

--- Strings/271_Encode_and_Decode_Strings.py
+++ Strings/271_Encode_and_Decode_Strings.py
@@ -27,3 +27,51 @@ class CodecV1:
             return None
         return s.split(chr(259))
 
+
+class CodecV2:
+    """ This approach doesn't depend on the set of input characters, and hence is more versatile and effective than
+        Approach 1.
+        Data stream is divided into chunks. Each chunk is preceded by its size.
+    """
+
+    def encode(self, strs: [str]) -> str:
+        """ Encodes a list of strings to a single string.
+            Iterate over the array of strings
+            For each string, compute its length
+            Append to encoded string: information about chunk size and chunk itself.
+        Time complexity: O(N)
+        Space complexity: O(1)
+        """
+        res = []
+        for s in strs:
+            res.extend([str(len(s)), ':', s])
+        return ''.join(res)
+
+    def decode_v1(self, s: str) -> [str]:
+        """ Decodes a single string to a list of strings.
+        Time complexity: O(N)
+        Space complexity: O(1)
+        """
+        i, n, res = 0, len(s), []
+        while i < n:
+            j = i
+            while s[j] != ':':
+                j += 1
+            size = int(s[i:j])
+            res.append(s[j + 1:j + 1 + size])
+            i = j + size + 1
+        return res
+
+    def decode_v2(self, s: str) -> [str]:
+        """ Decodes a single string to a list of strings.
+        Time complexity: O(N)
+        Space complexity: O(1)
+        """
+        i, n, res = 0, len(s), []
+        while i < n:
+            index = s.find(':', i)
+            size = int(s[i:index])
+            res.append(s[index + 1:index + 1 + size])
+            i = index + 1 + size
+        return res
+
