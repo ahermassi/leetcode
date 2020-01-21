3599f28f8d08af22b797b956bb73e0b230ba9920
Anouer Hermassi <ahermass@stevens.edu>
2020-01-21 11:54:40 -0500

    Two sum less than k - v2

--- Arrays/1099_Two_Sum_Less_Than_K.py
+++ Arrays/1099_Two_Sum_Less_Than_K.py
@@ -26,12 +26,39 @@ def two_sum_less_than_k_v1(A, K):
     return res
 
 
+def two_sum_less_than_k_v2(A, K):
+    """ Since 1 <= A[i] <= 1000, we can apply the counting sort, which takes linear time. Then, we use two indexes to
+        search for a pair, like in the solution above.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+    bucket = [None] * 1001
+    res = -1
+    for num in A:
+        bucket[num] = num
+    left, right = 0, len(bucket) - 1
+    while left < right:
+        if not bucket[left]:
+            left += 1
+        elif not bucket[right]:
+            right -= 1
+        else:
+            s = bucket[left] + bucket[right]
+            if s < K:
+                res = max(res, s)
+                left += 1
+            else:
+                right -= 1
+    return res
+
+
 class Test(unittest.TestCase):
     data = [([34, 23, 1, 24, 75, 33, 54, 8], 60, 58), ([10, 20, 30], 15, -1)]
 
     def test_two_sum_less_than_k(self):
         for test_a, test_k, result in self.data:
             self.assertEqual(result, two_sum_less_than_k_v1(test_a, test_k))
+            self.assertEqual(result, two_sum_less_than_k_v2(test_a, test_k))
 
 
 if __name__ == '__main__':
