24a1cc6b609ab83286ff7bc4e13d78a079aaae8a
Anouer Hermassi <ahermass@stevens.edu>
2020-01-22 11:02:59 -0500

    Designing a system for products suggestions - v2

--- Strings/1268_Search_Suggestions_System.py
+++ Strings/1268_Search_Suggestions_System.py
@@ -9,6 +9,35 @@ import unittest2 as unittest
 
 
 def suggested_products_v1(products, search_word):
+    """ For any two words w1 and w2 in 'products', if w1 is prefix of w2, w1 and w2 must be neighbours in the sorted
+        'products'. The same, we can binary search the position of each prefix of search word in 'products' and check
+        if the following 3 words are valid.
+    Time complexity: O(P * logP), where N is the length of products array
+    Space complexity: O(P)
+    """
+
+    def insertion_index(word):
+        left, right = 0, len(products) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if products[mid] < word:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+
+    products.sort()
+    res, prefix = [], ''
+    for c in search_word:
+        prefix += c
+        index = insertion_index(prefix)  # Find where 'prefix' can be inserted in order in the products array. Same as
+        # bisect_left()
+        suggestions = [products[i] for i in range(index, min(len(products), index + 3)) if prefix in products[i]]
+        res.append(suggestions)
+    return res
+
+
+def suggested_products_v2(products, search_word):
     """ Iterate through every substring of search_word asking each word in the products array if it starts with the
         substring. If it does, we add it to a heap which is comparing lexicographically, that way we just need to
         pop from the heap 3 times to find the appropriate suggestions.
@@ -39,6 +68,7 @@ class Test(unittest.TestCase):
     def test_suggested_products(self):
         for test_products, test_search_word, result in self.data:
             self.assertEqual(result, suggested_products_v1(test_products, test_search_word))
+            self.assertEqual(result, suggested_products_v2(test_products, test_search_word))
 
 
 if __name__ == '__main__':
