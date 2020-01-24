043680667f116c0f324c39eb7532fcca20c8e9dc
Anouer Hermassi <ahermass@stevens.edu>
2020-01-24 15:02:12 -0500

    Interval list intersections - v2

--- Arrays/986_Interval_List_Intersections.py
+++ Arrays/986_Interval_List_Intersections.py
@@ -4,7 +4,7 @@ Return the intersection of these two interval lists. """
 import unittest2 as unittest
 
 
-def interval_intersection(A, B):
+def interval_intersection_v1(A, B):
     """ There is guaranteed to be a overlap interval if:
         A[i].start <= B[j].end AND B[j].start <= A[i].end
         If overlap: overlap interval = bigger start index + smaller end index; increment the index of the lesser
@@ -23,10 +23,26 @@ def interval_intersection(A, B):
         else:  # a.start <= b.end AND b.start <= b.end
             res.append([max(a[0], b[0]), min(a[1], b[1])])
             # Advance the interval with smaller endpoint in hopes of finding another overlap
-            if a[1] < b[1]:
-                i += 1
-            else:
-                j += 1
+            if a[1] < b[1]:  # If 'a' has the smallest endpoint, it can only intersect b ..
+                i += 1  # .. so we can discard 'a' since it cannot intersect anything else
+            else:  # If 'b' has the smallest endpoint, it can only intersect a ..
+                j += 1  # .. so we can discard 'b' since it cannot intersect anything else
+    return res
+
+
+def interval_intersection_v2(A, B):
+    n, m = len(A), len(B)
+    i, j, res = 0, 0, []
+    while i < n and j < m:
+        a, b = A[i], B[j]
+        max_start, min_end = max(a[0], b[0]), min(a[1], b[1])  # Find the overlap, if there is any
+        if max_start <= min_end:
+            res.append([max_start, min_end])
+        # Remove the interval with the smallest endpoint
+        if a[1] < b[1]:  # If 'a' has the smallest endpoint, it can only intersect b ..
+            i += 1  # .. so we can discard 'a' since it cannot intersect anything else
+        else:  # If 'b' has the smallest endpoint, it can only intersect a ..
+            j += 1  # .. so we can discard 'b' since it cannot intersect anything else
     return res
 
 
@@ -36,7 +52,8 @@ class Test(unittest.TestCase):
 
     def test_interval_intersection(self):
         for test_a, test_b, result in self.data:
-            self.assertEqual(result, interval_intersection(test_a, test_b))
+            self.assertEqual(result, interval_intersection_v1(test_a, test_b))
+            self.assertEqual(result, interval_intersection_v2(test_a, test_b))
 
 
 if __name__ == '__main__':
