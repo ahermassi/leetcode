da1f4ac54e6fd4b5412649b90cdd8bb903c334c8
Anouer Hermassi <ahermass@stevens.edu>
2020-01-27 10:47:09 -0500

    Find all anagrams in a string - v2 update

--- HashTable/438_Find_All_Anagrams_in_a_String.py
+++ HashTable/438_Find_All_Anagrams_in_a_String.py
@@ -28,19 +28,39 @@ def find_anagrams_v1(s, p):
 
 
 def find_anagrams_v2(s, p):
+    """ A different sliding window approach. No hash map comparison is involved.
+        Find the frequency of characters in the string p using 'counter' hash map, two variables 'left' and 'right' to
+        represent the left and right boundaries of the sliding window, and a variable 'need' to represent the number
+        of characters in the string p that need to be matched.
+        If the character on the right boundary is already in the hash table, indicating that the character appears in
+        p, then 'need' is decremented by 1, and then the entry of the current character in the hash table is also
+        decremented by 1 anyways. If 'need' is reduced to 0 at this time, it means that the characters in p are
+        matched in the current window, and the left boundary is added to the result 'res'.
+        If the window size (right - left + 1) is equal to the length of p, it means that the leftmost character should
+        be removed from the window. If after removal (corresponding entry in the frequency map is incremented by 1)
+        the count of the left character is greater than 0, it means that the character is a character in p. Why ?
+        Well, because each character is decremented by 1 above, and if it is not a character in p, then the character's
+        frequency in the hash table should be 0, and it will be -1 after decrementing by 1, so that we know whether
+        the character belongs to p. So if the leftmost character we removed belongs to p, 'need' is incremented by 1.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
     n, m = len(s), len(p)
-    need = Counter(p)
-    left, right, missing, res = 0, 0, len(p), []
+    counter = Counter(p)
+    left, right, need, res = 0, 0, len(p), []
     while right < n:
-        if need[s[right]] > 0:
-            missing -= 1
-        need[s[right]] -= 1
-        if missing == 0:
+        cur_char = s[right]
+        if counter[cur_char] > 0:  # The current character is in p
+            need -= 1  # One less character is needed
+        counter[cur_char] -= 1  # Decrement the count of current character anyway, so when it is not part of p it gets
+        # a negative entry in the map
+        if need == 0:
             res.append(left)
-        if right - left == m - 1:
-            need[s[left]] += 1
-            if need[s[left]] > 0:
-                missing += 1
+        if right - left + 1 == m:  # Current window size is equal to p length
+            counter[s[left]] += 1  # Discard the leftmost character
+            if counter[s[left]] > 0:  # If the discarded character was part of p, then it would have an entry equal to
+                # 0 at least, and if it's the case it would be > 0 after being incremented
+                need += 1
             left += 1
         right += 1
     return res
