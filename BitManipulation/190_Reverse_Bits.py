016ec7f95ee46906be5a45691e4aee9990c7823a
Anouer Hermassi <ahermass@stevens.edu>
2020-01-29 11:52:28 -0500

    Reverse bits - update

--- BitManipulation/190_Reverse_Bits.py
+++ BitManipulation/190_Reverse_Bits.py
@@ -4,8 +4,8 @@
 def reverse_bits(n):
     """ Similar to 7- Reverse Integer. However, instead of using modulo and division by 10, we use bit manipulation.
         Shift 1 bit to left == multiply by 2
-        Shit 1 bit to right == divide by 2
-        Bit & 1 == determine if bit is 0 or 1
+        Shift 1 bit to right == divide by 2
+        Bit & 1 == determine if bit is 0 or 1. Similarly, x & 1 gets the the value of the least significant bit of x
         Let's work on an example. Say, n = 19, which is 00000000000000000000000000010011 in binary, so in the output we
         should get 11001000000000000000000000000000 in binary, which is 3355443200.
         We initialize answer to 0, so in binary it's 32 zeroes. We loop over 32 times, since every integer is gonna
@@ -13,8 +13,8 @@ def reverse_bits(n):
         1st line in the loop: n & 1, we check if last bit of n is set, is it 1 or 0; ans << 1 we shift all bits that we
         already have in our answer to the left, so after this shifting the bit on the right is 0; by using + we set
         the last bit in the answer to the value that we got in n & 1.
-        2nd line in the loop: we shift bits of our initial number n to the right, since we've already checked the last
-        bit of n, so we just move on to the next bit.
+        2nd line in the loop: we shift bits of our initial number n to the right, since we've already checked the least
+        significant bit of n, so we just move on to the next bit.
         So, in our example, we're gonna work on only first 5 right bits, since other bits are 0.
 
         answer = 0, in binary: 00000000000000000000000000000000
@@ -39,9 +39,11 @@ def reverse_bits(n):
 
         And after that in our example, we'll just shift 00000000000000000000000000011001 all the way to the left, which
         is gonna lead to 11001000000000000000000000000000.
+    Time complexity: O(1)
+    Space complexity: O(1)
     """
     res = 0
     for _ in range(32):
-        res = (res << 1) + (n & 1)
+        res = (res << 1) | (n & 1)
         n = n >> 1
     return res
