ffc9f323ae36f407215f90df862f9791fe1104e5
Anouer Hermassi <ahermass@stevens.edu>
2020-02-07 11:38:21 -0500

    Remove duplicates from sorted list - update

--- LinkedList/83_Remove_Duplicates_from_Sorted_List.py
+++ LinkedList/83_Remove_Duplicates_from_Sorted_List.py
@@ -15,7 +15,7 @@ def delete_duplicates_v1(head):
     """ Because the input list is sorted, we can determine if a node is a duplicate by comparing its value to the
         node after it in the list. If it is a duplicate, we change the next pointer of the current node so that it
         skips the next node and points directly to the one after the next node.
-        The loop invariant is: all nodes in the list up to the pointer current do not contain duplicate elements.
+        The loop invariant is: all nodes in the list up to the 'cur' pointer do not contain duplicate elements.
     Time complexity: O(N)
     Space complexity: O(1)
     """
@@ -29,14 +29,14 @@ def delete_duplicates_v1(head):
 
 
 def delete_duplicates_v2(head):
-    """ The previous solution deletes the duplicate node as the cur pointer traverses the list. This solution first
+    """ The previous solution deletes the duplicate node as the 'cur' pointer traverses the list. This solution first
         locates the range of duplicate nodes and then deletes them all at once.
         The loop invariant is: all nodes in the list up to the pointer 'cur' do not contain duplicate elements, and
         all nodes between 'cur' and 'temp' are duplicate.
     Time complexity: O(N)
     Space complexity: O(1)
     """
-    cur, temp = head, head
+    cur = temp = head
     while cur:
         while temp and temp.val == cur.val:  # 'temp' will stop at the node right after the last duplicate node of 'cur'
             temp = temp.next
