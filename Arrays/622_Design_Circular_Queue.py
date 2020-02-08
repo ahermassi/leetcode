a58fcdc41f48b9c0c837194805dc4e41aa2dd372
Anouer Hermassi <ahermass@stevens.edu>
2020-02-08 14:28:32 -0500

    Desigining a circular queue - v2

--- Arrays/622_Design_Circular_Queue.py
+++ Arrays/622_Design_Circular_Queue.py
@@ -3,8 +3,9 @@ operations are performed based on FIFO (First In First Out) principle and the la
 first position to make a circle. It is also called "Ring Buffer". """
 
 
-class MyCircularQueue:
-    """ To build a circular queue, we could form a virtual ring structure with the Array, via the manipulation of index.
+class MyCircularQueueV1:
+    """ Array implementation.
+        To build a circular queue, we could form a virtual ring structure with the Array, via the manipulation of index.
         Here we give a list of attributes and the thoughts behind each attribute.
             - queue: a fixed size array to hold the elements for the circular queue
             - capacity: the capacity of the circular queue, i.e. the maximum number of elements that can be hold in
@@ -59,6 +60,79 @@ class MyCircularQueue:
         Get the last item from the queue.
         """
         return -1 if self.isEmpty() else self.queue[self.rear]
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular queue is empty or not.
+        """
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular queue is full or not.
+        """
+        return self.size == self.capacity
+
+
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class MyCircularQueueV2:
+    """ Linked list implementation.
+        Different than a fixed size Array, a linked list could be more memory efficient, since it does not pre-allocate
+        memory for unused capacity.
+    Time complexity: O(1), all of the methods in our circular data structure is of constant time complexity
+    Space complexity: O(N)
+    """
+
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the queue to be k.
+        """
+        self.head = None
+        self.tail = None
+        self.size = 0
+        self.capacity = k
+
+    def enQueue(self, value: int) -> bool:
+        """
+        Insert an element into the circular queue. Return true if the operation is successful.
+        """
+        if self.isFull():
+            return False
+        if self.isEmpty():
+            node = ListNode(value)
+            self.head = self.tail = node
+        else:
+            self.tail.next = ListNode(value)
+            self.tail = self.tail.next
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        """
+        Delete an element from the circular queue. Return true if the operation is successful.
+        """
+        if self.isEmpty():
+            return False
+        self.head = self.head.next
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        """
+        Get the front item from the queue.
+        """
+        return -1 if self.isEmpty() else self.head.val
+
+    def Rear(self) -> int:
+        """
+        Get the last item from the queue.
+        """
+        return -1 if self.isEmpty() else self.tail.val
 
     def isEmpty(self) -> bool:
         """
