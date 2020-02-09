29ad68b0c207f1968c0dc79c5c9a5412c6e38e70
Anouer Hermassi <ahermass@stevens.edu>
2020-02-09 15:04:38 -0500

    Finding the kth node in inorder traversal

---
+++ Tree/230_Kth_Element_in_Inorder_Traversal.py
@@ -0,0 +1,28 @@
+""" Write a program that efficiently computes the kth node appearing in an in-order traversal. Assume
+that each node stores the number of nodes in the subtree rooted at that node. """
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def find_kth_node_binary_tree(root, k):
+    """ If k is greater than the number of nodes in the left subtree, the kth node cannot lie in the left subtree.
+        More precisely, if the left subtree has L nodes, then the kth node in the original tree is the (k - L - 1)th
+        node when we skip the left subtree. Conversely, if k < L, the desired node lies in the left subtree.
+    Time complexity: O(logN) best case, O(N) worst case of a skewed tree
+    Space complexity: O(1)
+    """
+    cur = root
+    while cur:
+        left_size = cur.left.size if cur.left else 0
+        if left_size + 1 < k:
+            cur = cur.right
+        elif left_size + 1 == k:
+            return cur
+        else:
+            cur = cur.left
+    return None
