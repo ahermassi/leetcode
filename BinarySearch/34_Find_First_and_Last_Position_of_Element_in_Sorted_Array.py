37f00efe7773f2eed47148894dd41a37368f75b4
Anouer Hermassi <ahermass@stevens.edu>
2020-02-11 15:51:53 -0500

    Find first and last position of element in sorted array - v2

--- BinarySearch/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ BinarySearch/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -36,7 +36,7 @@ def search_range_v1(nums, target):
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
-            if target > nums[mid]:
+            if nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
@@ -46,7 +46,7 @@ def search_range_v1(nums, target):
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
-            if target < nums[mid]:
+            if nums[mid] > target:
                 right = mid - 1
             else:
                 left = mid + 1
@@ -56,12 +56,59 @@ def search_range_v1(nums, target):
     return [left, right] if left <= right else [-1, -1]
 
 
+def search_range_v2(nums, target):
+    """ The fundamental idea of binary search is to maintain a set of candidate solutions. To find the first index, if
+        we see the element at index i equals 'target', although we do not know whether i is the first element equal to
+        'target', we do know that no subsequent elements can be the first one. Therefore we remove all elements with
+        index (i + 1) or more from the candidates.
+        Let's apply the above logic to the array [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], with target = 108.
+        We start with all indices as candidates, i.e., with [0, 9].
+        The midpoint index, 4 contains target. Therefore we can now update the candidate set to [0, 3], and record 4 as
+        an occurrence of 'target'.
+        The next midpoint is 1, and this index contains -10. We update the candidate set to [2,3].
+        The value at the midpoint is 2, so we update the candidate set to [3, 3].
+        Since the value at this midpoint is 108, we update the first seen occurrence of 'target' to 3.
+        Now the interval is [3, 2], which is empty, terminating the search.
+        The result is 3.
+        Using the same logic, we can find the last occurrence index.
+    Time complexity: O(logN)
+    Space complexity: O(1)
+    """
+
+    def find_first_index():
+        left, right, res = 0, len(nums) - 1, -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+            if nums[mid] == target:
+                res = mid
+        return res
+
+    def find_last_index():
+        left, right, res = 0, len(nums) - 1, -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+            if nums[mid] == target:
+                res = mid
+        return res
+
+    return [find_first_index(), find_last_index()]
+
+
 class Test(unittest.TestCase):
     data = [([5, 7, 7, 8, 8, 10], 8, [3, 4]), ([5, 7, 7, 8, 8, 10], 6, [-1, -1])]
 
     def test_search_range(self):
         for test_array, test_target, result in self.data:
             self.assertEqual(result, search_range_v1(test_array, test_target))
+            self.assertEqual(result, search_range_v2(test_array, test_target))
 
 
 if __name__ == '__main__':
