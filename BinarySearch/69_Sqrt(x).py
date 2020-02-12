a6d3fac1a43005383209b5dc1f2f2dde53ca35a2
Anouer Hermassi <ahermass@stevens.edu>
2020-02-12 12:53:28 -0500

    sqrt(x) - update

--- BinarySearch/69_Sqrt(x).py
+++ BinarySearch/69_Sqrt(x).py
@@ -7,22 +7,39 @@ import unittest2 as unittest
 
 def my_sqrt(x):
     """ The value a we're supposed to compute could be defined as: a^2 <= x < (a + 1)^2
-        For x ≥ 2, the square root is always smaller than or equal to x and larger than 0 : 1 < a < x/2
-        Use binary search to find an approximation of the two integers that the sqrt falls between.
+        Looking carefully at the problem, it should be clear that it is wasteful to take unit-sized increments.
+        For example, a^2 < x, then no number smaller than a can be the result, and if a^2 > x, then no number greater
+        than or equal to a can be the result.
+        This ability to eliminate large sets of possibilities is suggestive of binary search. Specifically, we can
+        maintain an interval consisting of values whose squares are unclassified with respect to x, i.e., might be less
+        than or greater than x.
+        We initialize the interval to [0, x]. We compare the square of mid = (left + right) // 2 with x, and use the
+        elimination rule to update the interval.
+        If mid^2 > x, we know all numbers greater than or equal to mid have a square greater than x, so we update the
+        candidate interval to [left, mid - 1].
+        If mid^2 <= x, we know all integers less than or equal to mid have a square less than or equal to x. Therefore,
+        we update the interval to [mid + 1, right].
+        The algorithm terminates when the interval is empty, in which case every number less than left has a square
+        less than or equal to x, and left's square is greater than x, so the result is left - 1, or right.
+        For example, if x = 21, we initialize the interval to [0, 21].
+        Now mid = (0 + 21) // 2 = 10. Since 10^2 > 21, we update the interval to [0, 9].
+        Now mid = (0 + 9) // 2 = 4. Since 4^2 < 21, we update the interval to [5, 9].
+        Now mid = (5 + 9) // 2 = 7. Since 7^2 > 21, we update the interval to [5, 6].
+        Now mid = (5 + 6) // 2 = 5. Since 5^2 > 21, we update the interval to [5, 4].
+        Now the right endpoint is less than the left endpoint, i.e., the interval is empty, so the result is 5 - 1 = 4,
+        which is the value returned.
     Time complexity: O(log x)
     Space complexity: O(1)
     """
-    if x <= 1:
-        return x
-    left, right = 1, x
-    while left <= right:
+    left, right = 0, x
+    while left <= right:  # Everything before left has square <= x, everything after right has square > x
         mid = (left + right) // 2
-        if mid ** 2 <= x < (mid + 1) ** 2:
-            return mid
-        if mid ** 2 > x:
+        mid_squared = mid * mid
+        if mid_squared > x:
             right = mid - 1
         else:
             left = mid + 1
+    return right  # The loop is stopped when left > right, and at this moment right * right <= x < left * left
 
 
 class Test(unittest.TestCase):
