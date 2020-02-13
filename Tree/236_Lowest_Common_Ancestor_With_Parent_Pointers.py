6bd2741efdd95927269593a9c418a794181577e2
Anouer Hermassi <ahermass@stevens.edu>
2020-02-13 17:42:55 -0500

    Optimizing the LCA algorithm for close ancestors

--- Tree/236_Lowest_Common_Ancestor_With_Parent_Pointers.py
+++ Tree/236_Lowest_Common_Ancestor_With_Parent_Pointers.py
@@ -28,3 +28,30 @@ def lowest_common_ancestor(p, q):
     while p != q:  # Now ascend both nodes until we reach the LCA
         p, q = p.par, q.par
     return p
+
+# The previous algorithm entails traversing all the way to the root even if the nodes whose LCA is being computed are
+# very close to their LCA. The following algorithm's time complexity should depend only on the distance from the nodes
+# to the LCA.
+
+
+def lowest_common_ancestor_optimized(p, q):
+    """ The previous approach is suboptimal because it potentially processes nodes well above the LCA. We can avoid
+        this by alternating moving upwards from the two nodes and storing the nodes visited as we move up in a hash
+        set. Each time we visit a node, we check to see if it has been visited before.
+    Time complexity: O(D0 + D1), where D0 is the distance from the LCA to the first node, and D1 is the distance from
+    the LCA to the second node. In the worst case, the nodes are leaves whose LCA is the root, and we end up using O(h)
+    time and space, where h is the height of the tree
+    """
+    ancestors = set()
+    while p or q:  # Ascend tree in tandem for these two nodes
+        if p:
+            if p in ancestors:
+                return p
+            ancestors.add(p)
+            p = p.parent
+        if q:
+            if q in ancestors:
+                return q
+            ancestors.add(q)
+            q = q.parent
+
