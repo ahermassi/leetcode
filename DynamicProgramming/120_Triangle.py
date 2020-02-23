f15a1ce6dca50a0a9b112b7e861d8721c62ed4c5
Anouer Hermassi <ahermass@stevens.edu>
2020-02-23 11:16:11 -0500

    Finding the minimum path sum in a triangle - v2

--- DynamicProgramming/120_Triangle.py
+++ DynamicProgramming/120_Triangle.py
@@ -19,3 +19,24 @@ def minimum_total_v1(triangle):
     n, res = len(triangle), []
     dfs(0, 0, 0)
     return min(res)
+
+
+def minimum_total_v2(triangle):
+    """ Bottom-up dynamic programming.
+        A far better way is to consider entries in the ith row. For any such entry, if we look at the minimum weight
+        path ending at it, the part of the path that ends at the previous row must also be a minimum weight path. This
+        gives us a DP solution. We iteratively compute the minimum weight of a path ending at each entry in row i using
+        the results at row (i - 1).
+    """
+    n, dp = len(triangle), [[0] * len(row) for row in triangle]
+    dp[0][0] = triangle[0][0]
+    for i in range(1, n):
+        for j in range(i + 1):  # We calculate the minimum path sum ending at each cell j of the current row i
+            if j == 0:  # The first cell has only 1 previous adjacent cell
+                dp[i][j] = dp[i - 1][j] + triangle[i][j]
+            elif j == i:  # The last cell has only 1 previous adjacent cell
+                dp[i][j] = dp[i - 1][j - 1] + triangle[i][j]
+            else:  # The minimum path sum ending at this cell is the least sum of its two previous adjacent cells plus
+                # the current cell's value
+                dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j]
+    return min(dp[-1])  # Each cell of the last row contains the minimum sum of the path ending at that cell
