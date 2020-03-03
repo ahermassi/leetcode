3868987f1f6f5ac7529d674e37a04c8f783aefa2
Anouer Hermassi <ahermass@stevens.edu>
2020-03-03 11:44:06 -0500

    The maze II - v1 update

--- DFS/505_The_Maze_II.py
+++ DFS/505_The_Maze_II.py
@@ -11,23 +11,23 @@ import unittest2 as unittest
 
 def shortest_distance_v1(maze, start, destination):
     """" BFS. DFS version is below but it TLEs.
-         Same logic as with 490- The Maze, but we also use a 'distance' array initialized with float('inf').
-         distance[i][j] again represents the minimum number of steps required to reach the position (i,j) from the
-         start position, such that distance[start[0]][start[1]] = 0
+         Same logic as in 490- The Maze, but we also use a 'distance' array initialized with float('inf').
+         distance[i][j] represents the minimum number of steps required to reach the position (i,j) from the start
+         position, such that distance[start[0]][start[1]] = 0
          When we reach any position next to a boundary or a wall during the traversal in a particular direction, as
          discussed earlier, we keep a track of the number of steps taken in the last direction in 'd' variable. Suppose
          we reach the position (k,l) starting from the last position (i,j). Now, for this position, we need to
          determine the minimum number of steps taken to reach this position starting from the start position. For this,
-         we check if the current path takes lesser steps to reach (k, l) than any other previous path taken to reach
+         we check if the current path takes less steps to reach (k,l) than any other previous path taken to reach
          the same position i.e. we check if distance[i][j] + d is less than distance[k][l].
-         If distance[i][j] + d is less than distance[k][l], we can reach the position (k, l) from the current route in
-         less number of steps. Thus, we need to update the value of distance[k][l as distance[i][j] + d. Further, now
+         If distance[i][j] + d is less than distance[k][l], we can reach the position (k,l) from the current route in
+         less number of steps. Thus, we need to update the value of distance[k][l] to distance[i][j] + d. Further, now
          we need to try to reach the destination from the end position (k,l), since this could lead to a shorter path
          to destination.
-         After this, we add the new position obtained (k, l) to the back of the queue, so that the various paths
+         After this, we add the new position obtained (k,l) to the back of the queue, so that the various paths
          possible from this new position will be explored later on when all the directions possible from the current
-         position (i, j) have been explored.
-         At the end, the entry in distance array corresponding to the destination's coordinates gives the required
+         position (i,j) have been explored.
+         At the end, the entry in 'distance' array corresponding to the destination's coordinates gives the required
          minimum distance to reach the destination. If the destination can't be reached, the corresponding entry will
          contain float('inf').
     Time complexity: O(N * M * max(N, M)), complete traversal of maze will be done in the worst case. Here, N and M
@@ -36,7 +36,7 @@ def shortest_distance_v1(maze, start, destination):
     Space complexity: O(N * M),  queue size can grow up to N * M in the worst case
     """
     n, m = len(maze), len(maze[0])
-    distance = [[float('inf') for _ in range(m)] for _ in range(n)]
+    distance = [[float('inf')] * m for _ in range(n)]
     distance[start[0]][start[1]] = 0
     queue = deque([(start[0], start[1])])
     while queue:
@@ -48,8 +48,7 @@ def shortest_distance_v1(maze, start, destination):
                 d += 1
                 new_i += x
                 new_j += y
-            if distance[i][j] + d < distance[new_i][new_j]:  # If we've just reached this position in fewer
-                # steps
+            if distance[i][j] + d < distance[new_i][new_j]:  # If we've just reached this position in fewer steps
                 distance[new_i][new_j] = distance[i][j] + d  # Update the shortest distance to this position
                 queue.append((new_i, new_j))  # Explore the remaining paths from there
     res = distance[destination[0]][destination[1]]
