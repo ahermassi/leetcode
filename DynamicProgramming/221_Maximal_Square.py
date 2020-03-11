e0c7ec54f708e93fdd504654c1b554670cd8cba4
Anouer Hermassi <ahermass@stevens.edu>
2020-03-11 10:36:41 -0400

    Maximal square - v2 update

--- DynamicProgramming/221_Maximal_Square.py
+++ DynamicProgramming/221_Maximal_Square.py
@@ -1,11 +1,10 @@
-''' Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its
-area. '''
+""" Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its
+area. """
 
 import unittest2 as unittest
 
 
 # For full details, check out this article: https://leetcode.com/articles/maximal-square/
-
 
 def maximal_square_v1(matrix):
     """ Top down, recursive solution with memoization.
@@ -15,6 +14,14 @@ def maximal_square_v1(matrix):
         After that, since the current cell is the bottom right corner, we recursively examine the remaining 3 corners,
         mainly top right (i - 1, j), bottom left (i, j - 1), and top left (i - 1, j - 1). The result is the minimum of
         the maximal side length that each corner contributes with plus 1.
+        So we go to the first 1 and ask it, 'Hey, what's the largest square of 1s that begins with you?'.
+        To calculate that, it needs to know the largest squares its adjacent cells can begin.
+        So, it'll ask the same question to its adjacent cells which will in turn will ask their adjacent cells and so
+        on. The cell that began the question will deduce that the largest square that begins with it is 1 + the
+        minimum of all the values its adjacent cells returned.
+        We then ask the same question to every 1 we find in the grid and keep track of the global maximum. In doing
+        so, we'll notice that the recursion causes many cells to be asked the same question again and again
+        (overlapping sub-problems), so we use memoization.
     Time complexity: O((N * M)^2), in worst case we need to traverse the complete matrix for every cell equal to 1
     Space complexity: O(N * M)
     """
@@ -40,23 +47,17 @@ def maximal_square_v1(matrix):
 
 
 def maximal_square_v2(matrix):
-    """ We initialize another matrix (dp) with the same dimensions as the original one initialized with all 0’s.
-        dp(i,j) represents the side length of the maximum square whose bottom right corner is the cell with index (i,j)
+    """ Bottom up dynamic programming.
+        We initialize another matrix (dp) with the same dimensions as the original one initialized with all 0’s.
+        dp[i, j] represents the side length of the maximal square whose bottom right corner is the cell at index (i,j)
         in the original matrix.
         Starting from index (0,0), for every 1 found in the original matrix, we update the value of the current element
         as:
-            dp(i, j)= min(dp(i−1, j), dp(i−1, j−1), dp(i, j−1))+1.
-        Logic : Top, Left, and Top Left decides the size of the square. If all of them are same, then the size of
-        square increases by 1. If they're not same, they can increase by 1 to the minimal square.
-        At first sight, this problem requires a DFS traversal - a dead giveaway that we need recursion. And it also
-        wants you to find the largest square. So you'd go to the first 1 and ask it, "Hey, what's the largest square of
-        1s that begins with you?". To calculate that it needs to know the largest squares its adjacent cells can begin.
-        So, it'll ask the same question to its adjacent cells which will in turn will ask their adjacent cells and so
-        on... The cell that began the question will deduce that the largest square that begins with it is 1 + the
-        minimum of all the values its adjacent cells returned.
-        You'd then ask the same question to every 1 you find in the grid and keep track of the global maximum. In doing
-        so, you'll notice that the recursion causes many cells to be asked the same question again and again
-        (overlapping sub-problems)- so you'd use memoization.
+            dp[i, j] = min(dp[i−1, j], dp[i−1, j−1], dp[i, j−1]) + 1
+        We use min() because that is what limits the size of the square. If for example any corner was a 0, we know we
+        don't have that row or column to form the bigger square at (i,j).
+        Logic : Top right, bottom left, and top left decide the size of the square. If all of them are same, then the
+        size of square increases by 1. If they're not same, they can increase by 1 to the minimal square.
     Time complexity: O(N * M)
     Space complexity: O(N * M)
     """
@@ -70,7 +71,7 @@ def maximal_square_v2(matrix):
                 # at first row/column has only 1 element. So if matrix[i][j] == c --> dp[i][j] = c; c in {0, 1}
                 dp[i][j] = int(matrix[i][j])
             elif matrix[i][j] == '1':
-                dp[i][j] = min(dp[i - 1][j],dp[i][j - 1], dp[i - 1][j - 1]) + 1  # min(top, left, top-left/diagonal) + 1
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1  # min(top right, bottom left, top left) + 1
                 max_len = max(max_len, dp[i][j])
     return max_len * max_len
 
