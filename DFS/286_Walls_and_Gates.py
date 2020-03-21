ed18d58051955453a666cbc0a830c4af712938d2
Anouer Hermassi <ahermass@stevens.edu>
2020-03-21 12:13:58 -0400

    Walls and gates - v2 update

--- DFS/286_Walls_and_Gates.py
+++ DFS/286_Walls_and_Gates.py
@@ -39,31 +39,38 @@ def walls_and_gates_v1(rooms):
 
 
 def walls_and_gates_v2(rooms):
-    """ Instead of searching from an empty room to the gates, how about searching the other way round? In other words,
+    """ Instead of searching from an empty room to the gates, how about searching the other way round ? In other words,
         we initiate a BFS from all gates. Since BFS guarantees that we search all rooms of distance d before searching
         rooms of distance d + 1, the distance to an empty room must be the shortest. So whenever an empty room is
         reached, it must be from the closest gate.
+        Each gate only looks at the areas within 1 space (immediate neighbors) before we check the next gate. So each
+        area within 1 space of the gates are checked for rooms and these rooms are marked, then added to the queue.
+        Once all gates are checked, each new space is checked, and so forth. So, once a room gets hit, it has to be
+        from the closest gate.
+        We can understand it by level-order BFS. First, we enqueue all 0s, let's say these these 0s are in level 1.
+        Then from each 0 of the queue, we will go up, down, left and right, all these positions that are rooms are at
+        level 1, and so forth. So assume we only have Gate A and Gate B, and we have a room C and all the other
+        positions are walls. Assume that distance AC is 3 and distance BC is 4. So for Gate A, room C is at its level 3,
+        and for Gate B room C is at its level 4. Since we are doing level order BFS, C will always first be accessed by
+        the gate that is closer to it which is A.
     Time complexity: O(N * M). Let us start with the case with only one gate. The breadth-first search takes at most
-    N* M steps to reach all rooms, therefore the time complexity is O(N * M). But what if you are doing BFS from
+    N * M steps to reach all rooms, therefore the time complexity is O(N * M). But what if we are doing BFS from
     k gates? Once we set a room's distance, we are basically marking it as visited, which means each room is visited at
     most once. Therefore, the time complexity does not depend on the number of gates and is O(N * M)
-    Space complexity: O(N * M), the space complexity depends on the queue's size. We insert at most N* M points into
+    Space complexity: O(N * M), the space complexity depends on the queue's size. We insert at most N * M points into
     the queue
     """
     if not rooms:
         return
-    n, m, queue = len(rooms), len(rooms[0]), deque()
-    for i in range(n):
-        for j in range(m):
-            if rooms[i][j] == 0:
-                queue.append((i, j))
+    n, m = len(rooms), len(rooms[0])
+    queue = deque([(i, j) for i in range(n) for j in range(m) if rooms[i][j] == 0])  # Enqueue all gates
     while queue:
         i, j = queue.popleft()
-        for x, y in (-1, 0), (1, 0), (0, -1), (0, 1):
-            new_i, new_j = i + x, j + y
-            if 0 <= new_i < n and 0 <= new_j < m and rooms[new_i][new_j] == 2147483647:
-                rooms[new_i][new_j] = rooms[i][j] + 1
-                queue.append((new_i, new_j))
+        for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+            if 0 <= x < n and 0 <= y < m and rooms[x][y] == 2 ** 31 - 1:  # If a room was hit (rooms[x][y] != INF), it
+                # has to be from the closest gate, so there is no need to explore it again
+                rooms[x][y] = 1 + rooms[i][j]
+                queue.append((x, y))
 
 
 class Test(unittest.TestCase):
