c1728753ed39a6a14e4bc4cfe2b3ac73e029f326
Anouer Hermassi <ahermass@stevens.edu>
2020-03-23 13:24:20 -0400

    Finding the sum of node values within a range - v2

--- Tree/938_Range_Sum_of _BST.py
+++ Tree/938_Range_Sum_of _BST.py
@@ -24,3 +24,20 @@ def range_sum_bst_v1(root, L, R):
     if root.val > R:
         return range_sum_bst_v1(root.left, L, R)
     return root.val + range_sum_bst_v1(root.right, L, R) + range_sum_bst_v1(root.left, L, R)
+
+
+def range_sum_bst_v2(root, L, R):
+    """ Another way to look at the problem.
+    Time complexity: O(N)
+    Space complexity: O(h)
+    """
+    if not root:
+        return 0
+    res = 0
+    if L <= root.val <= R:
+        res += root.val
+    if root.val > L:
+        res += range_sum_bst_v2(root.left, L, R)  # Left child is a possible candidate
+    if root.val < R:
+        res += range_sum_bst_v2(root.right, L, R)  # Right child is a possible candidate
+    return res
