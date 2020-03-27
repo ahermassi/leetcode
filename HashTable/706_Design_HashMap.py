4ec5c5b64ed7988aedce80fbe9359de99c9c2eda
Anouer Hermassi <ahermass@stevens.edu>
2020-03-27 10:42:58 -0400

    Design hashmap - update

--- HashTable/706_Design_HashMap.py
+++ HashTable/706_Design_HashMap.py
@@ -1,6 +1,5 @@
-""" Design a HashMap without using any built-in hash table libraries.
-All keys and values will be in the range of [0, 1000000].
-The number of operations will be in the range of [1, 10000].
+""" Design a HashMap without using any built-in hash table libraries. All keys and values will be in the range of
+[0, 1000000]. The number of operations will be in the range of [1, 10000].
 Please do not use the built-in HashMap library.
 """
 
@@ -14,7 +13,30 @@ class ListNode:
 
 
 class MyHashMap(object):
-    """ Simple implementation of Hash with chaining. """
+    """ Simple implementation of hash with chaining.
+        There are two main issues that we should tackle, in order to design an efficient hash map data structure:
+            1- Hash function design: The purpose of hash function is to map a key value to an address in the storage
+               space, similarly to the system that we assign a postcode to each mail address. As we can image, for a
+               good hash function, it should map different keys evenly across the storage space, so that we don't end
+               up with the case that the majority of the keys are concentrated in a few spaces.
+            2- Collision handling: Essentially the hash function reduces the vast key space into a limited address
+               space. As a result, there could be the case where two different keys are mapped to the same address,
+               which is what we call 'collision'. Since the collision is inevitable, it is important that we have a
+               strategy to handle the collision.
+        As one of the most intuitive implementations, we could adopt the modulo operator as the hash function, since
+        the key value is of integer type.
+        We organize the storage space as an array where each element is indexed with the output value of the hash
+        function.
+        In case of collision, where two different keys are mapped to the same address, we use a bucket to hold all the
+        values. The bucket is a container that hold all the values that are assigned by the hash function. We could use
+        a linked list to implement the bucket data structure.
+        This localization process can be done in two steps:
+            - For a given key value, first we apply the hash function to generate a hash key, which corresponds to the
+              address in our main storage. With this hash key, we would find the bucket where the value should be
+              stored.
+            - Now that we found the bucket, we simply iterate through the bucket to check if the desired <key, value>
+            pair does exist.
+    """
 
     def __init__(self):
         """
@@ -24,12 +46,7 @@ class MyHashMap(object):
         self.hash_map = [None] * self.size
 
     def put(self, key, value):
-        """
-        value will always be non-negative.
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
+        """ Value will always be non-negative. """
         index = key % self.size  # This is our hash function
         if not self.hash_map[index]:
             self.hash_map[index] = ListNode(key, value)
@@ -46,11 +63,7 @@ class MyHashMap(object):
             cur.next = ListNode(key, value)  # If not found, create a new pair (node)
 
     def get(self, key):
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        :type key: int
-        :rtype: int
-        """
+        """ Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key"""
         index = key % self.size
         cur = self.hash_map[index]
         while cur:
@@ -60,11 +73,7 @@ class MyHashMap(object):
         return -1
 
     def remove(self, key):
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        :type key: int
-        :rtype: None
-        """
+        """ Removes the mapping of the specified value key if this map contains a mapping for the key """
         index = key % self.size
         cur = prev = self.hash_map[index]
         if not cur:
