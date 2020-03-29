600460295e7936213995d839a052eadc53712ff2
Anouer Hermassi <ahermass@stevens.edu>
2020-03-28 21:53:49 -0400

    Finding the next greater elements in a circular array - v2

--- Stack/503_Next_Greater_Element_II.py
+++ Stack/503_Next_Greater_Element_II.py
@@ -25,12 +25,40 @@ def next_greater_elements_v1(nums):
     return res
 
 
+def next_greater_elements_v2(nums):
+    """ The approach is same as Next Greater Element I. The only difference here is that we use stack to keep the
+        indices of the decreasing sub-sequence. We store the indices instead of the elements since there could be
+        duplicates in the array. This time, we need to traverse the whole array twice since it is circular, so even
+        the very first elements can be potential next greater for the later ones.
+        We start traversing the array from left towards the right. For an element nums[i] encountered, we pop all the
+        elements stack[top] from the stack such that nums[stack[top]] < nums[i]. We continue the popping till we
+        encounter a stack[top] satisfying nums[stack[top]] >= nums[i]. Now, it is obvious that the current stack[top]
+        only can act as the next greater element for nums[i] (right now, considering only the elements lying to the
+        left of nums[i]).
+        If no element remains on the top of the stack, it means no larger element than nums[i] exists to its left.
+        Along with this, we also push the index of the element just encountered (nums[i]), i.e. i over the top of the
+        stack, so that nums[i] (or stack[top]) now acts as the next greater element for the elements lying to its left.
+        We go through two such passes over the complete array. This is done so as to complete a circular traversal
+        over the array.
+    Time complexity: O(N)
+    Space complexity: O(N)
+    """
+    n, greater, stack = len(nums), {}, []
+    for i in range(2 * n):
+        num = nums[i % n]
+        while stack and nums[stack[-1]] < num:
+            greater[stack.pop()] = num
+        stack.append(i % n)
+    return [greater.get(i, -1) for i in range(n)]
+
+
 class Test(unittest.TestCase):
     data = [([1, 2, 1], [2, -1, 2]), ([1, 1, 1, 1], [-1, -1, -1, -1])]
 
     def test_next_greater_elements(self):
         for test_nums, result in self.data:
             self.assertEqual(result, next_greater_elements_v1(test_nums))
+            self.assertEqual(result, next_greater_elements_v2(test_nums))
 
 
 if __name__ == '__main__':
