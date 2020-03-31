c726affc0a4eaf5fd4d6a553584f1de5e173ce13
Anouer Hermassi <ahermass@stevens.edu>
2020-03-31 10:19:20 -0400

    Integer to roman - update

--- Strings/12_Integer_to_Roman.py
+++ Strings/12_Integer_to_Roman.py
@@ -4,17 +4,23 @@ import unittest2 as unittest
 
 
 def int_to_roman(num):
-    """ More concise but tricky solution. Because we construct the roman numeral largest to smallest from left to right,
-        we use a decreasingly ordered mapping from decimals to roman symbols. We keep dividing the original number by
+    """ Representing a given integer as a roman numeral requires finding a sequence of symbols, where their
+        corresponding values add up to the integer. This sequence must be in order from largest to smallest, based on
+        symbol value.
+        We use a decreasingly ordered mapping from decimals to roman symbols. We keep dividing the original number by
         the largest possible decimal and convert the division result to the corresponding roman symbol which we append
         to the final string, get the modulo and re-iterate.
-    Time complexity: O(N)
+    Time complexity: O(1), as there is a finite set of roman numerals, there is a hard upper limit on how many times
+    the loop can iterate. This upper limit is 15 times, and it occurs for the number 3888, which has a representation
+    of MMMDCCCLXXXVIII
     Space complexity: O(1)
     """
     numerals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
     res = ''
     for numeral, roman in zip(numerals, romans):
+        if not num:
+            break
         res += (num // numeral) * roman
         num = num % numeral
     return res
