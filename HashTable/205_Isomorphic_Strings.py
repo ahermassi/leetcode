cba38a0507cf36694ad5b363c5daf8e2c897435b
Anouer Hermassi <ahermass@stevens.edu>
2020-04-06 11:48:40 -0400

    Isomorphic strings - update

--- HashTable/205_Isomorphic_Strings.py
+++ HashTable/205_Isomorphic_Strings.py
@@ -3,27 +3,30 @@ Two strings are isomorphic if the characters in s can be replaced to get t.
 All occurrences of a character must be replaced with another character while preserving the order of characters.
 No two characters may map to the same character but a character may map to itself. """
 
-from collections import defaultdict
 import unittest2 as unittest
 
 
 def is_isomorphic(s, t):
-    """ Use two dictionaries d1 and d2. Every key/value pair in d1 is mapping a character in s to its corresponding
-        character(s) in t. d2 is used for the exact opposite: to which character did a character in t map.
-        When a character in t is found to map to more than one character in s OR a character in s maps to more than
-        one character in t, the function returns False.
-    Time complexity: O(N + M) where N is the length of s and t is the length of t
-    Space complexity: O(N + M)
+    """ The idea is that we need to map a character to another one, for example, 'egg' and 'add', we need to construct
+        the mapping 'e' -> 'a', 'a' -> 'e', 'g' -> 'd', and 'd' -> 'g'. When we encounter a character for the first
+        time, we map it with the corresponding current character of the other string. If we encounter that character
+        later, then we go and fetch the previous mapping and compare it to the current character of the other string.
+        If they match, then fine. Otherwise, the strings can't be isomorphic.
+    Time complexity: O(N + M), where N is the length of s and t is the length of t
+    Space complexity: O(1), the hash maps can't store more than the size of the alphabet characters
     """
-    d1, d2 = defaultdict(set), {}
-    for ch1, ch2 in zip(s, t):
-        if ch2 in d2 and d2[ch2] != ch1:
+    s_chars, t_chars = {}, {}
+    for c1, c2 in zip(s, t):
+        if c1 not in s_chars and c2 not in t_chars:
+            s_chars[c1] = c2
+            t_chars[c2] = c1
+        elif s_chars.get(c1, c2) != c2 or t_chars.get(c2, c1) != c1:  # We use a default value with .get() to avoid
+            # returning False when both c1 and c2 are encountered for the first time
             return False
-        d1[ch1].add(ch2)
-        d2[ch2] = ch1
-    for v in d1.values():
-        if len(v) > 1:
-            return False
+        # The previous block is equivalent to:
+        # if (c1 in s_chars and s_chars[c1] != c2) or (c2 in t_chars and t_chars[c2] != c1):
+        #     return False
+        # s_chars[c1], t_chars[c2] = c2, c1
     return True
 
 
