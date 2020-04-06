2dfc0caa38cb78a6d12874fab36acef23855d199
Anouer Hermassi <ahermass@stevens.edu>
2020-04-06 10:39:12 -0400

    Two sum III data structure design - update

--- HashTable/170_Two_Sum_III_Data_structure_design.py
+++ HashTable/170_Two_Sum_III_Data_structure_design.py
@@ -2,39 +2,44 @@
 add - Add the number to an internal data structure.
 find - Find if there exists any pair of numbers which sum is equal to the value. """
 
+from collections import defaultdict
 import unittest2 as unittest
 
 
-class TwoSum(object):
-
-    """ Good old hash table techniques. """
+class TwoSum:
+    """ We can employ a hash table to index each number. Given a desired sum value S, for each number a, we just need
+        to verify if there exists a complement number (S - a) in the table.
+        For the add(number) function, we build a frequency hash map with the number as key and the frequency of the
+        number as the value.
+        For the find(value) function, we iterate through the hashtable over the keys. For each key 'number', we check
+        if there exists a complement (value - number) in the table. If so, we could terminate the loop and return the
+        result. In a particular case, where the number and its complement are equal, we then need to check if there
+        exists at least two copies of the number in the table.
+    Time complexity: O(1) for add(number), O(N) for find(value) where N is the total number of unique numbers
+    Space complexity: O(N), where N is the total number of unique numbers that we will see during the usage of the data
+    structure
+    """
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.numbers = {}
+        self.numbers = defaultdict(int)
 
-    def add(self, number):
+    def add(self, number: int) -> None:
         """
         Add the number to an internal data structure..
-        :type number: int
-        :rtype: None
         """
-        try:
-            self.numbers[number] += 1
-        except KeyError:
-            self.numbers[number] = 1
+        self.numbers[number] += 1
 
-    def find(self, value):
+    def find(self, value: int) -> bool:
         """
         Find if there exists any pair of numbers which sum is equal to the value.
-        :type value: int
-        :rtype: bool
         """
-        for i in self.numbers:
-            # If the complement of i is found, it should be either not equal to i or that i was added more than once
-            if value - i in self.numbers and (value - i != i or self.numbers[i] > 1):
+        numbers = self.numbers
+        for number, count in numbers.items():
+            complement = value - number
+            if complement in numbers and (complement != number or count > 1):
                 return True
         return False
 
