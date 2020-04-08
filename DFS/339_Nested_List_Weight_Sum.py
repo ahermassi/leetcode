5cf7cc4bd04dbbf93ea91c28df0079329eec6c84
Anouer Hermassi <ahermass@stevens.edu>
2020-04-08 10:54:59 -0400

    Nested lis weight sum - v1 update

--- DFS/339_Nested_List_Weight_Sum.py
+++ DFS/339_Nested_List_Weight_Sum.py
@@ -2,26 +2,25 @@
 Each element is either an integer, or a list -- whose elements may also be integers or other lists. """
 
 
-def depthSum_v1(nestedList):
-    """ Because the input is nested, it is natural to think about the problem in a recursive way. We go through
+def depth_sum_v1(nestedList):
+    """ Because the input is nested, it is natural to think about the problem in a recursive way. We iterate over
         the list of nested integers one by one, keeping track of the current depth. If a nested integer is an
         integer n, we calculate its sum as n * depth. If the nested integer is a list, we calculate the sum of
-        this list recursively using the same process but with depth + 1.
-    Time complexity: O(N) where N is the total number of nested elements in the input list. For example, the list
+        this list recursively using the same process but with (depth + 1).
+    Time complexity: O(N), where N is the total number of nested elements in the input list. For example, the list
     [ [[[[1]]]], 2 ] contains 4 nested lists and 2 nested integers (1 and 2), so N = 6
     Space complexity: at most O(D) recursive calls are placed on the stack, where D is the maximum level of nesting
     in the input. For example, D = 2 for the input [[1,1],2,[1,1]], and D = 3 for the input [1,[4,[6]]].
     """
 
-    def dfs(nestedList, depth):
-        sum = 0
-        for lst in nestedList:
-            if lst.isInteger():
-                sum += lst.getInteger() * depth
+    def dfs(nested_list, depth):
+        res = 0
+        for nested_integer in nested_list:
+            if nested_integer.isInteger():
+                res += nested_integer.getInteger() * depth
             else:
-                l = lst.getList()
-                sum += dfs(l, depth + 1)
-        return sum
+                res += dfs(nested_integer.getList(), depth + 1)
+        return res
 
     return dfs(nestedList, 1)
 
@@ -32,7 +31,7 @@ def depthSum_v2(nestedList):
     Space complexity: O(N)
     """
     sum, stack = 0, []
-    stack = [(lst, 1) for lst in nestedList]
+    stack = [(nested_integer, 1) for nested_integer in nestedList]
     while stack:
         elem, depth = stack.pop()
         if elem.isInteger():
