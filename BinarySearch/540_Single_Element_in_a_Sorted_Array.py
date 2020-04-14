be939b205dc845c39317250f90f0116ca29f6e70
Anouer Hermassi <ahermass@stevens.edu>
2020-04-14 10:57:47 -0400

    Finding the element that appears twice in a sorted array of pairs - v2

--- BinarySearch/540_Single_Element_in_a_Sorted_Array.py
+++ BinarySearch/540_Single_Element_in_a_Sorted_Array.py
@@ -46,12 +46,48 @@ def single_non_duplicate_v1(nums):
     return nums[left]
 
 
+def single_non_duplicate_v2(nums):
+    """ The single element is at the first even index not followed by its pair. After the single element, the pattern
+        changes to being odd indices followed by their pair. This means that the single element (an even index) and all
+        elements after it are even indices not followed by their pair. Therefore, given any even index in the array,
+        we can easily determine whether the single element is to the left or to the right.
+        We need to make sure our 'mid' index is even. We can do this by dividing 'left' and 'right' in the usual way,
+        but then decrementing it by 1 if it is odd.
+        Then we check whether or not the 'mid' index is the same as the one after it.
+        If it is, then we know that 'mid' is not the single element, and that the single element must be at an even
+        index after 'mid'. Therefore, we set left = mid + 2.
+        If it is not, then we know that the single element is at some index before 'mid'. Therefore, we set
+        right = mid - 1
+    Time complexity: O(logN)
+    Space complexity: O(1)
+    """
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if mid % 2 == 1:  # We want the first element of the middle pair, which should be at an even index if the left
+            # part is sorted.
+            # Example: Index: 0 1 2 3 4 5 6
+            #          Array: 1 1 3 3 4 8 8
+            #                     ^
+            mid -= 1
+        if nums[mid] == nums[mid + 1]:  # We found a pair. The single element must be on the right.
+            # Example: |1 1 3 3 5 6 6|
+            # Next:     1 1 3 3|5 6 6|
+            left = mid + 2
+        else:  # We didn't find a pair. The single element must be on the left.
+            # Example: |0 1 1 3 3 6 6|
+            # Next:    |0 1 1|3 3 6 6
+            right = mid - 1
+    return nums[left]
+
+
 class Test(unittest.TestCase):
     data = [([1, 1, 2, 3, 3, 4, 4, 8, 8], 2), ([3, 3, 7, 7, 10, 11, 11], 10)]
 
     def test_single_non_duplicate(self):
         for test_nums, result in self.data:
             self.assertEqual(result, single_non_duplicate_v1(test_nums))
+            self.assertEqual(result, single_non_duplicate_v2(test_nums))
 
 
 if __name__ == '__main__':
