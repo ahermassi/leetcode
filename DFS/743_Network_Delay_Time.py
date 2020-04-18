e6e46a40702cafcb3a6a53da4cd10682e83eb880
Anouer Hermassi <ahermass@stevens.edu>
2020-04-18 10:09:09 -0400

    Network delay time - v1 update

--- DFS/743_Network_Delay_Time.py
+++ DFS/743_Network_Delay_Time.py
@@ -13,25 +13,30 @@ def network_delay_time_v1(times, N, K):
     """ DFS. Let's record the time time[node] when the signal reaches the node. If some signal arrived earlier, we
         don't need to broadcast it anymore. Otherwise, we should broadcast the signal.
         We'll maintain time[node], the earliest that we arrived at each node. When visiting a node while t time has
-        elapsed, if this is the currently-fastest signal at this node, let's broadcast signals from this node.
+        elapsed, if this is the currently fastest signal at this node, let's broadcast signals from this node.
         To speed things up, at each visited node we'll consider signals exiting the node that are faster first, by
-        sorting the edges.
-    Time complexity: O(N ** N + ElogE), where E is the length of times. We can only fully visit each node up to N-1
+        sorting the edges. Sorting is effective because we are likely to be able to reduce the number of the
+        unnecessary calls to dfs(). In general, it is more likely to get the minimum time to reach one node if we start
+        from the lowest cost. Keep in mind that every time we find a better (faster) path to reach the node, we need to
+        call dfs() again from the node.
+        We visit each node at some time, and if that time is better than the fastest time we've reached this node, we
+        travel along outgoing edges in sorted order.
+    Time complexity: O(N^N + E * logE), where E is the length of times. We can only fully visit each node up to N-1
     times, one per each other node. Plus, we have to explore every edge and sort them.
     Space complexity: O(N + E), the size of the graph (O(E), plus the size of the implicit call stack in DFS (O(N))
     """
 
-    def dfs(node, t):
-        if t >= time[node]:  # We've arrived at i earlier than this in the past
+    def dfs(node, cur_time):
+        if time[node] <= cur_time:  # We arrived at this node earlier than this in the past
             return
-        time[node] = t
-        for u, v in sorted(graph[node]):
-            dfs(v, t + u)
+        time[node] = cur_time
+        for t, neighbor in sorted(graph[node]):
+            dfs(neighbor, cur_time + t)
 
     graph = defaultdict(list)
-    for u, v, w in times:
-        graph[u].append((w, v))  # Note that the edge is (time, destination) so we can sort on times later on
-    time = {i: float('inf') for i in range(1, N + 1)}  # time[i] == the earliest (least amount of time) we've reached i
+    for source, destination, time in times:
+        graph[source].append((time, destination))  # The edge is (time, destination) so we can sort on time later on
+    time = {i: float('inf') for i in range(1, N + 1)}  # time[i] = the earliest (least amount of time) we've reached i
     dfs(K, 0)
     return max(time.values()) if max(time.values()) != float('inf') else -1
 
