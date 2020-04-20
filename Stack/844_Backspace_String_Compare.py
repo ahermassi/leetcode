5cd3737f1f6435fa88859dbe94f59a3254cfa923
Anouer Hermassi <ahermass@stevens.edu>
2020-04-20 10:02:07 -0400

    Backspace string compare - removed unused import

--- Stack/844_Backspace_String_Compare.py
+++ Stack/844_Backspace_String_Compare.py
@@ -1,7 +1,6 @@
 """ Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a
 backspace character. """
 
-import itertools
 import unittest2 as unittest
 
 
