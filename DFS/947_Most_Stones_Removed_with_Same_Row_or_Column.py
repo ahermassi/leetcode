9852c0c6708917a99d09f9f367b3f7d2e9c07a68
Anouer Hermassi <ahermass@stevens.edu>
2020-04-21 17:40:31 -0400

    Most stones removed with same row or columns - update

--- DFS/947_Most_Stones_Removed_with_Same_Row_or_Column.py
+++ DFS/947_Most_Stones_Removed_with_Same_Row_or_Column.py
@@ -7,9 +7,10 @@ import unittest2 as unittest
 
 
 def remove_stones(stones):
-    """ Connected stones can be reduced to 1 stone, and hence the maximum number of stones that can be removed is:
-        stones number - islands number.
-        So just count the number of 'islands'.
+    """ Connected stones, or island, can be reduced to 1 "representative" stone that can't be matched to any other
+        neighbor. Hence, every island / connected component's "representative" is one of the stones that can't be
+        removed, so the maximum number of stones that can be removed is:
+            number of total stones - number of islands
         The whole problem is transformed to: What is the number of islands? You can show all your skills on a DFS
         implementation, and solve this problem as a normal one.
         The key point here is, we define an island as number of points that are connected by row or column. Every point
@@ -19,25 +20,23 @@ def remove_stones(stones):
     """
 
     def dfs(i, j):
-        # With each (i, j) pair in hand, we start by verifying if the pair still exists (hasn't been removed by another
-        # call to dfs). If it exists, we discard it from points set and proceed to examine all the points that share
+        # With each (i, j) pair in hand, we discard it from points set and proceed to examine all the points that share
         # the same row or column with current pair. What dfs basically does is remove all points that share a row or
-        # a column and thus forming an island, to finally sink the island.
-        if (i, j) in points:
-            points.discard((i, j))
-            for x in cols_that_share_this_row[i]:
-                if (i, x) in points:
-                    dfs(i, x)
-            for y in rows_that_share_this_col[j]:
-                if (y, j) in points:
-                    dfs(y, j)
+        # a column, thus forming an island, to finally sink the island and leave us with its single representative stone
+        points.remove((i, j))
+        for col in cols_share_this_row[i]:
+            if (i, col) in points:
+                dfs(i, col)
+        for row in rows_share_this_col[j]:
+            if (row, j) in points:
+                dfs(row, j)
 
     points = {(i, j) for i, j in stones}  # points set is used instead of the good old visited set
-    cols_that_share_this_row, rows_that_share_this_col = defaultdict(list), defaultdict(list)
+    cols_share_this_row, rows_share_this_col = defaultdict(list), defaultdict(list)
     island = 0
     for i, j in stones:
-        cols_that_share_this_row[i].append(j)  # column coordinates of points that share the same row with i
-        rows_that_share_this_col[j].append(i)  # row coordinates of points that share the same column with j
+        cols_share_this_row[i].append(j)  # column coordinates of points that share the same row with i
+        rows_share_this_col[j].append(i)  # row coordinates of points that share the same column with j
     for i, j in stones:
         if (i, j) in points:
             dfs(i, j)
