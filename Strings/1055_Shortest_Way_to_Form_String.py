233ffc91cdc14087866248ea8db0fada94ff47e0
Anouer Hermassi <ahermass@stevens.edu>
2020-04-24 12:31:46 -0400

    Shortest way to form string - v3 update

--- Strings/1055_Shortest_Way_to_Form_String.py
+++ Strings/1055_Shortest_Way_to_Form_String.py
@@ -3,7 +3,7 @@ deletions).
 Given two strings source and target, return the minimum number of sub sequences of source such that their concatenation
 equals target. If the task is impossible, return -1. """
 
-import bisect
+from bisect import bisect_left
 from collections import defaultdict
 import unittest2 as unittest
 
@@ -61,49 +61,49 @@ def shortest_way_v3(source, target):
     """ The idea is to create an inverted index that saves the offsets of where each character occurs in source. The
         index data structure is represented as a hash map, where the key is the character, and the value is the
         (sorted) list of offsets where this character appears. To run the algorithm, for each character in target, use
-        the index to get the list of possible offsets for this character. Iterate over target, searching for the next
-        index in source of each char. We can use binary search to efficiently search for the next index. If the next
-        index in source requires wrapping around to the start of source, increment result.
-        count.
+        the hash map to get the list of possible indices for this character. Then search this list for next index which
+        appears after the offset of the previous character. We can use binary search to efficiently search for the next
+        index in our map. If the next index in source requires wrapping around to the start of source, increment the
+        result count.
         Example with source = 'abcab', target = 'aabbaac'
         The inverted index data structure for this example would be:
             indices = {
-                a: [0, 3] # 'a' appears at index 0, 3 in source
-                b: [1, 4], # 'b' appears at index 1, 4 in source
+                a: [0, 3] # 'a' appears at indices 0, 3 in source
+                b: [1, 4], # 'b' appears at indices 1, 4 in source
                 c: [2], # 'c' appears at index 2 in source
             }
-        Initialize i = 0 (i represents the smallest valid next index) and res = 1 (number of passes through source).
-        Iterate through the target string 'aabbaac'
-        a => get the indices of character 'a' which is [0, 3]. Set i to 1.
-        a => get the indices of character 'a' which is [0, 3]. Set i to 4.
-        b => get the indices of character 'b' which is [1, 4]. Set i to 5.
-        b => get the indices of character 'b' which is [1, 4]. Increment res to 2, and Set i to 2.
-        a => get the indices of character 'a' which is [0, 3]. Set i to 4.
-        a => get the indices of character 'a' which is [0, 3]. Increment res to 3, and Set i to 1.
-        c => get the indices of character 'c' which is [2]. Set i to 3.
-        We're done iterating through target so return the number of loops (3).
-    Time complexity: O(N + M*logN), where N is the length of source and M is the length of target, O(N) to build the
-    index, and O(logN) for each query. There are M queries, so the total runtime is O(N + M*logN)
+        Initialize index = 0 (smallest valid next index), res = 0 (number of passes through source).
+        Iterate through the target string 'aabbaac':
+        a => get the indices of character 'a' which are [0, 3]. Set index to 1.
+        a => get the indices of character 'a' which are [0, 3]. Set index to 4.
+        b => get the indices of character 'b' which are [1, 4]. Set index to 5.
+        b => get the indices of character 'b' which are [1, 4]. Increment res to 1, and set index to 2.
+        a => get the indices of character 'a' which are [0, 3]. Set index to 4.
+        a => get the indices of character 'a' which are [0, 3]. Increment res to 2, and set index to 1.
+        c => get the indices of character 'c' which is [2]. Set index to 3.
+        We're done iterating through target so return the number of loops (res + 1 = 3).
+    Time complexity: O(N + M * logN), where N is the length of source and M is the length of target, O(N) to build the
+    index, and O(logN) for each query. There are M queries, so the total runtime is O(N + M * logN)
     Space complexity: O(N), which is the space needed to store the index
     """
     indices = defaultdict(list)
     for i, c in enumerate(source):
         indices[c].append(i)
-    res = 1
-    index = 0  # Next index of source to check
+    res = 0
+    index = 0  # Next index in source to check looking for the current character in target
     for c in target:
         if c not in indices:  # Cannot make target if char not in source
             return -1
-        char_indices = indices[c]
-        # bisect_left(A, x) returns the smallest index j s.t. A[j] >= x. If no such index j exists, it returns len(A)
-        j = bisect.bisect_left(char_indices, index)
+        char_indices = indices[c]  # Where in source string does the current target character occur ?
+        j = bisect_left(char_indices, index)  # bisect_left(A, x) returns the smallest index j such that A[j] >= x.
+        # If no such index j exists, it returns len(A)
         if j == len(char_indices):  # This means that current index is past the index of last element in char_indices.
-            # In other words, to select this character we need to wrap around to beginning of source.
+            # In other words, to match this character in source we need to wrap around to beginning of source.
             res += 1
-            index = char_indices[0] + 1  # Next index in source to check
+            index = char_indices[0] + 1  # Next index in source to check looking for the current character in target
         else:
-            index = char_indices[j] + 1  # Next index in source to check
-    return res
+            index = char_indices[j] + 1
+    return res + 1
 
 
 # Following solution is not very intuitive
