81d13a45fb06e4d81d17e4099ee13a3b29ddf64c
Anouer Hermassi <ahermass@stevens.edu>
2020-05-07 11:27:59 -0400

    Finding the maximum number of enemies that a bomb can kill - v2

--- DynamicProgramming/361_Bomb_Enemy.py
+++ DynamicProgramming/361_Bomb_Enemy.py
@@ -44,12 +44,49 @@ def max_killed_enemies_v1(grid):
     return res
 
 
+def max_killed_enemies_v2(grid):
+    """ Walk through the matrix. At the START of each non-wall-streak (row-wise or column-wise), count the number of
+        hits in that streak and remember it. As we are traversing along the row, the enemies killed between any walls,
+        denoted by 'row_hits', is same, but that's not the case with columns because as we traverse a particular row
+        our columns keep on changing. This is the reason we need to track the number of enemies killed in each column
+        using an array 'col_hits'. Even for columns, the enemies killed between walls is same, so we need to
+        recalculate only if the previous column was a wall. So for each row, we visit each column once. Once we visit
+        second row (and other rows too), we don't want to recompute for columns unless the above column is a wall.
+        It is a DP-like solution, where 'col_hits' is storing column enemies count and is only updated once for
+        consecutive enemies column and reused for later calculation.
+    Time complexity: O(N * M)
+    Space complexity: O(M)
+    """
+    if not grid:
+        return 0
+    n, m, res = len(grid), len(grid[0]), 0
+    row_hits, col_hits = 0, [0] * m
+    for i in range(n):
+        for j in range(m):
+            if j == 0 or grid[i][j - 1] == 'W':
+                row_hits = 0
+                k = j
+                while k < m and grid[i][k] != 'W':
+                    row_hits += grid[i][k] == 'E'
+                    k += 1
+            if i == 0 or grid[i - 1][j] == 'W':
+                col_hits[j] = 0
+                k = i
+                while k < n and grid[k][j] != 'W':
+                    col_hits[j] += grid[k][j] == 'E'
+                    k += 1
+            if grid[i][j] == '0':
+                res = max(res, row_hits + col_hits[j])
+    return res
+
+
 class Test(unittest.TestCase):
     data = [([['0', 'E', '0', '0'], ['E', '0', 'W', 'E'], ['0', 'E', '0', '0']], 3)]
 
-    def test_increasing_triplet(self):
+    def test_max_killed_enemies(self):
         for test_grid, result in self.data:
             self.assertEqual(result, max_killed_enemies_v1(test_grid))
+            self.assertEqual(result, max_killed_enemies_v2(test_grid))
 
 
 if __name__ == '__main__':
