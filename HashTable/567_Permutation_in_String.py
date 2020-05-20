66b14c6bc814bcac6a2469bbcbca103248a485bf
Anouer Hermassi <ahermass@stevens.edu>
2020-05-20 11:40:13 -0400

    Checking if a string contains a permutation of another string - v2

--- HashTable/567_Permutation_in_String.py
+++ HashTable/567_Permutation_in_String.py
@@ -1,7 +1,7 @@
 """ Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words,
 one of the first string's permutations is the substring of the second string. """
 
-from collections import Counter
+from collections import Counter, defaultdict
 import unittest2 as unittest
 
 
@@ -28,12 +28,39 @@ def check_inclusion_v1(s1, s2):
     return False
 
 
+def check_inclusion_v2(s1, s2):
+    """ Instead of generating the hash map afresh for every window considered in s2, we just need to maintain a sliding
+        window with length of s1, move from beginning to the end of s2. When a character moves in from right of the
+        window, we subtract 1 from that character count from the map. When a character moves out from left of the
+        window, we add 1 to that character count. We can maintain the window by deleting the value of s2[i - len(s1)]
+        when it gets larger than len(s1). After, we only need to check if the two frequency maps are equal.
+    Time complexity: O(N + N * M) ~= O(N * M), where N is the length of string s1 and M is the length of string s2.
+    We could argue that comparing the frequency maps is O(1) since they contain at most 26 key-value pairs, which
+    results in an O(N + M) time complexity
+    Space complexity: O(1)
+    """
+    if len(s1) > len(s2):
+        return False
+    n, m = len(s1), len(s2)
+    chars, window = Counter(s1), defaultdict(int)
+    for i in range(m):
+        window[s2[i]] += 1
+        if i >= n:
+            window[s2[i - n]] -= 1
+            if window[s2[i - n]] == 0:
+                del window[s2[i - n]]
+        if window == chars:
+            return True
+    return False
+
+
 class Test(unittest.TestCase):
     data = [('ab', 'eidbaooo', True), ('ab', 'eidboaoo', False)]
 
     def test_check_inclusion(self):
         for test_s1, test_s2, result in self.data:
             self.assertEqual(result, check_inclusion_v1(test_s1, test_s2))
+            self.assertEqual(result, check_inclusion_v2(test_s1, test_s2))
 
 
 if __name__ == '__main__':
