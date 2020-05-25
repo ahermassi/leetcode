ead89a242b848a52b7909303baeaad9970cb4f78
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-05-25 11:24:35 -0400

    Finding the maximum length of a subarray that sums to k

---
+++ HashTable/325_Maximum_Size_Subarray_Sum_Equals_k.py
@@ -0,0 +1,48 @@
+""" Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one,
+return 0 instead. """
+
+import unittest2 as unittest
+
+
+def max_sub_array_len(nums, k):
+    """ Similarly to 560- Subarray Sum Equals K, we use a hash map 'prefix_sum' to store the sum of all elements before
+        index i as key, and i as value. For each i, we check not only the current sum but also (cur_sum - k) to see if
+        there is a previous sum such that cur_sum - previous_sum = k.
+        The idea is that if there is a number in the hash map where (cur_sum - k) equals to a number already in the
+        map, there must be a contiguous section from that point to current index i where the sum of all items is k.
+        The distance between these two points is the length of the sub array and is a candidate for our answer.
+        Let's say we've iterated to index 5 (randomly chosen) and our sum from index 0 to 5 so far is 7, and k is 3.
+        (cur_sum - k) in this case is 4. What prefix_sum[cur_sum - k] returns is the index where the sum of every
+        element up to that index from index 0 is (cur_sum - k), or (7 - 4) == 3, in our example. Let's say that the
+        index returned is 2. So knowing that at index 2 the total sum is 4, and at index 5 the total sum is 7, that
+        means the elements between index 2 and index 5 incremented the total sum by 3, or k.
+        Note that we need to initialize the hash map to {0: -1}. This way, whenever cur_sum = 0, the value at
+        prefix_sum[0] remains -1 instead of being changed to the current index, i. We want to include consecutive
+        values in the sub array that total to 0 in order to get the longest sub array count that sums up to k.
+        Take the example nums = [1, -1, 5, -2 , 3] and k = 3. When we use prefix_sum = {0:-1} in the beginning, then
+        at i = 1, cur_sum = 0. When we reach i = 3, cur_sum = 3. cur_sum - k = 0 and i - prefix_sum[cur_sum - k] = 4
+        (which is the longest sub array length that totals k). If instead prefix_sum = {} in the beginning,
+        prefix_sum[0] gets updated to i = 1 and, at i = 3, i - prefix_sum[cur_sum - k] = 2.
+    Time complexity: O(N)
+    Space complexity: O(N)
+    """
+    prefix_sum, cur_sum, res = {0: -1}, 0, 0
+    for i, num in enumerate(nums):
+        cur_sum += num
+        if cur_sum - k in prefix_sum:
+            res = max(res, i - prefix_sum[cur_sum - k])
+        if cur_sum not in prefix_sum:
+            prefix_sum[cur_sum] = i
+    return res
+
+
+class Test(unittest.TestCase):
+    data = [([1, -1, 5, -2, 3], 3, 4), ([-2, -1, 2, 1], 1, 2)]
+
+    def test_max_sub_array_len(self):
+        for test_nums, test_k, result in self.data:
+            self.assertEqual(result, max_sub_array_len(test_nums, test_k))
+
+
+if __name__ == '__main__':
+    unittest.main()
