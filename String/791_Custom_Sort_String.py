2746ab6a055dfd4f82c504e3e0f5c9b6d309b8bf
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-05-26 11:20:15 -0400

    Sorting a string according to characters of another string - v2

--- String/791_Custom_Sort_String.py
+++ String/791_Custom_Sort_String.py
@@ -3,7 +3,7 @@ S was sorted in some custom order previously. We want to permute the characters 
 S was sorted. More specifically, if x occurs before y in S, then x should occur before y in the returned string.
 Return any permutation of T (as a string) that satisfies this property. """
 
-import unittest2 as unittest
+from collections import Counter
 
 
 def custom_sort_string_v1(S, T):
@@ -24,4 +24,24 @@ def custom_sort_string_v1(S, T):
         else:
             bucket[indices[c]].append(c)
     res = [''.join(chars) for chars in bucket]
+    return ''.join(res)
+
+
+def custom_sort_string_v2(S, T):
+    """ Let's first write to our answer the elements of T that occur in S, in order of S. After, we'll write any
+        elements of T we didn't write. This obviously keeps all the ordering relationships we wanted.
+        In the second write, the order doesn't matter because those elements aren't in S, so there are no ordering
+        relationships these elements have to satisfy.
+        The trick is to count the frequency of characters of T. After we have some counter[char] = (the number of
+        occurrences of char in T), we can write these elements in the order we want.
+        The order is S + (characters not in S in any order).
+    Time complexity: O(N + M)
+    Space complexity: O(M)
+    """
+    counter, res = Counter(T), []
+    for c in S:  # Write all T characters that occur in S, in the order of S
+        res.append(c * counter[c])
+        del counter[c]  # Denotes that we do not need to write 'c' to our answer anymore
+    for c in counter:  # Write all remaining characters that don't occur in S and are left in the hash map
+        res.append(c * counter[c])
     return ''.join(res)
