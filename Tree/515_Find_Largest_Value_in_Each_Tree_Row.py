7fbfe968c8f6064367954eede5f70de0d12e97e1
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-06-02 11:14:38 -0400

    Finding largest value in each row of a binary tree - v2

--- Tree/515_Find_Largest_Value_in_Each_Tree_Row.py
+++ Tree/515_Find_Largest_Value_in_Each_Tree_Row.py
@@ -20,3 +20,25 @@ def largest_values_v1(root):
         res.append(max_value)
     return res
 
+
+def largest_values_v2(root):
+    """ DFS. Do a pre-order traversal and use depth to expand result list size and put the max value in the appropriate
+        position.
+    Time complexity: O(N)
+    Space complexity: O(N)
+    """
+
+    def dfs(root, depth):
+        if not root:
+            return
+        if depth == len(res):
+            res.append(root.val)
+        else:
+            res[depth] = max(res[depth], root.val)
+        dfs(root.left, depth + 1)
+        dfs(root.right, depth + 1)
+
+    res = []
+    dfs(root, 0)
+    return res
+
