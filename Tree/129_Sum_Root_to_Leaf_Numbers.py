d1cb353da12a3e89f88e45b8c831202da91f3c3e
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-06-04 10:47:26 -0400

    Finding the total sum of all root-to-leaf paths - v1

---
+++ Tree/129_Sum_Root_to_Leaf_Numbers.py
@@ -0,0 +1,26 @@
+""" Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+Find the total sum of all root-to-leaf numbers. """
+
+
+def sum_numbers_v1(root):
+    """ Recursive pre-order traversal is extremely simple: follow Root->Left->Right direction, i.e. do all the business
+        with the node (update the current total and root-to-leaf sum), and then do the recursive calls for the left and
+        right child nodes.
+    Time complexity: O(N)
+    Space complexity: O(h), to keep the recursion stack, where h is tree height
+    """
+
+    def dfs(root, total):
+        if not root:
+            return
+        total = total * 10 + root.val
+        if not root.left and not root.right:
+            res[0] += total
+        else:
+            dfs(root.left, total)
+            dfs(root.right, total)
+
+    res = [0]
+    dfs(root, 0)
+    return res[0]
