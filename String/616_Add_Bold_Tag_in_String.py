96fc56117dd0ebdce4ac08fb178bbe88efbffa8d
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-06-12 12:01:34 -0400

    Adding pairs of bold tags in a string - v2

--- String/616_Add_Bold_Tag_in_String.py
+++ String/616_Add_Bold_Tag_in_String.py
@@ -35,6 +35,39 @@ def add_bold_tag_v1(s, dict):
     return ''.join(res)
 
 
+def add_bold_tag_v2(s, dict):
+    """ We can create a list of intervals with opening/closing positions, e.g. [open_tag_index, close_tag_index].
+        After that, we merge the list of intervals similar to 56- Merge Intervals. Finally, we go over the merged
+        intervals list and insert the tags into the string in the appropriate positions.
+        Example: s = 'aaabbcc', dict = ['aaa, 'aab', 'bc']
+        We find the start/end index of each string in dict and convert to interval to get:
+        [[0, 3], [1, 4], [4, 6]]
+          aaa     aab      bc
+        We merge these intervals: [0,6], so we know 'aaabbc' needs to be surrounded by bold tag.
+    Time complexity:
+    Space complexity:
+    """
+    n, intervals, merged, res = len(s), [], [], []
+    for i in range(n):
+        for word in dict:
+            if s[i:].startswith(word):
+                intervals.append([i, min(n - 1, i + len(word) - 1)])
+    if not intervals:
+        return s
+    for start, end in intervals:
+        if not merged or start > merged[-1][1] + 1:  # + 1 to account for cases [x, y], [y+1, z] -> [x, z]
+            merged.append([start, end])
+        else:
+            merged[-1][1] = max(merged[-1][1], end)
+    previous_index = -1
+    for i, (start, end) in enumerate(merged):
+        res.append(s[previous_index + 1:start])
+        res.append('<b>' + s[start:end + 1] + '</b>')
+        previous_index = end
+    res.append(s[merged[-1][1] + 1:])
+    return ''.join(res)
+
+
 class Test(unittest.TestCase):
     data = [('abcxyz123', ['abc', '123'], '<b>abc</b>xyz<b>123</b>'),
             ('aaabbcc', ['aaa', 'aab', 'bc'], '<b>aaabbc</b>c')]
@@ -42,6 +75,7 @@ class Test(unittest.TestCase):
     def test_add_bold_tag(self):
         for test_s, test_dict, result in self.data:
             self.assertEqual(result, add_bold_tag_v1(test_s, test_dict))
+            self.assertEqual(result, add_bold_tag_v2(test_s, test_dict))
 
 
 if __name__ == '__main__':
