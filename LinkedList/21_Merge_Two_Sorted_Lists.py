c4336f73cdcec958211d9c4edcd5f842077127c6
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-06-12 16:26:54 -0400

    Merge two sorted lists - update

--- LinkedList/21_Merge_Two_Sorted_Lists.py
+++ LinkedList/21_Merge_Two_Sorted_Lists.py
@@ -16,32 +16,13 @@ class ListNode:
 
 
 def merge_two_lists_v1(l1, l2):
-    """ We can recursively define the result of a merge operation on two lists as the following:
-        list1[0] + merge(list1[1:], list2)  if  list1[0] <= list2[0]
-        list2[0] + merge(list1, list2[1:])  otherwise
-        Namely, the smaller of the two lists' heads plus the result of a merge on the rest of the elements.
-    Time complexity: O(N + M), there will be exactly one call to merge_two_lists per element in each list. Therefore,
-    the time complexity is linear in the combined size of the lists.
-    Space complexity: O(N + M), The first call to merge_two_lists does not return until the ends of both l1 and l2 have
-    been reached, so N + M stack frames consume O(N + M) space. (draw the stack frames for a clearer overview)
-    """
-    if not l1 or not l2:
-        return l1 or l2
-    if l1.val <= l2.val:
-        l1.next = merge_two_lists_v1(l1.next, l2)  # This is the case list1[0] + merge(list1[1:], list2)
-        return l1
-    l2.next = merge_two_lists_v1(l1, l2.next)  # This is the case list2[0] + merge(list1, list2[1:])
-    return l2
-
-
-def merge_two_lists_v2(l1, l2):
-    """ We have a dummy head, and a tail indicating last node in this growing List. At each step we append either l1
+    """ We have a dummy head, and a tail indicating last node in this growing List. At each step, we append either l1
         or l2 to tail, and update tail, l1 or l2 accordingly. After the loop terminates, at most one of l1 and l2 is
         non-null. Therefore (because the input lists were in SORTED order), if either list is non-null, it contains only
         elements greater than all of the previously-merged elements.
     Time complexity: O(N + M), the while loop runs for a number of iterations equal to sum of lengths of the two lists.
-    Space complexity: O(1), the iterative approach only allocates a few pointers, so it has a constant overall memory
-    footprint.
+    Space complexity: O(1), the iterative approach only allocates a few pointers so it has a constant overall memory
+    footprint
     """
     head = tail = ListNode(0)  # Dummy head and tail, starting at same position
     while l1 and l2:
@@ -54,8 +35,27 @@ def merge_two_lists_v2(l1, l2):
         tail = tail.next
     tail.next = l1 or l2  # If we exhausted either list, we just append the other list to the end of the merged list
     # since the list still standing will have all elements greater than the last item in the merged list so far (and
-    # be in sorted order)
+    # is in sorted order)
     return head.next
+
+
+def merge_two_lists_v2(l1, l2):
+    """ We can recursively define the result of a merge operation on two lists as the following:
+            list1[0] + merge(list1[1:], list2)  if  list1[0] <= list2[0]
+            list2[0] + merge(list1, list2[1:])  otherwise
+        Namely, the smaller of the two lists' heads plus the result of a merge on the rest of the elements.
+    Time complexity: O(N + M), there will be exactly one call to merge_two_lists per element in each list. Therefore,
+    the time complexity is linear in the combined size of the lists.
+    Space complexity: O(N + M), The first call to merge_two_lists does not return until the ends of both l1 and l2 have
+    been reached, so N + M stack frames consume O(N + M) space
+    """
+    if not l1 or not l2:
+        return l1 or l2
+    if l1.val <= l2.val:
+        l1.next = merge_two_lists_v1(l1.next, l2)  # This is the case list1[0] + merge(list1[1:], list2)
+        return l1
+    l2.next = merge_two_lists_v1(l1, l2.next)  # This is the case list2[0] + merge(list1, list2[1:])
+    return l2
 
 
 class Test(unittest.TestCase):
