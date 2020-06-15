6e58f15552885cfe06745729a61418fd67bca025
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-06-14 20:39:04 -0400

    Design Tic-Tac-Toe - update

--- Arrays/348_Design_Tic-Tac-Toe.py
+++ Arrays/348_Design_Tic-Tac-Toe.py
@@ -21,39 +21,21 @@ class TicTacToe(object):
     """
 
     def __init__(self, n):
-        """
-        Initialize your data structure here.
-        :type n: int
-        """
         self.rows = [0] * n
         self.cols = [0] * n
         self.size = n
-        self.diag1 = 0
-        self.diag2 = 0
+        self.diag = 0
+        self.anti_diagonal = 0
 
     def move(self, row, col, player):
-        """
-        Player {player} makes a move at ({row}, {col}).
-        @param row The row of the board.
-        @param col The column of the board.
-        @param player The player, can be either 1 or 2.
-        @return The current winning condition, can be either:
-                0: No one wins.
-                1: Player 1 wins.
-                2: Player 2 wins.
-        :type row: int
-        :type col: int
-        :type player: int
-        :rtype: int
-        """
-        val = 1 if player == 1 else -1
-        self.rows[row] += val
-        self.cols[col] += val
+        coef = 1 if player == 1 else -1
+        self.rows[row] += coef
+        self.cols[col] += coef
         if col == row:
-            self.diag1 += val
+            self.diag += coef
         if col + row == self.size - 1:
-            self.diag2 += val
-        if val * self.size in {self.rows[row], self.cols[col], self.diag1, self.diag2}:  # To account for both n and -n
+            self.anti_diagonal += coef
+        if coef * self.size in {self.rows[row], self.cols[col], self.diag, self.anti_diagonal}:  # Account for n and -n
             return player
         return 0
 
