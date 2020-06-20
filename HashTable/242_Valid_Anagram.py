9889a5258f074fdc8983c0e80ce4cb65365f4bc8
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-06-20 11:04:22 -0400

    Valid anagram - update

--- HashTable/242_Valid_Anagram.py
+++ HashTable/242_Valid_Anagram.py
@@ -1,26 +1,24 @@
 """ Given two strings s and t , write a function to determine if t is an anagram of s. """
 
-from collections import defaultdict
+from collections import Counter
 import unittest2 as unittest
 
 
 def is_anagram(s, t):
     """ To examine if t is a rearrangement of s, we can count occurrences of each letter in the two strings and
-    compare them. First increment the counter for s, then decrement the counter for t. If at any point the counter
-    drops below zero, we know that t contains an extra letter not in s and return false immediately.
-    Follow up: What if the inputs contain unicode characters? How would you adapt your solution to such case?
-    A hash table is a more generic solution and could adapt to any range of characters (which is what we did)
-    Time complexity: O(N) where N is the length of s (or t)
+        compare them. First increment the counter for s, then decrement the counter for t. If at any point the counter
+        drops below zero, we know that t contains an extra letter not in s and return false immediately.
+        Follow up: What if the inputs contain unicode characters? How would you adapt your solution to such case?
+        A hash table is a more generic solution and could adapt to any range of characters (which is what we did).
+    Time complexity: O(N), where N is the length of s (or t)
     Space complexity: O(1), the table's size stays constant no matter how large N is
     """
     if len(s) != len(t):
         return False
-    chars = defaultdict(int)
-    for c in s:
-        chars[c] += 1
+    counter = Counter(s)
     for c in t:
-        chars[c] -= 1
-        if chars[c] < 0:
+        counter[c] -= 1
+        if counter[c] < 0:
             return False
     return True
 
