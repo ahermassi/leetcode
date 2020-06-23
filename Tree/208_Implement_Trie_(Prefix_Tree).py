cb7205f4ef0fdcb25b3de7e9bb9ee2372a9fc7fe
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-06-23 11:15:20 -0400

    Implement trie (prefix tree) - update

--- Tree/208_Implement_Trie_(Prefix_Tree).py
+++ Tree/208_Implement_Trie_(Prefix_Tree).py
@@ -12,28 +12,20 @@ class TrieNode:
 class Trie(object):
     """ Do yourself a favor and checkout this article: https://leetcode.com/articles/implement-trie-prefix-tree/
     Time complexity:
-        insert(word): O(k) where k is the length of word
+        insert(word): O(k), where k is the length of word
         search(word): O(k)
         startsWith(prefix): O(k)
     Space complexity:
-        insert(word): O(k), in the worst case newly inserted key doesn't share a prefix with the the keys already
+        insert(word): O(k), in the worst case the newly inserted key doesn't share a prefix with the the keys already
         inserted in the trie. We have to add k new nodes
         search(word): O(1)
         startsWith(prefix): O(1)
     """
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.root = TrieNode()
 
     def insert(self, word):
-        """
-        Inserts a word into the trie.
-        :type word: str
-        :rtype: None
-        """
         root = self.root
         for c in word:
             if c not in root.children:
@@ -43,11 +35,6 @@ class Trie(object):
         root.end_of_word = True
 
     def search(self, word):
-        """
-        Returns if the word is in the trie.
-        :type word: str
-        :rtype: bool
-        """
         root = self.root
         for c in word:
             if c not in root.children:
@@ -56,11 +43,7 @@ class Trie(object):
         return root.end_of_word
 
     def startsWith(self, prefix):
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        :type prefix: str
-        :rtype: bool
-        """
+        """ Returns if there is any word in the trie that starts with the given prefix. """
         root = self.root
         for c in prefix:
             if c not in root.children:
