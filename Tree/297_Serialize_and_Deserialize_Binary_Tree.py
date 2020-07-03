2d64d3cbc76bc2bbdb8ebbc037bda307225090a2
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-07-03 10:42:35 -0400

    Serialize and deserialize a binary tree - v1

---
+++ Tree/297_Serialize_and_Deserialize_Binary_Tree.py
@@ -0,0 +1,63 @@
+""" Serialization is the process of converting a data structure or object into a sequence of bits so that it can be
+stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the
+same or another computer environment.
+Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your
+serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a
+string and this string can be deserialized to the original tree structure. """
+
+
+from collections import deque
+
+# Definition for a binary tree node.
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# Great explanation: https://www.youtube.com/watch?v=suj1ro8TIVY
+
+
+class CodecV1:
+    """ The idea is simple: print the tree in pre-order traversal and use 'X' to denote null node and separate nodes
+        with ','. For deserialization, we use a queue to store the pre-order traversal, and since we have 'X' as null
+        node, we know exactly where to end building subtrees.
+        The intuition for the deserialization is the recognition that the first node in the pre-order sequence is the
+        root, and the sequence for the root's left subtree appears before all the nodes in the root's right subtree.
+        It is not easy to see where the left subtree sequence ends. However, if we solve the problem recursively, we
+        can assume that the routine correctly computes the left subtree, which will also tell us where the right
+        subtree begins.
+    """
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        Time complexity: O(N), we visit each node exactly once
+        Space complexity: O(N), we keep the entire tree
+        """
+        if not root:
+            return 'X'
+        left = self.serialize(root.left)
+        right = self.serialize(root.right)
+        return ','.join([str(root.val), left, right])
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        Time complexity: O(N), we visit each node exactly once
+        Space complexity: O(N)
+        """
+
+        def buildTree(queue):
+            if not queue:
+                return None
+            value = queue.popleft()
+            if value == 'X':
+                return None
+            root = TreeNode(value)
+            root.left = buildTree(queue)
+            root.right = buildTree(queue)
+            return root
+
+        queue = deque(data.split(','))
+        return buildTree(queue)
