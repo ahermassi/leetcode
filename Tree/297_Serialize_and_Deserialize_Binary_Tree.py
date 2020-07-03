4908037e91fa5968e563f565b630fb8fbe18b228
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-07-03 10:57:25 -0400

    Serialize and deserialize a binary tree - v2

--- Tree/297_Serialize_and_Deserialize_Binary_Tree.py
+++ Tree/297_Serialize_and_Deserialize_Binary_Tree.py
@@ -61,3 +61,43 @@ class CodecV1:
 
         queue = deque(data.split(','))
         return buildTree(queue)
+
+
+class CodecV2:
+    """ We can also use a BFS traversal to serialize/deserialize the tree, similar to how Leetcode does it. """
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        Time complexity: O(N)
+        Space complexity: O(N)
+        """
+        if not root:
+            return 'X'
+        values, queue = [], deque([root])
+        while queue:
+            node = queue.popleft()
+            if not node:
+                values.append('X')
+            else:
+                values.append(str(node.val))
+                queue.extend([node.left, node.right])
+        return ','.join(values)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        Time complexity: O(N)
+        Space complexity: O(N)
+        """
+        if data[0] == 'X':
+            return None
+        data = deque(data.split(','))
+        root = TreeNode(data.popleft())
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            left, right = data.popleft(), data.popleft()
+            node.left = TreeNode(left) if left != 'X' else None
+            node.right = TreeNode(right) if right != 'X' else None
+            queue.extend([kid for kid in (node.left, node.right) if kid])
+        return root
+
