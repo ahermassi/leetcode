b4d42f87f20df2a11427d4ca9d41d901ca80ee2a
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-07-07 11:47:14 -0400

    Finding the longest increasing path of numbers in a matrix - v2

--- DFS/329_Longest_Increasing_Path_in_a_Matrix.py
+++ DFS/329_Longest_Increasing_Path_in_a_Matrix.py
@@ -2,6 +2,7 @@
 From each cell, you can either move to four directions: left, right, up or down. You may NOT move diagonally or move
 outside of the boundary (i.e. wrap-around is not allowed). """
 
+from collections import deque
 import unittest2 as unittest
 
 
@@ -45,12 +46,63 @@ def longest_increasing_path_v1(matrix):
     return res
 
 
+def longest_increasing_path_v2(matrix):
+    """ The result of each cell only related to the result of its neighbors. Can we use dynamic programming?
+        If we define the longest increasing path starting from cell (i, j) as a function f(i,j), then we have the
+        following transition function:
+            f(i,j) = 1 + max{f(x,y)∣(x,y) is a neighbor of (i,j) and matrix[x][y] > matrix[i][j]}
+        This formula is the same as used in the previous approaches. With such transition function, we may think that
+        it is possible to use dynamic programming to deduce all the results without employing DFS!
+        That is right with one thing missing: we don't have the dependency list.
+        For dynamic programming to work, if problem B depends on the result of problem A, then we must make sure that
+        problem A is calculated before problem B. Such order is natural and obvious for many problems. For example, the
+        famous Fibonacci sequence:
+            F(0) = 1, F(1) = 1, F(n) = F(n - 1) + F(n - 2)
+        The sub-problem F(n) depends on its two predecessors. Therefore, the natural order from 0 to n is the correct
+        order. The dependent is always behind the dependee.
+        The terminology of such dependency order is Topological order or Topological sorting:
+        The idea is that in a DAG, we will have some vertex that doesn't depend on others which we call 'leaves'.
+        We put these leaves in a list (their internal ordering does matter), and then we remove them from the DAG.
+        After the removal, there will be new leaves. We do the same repeatedly as if we are peeling an onion layer by
+        layer. In the end, the list will have a valid topological ordering of our vertices.
+        In our problem, we want the longest path in the DAG, which equals to the total number of layers of the 'onion'.
+        Thus, we can count the number of layers during 'peeling' and return the counts in the end.
+        The logic is similar to 210- Course Schedule II.
+    Time complexity: O(N * M), the the topological sort is O(V + E) = O(N * M). V is the total number of vertices and
+    E is the total number of edges. In our problem, O(V) = O(N * M), O(E) = O(4V) = O(N * M)
+    Space complexity: O(N * M), we need to store the out degrees and each level of leaves in the queue
+    """
+    if not matrix:
+        return 0
+    n, m = len(matrix), len(matrix[0])
+    outdegree = [[0] * m for _ in range(m)]  # outdegree[i][j] is the number of adjacent cells bigger than matrix[i][j]
+    for i in range(n):
+        for j in range(m):
+            for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+                if 0 <= x < n and 0 <= y < m and matrix[x][y] > matrix[i][j]:
+                    outdegree[i][j] += 1
+    queue = deque([(i, j) for i in range(n) for j in range(m) if outdegree[i][j] == 0])
+    length = 0
+    while queue:
+        n = len(queue)
+        for _ in range(n):
+            i, j = queue.popleft()
+            for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+                if 0 <= x < n and 0 <= y < m and matrix[x][y] < matrix[i][j]:
+                    outdegree[x][y] -= 1
+                    if outdegree[x][y] == 0:
+                        queue.append((x, y))
+        length += 1
+    return length
+
+
 class Test(unittest.TestCase):
     data = [([[9, 9, 4], [6, 6, 8], [2, 1, 1]], 4), ([[3, 4, 5], [3, 2, 6], [2, 2, 1]], 4)]
 
     def test_longest_increasing_path(self):
         for test_matrix, result in self.data:
             self.assertEqual(result, longest_increasing_path_v1(test_matrix))
+            self.assertEqual(result, longest_increasing_path_v2(test_matrix))
 
 
 if __name__ == '__main__':
