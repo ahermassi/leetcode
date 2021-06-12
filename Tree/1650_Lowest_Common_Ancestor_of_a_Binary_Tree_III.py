daa613cffede59aec46ddf0495c6f7fff0e0c5c8
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-06-12 09:28:34 -0600

    Find the LCA of two nodes with parent pointer - v2

--- Tree/1650_Lowest_Common_Ancestor_of_a_Binary_Tree_III.py
+++ Tree/1650_Lowest_Common_Ancestor_of_a_Binary_Tree_III.py
@@ -25,3 +25,27 @@ def lowest_common_ancestor_v1(p, q):
     while q not in ancestors_path:
         q = q.parent
     return q
+
+
+def lowest_common_ancestor_v2(p, q):
+    """ We can optimize the previous solution by not using extra space. The idea is fairly simple and similar to
+    finding the convergence point of 2 linked lists in 160_Intersection_of_Two_Linked_Lists. We keep two pointers,
+    pa and pb. Originally, these pointers point to q and p, respectively. Then we follow their parent pointers until
+    they point to the same node. When either of the pointers points to root, we set it to the other original starting
+    node. For example, when pa points to root (i.e pa.parent is None), assign pa to q.
+    The idea is if you switch head, the possible difference between lengths would be countered. On the second
+    traversal, they either hit or miss. If they didn't meet, they will hit the end at the same iteration,
+    pa == pb == None, return either one of them is the same, None.
+    This works because pointer pa walks through paths of p AND q (since once it hits null, it goes to q's path
+    head). Pointer pb also walks through paths of q AND p. Regardless of the length of the two paths, the sum of
+    the lengths are the same (i.e. a+b = b+a), which means that the pointers sync up at the point of intersection.
+    If the paths never intersected, it's fine too, because they'll sync up at the end of each path, both of which
+    are null.
+    Time complexity: O(h1 + h2), where h1 is the height of node p and h2 is the height of node q
+    Space complexity: O(1)
+    """
+    pa, pb = p, q
+    while pa != pb:
+        pa = pa.parent if pa else q
+        pb = pb.parent if pb else p
+    return pa
