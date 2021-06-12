9c3e59849fcd49ef5bb0f862b5e7b3eb578f35d7
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-06-12 12:30:37 -0600

    Quick select - update

--- Heap/973_K_Closest_Points_to_Origin.py
+++ Heap/973_K_Closest_Points_to_Origin.py
@@ -29,39 +29,38 @@ def k_closest_v1(points, K):
 def k_closest_v2(points, K):
     """ This solution is based on quick sort, we can also call it quick select. In the quick sort, we will always
         choose a pivot to compare with other elements.
-        Partition in quick select picks a pivot (either randomly or first/last element). Then it rearranges the list
-        in a way that all elements less than pivot are on left side of pivot and others on right. It then returns
+        Partition in quick select picks a pivot (either randomly or first/last element). Then it rearranges the array
+        in a way that all elements less than the pivot are on left side of pivot and others on right. It then returns
         index of the pivot element.
         Now here we are finding kth smallest element. After partition cases are:
             1- K == pivot, then we have already found kth smallest. This is because the way partition is working.
-                There are exactly (k - 1) elements that are smaller than the kth element.
+                There are exactly (k - 1) elements that are smaller than the kth smallest element.
             2- K < pivot, then kth smallest is on the left side of pivot.
             3- K > pivot, then kth smallest is on the right side of pivot. To find it, we actually have to find
-                k-pivot smallest number on right.
+                (k - pivot)th smallest number to the right.
     Time complexity: the average time complexity is O(N) , but just like quick sort, in the worst case, this solution
     would be degenerated to O(N^2)
     Space complexity: O(N) where N is the length of points
     """
 
     def partition(left, right):
-        random_index = randint(left, right)  # Select a random pivot index between left and right, so that even when
+        pivot_index = randint(left, right)  # Select a random pivot index between left and right, so that even when
         # the worst case input would be provided the algorithm wouldn't be affected
-        pivot = points[random_index]
-        points[random_index], points[right] = points[right], points[random_index]
+        pivot = points[pivot_index]
+        points[pivot_index], points[right] = points[right], points[pivot_index]
         pivot_distance = distance(pivot[0], pivot[1])
-        i, j = left - 1, left  # i will keep track of the 'tail' of the section of items less than the pivot so that
+        i = j = left  # i will keep track of the 'tail' of the section of items less than the pivot so that
         # at the end we can 'sandwich' the pivot between the section less than it and the section greater than it.
-        # j will scan for us
+        # j will scan for us. All the elements before i (excluding i) are less than or equal to the pivot
         while j < right:
             if distance(points[j][0], points[j][1]) <= pivot_distance:  # If this point's distance from origin is less
                 # than the pivot's distance, it needs to be moved to the section of items less than the pivot
-                i += 1  # Move i forward so that we can swap the value at j into the tail of the items less than the
-                # pivot
                 points[i], points[j] = points[j], points[i]  # Execute the swap
+                i += 1
             j += 1
-        points[i + 1], points[right] = points[right], points[i + 1]  # Swap the pivot value right after the section of
-        # items less than the pivot. i keeps the tail of this section
-        return i + 1  # Return the pivot's final resting position
+        points[i], points[right] = points[right], points[i]  # Bring the pivot back after the section of items less
+        # than the pivot. i keeps the tail of this section
+        return i  # Return the pivot's final resting position
 
     distance = lambda x, y: x * x + y * y
     left, right = 0, len(points) - 1
