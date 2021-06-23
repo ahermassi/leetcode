1a8cf7e9d4b36617eada801f0e761efde5484e3e
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-06-23 07:56:37 -0600

    Maximum swap - update

--- Arrays/670_Maximum_Swap.py
+++ Arrays/670_Maximum_Swap.py
@@ -4,10 +4,10 @@ maximum valued number you could get. """
 import unittest2 as unittest
 
 
-def maximum_swap(num):
+def maximum_swap_v1(num):
     """ At each digit of the input number in order, if there is a larger digit that occurs later, we know that the best
         swap must occur with the digit we are currently considering.
-        We first compute last_index[d] = i, the index i of the last occurrence of digit d (if it exists).
+        We first compute last_occurrence[d] = i, the index i of the last occurrence of digit d.
         Afterwards, when scanning the number from left to right, if there is a larger digit that occurs after the
         current index, we will swap it with the largest such digit. If there are multiple such digits, we will swap it
         with the one that occurs the latest.
@@ -16,15 +16,19 @@ def maximum_swap(num):
         Rightmost 8, because no matter which 8 is chosen, we gain the same at 4's position. However, if we choose the
         1st 8 (from the left), then we lose 400 for that position. If we choose the 2nd 8, then we only lose 4. So we
         always choose the rightmost one.
+        Thinking process:
+        To make num maximum, we need to find the most significant digit who has a higher value to its right. It makes
+        the direction to scan from left(most significant) to right(least significantly).
     Time complexity: O(N)
     Space complexity: O(N)
     """
     digits = [int(c) for c in str(num)]
-    last_index, n = {val: i for i, val in enumerate(digits)}, len(digits)
+    last_occurrence, n = {val: i for i, val in enumerate(digits)}, len(digits)
     for i in range(n - 1):
-        for val in reversed(range(digits[i] + 1, 10)):  # Start from 9 down to (current digit + 1)
-            if val in last_index and last_index[val] > i:  # This greater digit occurs at a later index
-                digits[i], digits[last_index[val]] = digits[last_index[val]], digits[i]
+        cur_digit = digits[i]
+        for val in reversed(range(cur_digit + 1, 10)):  # Start from 9 down to (current digit + 1)
+            if val in last_occurrence and last_occurrence[val] > i:  # This greater digit occurs at a later index
+                digits[i], digits[last_occurrence[val]] = digits[last_occurrence[val]], digits[i]
                 return int(''.join(map(str, digits)))
     return num
 
@@ -34,7 +38,7 @@ class Test(unittest.TestCase):
 
     def test_maximum_swap(self):
         for test_num, result in self.data:
-            self.assertEqual(result, maximum_swap(test_num))
+            self.assertEqual(result, maximum_swap_v1(test_num))
 
 
 if __name__ == '__main__':
