41eb28547111d8daad7aa36db8851c0fb4c95068
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-07-02 10:51:21 -0600

    Pow(x,n) - v2 update

--- BinarySearch/50_Pow(x, n).py
+++ BinarySearch/50_Pow(x, n).py
@@ -41,6 +41,19 @@ def my_pow_v1(x, n):
 
 def my_pow_v2(x, n):
     """ Iterative Fast Power.
+        We can use the binary representation of n to better understand the problem. Let the binary representation of n
+        to be b_1, b_2, ..., b_k, from the Least Significant Bit (LSB) to the Most Significant Bit(MSB). For the ith
+        bit, if b_i = 1, it means we need to multiply the result by x ^ {2 ^ i}.
+        Using the formula (x ^ n) ^ 2 = x ^ {2 * n}, initially x ^ 1 = x, and for each i > 1, we can use the result
+        of x ^ {2 ^ {i - 1}} to get x ^ {2 ^ i} in one step. After that, for every i that satisfies b_i = 1, we can
+        multiply x ^ {2 ^ i} to the result.
+        For example, let's say n = 9 = 2 ^ 3 + 2 ^ 0 = 1001 in binary. Then:
+        x ^ 9 = x ^ (2 ^ 3 + 2 ^ 0) = x ^ (2 ^ 3) * x ^ (2 ^ 0)
+        We can see that every time we encounter a 1 in the binary representation of n, we need to multiply the answer
+        with x ^ (2 ^ i), where i is the ith bit of the exponent. Thus, we can keep a running total of repeatedly
+        squaring x - (x, x ^ 2, x ^ 4, x ^ 8, etc) and multiply it by the answer when we see a 1 in the binary
+        representation of n. In other words, for each bit that is right shifted, x becomes square of itself. And for
+        each bit, the result is multiplied by x only if the bit is turned on.
         Example: x = 2, n = 10
                           res           = 1,    x = 2,   n = 10
         n % 2 == 1 ? No;  res           = 1,    x = 4,   n = 5
@@ -54,10 +67,15 @@ def my_pow_v2(x, n):
         x, n = 1 / x, -n
     res = 1
     while n:
-        if n % 2 == 1:
+        if n & 1 == 1:  # n & 1 gets the the value of the least significant bit of n.
+            # n = 100101, n & 1 = 1  ;  n = 100100, n & 1 = 0
             res *= x
         x *= x
-        n //= 2
+        n = n >> 1
+        # Shifting by n bits to the right is equivalent to dividing by 2^n. So shifting by 1 bit to the right
+        # is equivalent to dividing by 2.
+        # Shifting by k bits to the right brings the kth bit to the rightmost index (k is 0-based). So shifting by
+        # 1 bit to the right brings the bit at index 1 (next bit) to the rightmost index
     return res
 
 
@@ -67,6 +85,7 @@ class Test(unittest.TestCase):
     def test_my_pow(self):
         for test_x, test_n, result in self.data:
             self.assertEqual(result, my_pow_v1(test_x, test_n))
+            self.assertEqual(result, my_pow_v2(test_x, test_n))
 
 
 if __name__ == '__main__':
