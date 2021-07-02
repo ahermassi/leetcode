ba9b15f4b602612d6b0ad43df09fa58f41195eca
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-07-01 18:01:50 -0600

    Simplify path - update

--- Stack/71_Simplify_Path.py
+++ Stack/71_Simplify_Path.py
@@ -6,18 +6,28 @@ import unittest2 as unittest
 
 
 def simplify_path(path):
-    """ The main idea is to push to the stack every valid folder name (not in {'','.','..'}), popping only if there's
-        something to pop and we meet '..'
+    """ Suppose that to a path like '/a/b/c', we add another component like '/a/b/c/..' Now, this is interesting
+        because the '..' is no longer a sub-directory name. It has a special meaning and an indication to the operating
+        system to move up one level in the directory structure thus transforming the overall path to just '/a/b'. It's
+        as if we 'popped out' the subdirectory c from the overall path. That's the core idea of this problem.
+        Split the input string using '/' as the delimiter.
+        If the current component is a '.' or an empty string, we will do nothing and simply continue. Well if you
+        think about it, the split string array for the string '/a//b' would be ['a','','b']. Yes, that's an empty
+        string in between 'a' and 'b'. Again, from the perspective of the overall path, it doesn't mean anything.
+        If we encounter a double-dot '..', we have to do some processing. This simply means go one level up in the
+        current directory path. So, we will pop an entry from our stack if it's not empty.
+        Finally, if the component we are processing right now is not one of the special characters, then we will simply
+        add it to our stack because it's a legitimate directory name.
     Time complexity: O(N)
     Space complexity: O(N)
     """
-    folders = [folder for folder in path.split('/') if folder and folder != '.']
+    folders = [folder for folder in path.split('/')]
     stack = []
     for folder in folders:
         if folder == '..':
             if stack:
                 stack.pop()
-        else:
+        elif folder and folder != '.':
             stack.append(folder)
     return '/' + '/'.join(stack)
 
