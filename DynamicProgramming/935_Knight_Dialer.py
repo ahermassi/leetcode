52647afb18dcd3389e1559104a31219a468caab9
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-07-07 15:38:37 -0600

    Count the number of distinct phone numbers a knight can dial - v4

--- DynamicProgramming/935_Knight_Dialer.py
+++ DynamicProgramming/935_Knight_Dialer.py
@@ -10,6 +10,9 @@ number of length n. All jumps should be valid knight jumps.
 As the answer may be very large, return the answer modulo 109 + 7. """
 
 
+# Great walkthrough of all the possible solutions:
+# https://medium.com/hackernoon/google-interview-questions-deconstructed-the-knights-dialer-f780d516f029
+
 def knight_dialer_v1(n):
     """ How can we count phone numbers without generating them? It can be done, but not without an additional insight.
         Notice how the count of numbers that can be generated from a given starting position in N hops is equal to the
@@ -104,3 +107,40 @@ def knight_dialer_v3(n):
     for i in range(10):
         res += dfs(i, n)
     return res % (10 ** 9 + 7)
+
+
+def knight_dialer_v4(n):
+    """ Bottom-up Dynamic Programming.
+        The minor limitation of the recursive memoizing solution is clear when we look at the recurrence relation:
+
+             T(P, N) =         Σ           T(p, N-1)
+                      {p ∈ neighbors(P)}
+
+            where P is a starting position
+
+        Notice that the results for N hops depend only on the results for calls with (N - 1) hops. Meanwhile, the cache
+        contains entries for every (nonzero) number of hops. Also notice that the code starts with the largest number
+        of hops and recurses directly down to the smallest. If we imagine the entire function call graph as a sort of
+        virtual tree, we’ll quickly see we’re performing a depth-first traversal.
+        Can we perform a breadth-first traversal instead, where we start at the top and “visit” function calls for
+        (N - 1) hops only after we’ve visited those for N hops? Sadly, no. The values of function calls with nonzero
+        hops absolutely require the values from smaller hop counts, so we won’t get any results until we reach the
+        1-hop layer and start returning numbers rather than additional function calls
+        We can, however, reverse the order: Visit layers with N hops only after we’ve visited layers with (N - 1)
+        hops. We know that the values of 1-hop function calls are always equal to 1 (the base case). We also know how
+        to combine (N - 1) hop values to get N hop values, using the recurrence relation. We can start with a base case
+        of 1 hop and induce all values greater than 1.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+    next_hop = {0: (4, 6), 1: (6, 8), 2: (7, 9), 3: (4, 8), 4: (0, 3, 9), 5: (), 6: (0, 1, 7), 7: (2, 6), 8: (1, 3),
+                9: (2, 4)}
+    cur_counts = [1] * 10  # cur_counts[i] is the number of phone number we can dial starting from position (digit) i
+    # at the current iteration
+    for _ in range(n - 1):  # The phone number needs to have a length n (n hops)
+        next_counts = [0] * 10
+        for cell in range(10):
+            for neighbor in next_hop[cell]:
+                next_counts[cell] += cur_counts[neighbor]  # This is the recurrence relation
+        cur_counts = next_counts
+    return sum(cur_counts) % (10 ** 9 + 7)
