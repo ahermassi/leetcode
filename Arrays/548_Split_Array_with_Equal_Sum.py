2192985c571c452fe6d922c15e4555c256d44cee
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-07-07 09:12:58 -0600

    Find a triplet (i,j,k) that splits the array into 4 equal sums - v3

--- Arrays/548_Split_Array_with_Equal_Sum.py
+++ Arrays/548_Split_Array_with_Equal_Sum.py
@@ -56,3 +56,43 @@ def split_array_v2(nums):
                 if s1 == s2 == s3 == s4:
                     return True
     return False
+
+
+def split_array_v3(nums):
+    """ The idea is to find (i, j) such that sum(0, i - 1) == sum(i + 1, j - 1) and find (j, k) (for the same j)
+        such that sum(j + 1, k - 1) == sum(k + 1, n - 1)
+        In this approach, firstly we form a cumulative sum array 'prefix_sum', where prefix_sum[i] stores the
+        cumulative sum of the array 'nums' up to the ith index. Then, we start by traversing over the possible
+        positions for the middle cut formed by j. For every j, we first find all the left cut's positions i that lead
+        to equalizing the sum of the first and the second part, i.e. prefix_sum[i-1] = prefix_sum[j-1] - prefix_sum[i],
+        and store such sums in a new hash set is formed for every j chosen. Thus, the presence of a sum in the set
+        implies that such a sum is possible for having equal sum of the first and second part for the current position
+        of the middle cut index j.
+        Then, we go for the right cut and find the position of the right cut that leads to equal sum of the third and
+        the fourth part, i.e. prefix_sum[n-1] - prefix_sum[k] = prefix_sum[k-1] - prefix_sum[j], for the same middle
+        cut j as chosen earlier. We also look if the same sum exists in the set. If so, such a triplet (i, j, k)
+        exists which satisfies the required criteria, otherwise not.
+        This idea is very similar to the Two Sum problem. If we are required to check whether two lists have a common
+        element, we can use a hash set to store one list and check the other using the set.
+    Time complexity: O(N^2)
+    Space complexity: O(N)
+    """
+    n = len(nums)
+    if n < 6:
+        return False
+    prefix_sum = [0] * n
+    prefix_sum[0] = nums[0]
+    for i in range(1, n):
+        prefix_sum[i] = prefix_sum[i - 1] + nums[i]
+    for j in range(3, n - 3):
+        sums = set()
+        for i in range(1, j - 1):
+            if prefix_sum[i - 1] == prefix_sum[j - 1] - prefix_sum[i]:
+                sums.add(prefix_sum[i - 1])
+        # Stay within the same j loop because we need to check all the possible cuts for the same current j index
+        for k in range(j + 2, n - 1):
+            if prefix_sum[k - 1] - prefix_sum[j] == prefix_sum[n - 1] - prefix_sum[k] and prefix_sum[k - 1] - \
+                    prefix_sum[j] in sums:
+                return True
+    return False
+
