f0235208d68ee8edb28c4f9107058a1250de5d45
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-07-08 08:58:07 -0600

    Minimum knight moves - v1

--- BFS/1197_Minimum_Knight_Moves.py
+++ BFS/1197_Minimum_Knight_Moves.py
@@ -8,6 +8,34 @@ from collections import deque
 import unittest2 as unittest
 
 
+def min_knight_moves_v1(x, y):
+    """ The key to solving this problem is based on the BFS strategy. The idea is that starting from the origin, we
+        explore the neighborhood following the order that is determined by the distance to the origin, i.e. we first
+        explore all the points within a single step from the origin, then we explore all the points that can be reached
+        with two steps, so on and so forth. During the exploration process, as soon as we reach the target point, we
+        then can call the current path the shortest path, since our exploration follows the order of distance.
+        We can imagine the whole process as if we send a sound wave to determine the distance to an unknown object.
+        The sound wave propagates in all directions with the same speed, while its scope grows as a circle. Once the
+        circle reaches the target object, the radius is the shortest distance between the origin and the target object.
+    Time complexity: Due to the nature of BFS, before reaching the target, we will have covered all the neighborhoods
+    that are closer to the start point. The aggregate of these neighborhoods forms a circle, and the area can be
+    approximated by the area of a square with an edge length of max(2|x|, 2|y|). The number of cells within this square
+    would be (max(2|x|, 2|y|)) ^ 2. Hence, the overall time complexity of the algorithm is O((max(|x|, |y|)) ^ 2).
+    """
+    queue = deque([(0, 0, 0)])
+    visited = set()
+    directions = [(-1, 2), (-2, 1), (-1, -2), (-2, -1), (1, 2), (2, 1), (1, -2), (2, -1)]
+    while queue:
+        i, j, distance = queue.popleft()
+        if (i, j) == (x, y):
+            return distance
+        for a, b in directions:
+            new_i, new_j = i + a, j + b
+            if (new_i, new_i) not in visited:
+                visited.add((new_i, new_j))
+                queue.append((new_i, new_j, distance + 1))
+
+
 def min_knight_moves(x, y):
     """ The moves are symmetric. Hence, we can just assume the problem to be in the first quadrant and push only
         positive coordinates to the queue. Consequently, a simple BFS would give us the required result.
