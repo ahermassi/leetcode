abba36cb708f5ffbd88ccce0263ae7a824fe3e15
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-07-13 09:21:29 -0600

    Multiply strings - v2

--- String/43_Multiply_Strings.py
+++ String/43_Multiply_Strings.py
@@ -55,6 +55,34 @@ def multiply_v1(num1, num2):
     return ''.join(map(str, res[i:])) if i < len(res) else '0'
 
 
+def multiply_v2(num1, num2):
+    """ Similar algorithm but with an extra loop. If we break the multiplication it into pieces, it will have the
+        following steps:
+            - Compute products from each pair of digits from num1 and num2
+            - Carry each element over
+        Example: num1 = "12", num2 = "19"
+        res = [0, 1, 11,18], after first loop
+        res = [0, 2, 2, 8], after second loop
+    Time complexity: O(N * M)
+    Space complexity: O(N + M)
+    """
+    n, m = len(num1), len(num2)
+    res = [0] * (n + m)
+    for i in reversed(range(n)):
+        for j in reversed(range(m)):
+            mul = (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+            res[i + j + 1] += mul
+    carry = 0
+    for i in reversed(range(len(res))):
+        res[i] += carry
+        carry = res[i] // 10
+        res[i] %= 10
+    ptr = 0
+    while ptr < len(res) and res[ptr] == 0:
+        ptr += 1
+    return ''.join(map(str, res[ptr:])) if ptr < len(res) else '0'
+
+
 class Test(unittest.TestCase):
 
     data = [('2', '3', '6'), ('123', '456', '56088')]
@@ -62,6 +90,7 @@ class Test(unittest.TestCase):
     def test_multiply(self):
         for test_num1, test_num2, result in self.data:
             self.assertEqual(result, multiply_v1(test_num1, test_num2))
+            self.assertEqual(result, multiply_v2(test_num1, test_num2))
 
 
 if __name__ == '__main__':
