a792f5d1d216e54dc8e72628c7dc73288d225e16
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-07-18 12:48:44 -0600

    Add bold tag in string - v1 update

--- String/616_Add_Bold_Tag_in_String.py
+++ String/616_Add_Bold_Tag_in_String.py
@@ -5,16 +5,21 @@ closed bold tag. Also, if two substrings wrapped by bold tags are consecutive, y
 import unittest2 as unittest
 
 
-def add_bold_tag_v1(s, dict):
+def add_bold_tag_v1(s, words):
     """ Let's try to learn which letters end up bold, since the resulting answer will just be the canonical one - we
         put bold tags around each group of bold letters.
         To do this, we'll check for all occurrences of each word and mark the corresponding letters bold.
         Let's work on first setting bold[i] = true if and only if the ith letter is bold. For each starting position i
-        in S, for each word, if S[i:] starts with that word, we'll set the appropriate letters bold.
-        Now armed with the correct bold, let's try to output the answer. A letter in position i is the first bold
-        letter of the group if bold[i] && (i == 0 || !bold[i-1]), and is the last bold letter if
-        bold[i] && (i == N-1 || !bold[i+1]).
-        Once we know which letters are the first and last bold letters of a group, we know where to put the '<b>' and
+        in s, for each word, if s[i:] starts with that word, we'll set the appropriate letters bold.
+        Now armed with the correct bold, let's try to output the answer.
+
+        A letter in position i is the first bold letter of the group if:
+            bold[i] && (i == 0 || !bold[i-1])
+
+        A letter in position i is the last bold letter of the group if:
+            bold[i] && (i == N-1 || !bold[i+1]).
+
+        Once we know which letters are the first and last bold letters of a group, we know where to place the '<b>' and
         '</b>' tags.
     Time complexity: O(N * W), where N is the length of s and W is the number of words
     Space complexity: O(N)
@@ -22,12 +27,12 @@ def add_bold_tag_v1(s, dict):
     n = len(s)
     bold = [False] * n
     for i in range(n):
-        for word in dict:
+        for word in words:
             if s[i:].startswith(word):
                 bold[i:i + len(word)] = [True] * len(word)
     res = []
     for i, c in enumerate(s):
-        if bold[i] and (not i or not bold[i - 1]):
+        if bold[i] and (i == 0 or not bold[i - 1]):
             res.append('<b>')
         res.append(c)
         if bold[i] and (i == n - 1 or not bold[i + 1]):
