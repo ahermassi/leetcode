08c4226182da4fb156b2b2b9625d152f1d731085
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-07-18 09:26:58 -0600

    Flatten nested list iterator - v2 update

--- Stack/341_Flatten_Nested_List_Iterator.py
+++ Stack/341_Flatten_Nested_List_Iterator.py
@@ -59,24 +59,56 @@ class NestedIteratorV2(object):
         valid value since it can only return an int. So we should first flatten the list and then check if the stack is
         really empty. The best way is to advance to the next actual Integer BEFORE we call next(), then hasNext() is
         just checking if the pointer is at an integer.
+        Notice that the stack is shared between calls. This means that next() will find an integer and return it, while
+        still preserving the state of the stack. We can then call next() again to get the next integer, and so forth.
+        The loop condition checks if the top of the stack is still a list. The loop body should push the contents of
+        the list onto the stack (in reverse). Eventually, there will be an integer on the top of the stack, OR the
+        stack will be empty. Being able to get the next integer to the top of the stack allows the next() and hasNext()
+        methods to access it.
     """
     def __init__(self, nestedList):
+        """
+        Time complexity: O(N + L), the worst-case occurs when the initial input nestedList consists entirely of
+        integers and empty lists (everything is in the top-level). In this case, every item is reversed and stored,
+        giving a total time complexity of O(N + L).
+        Space complexity: O(N + L), in the worst case, where the top list contains N integers, or L empty lists, it
+        will cost O(N + L) space.
+        """
         self.stack = nestedList[::-1]
         self.advanceToNextInteger()
 
     def next(self):
+        """
+        Time complexity: O(1) or O(L/N), all of this method is O(1), except for possibly the call to
+        advanceToNextInteger(), giving us a time complexity the same as advanceToNextInteger().
+        """
         res = self.stack.pop().getInteger()
         self.advanceToNextInteger()
         return res
 
     def hasNext(self):
+        """
+        Time complexity: O(1)
+        """
         return self.stack
 
     def advanceToNextInteger(self):
-        while self.stack:
-            top = self.stack[-1]
-            if not top.isInteger():
-                self.stack.extend([val for val in self.stack.pop().getList()[::-1]])  # Flatten
+        """
+        Time complexity: O(1) or O(L/N), if the top of the stack is an integer, then this function does nothing, taking
+        O(1) time. Otherwise, it needs to process the stack until an integer is on top. The best way of analyzing the
+        time complexity is to look at the total cost across all calls to advanceToNextInteger() and then divide by the
+        number of calls made. Once the iterator is exhausted, advanceToNextInteger() must have seen every integer at
+        least once, costing O(N) time. Additionally, it has seen every list (except the first) on the stack at least
+        once also, so this costs O(L) time. Adding these together, we get O(N + L) time.
+        The amortized time of a single advanceToNextInteger() is the total cost, O(N + L), divided by the number of
+        times it's called. In order to get all integers, we need to have called it N times. This gives us an amortized
+        time complexity of O((N + L) / N) = O(1 + L/N) = O(L/N).
+        """
+        # While the stack contains a nested list at the top...
+        while self.stack and not self.stack[-1].isInteger():
+            # Unpack the list at the top by putting its items onto the stack in reverse order.
+            for element in reversed(self.stack.pop().getList()):
+                self.stack.append(element)
 
 
 # Your NestedIterator object will be instantiated and called as such:
