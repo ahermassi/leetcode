25cf20bcf2814ad24803ad13731d675c5c85b36f
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-07-24 08:41:23 -0600

    Remove sub-folders from a list of folders - v2

--- String/1233_Remove_Sub-Folders_from_the_Filesystem.py
+++ String/1233_Remove_Sub-Folders_from_the_Filesystem.py
@@ -23,3 +23,44 @@ def remove_sub_folders_v1(folder):
             # Need '/' to ensure a parent. Example: /a/b is NOT parent of /a/bc
             parent_folders.append(f)
     return parent_folders
+
+
+class Trie(object):
+    def __init__(self):
+        self.children = dict()
+        self.path_index = -1
+
+
+def remove_sub_folders_v2(folder):
+    """ Use 'path_index' to save each folder path's index in a trie node. When we search the trie, if we find a parent
+        folder (path_index >= 0), we append it to the result. Otherwise, we keep exploring all the parent folders on
+        the current trie branch.
+    Time complexity: O(N + M), where N is the number of folders and M is the length of longest path in 'folder'.
+    O(N) to create the trie and O(M) to find parent folders.
+    Space complexity: O(N), where N is the number of folders
+    """
+
+    def insert_folders(root):
+        for i, path in enumerate(folder):
+            cur = root
+            for part in path.split('/')[1:]:  # When we split a path by '/', the first part is an empty string
+                if part not in cur.children:
+                    cur.children[part] = Trie()
+                cur = cur.children[part]
+            cur.path_index = i
+
+    def dfs(root):
+        if root.path_index != -1:
+            res.append(folder[root.path_index])
+        else:
+            for child in root.children.values():
+                dfs(child)
+
+    root = Trie()
+    insert_folders(root)
+    res = []
+    dfs(root)
+    return res
+
+
+
