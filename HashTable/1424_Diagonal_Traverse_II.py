034f5f98b9b56a4e5e87357ac92d54e98b865646
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-07-30 09:45:13 -0600

    Print marix elements in diagonal order - v1

---
+++ HashTable/1424_Diagonal_Traverse_II.py
@@ -0,0 +1,26 @@
+""" Given a list of lists of integers, nums, return all elements of nums in diagonal order as shown in the below images.
+"""
+
+from collections import defaultdict
+
+
+def find_diagonal_order_v1(nums):
+    """ Similar to 498- Diagonal Traverse.
+        In a 2D matrix, elements in the same diagonal have the same sum of their indices. So if we have all elements
+        with the same sum of their indices together, then it’s just a matter of printing those elements in order.
+        We can loop through the matrix, store each element by the sum of its indices in a hash map. We end up with
+        a collection of all elements on shared diagonals.
+        Note: Here, diagonals are from bottom to top, but we traverse the input matrix from first row to last row.
+        Hence we need to print the elements in reverse order.
+    Time complexity: O(N * M)
+    Space complexity: O(N * M)
+    """
+    diagonals = defaultdict(list)
+    n = len(nums)
+    for i in range(n):
+        for j in range(len(nums[i])):
+            diagonals[i + j].append(nums[i][j])
+    res = []
+    for values in diagonals.values():
+        res.extend(values[::-1])
+    return res
