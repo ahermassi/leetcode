925a4b70870c08749d175971864119815ed153bd
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-08-01 14:23:20 -0600

    Path sum II - v1 update

--- Tree/113_Path_Sum_II.py
+++ Tree/113_Path_Sum_II.py
@@ -12,6 +12,34 @@ class TreeNode(object):
 
 
 def path_sum_v1(root, sum):
+    """ If we simply consider the depth first traversal on the tree, all it does is traverse one branch after another.
+        All we need to do here is to simply execute the depth first traversal and maintain two things along the way:
+            - Running sum of all the nodes traversed till that point in recursion
+            - List of all those nodes
+        If ever the sum becomes equal to the required sum, and the node where this happens is a leaf node, we can
+        simply add the list of nodes to our final solution. We keep on doing this for every branch of the tree and we
+        will get all the root to leaf paths in this manner that add up to a certain value.
+    Time complexity: O(N)
+    Space complexity: In the worst case, the tree is completely unbalanced and we would keep all N nodes in the stack
+    so O(N); in the best case (the tree is completely balanced), it is O(logN) which is the height of the tree
+    """
+
+    def dfs(root, path, remaining):
+        if not root:
+            return
+        if root.val == remaining and not root.left and not root.right:
+            path.append(root.val)  # Only when we're sure that this node leads to a correct path we add it to final list
+            res.append(path)
+            return
+        dfs(root.left, path + [root.val], remaining - root.val)
+        dfs(root.right, path + [root.val], remaining - root.val)
+
+    res = []
+    dfs(root, [], sum)
+    return res
+
+
+def path_sum_v2(root, sum):
     """ Good old DFS using stack.
     Time complexity: O(N)
     Space complexity: in the worst case, the tree is completely unbalanced and we would keep all N nodes in the stack
@@ -26,28 +54,6 @@ def path_sum_v1(root, sum):
             res.append(path + [node.val])
         else:
             stack.extend([(kid, path + [node.val], s - node.val) for kid in (node.right, node.left) if kid])
-    return res
-
-
-def path_sum_v2(root, sum):
-    """ Same DFS idea but recursively.
-    Time complexity: O(N)
-    Space complexity: in the worst case, the tree is completely unbalanced and we would keep all N nodes in the stack
-    so O(N); in the best case (the tree is completely balanced), it is O(log N) which is the height of the tree
-    """
-
-    def dfs(root, path, sum):
-        if not root:
-            return
-        if root.val == sum and not root.left and not root.right:
-            path.append(root.val)  # Only when we're sure that this node leads to a correct path we add it to final list
-            res.append(path)
-        else:
-            dfs(root.left, path + [root.val], sum - root.val)
-            dfs(root.right, path + [root.val], sum - root.val)
-
-    res = []
-    dfs(root, [], sum)
     return res
 
 
