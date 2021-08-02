ef6dacc33fbaeac41449cfac15df06cdf10c232b
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-08-02 08:19:57 -0600

    Design Tic-Tac-Toe - update

--- Arrays/348_Design_Tic-Tac-Toe.py
+++ Arrays/348_Design_Tic-Tac-Toe.py
@@ -10,9 +10,17 @@ import unittest2 as unittest
 class TicTacToe(object):
     """ Pretty simple logic. Since we know that each move will be legal, we don't need to keep track of the actual grid.
         If we assign the value of 1 for player 1 and -1 for player 2, we just keep track of the changes to individual
-        rows and columns (and the two diagonals). If the value reaches either n or -n, we know that one of the players
-        won. And since the player can only either draw or win during a move, player is returned if a win condition is
-        reached.
+        rows and columns (and the two diagonals). On every move, we must determine whether a player has marked all of
+        the cells in a row or column. In other words, we could say that, if there are n rows and n columns on a board,
+        the player must have marked a certain row or column n times. Also, there are always n cells on the diagonal or
+        anti-diagonal. Thus, to win by either of these, a player must have marked the cells on the diagonal or
+        anti-diagonal n times.
+        With this, we can say that, if the value of rows[i] is equal to n, player 1 has marked ith row n times.
+        Similarly, if the value of rows[i] is equal to -n, then player 2 has marked the ith row n times. Similar logic
+        applies to the columns and diagonals. We must finally determine whether the current player has won the game.
+        If any row, column, diagonal, or anti-diagonal is equal to n (for player 1) or -n (for player 2) then the
+        current player has won the game. And since the player can only either draw or win during a move, player is
+        returned if a win condition is reached.
         The important abstraction in this approach is rows/cols arrays. rows[i] represents ith row in the actual game
         grid. Since we're only interested in the entire content of a row (sum of values in this case), this abstraction
         makes a lot of sense.
@@ -24,18 +32,19 @@ class TicTacToe(object):
         self.rows = [0] * n
         self.cols = [0] * n
         self.size = n
-        self.diag = 0
+        self.diagonal = 0
         self.anti_diagonal = 0
 
     def move(self, row, col, player):
-        coef = 1 if player == 1 else -1
-        self.rows[row] += coef
-        self.cols[col] += coef
+        add = 1 if player == 1 else -1
+        self.rows[row] += add
+        self.cols[col] += add
         if col == row:
-            self.diag += coef
+            self.diagonal += add
         if col + row == self.size - 1:
-            self.anti_diagonal += coef
-        if coef * self.size in {self.rows[row], self.cols[col], self.diag, self.anti_diagonal}:  # Account for n and -n
+            self.anti_diagonal += add
+        if add * self.size in {self.rows[row], self.cols[col], self.diagonal, self.anti_diagonal}:
+            # Account for n and -n
             return player
         return 0
 
