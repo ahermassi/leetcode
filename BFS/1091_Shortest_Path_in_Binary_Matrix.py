ec451e2361108c77706fe2d3f94b0d4767cff1fb
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-08-13 08:06:43 -0600

    Shortest path in binary matrix - v1 update

--- BFS/1091_Shortest_Path_in_Binary_Matrix.py
+++ BFS/1091_Shortest_Path_in_Binary_Matrix.py
@@ -13,30 +13,42 @@ import unittest2 as unittest
 
 
 def shortest_path_binary_matrix_v1(grid):
-    """ Do a breadth first search to find the shortest path.
-        Using DFS can help us determine if there is a path from source to destination. However, the time complexity is
-        terrible. We may not get the shortest path if we use DFS, we might end up with just some path. Also, using DFS
-        in this particular problem will lead to TLE. DFS does not guarantee that if node 1 is visited before another
-        node 2 starting from a source vertex, then node 1 is closer to the source than node 2. Hence, BFS is our best
-        shot since it goes from level to level from source to destination.
-    Time complexity: O(N)
-    Space complexity: O(N)
+    """ The grid is a graph, and we need to find the length of the shortest path from the top-left to the bottom-right
+        cell. Finding the shortest path between two nodes in a graph is almost always done using BFS. BFS works by
+        firstly identifying all of the nodes that can be reached in 1 step from the top-left cell, then those in 2
+        steps, then 3 steps, etc., until it "finds" the target node (the bottom-right cell). We'll use a queue to
+        keep track of cells that we have numbered but haven't yet numbered the *neighbors* of. We'd need a 'visited'
+        set to avoid infinite looping around cycles.
+        The simplest BFS variant is to put the distances on the queue, alongside the row and column (triplets instead
+        of pairs).
+        Exploring the cell's neighbors involves identifying all open cells adjacent to the current cell that still have
+        a 0 in them. For each of these cells, we associate the (distance + 1) to them.
+        We return -1 if the loop terminates without returning, as this means we ran out of cells to explore before
+        reaching the bottom-right cell.
+    Time complexity: O(N), where N is the number of cells in the grid. Each cell was guaranteed to be enqueued at most
+    once. This is because a condition for a cell to be enqueued was that it had a zero in the grid, and when enqueuing,
+    we also add the cell to the 'visited' set. The outer loop runs as long as there are still cells in the queue,
+    dequeuing one each time. Therefore, it runs at most N times, giving a time complexity of O(N).
+    Space complexity: O(N), given that BFS will have nodes of at most two unique distances on the queue at any one
+    time, it would be reasonable to wonder if the worst-case space complexity is actually lower. But actually, it turns
+    out that there are cases with massive grids where the number of cells at a single distance is proportional to N.
+    So even with cells of a single distance on the queue, in the worst case, the space needed is O(N).
     """
     if grid[0][0] or grid[-1][-1]:
         return -1
-    n, m = len(grid), len(grid[0])
+    n = len(grid)
+    directions = {(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)}
     queue = deque([(0, 0, 1)])
     visited = {(0, 0)}
     while queue:
         i, j, distance = queue.popleft()
-        if i == n - 1 and j == m - 1:
+        if i == j == n - 1:
             return distance
-        for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1), (i - 1, j - 1), (i - 1, j + 1), (i + 1, j - 1), \
-                    (i + 1, j + 1):
-            if not 0 <= x < n or not 0 <= y < m or grid[x][y] or (x, y) in visited:
-                continue
-            queue.append((x, y, distance + 1))
-            visited.add((x, y))
+        for x, y in directions:
+            a, b = i + x, j + y
+            if 0 <= a < n and 0 <= b < n and not grid[a][b] and (a, b) not in visited:
+                queue.append((a, b, distance + 1))
+                visited.add((a, b))
     return -1
 
 
