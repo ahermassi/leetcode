b746553e6cc5d008e0f65bd1d38ca47e6002f334
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-08-26 07:57:54 -0600

    Reorganize string - v1 update

--- Heap/767_Reorganize_String.py
+++ Heap/767_Reorganize_String.py
@@ -7,36 +7,49 @@ from heapq import heappush, heappop
 import unittest2 as unittest
 
 
-def reorganize_string_v1(S):
-    """ This is similar to 621- Task Scheduler problem, with the cooling interval is equal to 1.
+def reorganize_string_v1(s):
+    """ This is similar to 621- Task Scheduler problem, with a cooling interval equal to 1.
         This is a greedy approach that tries to write the most common letter followed by the second most common letter
         and so on.
-        We create a heap of (count, letter). The implementation stores negative counts because Python's heap is min
+        We create a heap of (count, letter). The implementation stores negative counts because Python's heap is a min
         heap. We pop the top two elements from the heap (representing different letters with positive remaining count),
         and then write the most frequent one that isn't the same as the most recent one written. After, we push the
-        correct counts back onto the heap.
+        correct counts back to the heap.
         At the end, we might have one element still on the heap, which must have a count of one. If we do, we'll add
         that to the answer too.
+        We pop two letters at a time from the heap, add them to our result string, decrement their frequencies and push
+        them back into the heap. Why do we have to pop two items/letters at a time? Because if we only pop one at a
+        time, we will keep popping and pushing the same letter over and over again if that letter has a frequency
+        greater than 1. Hence, by popping two at a time, adding them to the result, decrementing their frequencies, and
+        finally pushing them back to the heap, we guarantee that we are always alternating between letters.
+        Since we are always popping two items at a time, we will definitely run into an out of bounds error if we have
+        an odd number of unique items in the given string. To avoid this, we need to make sure our heap has at least
+        two items at any given time. We achieve this by running the main logic inside a 'while len(heap) > 1' instead
+        of 'while heap'.
+        If the last item has a frequency greater than 1, then return "" because we can't escape having the same letter
+        repeated contiguously. Otherwise, if the item has a frequency equal to 1, we pop it and add it to the result.
     Time complexity: O(N logA), where N is the length of the string and A the length of alphabet. Calculating the
     letter frequency takes O(N) time, and building the max heap takes O(A logA) which is actually constant and not
     considered dominant if alphabet size is fixed. Rebuilding the string takes N steps and in each step the max heap
     takes at most O(logA) time to reorganize itself. So overall time complexity is O(N logA)
     Space complexity: O(A), if A is fixed this complexity is O(1)
     """
-    counter, heap, res = Counter(S), [], ''
-    if any(count > (len(S) + 1) / 2 for c, count in counter.items()):
+    counter, heap, res = Counter(s), [], []
+    if any(count > (len(s) + 1) / 2 for count in counter.values()):
         return ''  # The task is only impossible if the frequency of any letter exceeds (N+1) / 2
     for k, v in counter.items():  # Using negative values to create a max heap
         heappush(heap, [-v, k])
     while len(heap) >= 2:
-        a_instance, a = heappop(heap)
-        b_instance, b = heappop(heap)
-        res += a + b
-        if a_instance != -1:
-            heappush(heap, [a_instance + 1, a])
-        if b_instance != -1:
-            heappush(heap, [b_instance + 1, b])
-    return res if not heap else res + heap[0][1]
+        count_a, a = heappop(heap)
+        count_b, b = heappop(heap)
+        res.extend([a, b])
+        if count_a != -1:
+            heappush(heap, [count_a + 1, a])
+        if count_b != -1:
+            heappush(heap, [count_b + 1, b])
+    if heap:
+        res.append(heap[0][1])
+    return ''.join(res)
 
 
 def reorganize_string_v2(S):
