4b90bb8158b257a3023760481def186965fff7c3
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-08-29 08:50:49 -0600

    Find if we can change one character in string to match words in dictionary - v2

--- HashTable/676_Implement_Magic_Dictionary.py
+++ HashTable/676_Implement_Magic_Dictionary.py
@@ -50,3 +50,73 @@ class MagicDictionaryV1(object):
             if different_chars == 1:
                 return True
         return False
+
+
+class TrieNode(object):
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.is_word = False
+
+
+class MagicDictionaryV2(object):
+    """ Use a trie to store the words in a space-efficient manner. During the search, we use a boolean 'change_allowed'
+        to tell whether we still can change a character in the search word. Let 'c' be th character at the current
+        index. We need to take care of 4 cases:
+            - If 'c' is not a child of the current trie node:
+                * If no 'change_allowed', we return False as no further character changes are possible.
+                * If 'change_allowed', we continue the search from all the trie nodes of the current node's children.
+                  Each of these nodes accounts for a character change and change_allowed=False is passed down.
+                  If any of these searches returns True, we're done. Otherwise, we return False.
+            - If 'c' is a child of the current trie node:
+                * If 'change_allowed', we carry on the search from the trie node associated with 'c' with still a
+                  possibility of 'change_allowed'. If the search returns a favorable result, we're done. Otherwise, we
+                  continue the search from all the trie nodes of the current node's children whose values are not equal
+                  to 'c'. Each of these nodes accounts for a character change and change_allowed=False is passed down.
+                  If any of these searches returns True, we're done. Otherwise, we return False.
+                * If no 'change_allowed', we carry on the search from the trie node associated with 'c' with no more
+                  possibility of 'change_allowed'. The result of this search determines the final outcome.
+    """
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.trie = TrieNode()
+
+    def buildDict(self, dictionary):
+        """
+        :type dictionary: List[str]
+        """
+        for word in dictionary:
+            self.add_word(word)
+
+    def add_word(self, word):
+        trie = self.trie
+        for c in word:
+            trie = trie.children[c]
+        trie.is_word = True
+
+    def search(self, search_word):
+
+        def word_exists(trie, index, change_allowed):
+            if index == n:
+                return trie.is_word and not change_allowed
+            c = search_word[index]
+            if c not in trie.children:
+                if not change_allowed:
+                    return False
+                for child in trie.children:
+                    if word_exists(trie.children[child], index + 1, False):
+                        return True
+                return False
+            if change_allowed:
+                if word_exists(trie.children[c], index + 1, True):
+                    return True
+                for child in trie.children:
+                    if child != c and word_exists(trie.children[child], index + 1, False):
+                        return True
+                return False
+            return word_exists(trie.children[c], index + 1, False)
+
+        n = len(search_word)
+        return word_exists(self.trie, 0, True)
