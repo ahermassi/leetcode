0e8cd999df4e5ae241119fe15af85b8b8e927bea
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-08-31 08:15:54 -0600

    Rotated digits - v3

--- DynamicProgramming/788_Rotated_Digits.py
+++ DynamicProgramming/788_Rotated_Digits.py
@@ -47,11 +47,45 @@ def rotated_digits_v2(n):
     return good
 
 
+def rotated_digits_v3(n):
+    """ Dynamic Programming.
+        The point is to reduce calculating the same sub problems. In brute force, if n = 112, we need to judge digit by
+        digit for n = 1 to n = 112. However, the same pattern like 11 is actually re-calculated because we already
+        processed n = 11 and know it's not a good number. In this solution, n = 112 is split into 2 sub-problems,
+        x = 11 and y = 2, and since these two sub-problems have already been calculated, we can deduce the answer for
+        n = 112.
+        We use the following notations:
+        dp[i] = 0, if i is an invalid number (so it's not good)
+        dp[i] = 1, if i is a valid number but rotates to itself (so it's not good)
+        dp[i] = 2, if i is a good number
+    Time complexity: O(N)
+    Space complexity: O(N)
+    """
+    dp = [0] * (n + 1)
+    res = 0
+    for i in range(n + 1):
+        if i < 10:
+            if i in {0, 1, 8}:
+                dp[i] = 1
+            elif i in {2, 5, 6, 9}:
+                dp[i] = 2
+                res += 1
+        else:
+            part_one, part_two = dp[i // 10], dp[i % 10]
+            if part_one == part_two == 1:
+                dp[i] = 1
+            elif part_one * part_two in {2, 4}:  # Either or both of the two parts is/are equal to 2
+                dp[i] = 2
+                res += 1
+    return res
+
+
 class Test(unittest.TestCase):
 
     def test_rotated_digits(self):
         self.assertEqual(4, rotated_digits_v1(10))
         self.assertEqual(4, rotated_digits_v2(10))
+        self.assertEqual(4, rotated_digits_v3(10))
 
 
 if __name__ == '__main__':
