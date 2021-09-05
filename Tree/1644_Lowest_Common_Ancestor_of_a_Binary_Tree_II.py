b0377a6532028521844d358c70070f3f3396bc9d
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-09-05 13:33:24 -0600

    Find the LCA of two nodes if one of them doesn't exist - v2

--- Tree/1644_Lowest_Common_Ancestor_of_a_Binary_Tree_II.py
+++ Tree/1644_Lowest_Common_Ancestor_of_a_Binary_Tree_II.py
@@ -34,3 +34,43 @@ def lowest_common_ancestor_v1(root, p, q):
     while q not in p_ancestor:
         q = parent[q]
     return q
+
+
+def lowest_common_ancestor_v2(root, p, q):
+    """ Similar to the second solution of 236- Lowest Common Ancestor of a Binary Tree, but with some tweaks.
+        Question 236 has two important premises:
+            1- It is guaranteed that both p and q are in the tree
+            2- A node can be a descendant of itself
+        Because of these premises, we can return either p OR q as soon as we find one of them.
+        But for this question, the premises are different:
+            1- It is NOT guaranteed that both p and q are in the tree
+            2- A node can still be a descendant of itself.
+        Hence:
+            - We need a way to record if we've seen both p and q
+            - We need to traverse the entire tree even after we've found one of the nodes p/q
+        Use can use either boolean or integers as flags. Moreover, we need to recurse on the left and right subtrees
+        before checking if the current node matches p or q. Otherwise, we'd return and won't traverse the rest of the
+        tree. It is important that we keep searching the entire tree: We cannot instantly return the node we found
+        because we don't know if both p and q exist in the tree.
+    Time complexity: O(N)
+    Space complexity: O(h), or O(N) in the case of a skewed tree
+    """
+
+    def find_lca(root):
+        if not root:
+            return None
+        search_left = find_lca(root.left)  # This is to find nodes p and q NOT the LCA
+        search_right = find_lca(root.right)
+        if root == p or root == q:  # Once we found either p or q, we have to update the counter
+            nodes_found[0] += 1
+            return root
+        if search_left and search_right:  # # We got something back on the left AND right. That means this node is the
+            # LCA because our recursion returns from bottom to top, so we return what we hold: 'root'
+            return root
+        # Either one of the children returned a node, meaning either p or q found on left or right branch. Return
+        # whatever we got.
+        return search_left or search_right
+
+    nodes_found = [0]
+    lca = find_lca(root)
+    return lca if nodes_found[0] == 2 else None
