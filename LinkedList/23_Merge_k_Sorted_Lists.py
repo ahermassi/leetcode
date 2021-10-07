8c3ca1e5f7db7d5b3d52e8e57540300472482571
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-10-07 07:49:12 -0600

    Merge K sorted lists - v1 update

--- LinkedList/23_Merge_k_Sorted_Lists.py
+++ LinkedList/23_Merge_k_Sorted_Lists.py
@@ -14,26 +14,26 @@ class ListNode:
 def merge_k_lists_v1(lists):
     """ Compare every k nodes (head of every linked list) and get the node with the smallest value. We then extend the
         final sorted linked list with the selected nodes. Optimize the comparison process using a priority queue to
-        find the next element to add. To make the implementation simple we 'monkey patch' the ListNode class to have a
-        custom less-than function using setattr. Note that simply using the tuple trick and pushing (node.val, node) to
-        the priority queue will not work because the lists can have equal values.
+        find the next element to add. To make the implementation simple we define the __lt__ method of the ListNode
+        class to make ListNode objects comparable. Note that simply using the tuple trick and pushing (node.val, node)
+        to the priority queue will not work because the lists can have equal values.
     Time complexity: O(N logK), the comparison cost will be reduced to O(logK) for every pop and insertion into the
     priority queue and there are N nodes in the final linked list
     Space complexity: O(K), for the heap
     """
-    setattr(ListNode, "__lt__", lambda self, other: self.val < other.val)
-    dummy = tail = ListNode(0)
+    ListNode.__lt__ = lambda self, other: self.val < other.val
+    dummy_head = dummy_tail = ListNode(0)
     heap = []
     for head in lists:
         if head:
             heappush(heap, head)
     while heap:
         node = heappop(heap)
-        tail.next = node
-        tail = tail.next
+        dummy_tail.next = node
         if node.next:
             heappush(heap, node.next)
-    return dummy.next
+        dummy_tail = dummy_tail.next
+    return dummy_head.next
 
 
 def merge_k_lists_v2(lists):
