0854c3fcff240d1cae4faf8df7342aece0a71747
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-10-18 17:17:30 -0600

    Find the minimum number of stickers needed to spell a word - v1

---
+++ Backtracking/691_Stickers_to_Spell_Word.py
@@ -0,0 +1,60 @@
+""" We are given n different types of stickers. Each sticker has a lowercase English word on it.
+
+You would like to spell out the given string target by cutting individual letters from your collection of stickers
+and rearranging them. You can use each sticker more than once if you want, and you have infinite quantities of each
+sticker.
+
+Return the minimum number of stickers that you need to spell out target. If the task is impossible, return -1.
+
+Note: In all test cases, all words were chosen randomly from the 1000 most common US English words, and target was
+chosen as a concatenation of two random words. """
+
+from collections import Counter, defaultdict
+
+
+def min_stickers_v1(stickers, target):
+    """ Backtracking.
+
+        We start by building a frequency map of the target word. dfs() function has 3 parameters:
+            - 'index': The index of the current character of 'target' being processed
+            - 'stickers_used': The number of stickers used so far
+            - 'chars_used': Frequency map of all the stickers' characters we have used up to the current index 'index'.
+
+        At any point during the recursion, let 'c' be the target character at index 'index'.
+        If chars_used[c] is greater than the frequency of c in target's frequency map, it means we have exhausted all
+        the occurrences of 'c' needed to build 'target' and we can move on the next index.
+        Otherwise, we look at all the stickers at hand, and for each sticker if it contains 'c' we can consider it as
+        a candidate sticker and add its information, which is its characters and their frequencies, to 'chars_used'
+        and proceed to the next index. We then delete the frequencies from 'chars_used' (backtrack).
+
+        To avoid TLE, we only process the stickers if the number of stickers used so far plus 1 is less than the global
+        minimum found so far, i.e. proceeding further COULD yield a better answer.
+
+    Time complexity:
+    Space complexity:
+    """
+
+    def dfs(index, stickers_used, chars_used):
+        if index == n:
+            res[0] = min(res[0], stickers_used)
+            return
+        c = target[index]
+        if chars_used[c] >= counter[c]:
+            dfs(index + 1, stickers_used, chars_used)
+        elif stickers_used + 1 < res[0]:
+            for sticker in stickers:
+                if c in sticker:
+                    # If 'c' is in the sticker, we use the ENTIRE sticker, and that's how we get the minimum number of
+                    # stickers needed as one sticker carries other characters along with 'c'
+                    for ch in sticker:
+                        chars_used[ch] += 1
+                    dfs(index + 1, stickers_used + 1, chars_used)
+                    for ch in sticker:
+                        chars_used[ch] -= 1
+
+    if any(c not in set(''.join(stickers)) for c in target):
+        return -1
+    n, res = len(target), [float('inf')]
+    counter = Counter(target)
+    dfs(0, 0, defaultdict(int))
+    return res[0]
