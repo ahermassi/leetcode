61620f75bead656b501f2f5205dba9cd6f603f6d
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-10-21 08:00:15 -0600

    Convert an integer to English words - v1

---
+++ HashTable/273_Integer_to_English_Words.py
@@ -0,0 +1,43 @@
+""" Convert a non-negative integer num to its English words representation. """
+
+
+def number_to_words_v1(num):
+    """ Let's simplify the problem by representing it as a set of simple sub-problems.
+
+        We could split the initial integer 1234567890 into groups containing no more than three digits: 1.234.567.890.
+        That results in the following representation: "1 Billion 234 Million 567 Thousand 890" and reduces the initial
+        problem to how to convert a 3-digit integer to an English word.
+
+        We could further split 234: "2 Hundred 34" resulting into two sub-problems : Convert a 1-digit integer and
+        convert a 2-digit integer. The first one is trivial. The second one could be reduced to the first one for all
+        2-digit integers except the ones from 10 to 19 which should be considered separately.
+
+
+    Time complexity: O(log10 N), intuitively the output is proportional to the number of digits in the input
+    Space complexity: O(1)
+    """
+
+    def decompose(num):
+        if num < 20:
+            res = less_than_20[num]
+        elif num < 100:
+            res = tens[num // 10] + ' ' + decompose(num % 10)
+        else:
+            res = less_than_20[num // 100] + ' Hundred ' + decompose(num % 100)
+        return res.rstrip()
+
+    less_than_20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+                    "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+    tens = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+    thousands = ['', ' Thousand', ' Million', ' Billion']
+    if not num:
+        return 'Zero'
+    group = 0
+    words = []
+    while num:
+        if num % 1000:
+            words.append(decompose(num % 1000) + thousands[group])
+        num //= 1000
+        group += 1
+    # We need to reverse 'words' because we're processing the groups of 3-digit integers from right to left
+    return ' '.join(words[::-1]).rstrip()
