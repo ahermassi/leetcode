efb8d3adc8158057d9c280b7bcc89822249bb933
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-10-30 08:32:41 -0600

    Longest increasing path in a matrix - v2 update

--- DFS/329_Longest_Increasing_Path_in_a_Matrix.py
+++ DFS/329_Longest_Increasing_Path_in_a_Matrix.py
@@ -60,35 +60,47 @@ def longest_increasing_path_v1(matrix):
 
 
 def longest_increasing_path_v2(matrix):
-    """ The result of each cell only related to the result of its neighbors. Can we use dynamic programming?
+    """ The result of each cell is related only to the result of its neighbors. Can we use Dynamic Programming?
+
         If we define the longest increasing path starting from cell (i, j) as a function f(i,j), then we have the
         following transition function:
-            f(i,j) = 1 + max{f(x,y)∣(x,y) is a neighbor of (i,j) and matrix[x][y] > matrix[i][j]}
-        This formula is the same as used in the previous approaches. With such transition function, we may think that
+
+                f(i,j) = 1 + max{f(x,y)∣(x,y) is a neighbor of (i,j) and matrix[x][y] > matrix[i][j]}
+
+        This formula is the same as used in the previous approach. With such transition function, we may think that
         it is possible to use dynamic programming to deduce all the results without employing DFS!
-        That is right with one thing missing: we don't have the dependency list.
+        That is right with one thing missing: We don't have the dependency list.
+
         For dynamic programming to work, if problem B depends on the result of problem A, then we must make sure that
         problem A is calculated before problem B. Such order is natural and obvious for many problems. For example, the
         famous Fibonacci sequence:
-            F(0) = 1, F(1) = 1, F(n) = F(n - 1) + F(n - 2)
+
+                F(0) = 1, F(1) = 1, F(n) = F(n - 1) + F(n - 2)
+
         The sub-problem F(n) depends on its two predecessors. Therefore, the natural order from 0 to n is the correct
         order. The dependent is always behind the dependee.
-        The terminology of such dependency order is Topological order or Topological sorting:
-        The idea is that in a DAG, we will have some vertex that doesn't depend on others which we call 'leaves'.
+
+        The terminology of such dependency order is Topological Order or Topological Sorting:
+
+                Topological Sorting for Directed Acyclic Graph (DAG) is a linear ordering of vertices such
+                that for every directed edge (u,v) vertex u comes before v in the ordering
+
+        The idea is that in a DAG, we will have some vertices that don't depend on others which we call 'leaves'.
         We put these leaves in a list (their internal ordering does matter), and then we remove them from the DAG.
-        After the removal, there will be new leaves. We do the same repeatedly as if we are peeling an onion layer by
+        After removal, there will be new leaves. We do the same repeatedly as if we are peeling an onion layer by
         layer. In the end, the list will have a valid topological ordering of our vertices.
+
         In our problem, we want the longest path in the DAG, which equals to the total number of layers of the 'onion'.
-        Thus, we can count the number of layers during 'peeling' and return the counts in the end.
+        Thus, we can count the number of layers during 'peeling' and return the count in the end.
+
         The logic is similar to 210- Course Schedule II.
-    Time complexity: O(N * M), the the topological sort is O(V + E) = O(N * M). V is the total number of vertices and
+
+    Time complexity: O(N * M), the topological sort is O(V + E) = O(N * M). V is the total number of vertices and
     E is the total number of edges. In our problem, O(V) = O(N * M), O(E) = O(4V) = O(N * M)
     Space complexity: O(N * M), we need to store the out degrees and each level of leaves in the queue
     """
-    if not matrix:
-        return 0
     n, m = len(matrix), len(matrix[0])
-    outdegree = [[0] * m for _ in range(n)]  # outdegree[i][j] is the number of adjacent cells bigger than matrix[i][j]
+    outdegree = [[0] * m for _ in range(n)]  # outdegree[i][j] is the number of adjacent cells greater than matrix[i][j]
     for i in range(n):
         for j in range(m):
             for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
@@ -100,7 +112,8 @@ def longest_increasing_path_v2(matrix):
         size = len(queue)
         for _ in range(size):
             i, j = queue.popleft()
-            for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+            for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):  # Immediate neighbors form the adjacency list
+                # We're retrieving the nodes of the path backwards: from those with largest values to the smallest
                 if 0 <= x < n and 0 <= y < m and matrix[x][y] < matrix[i][j]:
                     outdegree[x][y] -= 1
                     if outdegree[x][y] == 0:
