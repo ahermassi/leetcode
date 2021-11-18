fed3f1aa1114c5d15b15c9658adf8079433e40ae
Anouer Hermassi <hermassi.anouer@gmail.com>
2021-11-18 08:33:54 -0500

    Find all the shortest transformation sequences from one word to another - v3

--- BFS/126_Word_Ladder_II.py
+++ BFS/126_Word_Ladder_II.py
@@ -54,7 +54,7 @@ def findLadders_v1(begin_word, end_word, word_list):
         Store the words present in wordList in an unordered set so that the words can be efficiently removed during
         the breadth-first search.
 
-        Perform the BFS and once a level is finished remove the visited words from the wordList.
+        Perform the BFS and once a level is finished remove the next_queue words from the wordList.
 
         Start from beginWord and while keeping track of the current path as cur_path traverse all the possible paths,
         and whenever the path leads to the endWord store the path in the final result list.
@@ -65,7 +65,7 @@ def findLadders_v1(begin_word, end_word, word_list):
     word_list = set(word_list)
     res = []
     queue = deque([[begin_word]])
-    visited = set()
+    next_queue = set()
     while queue:
         size = len(queue)
         for _ in range(size):
@@ -78,14 +78,14 @@ def findLadders_v1(begin_word, end_word, word_list):
                     if intermediate_word in word_list:
                         new_path = cur_path[:]  # Path will be reused in the loop so copy it into a new path
                         new_path.append(intermediate_word)
-                        visited.add(intermediate_word)
+                        next_queue.add(intermediate_word)
                         if intermediate_word == end_word:
                             res.append(new_path)
                         else:
                             queue.append(new_path)
-        # 'visited' records all the visited nodes on this level. These words will never be visited again after this
+        # 'next_queue' records all the next_queue nodes on this level. These words will never be next_queue again after this
         # level and should be removed from words list.
-        for word in visited:
+        for word in next_queue:
             word_list.discard(word)
     return res
 
@@ -104,8 +104,8 @@ def findLadders_v2(begin_word, end_word, word_list):
         We still need to rule out all nodes that we have searched previously. This helps avoid add edges connecting to
         strings from the previous level (e.g avoid any back-track path). Meanwhile, if two nodes have the same child
         node, we need to add that child node to both node's children/neighbors list as we need to backtrack all valid
-        paths. So unlike a regular BFS, we can't use a "visited" set but more like an "explored" set. Otherwise,
-        e.g. neighbors: {x->z, y->z}, z won't be added to y's children list if x is visited first and z is already seen
+        paths. So unlike a regular BFS, we can't use a "next_queue" set but more like an "explored" set. Otherwise,
+        e.g. neighbors: {x->z, y->z}, z won't be added to y's children list if x is next_queue first and z is already seen
         in x's search.
 
         Example:
@@ -173,4 +173,87 @@ def findLadders_v2(begin_word, end_word, word_list):
         if end_word_found:
             break
         queue, next_queue = next_queue, set()
+    return build_paths_from(begin_word)
+
+
+def findLadders_v3(begin_word, end_word, word_list):
+    """ Bi-Directional BFS + DFS.
+
+        This approach is very similar to the previous one in that both approaches will use the same Directed Acyclic
+        Graph (DAG). The difference lies in the way that the graph is produced, which is better optimized in this case.
+
+        In the previous solution, we performed BFS starting from beginWord and explored down the ladder until we
+        reached endWord. However, now we will start from beginWord stored in begin_queue and from endWord stored in
+        end_queue and explore towards the middle of the ladder. At each iteration, we will connect the words to their
+        neighbors.
+
+        At each iteration, we will have two options, either to connect edges using the begin_queue or by using the
+        end_queue. The optimal choice is to use the queue that contains fewer words. In this solution, for simplicity,
+        we will swap begin_queue and end_queue whenever begin_queue is larger. In doing so, we will always choose the
+        begin_queue.
+
+        We will traverse the begin_queue and find the neighbors of each of the words present. We will also store the
+        next layer words in the next_queue list. Once a level is finished, we can assign the begin_queue to next_queue.
+        We will repeat this process until either the size of begin_queue becomes zero, signifying that there is no
+        valid sequence from beginWord to endWord, or until the contents of begin_queue and end_queue overlap.
+
+        The reason for stopping when begin_queue and end_queue overlap is because after completing this level, it is
+        guaranteed that beginWord and endWord will be connected by at least one sequence that that passes through the
+        words that exist in both queues. Thus, the DAG will be completed.
+
+        The remaining process is the same as the previous approach: Once the queue has been traversed, at each
+        iteration, we will remove those words from the wordList to avoid including any edges to previous layers. Once
+        the graph is complete, we will use backtracking to find all of the paths between beginWord and endWord.
+
+        Store the words present in wordList in an unordered set so that the words can be efficiently removed during
+        the breadth-first search.
+        Perform a bidirectional BFS. Initialize two queues, begin_queue with beginWord and end_queue with endWord.
+        At each iteration, add the edges to the adjacency list by extending the shorter queue.
+        The parameter 'direction' is used to decide in which direction the edges should be connected, where 1 indicates
+        towards endWord (down the ladder) and vice versa.
+        If a sequence connecting beginWord to endWord does not exist, return an empty list. Otherwise, start from
+        beginWord and while keeping track of the current path traverse all the possible paths, whenever the path leads
+        to the endWord store the path in the result list.
+
+    Time complexity:
+    Space complexity:
+    """
+
+    def build_paths_from(word):
+        if word == end_word:
+            return [[word]]
+        res = []
+        for neighbor in neighbors[word]:
+            rest = build_paths_from(neighbor)
+            for lst in rest:
+                res.append([word] + lst)
+        return res
+
+    if end_word not in word_list:
+        return []
+    word_list = set(word_list)
+    begin_queue, end_queue = {begin_word}, {end_word}
+    next_queue = set()
+    neighbors = defaultdict(list)
+    end_word_found = False
+    direction = 1
+    while begin_queue:
+        word_list -= set(begin_queue)
+        for word in begin_queue:
+            n = len(word)
+            for i in range(n):
+                for c in string.ascii_lowercase:
+                    intermediate_word = word[:i] + c + word[i + 1:]
+                    if intermediate_word in word_list:
+                        neighbors[word].append(intermediate_word) if not direction == 1 else neighbors[intermediate_word].append(word)
+                        if intermediate_word in end_queue:
+                            end_word_found = True
+                        else:
+                            next_queue.add(intermediate_word)
+        if end_word_found:
+            break
+        begin_queue, next_queue = next_queue, set()
+        if len(end_queue) < len(begin_queue):
+            begin_queue, end_queue = end_queue, begin_queue
+            direction = -direction
     return build_paths_from(begin_word)
