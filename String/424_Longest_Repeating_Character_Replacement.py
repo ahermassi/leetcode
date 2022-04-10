019e6ce54ad76d8f71ed1fe4d6435353ef5ce040
Anouer <hermassi.anouer@gmail.com>
2022-04-10 12:48:34 -0400

    Longest repeating character replacement - v2

--- String/424_Longest_Repeating_Character_Replacement.py
+++ String/424_Longest_Repeating_Character_Replacement.py
@@ -10,7 +10,7 @@ import unittest2 as unittest
 # Video explanation: https://www.youtube.com/watch?v=gqXU1UyA8pk
 
 
-def character_replacement(s, k):
+def character_replacement_v1(s, k):
     """ Idea: Since the number of allowed replacements (k) is limited, we want all the characters in a particular window
         to match the most frequent character in that window.
         If we want to replace the characters in a substring and make it into the longest repeating, then we definitely
@@ -42,12 +42,33 @@ def character_replacement(s, k):
     return res
 
 
+def character_replacement_v2(s, k):
+    """ Same idea but we keep shrinking the window from the left AS LONG AS the max number of character replacements
+        exceeds k.
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+    n = len(s)
+    left = right = max_frequency = res = 0
+    counter = defaultdict(int)
+    while right < n:
+        c = s[right]
+        counter[c] += 1
+        max_frequency = max(max_frequency, counter[c])
+        while right - left +  1 - max_frequency > k:
+            counter[s[left]] -= 1
+            left += 1
+        res = max(res, right - left + 1)
+        right += 1
+    return res
+
+
 class Test(unittest.TestCase):
     data = [('ABAB', 2, 4), ('AABABBA', 1, 4)]
 
     def test_character_replacement(self):
         for test_s, test_k, result in self.data:
-            self.assertEqual(result, character_replacement(test_s, test_k))
+            self.assertEqual(result, character_replacement_v2(test_s, test_k))
 
 
 if __name__ == '__main__':
