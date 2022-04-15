f1e4ee38037685d7a2e1eedc9724cf0c635abf8d
Anouer <hermassi.anouer@gmail.com>
2022-04-15 08:11:48 -0400

    Top K frequent elements - v4 update

--- Heap/347_Top_K_Frequent_Elements.py
+++ Heap/347_Top_K_Frequent_Elements.py
@@ -71,13 +71,26 @@ def top_k_frequent_v4(nums, k):
         Note that elements are unique, but their frequencies are not. If by chance our pivot element took (N - k)th
         final position, then k elements on the right are these top k frequent we're looking for. If not, we can choose
         one more pivot and place it in its perfect position.
+
         Work with 'unique_nums' array. Use a partition scheme to place the pivot into its perfect position 'pivot_index'
-        in the sorted array, move less frequent elements to the left of pivot, and more frequent or of the same
-        frequency to the right.
+        in the sorted array. As an output, we have an array where the pivot is on its perfect position in the ascending
+        sorted array, sorted by the frequency. All elements on the left of the pivot are less frequent than the pivot,
+        and all elements on the right are more frequent or have the same frequency.
+
+        There are different partition algorithms. The most simple one is Lomuto's Partition Scheme, and so is what
+        we will use. Here is how it works:
+
+        Move pivot at the end of the array using swap.
+        Set the store pointer i and scan pointer j at the beginning of the array: i = j = left.
+        Iterate over the array and move all less frequent elements to the left: swap(i, j).
+        Move i one step to the right after each swap.
+        Once the entire array scanned, move the pivot to its original place and return this index.
+
         Compare 'pivot_index' and (N - k).
             - If pivot_index == N - k, the pivot is (N - k)th most frequent element, and all elements on the right are
               more frequent or of the same frequency. Return these top k frequent elements: unique_nums[N - k:]
             - Otherwise, choose the side of the array to proceed accordingly.
+
     Time complexity: O(N) in the average case, O(N^2) in the worst case. In the worst-case of constantly bad chosen
     pivots, the problem is not divided by half at each step, it becomes just one element less, that leads to O(N^2)
     time complexity. It happens, for example, if at each step we choose the pivot not randomly, but take the rightmost
@@ -87,11 +100,11 @@ def top_k_frequent_v4(nums, k):
 
     def partition(left, right):
         pivot_index = randint(left, right)
-        pivot = unique_nums[left]
+        pivot = unique_nums[pivot_index]
         unique_nums[pivot_index], unique_nums[right] = unique_nums[right], unique_nums[pivot_index]
         i = j = left  # i will keep track of the 'tail' of the section of items less than the pivot so that
         # at the end we can 'sandwich' the pivot between the section less than it and the section equal to or greater
-        # than it. j will scan for us. All the elements before i (excluding i) are less than the pivot
+        # than it. j will scan for us. All the elements before i (excluding i) are less than the pivot.
         while j < right:
             if counter[unique_nums[j]] < counter[pivot]:
                 unique_nums[i], unique_nums[j] = unique_nums[j], unique_nums[i]
@@ -104,7 +117,10 @@ def top_k_frequent_v4(nums, k):
     counter = Counter(nums)
     unique_nums = list(counter.keys())
     n = len(unique_nums)
-    k = n - k  # kth top frequent element is (n - k)th less frequent.
+    # kth largest element in an array of n elements is (n - k + 1)th smallest (think 1st largest is nth smallest).
+    # Similarly, kth most frequent element is (n - k + 1)th less frequent.
+    # Since indexing is 0-based, kth most frequent is at index (n - k) in the frequency-sorted array.
+    k = n - k
     left, right = 0, len(unique_nums) - 1
     while True:
         pivot_index = partition(left, right)
@@ -121,9 +137,10 @@ class Test(unittest.TestCase):
 
     def test_top_k_frequent(self):
         for test_array, test_k, result in self.data:
-            self.assertEqual(result, top_k_frequent_v1(test_array, test_k))
-            self.assertEqual(result, top_k_frequent_v2(test_array, test_k))
-            self.assertEqual(result, top_k_frequent_v3(test_array, test_k))
+            # self.assertEqual(result, top_k_frequent_v1(test_array, test_k))
+            # self.assertEqual(result, top_k_frequent_v2(test_array, test_k))
+            # self.assertEqual(result, top_k_frequent_v3(test_array, test_k))
+            self.assertEqual(result, top_k_frequent_v4(test_array, test_k))
 
 
 if __name__ == '__main__':
