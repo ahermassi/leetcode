c7e34d6dac514d58c7e37a95d5737a861ca00419
Anouer <hermassi.anouer@gmail.com>
2022-04-18 11:24:00 -0400

    3Sum - v2

--- Arrays/15_3Sum.py
+++ Arrays/15_3Sum.py
@@ -6,7 +6,7 @@ The solution set must not contain duplicate triplets. """
 import unittest2 as unittest
 
 
-def three_sum(nums):
+def three_sum_v1(nums):
     """ We will follow the same two pointers pattern as in other similar sum problems. It requires the array to be
         sorted, so we'll do that first. To make sure the result contains unique triplets, we need to skip duplicate
         values. It is easy to do because repeating values are next to each other in a sorted array.
@@ -57,12 +57,52 @@ def three_sum(nums):
     return res
 
 
+def three_sum_v2(nums):
+    """ Since triplets must sum up to the target value, we can try the hash table approach from the Two Sum solution.
+
+        We move our pivot element nums[i] and analyze elements to its right. We find all pairs whose sum is equal
+        -nums[i] using the Two Sum: One-pass Hash Table approach, so that the sum of the pivot element (nums[i])
+        and the pair (-nums[i]) is equal to zero.
+
+        To do that, we process each element nums[j] to the right of the pivot, and check whether a complement
+        -(nums[i] + nums[j]) is already in the hashset. If it is, we found a triplet. Then, we add nums[j] to the
+        hashset, so it can be used as a complement from that point on.
+
+        Like in the previous approach, we will also sort the array, so we can skip repeated values.
+
+    Time complexity: O(N logN + N^2) = O(N^2)
+    Space complexity: O(N)
+    """
+    nums.sort()
+    n, res = len(nums), []
+    for i in range(n - 2):
+        if nums[i] > 0:
+            break
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        a = nums[i]
+        j = i + 1
+        seen = set()
+        while j < n:
+            b = nums[j]
+            c = - (a + b)
+            if c in seen:
+                res.append([a, b, c])
+                # Increment j while the next value is the same as before to avoid duplicates in the result
+                while j < n - 1 and nums[j] == nums[j + 1]:
+                    j += 1
+            seen.add(b)
+            j += 1
+    return res
+
+
 class Test(unittest.TestCase):
     data = [([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]])]
 
     def test_three_sum(self):
         for test_array, result in self.data:
-            self.assertEqual(result, three_sum(test_array))
+            self.assertEqual(result, three_sum_v1(test_array))
+            self.assertEqual(result, three_sum_v2(test_array))
 
 
 if __name__ == '__main__':
