f9fdc89a3d57650e9559b48ee57e4d931b577fd2
Anouer <hermassi.anouer@gmail.com>
2022-04-24 09:22:57 -0400

    Encode and decode strings - v1 update

--- String/271_Encode_and_Decode_Strings.py
+++ String/271_Encode_and_Decode_Strings.py
@@ -5,27 +5,21 @@ is decoded back to the original list of strings. """
 class CodecV1:
     """ Naive solution here is to join strings using delimiters.
         What to use as a delimiter? Each string may contain any possible characters out of 256 valid ascii characters.
-        It's convenient to use two different non-ASCII characters to distinguish between situations of 'empty array'
-        and of 'array of empty strings'.
     """
 
-    def encode(self, strs: [str]) -> str:
+    def encode(self, strs):
         """ Encodes a list of strings to a single string.
-        Time complexity: O(N)
+        Time complexity: O(N), where N is a number of strings in the input array
         Space complexity: O(1)
         """
-        if not strs:
-            return chr(258)
-        return chr(259).join(strs)
+        return chr(258).join(strs)
 
-    def decode(self, s: str) -> [str]:
+    def decode(self, s):
         """ Decodes a single string to a list of strings.
-        Time complexity: O(N)
+        Time complexity: O(N), O(N), where N is a number of strings in the input array
         Space complexity: O(1)
         """
-        if s == chr(258):
-            return None
-        return s.split(chr(259))
+        return s.split(chr(258))
 
 
 class CodecV2:
