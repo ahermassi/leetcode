b7486f06988ac8208b144f05b3e0ea26debab15b
Anouer <hermassi.anouer@gmail.com>
2022-04-24 09:30:57 -0400

    Encode and decode strings - v2 update

--- String/271_Encode_and_Decode_Strings.py
+++ String/271_Encode_and_Decode_Strings.py
@@ -21,51 +21,43 @@ class CodecV1:
         """
         return s.split(chr(258))
 
+# Video explanation: https://www.youtube.com/watch?v=B1k_sxOSgv8
+
 
 class CodecV2:
     """ This approach doesn't depend on the set of input characters, and hence is more versatile and effective than
         Approach 1.
-        Data stream is divided into chunks. Each chunk is preceded by its size.
+        Data stream is divided into chunks. Each chunk is preceded by its size and '#' delimiter between the length and
+        the actual string. It acts as a boundary to show where the length string ends when the length has multiple
+        digits and/or the string itself starts with a digit. If we know all substring are shorter than 10 characters,
+        then we wouldn't have to use any separator like '#'.
     """
 
     def encode(self, strs: [str]) -> str:
-        """ Encodes a list of strings to a single string.
-            Iterate over the array of strings
-            For each string, compute its length
-            Append to encoded string: information about chunk size and chunk itself.
+        """ Encode a list of strings to a single string.
+            Iterate over the array of strings. For each string, compute its length and append it to the encoded string:
+            Information about chunk size and chunk itself.
         Time complexity: O(N)
         Space complexity: O(1)
         """
         res = []
         for s in strs:
-            res.extend([str(len(s)), ':', s])
+            res.extend([str(len(s)), '#', s])
         return ''.join(res)
 
     def decode_v1(self, s: str) -> [str]:
-        """ Decodes a single string to a list of strings.
+        """ Decode a single string to a list of strings.
         Time complexity: O(N)
         Space complexity: O(1)
         """
-        i, n, res = 0, len(s), []
+        n, res = len(s), []
+        i = 0
         while i < n:
             j = i
-            while s[j] != ':':
+            while s[j] != '#':
                 j += 1
-            size = int(s[i:j])
-            res.append(s[j + 1:j + 1 + size])
-            i = j + size + 1
-        return res
-
-    def decode_v2(self, s: str) -> [str]:
-        """ Decodes a single string to a list of strings.
-        Time complexity: O(N)
-        Space complexity: O(1)
-        """
-        i, n, res = 0, len(s), []
-        while i < n:
-            index = s.find(':', i)
-            size = int(s[i:index])
-            res.append(s[index + 1:index + 1 + size])
-            i = index + 1 + size
+            length = int(s[i:j])
+            res.append(s[j + 1:j + 1 + length])
+            i = j + 1 + length
         return res
 
