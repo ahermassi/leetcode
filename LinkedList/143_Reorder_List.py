5d6306d043405c6b21baa703da7b742d3445bd7d
Anouer <hermassi.anouer@gmail.com>
2022-04-26 12:18:14 -0400

    Reorder list - v1 update

--- LinkedList/143_Reorder_List.py
+++ LinkedList/143_Reorder_List.py
@@ -11,9 +11,26 @@ class ListNode(object):
         self.val = x
         self.next = None
 
+# Video explanation: https://www.youtube.com/watch?v=S5bfdUTrKLM
+
 
 def reorder_list_v1(head):
-    """ Find the middle of the list. Reverse the half after middle. Start reordering one by one.
+    """ Reverse the second half of the list and merge the two halves.
+
+        Let's use two pointers, slow and fast. While the slow pointer moves one step forward, the fast pointer moves
+        two steps forward, i.e. fast traverses twice as fast as slow. When the fast pointer reaches the end of the list,
+        the slow pointer should be in the middle.
+
+        Let's traverse the list starting from the middle node slow and its virtual predecessor None. For each current
+        node, save its neighbours: The previous node prev and the next node nxt = cur.next.
+        While we're moving along the list, change the node's next pointer to point to the previous node and shift the
+        current node to the right for the next iteration
+
+        Let's pick the first node of each list - first and second, and save their successors. While we're traversing the
+        list, set the first node's next pointer to point to the second node, and the second node's next pointer to point
+        to the successor of the first node. For this iteration the job is done, and for the next iteration move to the
+        previously saved nodes' successors.
+
     Time complexity: O(N)
     Space complexity: O(1)
     """
@@ -22,18 +39,21 @@ def reorder_list_v1(head):
     slow, fast = head, head
     while fast and fast.next:
         slow, fast = slow.next, fast.next.next
-    cur, pre = slow, None
+    cur, prev = slow, None
     while cur:
         nxt = cur.next
-        cur.next = pre
-        pre, cur = cur, nxt
+        cur.next = prev
+        prev, cur = cur, nxt
     # That's equivalent to the more Pythonic following block:
     # while cur:
-    #     pre, cur.next, cur = cur, pre, cur.next
-    first, second = head, pre  # 'pre' points to the head of second reversed half, which used to be the last 'cur'
+    #     prev, cur.next, cur = cur, prev, cur.next
+    first, second = head, prev  # 'prev' points to the head of second reversed half, which used to be the last 'cur'
     while second.next:  # Rewire the nodes
-        first.next, first = second, first.next
-        second.next, second = first, second.next
+        nxt1, nxt2 = first.next, second.next
+        first.next = second
+        first = nxt1
+        second.next = first
+        second = nxt2
     return head
 
 
