e1aa46b940240d94ed09a0a17e93f43506efaa8e
Anouer <hermassi.anouer@gmail.com>
2022-05-05 09:11:39 -0400

    Find minimum in rotated sorted array - v2 update

--- BinarySearch/153_Find_Minimum_in_Rotated_Sorted_Array.py
+++ BinarySearch/153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -61,7 +61,25 @@ def find_min_v1(nums):
 
 def find_min_v2(nums):
     """ The main idea for our checks is to converge the left and right bounds to the start of the pivot, or Inflection
-        Point, and never disqualify the index for a possible minimum value.
+         Point, and never disqualify the index for a possible minimum value.
+
+         Remember that:
+
+            - All the elements to the left of Inflection Point > leftmost element of the array
+            - All the elements to the right of Inflection Point < rightmost element of the array
+
+        Notice the numbers are divided into two sections: numbers larger than the rightmost element of the array and
+        numbers smaller than it. The minimum element is at the boundary between the two sections.
+
+        A better way to visualize thus algorithm is thinking that we apply a filter of "< rightmost element" to each
+        value in the array, which conceptually produces a boolean array.
+        For example, nums = [3,4,5,1,2] => filter( < rightmost element) => [F, F, F, F, T, T]
+
+        Now the problem becomes finding the first T which can easily be solved using binary search.
+
+        Binary search can work beyond sorted arrays, as long as there is a BINARY DECISION we can use to shrink the
+        search range.
+
     Time complexity: O(logN)
     Space complexity: O(1)
     """
@@ -72,18 +90,24 @@ def find_min_v2(nums):
             left = mid + 1
             # If nums[mid] > nums[right], we know that the discrepancy must have occurred somewhere to the right of mid.
             # mid can't be the minimum, so we can safely move left to mid + 1, which ensures the interval is always
-            # shrinking
+            # shrinking.
             # Example: [3, 4, 5, 6, 7, 8, 9, 1, 2]. In the first iteration, we start with mid = 4, right = 9.
             # nums[mid] > nums[right], so we know that at some point to the right of mid, the pivot must have occurred.
             # We also know that the number at mid is greater than AT LEAST one number to the right, so we can use
             # mid + 1 and never consider mid again.
         else:
             right = mid
-            # Since we use round up for mid, and left < right, right would never be the same as mid.
-            # Therefore, we compare nums[mid] with nums[right], since they will never be the same.
-            # If nums[mid] <= nums[right], we know the minimum should be in the left part, so we are moving left.
-            # We can always make right = mid while we don't have to worry the loop will not end, since we know right
-            # would never be the same as mid, making right = mid ensure the interval is always shrinking.
+            # Example: [8,9,1,2,3,4,5,6,7]
+            # In the first iteration, when we start with mid = 4, right = 9.
+            # nums[mid] <= nums[right]; we know the numbers continued increasing to the right of mid, so
+            # they never reached the pivot and wrapped around. Therefore, we know the pivot must be at index <= mid.
+            # We also now know mid stores a smaller value than at least one other index in the list (to its right),
+            # so we do not discard it by doing right = mid - 1. It still might have the minimum value.
+            # Since we use round down for mid, and left < right, right would never be the same as mid. This situation
+            # becomes very clear in the case of a two-element array (or just in general when the left and right pointers
+            # are adjacent, .i.e. right = left + 1, in which case mid = left) Therefore, we can always make right = mid
+            # while we don't have to worry the loop will not end, ensuring the interval is always shrinking.
+
     # At this point, left and right converge to a single index (for minimum value). Our if/else block forces the bounds
     # of left/right to shrink each iteration.
     # We shrink the left/right bounds to one value, without ever disqualifying a possible minimum
