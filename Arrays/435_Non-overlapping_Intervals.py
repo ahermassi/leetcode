1a776aa144168eace348633061897aa375f6eee8
Anouer <hermassi.anouer@gmail.com>
2022-05-09 14:00:55 -0400

    Non-overlapping intervals - v3 update

--- Arrays/435_Non-overlapping_Intervals.py
+++ Arrays/435_Non-overlapping_Intervals.py
@@ -96,23 +96,26 @@ def erase_overlap_intervals_v2(intervals):
 
 
 def erase_overlap_intervals_v3(intervals):
-    """ Dynamic programming. TLE.
+    """ Dynamic Programming. TLE.
+
         Let dp[i] be the maximum number of valid intervals that can be included in the final list if only the intervals
-        up to and including the ith interval are considered. To find dp[i+1], we can't consider the value of dp[i-1]
-        only, because it could be possible that the (i-1)th or any previous interval is overlapping with the ith
-        interval. Thus, we need to consider the maximum of all dp[j]'s such that j ≤ i and jth and ith intervals don't
-        Therefore:
-            dp[i] = max(dp[j] for 0 <= j < i such that jth and ith intervals don't overlap) + 1
+        up to and including the ith interval are considered.
+
+        To find dp[i+1], we can't consider the value of dp[i-1] only, because it could be possible that the (i-1)th or
+        any previous interval is overlapping with the ith interval. Thus, we need to consider the maximum of all
+        dp[j]'s such that j < i and jth and ith intervals don't overlap. Therefore:
+
+                    dp[i] = max(dp[j] for 0 <= j < i such that jth and ith intervals don't overlap) + 1
+
         In the end, to obtain the maximum number of intervals that can be included in the final list, let's call it
         'res', we need to find the maximum value in the dp array. The final result will be the total number of input
         intervals given minus the result just obtained: len(intervals) - result
+
     Time complexity: O(N^2)
     Space complexity: O(N)
     """
-    if not intervals:
-        return 0
-    n = len(intervals)
     intervals.sort()
+    n = len(intervals)
     dp = [0] * n
     dp[0] = 1
     for i in range(1, n):
