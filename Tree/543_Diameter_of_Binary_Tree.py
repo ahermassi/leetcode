2d1e840a5724bc2c9584de70560e0ad591e65eaa
Anouer <hermassi.anouer@gmail.com>
2022-05-12 08:33:01 -0400

    Diameter of binary tree - update

--- Tree/543_Diameter_of_Binary_Tree.py
+++ Tree/543_Diameter_of_Binary_Tree.py
@@ -13,55 +13,66 @@ class TreeNode(object):
         self.right = None
 
 
-def diameter_of_binary_tree_v1(root):
-    """ The diameter is the maximum of either:
-            1- Passing through the root, in which case the longest path would be using the maximum height of left and
-               right child
-            2- The diameter of the left child
-            3- The diameter of the right child
+# Video explanation: https://www.youtube.com/watch?v=bkxqA8Rfv04
+
+def diameter_of_binary_tree(root):
+    """ The key observation to make is:
+
+                    The longest path has to be between two leaf nodes
+
+         We can prove this with contradiction. Imagine that we have found the longest path, and it is not between two
+         leaf nodes. We can extend that path by 1, by adding the child node of one of the end nodes (as at least one
+         must have a child, given that they aren't both leaves). This contradicts the fact that our path is the longest
+         path. Therefore, the longest path must be between two leaf nodes.
+
+        Moreover, we know that in a tree, nodes are only connected with their parent node and 2 children. Therefore, we
+        know that the longest path in the tree would consist of a node, its longest left branch, and its longest right
+        branch. So, our algorithm to solve this problem will find the node where the sum of its longest left and right
+        branches is maximized. This would hint at us to apply Depth-first search (DFS) to count each node's branch
+        lengths, because it would allow us to dive deep into the leaves first, and then start counting the edges
+        upwards.
+
+        Let's try to be more specific about how to apply DFS to this question. To count the lengths of each node's left
+        and right branches, we can implement a recursive function height(root) which takes a TreeNode as input and
+        returns the height of that node.
+
         So, we can solve this problem with two different cases:
-            1- If the longest path will include the root node, then the longest path must be: left height + right height
-            2- If the longest path does not include the root node, this problem is divided into 2 sub-problems:
-               set left child and right child as the new root separately, and repeat previous step.
+
+            1- If the longest path will include the current root node, then the longest path must be:
+                        left height + right height + 2
+                 The +2 accounts for 1 edge leading to each tree on the left and right
+
+            2- If the longest path does not include the current root node, this problem is divided into 2 sub-problems:
+                 Set left child and right child as the new root separately, and repeat previous step.
+
         Conclusion:
-        Diameter of a tree with regards to root root can be defined as:
-            Maximum(Diameter of left subtree, Diameter of right subtree, Longest path between two nodes which passes
+
+        Diameter of a tree in regard to root can be defined as:
+
+                Maximum(Diameter of left subtree, Diameter of right subtree, Longest path between two nodes which passes
                 through the root)
+
         Now, the diameter of left and right subtrees can be solved recursively. Longest path between two nodes which
-        passes through the root can be calculated as: height of left subtree + height of right subtree. Therefore:
-            Diameter = max(Diameter of left subtree, Diameter of right subtree, left height + right height)
+        passes through the root can be calculated as: height of left subtree + height of right subtree + 2. Therefore:
+
+                Diameter = max(Diameter of left subtree, Diameter of right subtree, left height + right height + 2)
+
     Time complexity: O(N)
     Space complexity: O(N)
     """
-    def dfs(root):
+
+    def height(root):
         if not root:
-            return 0, 0  # Return height, diameter
-        left_height, left_diameter = dfs(root.left)
-        right_height, right_diameter = dfs(root.right)
-        cur_height = 1 + max(left_height, right_height)
-        cur_diameter = max(left_height + right_height, left_diameter, right_diameter)  # Cases 1, 2, 3 respectively
-        return cur_height, cur_diameter
+            return -1
+        left_height = height(root.left)
+        right_height = height(root.right)
+        diameter[0] = max(diameter[0], left_height + right_height + 2)  # The "+2" accounts for the edge on the left
+        # plus the edge on the right
+        return max(left_height, right_height) + 1
 
-    return dfs(root)[1]
-
-
-class SolutionV2:
-    """ Using a class variable. """
-
-    def __init__(self):
-        self.diameter = 0
-
-    def diameter_of_binary_tree(self, root: TreeNode) -> int:
-
-        def height(root):
-            if not root:
-                return 0
-            left_height, right_height = height(root.left), height(root.right)
-            self.diameter = max(self.diameter, left_height + right_height)
-            return 1 + max(left_height, right_height)
-
-        height(root)
-        return self.diameter
+    diameter = [0]
+    height(root)
+    return diameter[0]
 
 
 class Test(unittest.TestCase):
@@ -72,7 +83,7 @@ class Test(unittest.TestCase):
     root.left.right = TreeNode(5)
 
     def test_diameter_of_binary_tree(self):
-        self.assertEqual(3, diameter_of_binary_tree_v1(self.root))
+        self.assertEqual(3, diameter_of_binary_tree(self.root))
 
 
 if __name__ == '__main__':
