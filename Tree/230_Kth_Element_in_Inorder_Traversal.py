6937f9b1725f06e755c2c8d3d18a96560ec83e1c
Anouer <hermassi.anouer@gmail.com>
2022-05-17 09:45:30 -0400

    Kth smallest element in inorder traversal - update

--- Tree/230_Kth_Element_in_Inorder_Traversal.py
+++ Tree/230_Kth_Element_in_Inorder_Traversal.py
@@ -11,18 +11,19 @@ class TreeNode(object):
 
 def find_kth_node_binary_tree(root, k):
     """ If k is greater than the number of nodes in the left subtree, the kth node cannot lie in the left subtree.
-        More precisely, if the left subtree has L nodes, then the kth node in the original tree is the (k - L - 1)th
+         More precisely, if the left subtree has L nodes, then the kth node in the original tree is the (k - L - 1)th
         node when we skip the left subtree. Conversely, if k < L, the desired node lies in the left subtree.
+
     Time complexity: O(logN) best case, O(N) worst case of a skewed tree
     Space complexity: O(1)
     """
     cur = root
     while cur:
         left_size = cur.left.size if cur.left else 0
-        if left_size + 1 < k:
-            cur = cur.right
-        elif left_size + 1 == k:
+        if left_size == k - 1:
             return cur
+        if left_size < k - 1:
+            cur = cur.right
         else:
             cur = cur.left
     return None
