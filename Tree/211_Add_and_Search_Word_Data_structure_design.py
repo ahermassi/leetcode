fbc5657df951b92acd38c2f0c3860434586eb488
Anouer <hermassi.anouer@gmail.com>
2022-05-20 08:51:14 -0400

    Design add and search words data structure - v1 update

--- Tree/211_Add_and_Search_Word_Data_structure_design.py
+++ Tree/211_Add_and_Search_Word_Data_structure_design.py
@@ -4,47 +4,59 @@ bool search(word)
 search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it
 can represent any one letter. """
 
+from collections import defaultdict
 import unittest2 as unittest
+
+# Video explanation: https://www.youtube.com/watch?v=BTf05gs_8iU
 
 
 class TrieNode:
     def __init__(self):
-        self.children = {}
+        self.children = defaultdict(TrieNode)
         self.end_of_word = False
 
 
 class WordDictionaryV1(object):
-    """ Use a trie to store the words, and backtracking to check each character of word to search.
+    """
+        It's quite easy to write the solution using such data structures as hashmap or balanced tree. Once the hash
+        table increases in size, there are a lot of hash collisions
+
+        Trie could use less space compared to hashmap when storing many keys with the same prefix.
+
+        Use a trie to store the words, and backtracking to check each character of word to search.
     """
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.root = TrieNode()
 
     def addWord(self, word):
         """ Adds a word into the data structure.
+
+            At each step, we have to verify if the child node to add is already present. If yes, just go one step down.
+            If not, add it into the trie and then go one step down.
+
         Time complexity: O(M), where M is the length of word
-        Space complexity: O(M) in the worst case where the newly inserted word doesn't share a prefix with the the
+        Space complexity: O(M) in the worst case where the newly inserted word doesn't share a prefix with the
         words already inserted in the trie. We have to add M new nodes.
         """
         root = self.root
         for c in word:
-            if c not in root.children:
-                root.children[c] = TrieNode()
             root = root.children[c]
         root.end_of_word = True
 
     def search(self, word):
         """ Returns if the word is in the data structure. A word could contain the dot character '.' to represent
             any one letter.
+
             The function tries to find a suffix of the string that starts at 'index'. If the value of one of the
             children of the current trie node is equal to the character at current 'index', we recursively try to find
             the rest of the string starting at ('index' + 1), and the exploration starts from that child.
+
+            The presence of '.' characters forces us to explore all possible paths at each . level.
             If the first character of the suffix is the wildcard '.', then all the children of the current trie node
             are good candidates to hold the rest of the string. If any of the children returns a positive result,
             we win.
+
         Time complexity: O(M) if we exclude wildcards (.), worst case search will iterate through the longest word
         (i.e. linear to the maximum number of characters in a word). With wildcards, it will iterate through the whole
         tree in the worst case (i.e. linear to the total number of characters), which is O(26^M) when we have an
@@ -54,19 +66,22 @@ class WordDictionaryV1(object):
         we search in 26 more branches until we find the match.
         """
 
-        def dfs(root, index):
+        def dfs(node, index):
             if index == n:
-                return root.end_of_word
+                return node.end_of_word
             c = word[index]
             if c == '.':
-                # Can we find the REST of the string in any of the children ?
-                return any(dfs(child, index + 1) for child in root.children.values())
-            if c not in root.children:
+                # Can we find the REST of the string in any of the children?
+                for child in node.children.values():
+                    if dfs(child, index + 1):
+                        return True
                 return False
-            return dfs(root.children[c], index + 1)  # Start of the prefix was found, so keep following that path
+            if c not in node.children:
+                return False
+            return dfs(node.children[c], index + 1)  # Start of the prefix was found, so keep following that path
 
-        n, root = len(word), self.root
-        return dfs(root, 0)
+        n = len(word)
+        return dfs(self.root, n)
 
 
 class WordDictionaryV2(object):
