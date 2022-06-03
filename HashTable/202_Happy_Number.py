c14acb2ea19c9d092045e8c26a605dfa0c5769f0
Anouer <hermassi.anouer@gmail.com>
2022-06-03 08:11:21 -0400

    Happy number - v1 update

--- HashTable/202_Happy_Number.py
+++ HashTable/202_Happy_Number.py
@@ -8,27 +8,44 @@ import unittest2 as unittest
 
 
 def is_happy_v1(n):
-    """ There are 2 parts to the algorithm we'll need to design and code.
+    """ There are 2 parts to the algorithm we'll need to design and code:
+
             1- Given a number n, what is its next number?
             2- Follow a chain of numbers and detect if we've entered a cycle.
+
         Part 1 can be done using the division and modulus operators to repeatedly take digits off the number until none
         remains, and then squaring each removed digit and adding them together.
+
         Part 2 can be done using a hash set. Each time we generate the next number in the chain, we check if it's
         already in our hash set. If it is not in the hash set, we should add it. If it is in the hash set, that means
         we're in a cycle and so should return false.
-    Time complexity: we are processing each digit in the number, and the number of digits in a number is given by logn.
-    Space complexity: O(logn) ?
+
+    Time complexity: O(log n), we are processing each digit in the number, and the number of digits in a number is given
+    by log(n). Think about what would happen if we had a number with 1 million digits in it. The first step of the
+    algorithm would process those million digits, and then the next value would be, at most (pretend all the digits
+    are 9), be 81 * 1,000,000 = 81,000,000. In just one step, we've gone from a million digits, down to just 8.
+    The largest possible 8-digit number we could get is 99,999,999, which then goes down to 81 * 8 = 648. And then from
+    here, the cost will be the same as if we'd started with a 3-digit number. Starting with 2 million digits
+    (a massively larger number than one with a 1 million digits) would only take roughly twice as long, as again, the
+    dominant part is summing the squares of the 2 million digits, and the rest is tiny in comparison.
+    What is the maximum number of loops?
+    Imagine the input has x digits, so the first sum of squares is not bigger than 81x (with all digits equals 9).
+    And we can prove that when x > 2, sum of squares of the input will be definitely smaller than input itself
+    (compare a * a and a*10^x with a in one digit), which means the next sum of squares will be smaller than the first
+    one, which means the maximum square sum is not bigger than 81x. So in the worst case, suppose x is bigger than 2,
+    the maximum number of loops should be at most 81x times (from 1 to 81x, which is clearly not reachable).
+    Space complexity: O(log n) ?
     """
 
     def digit_square_sum(n):
         total_sum = 0
         while n:
             digit = n % 10
-            n = n // 10
             total_sum += digit ** 2
+            n = n // 10
         return total_sum
 
-    seen = {n}
+    seen = set()
     while n != 1:
         n = digit_square_sum(n)
         if n in seen:
