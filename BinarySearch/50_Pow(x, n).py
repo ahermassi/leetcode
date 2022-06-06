05cb24b894366d465e949ca0bf17cc905fb03e88
Anouer <hermassi.anouer@gmail.com>
2022-06-06 08:32:36 -0400

    pow(x, n) - v1 update

--- BinarySearch/50_Pow(x, n).py
+++ BinarySearch/50_Pow(x, n).py
@@ -6,11 +6,16 @@ import unittest2 as unittest
 def my_pow_v1(x, n):
     """ Assuming we have got the result of (x^n), how can we get (x^2n) ? Obviously, we do not need to multiply x
         for another n times. Using the formula (x^n)^2 = x^2n, we can get (x^2n) at the cost of only one computation.
+
         Using this optimization, we can reduce the time complexity of our algorithm.
-        If n is even, x^2n = x^n * x^n
-        If n is odd, x^2n = x * x^n * x^n
+
+            - If n is even, x^2n = x^n * x^n
+
+            - If n is odd, x^2n = x * x^n * x^n
+
         This approach can be easily implemented using recursion. We call this method 'Fast Power', because we only need
-        at most O(logn) computations to get (x^n).
+        at most O(log n) computations to get (x^n).
+
         Example: x = 5, n = 100
         x^100 = x^50 * x^50 = x^(50*2)
         x^50 = x^25 * x^25 = x^(25*2)
@@ -20,19 +25,20 @@ def my_pow_v1(x, n):
         x^3 = x * x^1 * x^1 = x * x^(1*2)
         x^1 = x * x^0 * x^0 = x * x^(0*2)
         x^0 = 1: base case
-        So we went from calculating x^100 to: 100 -> 50 -> 25 -> 12 -> 6 -> 3 -> 1 -> 0, giving logn time complexity.
-    Time complexity: O(logn), each time we apply the formula (x ^ n) ^ 2 = x ^ {2 * n}, n is reduced by half. Thus we
-    need at most O(logn) computations to get the result
-    Space complexity: O(logn)
+        So we went from calculating x^100 to: 100 -> 50 -> 25 -> 12 -> 6 -> 3 -> 1 -> 0, giving log n time complexity.
+
+    Time complexity: O(log n), each time we apply the formula (x ^ n) ^ 2 = x ^ 2n, n is reduced by half. Thus we
+    need at most O(log n) computations to get the result
+    Space complexity: O(log n)
     """
 
     def get_power(x, n):
         if n == 0:
             return 1
-        half = get_power(x, n // 2)
+        half_power = get_power(x, n // 2)
         if n % 2 == 0:
-            return half * half
-        return x * half * half
+            return half_power * half_power
+        return x * half_power * half_power
 
     if n < 0:
         x, n = 1/x, -n
