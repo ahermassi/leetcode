f08d25a02c56f28d0f2b4c30b7c485de8e32a1b5
Anouer <hermassi.anouer@gmail.com>
2022-06-28 08:07:16 -0400

    Surrounded regions - v1 update

--- DFS/130_Surrounded_Regions.py
+++ DFS/130_Surrounded_Regions.py
@@ -4,50 +4,72 @@ from collections import deque
 
 import unittest2 as unittest
 
+# Video explanation: https://www.youtube.com/watch?v=9z2BunfoZ5Y
+
 
 def solve_v1(board):
-    """ It is easier to focus on the inverse problem, namely identifying 'O's that can reach the boundary. The reason
-        that the inverse is simpler is that if an 'O' is adjacent to an 'O that can reach the boundary, then the first
+    """ If we are asked to summarize the algorithm in one sentence, it would be that we enumerate all those candidate
+         cells (i.e. the ones filled with O), and check one by one if they are captured or not, i.e. we start with a
+         candidate cell (O), and then apply either DFS or BFS strategy to explore its surrounding cells.
+
+        It is easier to focus on the inverse problem, namely identifying 'O's that can reach the border. The reason
+        that the inverse is simpler is that if an 'O' is adjacent to an 'O that can reach the border, then the first
         'O' can reach it too.
+
         Surrounded regions shouldn't be on the border, which means that any 'O' on the border of the board is not
         flipped to 'X'.
+
         Any 'O' that is not on the border but is connected to an 'O' on the border will not be flipped to 'X'.
         Any 'O' that is not on the border and is not connected to an 'O' on the border will be flipped to 'X'.
-        The idea is to first find all 'O's on the edge, and do DFS from these 'O's. Mark all 'O's encountered as '1'
-        since these 'O's are found by doing DFS from the 'O's on the edge, which means they are connected to the edge
-        'O's and hence they are the 'O's that will remain as 'O' in the result (not surrounded).
-        At the end of DFS, there are some 'O's that could not be reached. These are the 'O's that need to be turned to
-        'X' (surrounded).
-            1- Check the four borders of the matrix. If a cell is 'O', alter it and all its neighbor 'O's to '1'. Then
-               after DFS:
-            2- Alter all the 'O's to 'X'
-            3- Alter all the '1's to 'O'
+
+        The algorithm consists of three steps:
+
+            1- We select all the cells that are located on the borders of the board.
+
+            2- Start from each of the above selected cell, we then perform the DFS traversal.
+                 If a cell on the border happens to be O, then we know that this cell is alive, together with the other
+                 O cells that are connected to this border cell. Two cells are connected, if there exists a path
+                 consisting of only O letter that bridges between the two cells.
+                Based on the above conclusion, the goal of our DFS traversal would be to mark out all those connected O
+                cells that is originated from the border, with any distinguished letter such as T
+
+            3- Once we iterate through all border cells, we would then obtain three types of cells:
+                    - The one with the X letter: The cell that we could consider as the wall.
+                    - The one with the O letter: The cells that are spared in our DFS traversal, i.e. these cells have
+                       no connection to the border, therefore they are captured. We then should replace
+                       these cells with X letter.
+                    - The one with the T letter: These are the cells that are marked during our DFS traversal, i.e.
+                       these are the cells that have at least one connection to the borders, therefore they are not
+                       captured. As a result, we would revert the cell to its original letter O.
+
         Rather than using a 'visited' set to keep track of the visited cells, we simply mark visited cells in place.
-    Time complexity: O(N * M)
-    Space complexity: O(N * M)
+
+    Time complexity: O(N * M), in the worst case where it contains only the O cells on the board, we would traverse each
+    cell twice: once during the DFS traversal and the other time during the cell reversion in the last step.
+    Space complexity: O(N * M), during the recursive calls of DFS() function we would consume some space in the function
+    call stack, i.e. the call stack will pile up along with the depth of recursive calls. The maximum depth of recursive
+    calls would be N*M as in the worst scenario mentioned in the time complexity.
     """
 
     def dfs(i, j):
         if not 0 <= i < n or not 0 <= j < m or board[i][j] != 'O':
             return
-        board[i][j] = '1'
+        board[i][j] = 'T'
         for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
             dfs(x, y)
 
-    if not board:
-        return None
     n, m = len(board), len(board[0])
     for i in range(n):
         dfs(i, 0)  # Left border
         dfs(i, m - 1)  # Right border
-    for i in range(m):
-        dfs(0, i)  # Top border
-        dfs(n - 1, i)  # Bottom border
+    for j in range(m):
+        dfs(0, j)  # Top border
+        dfs(n - 1, j)  # Bottom border
     for i in range(n):
         for j in range(m):
             if board[i][j] == 'O':
                 board[i][j] = 'X'
-            elif board[i][j] == '1':
+            elif board[i][j] == 'T':
                 board[i][j] = 'O'
 
 
