ad52045690624ad9b5a12f4c560d8a1acb685036
Anouer <hermassi.anouer@gmail.com>
2023-11-15 07:29:19 -0500

    Two sum - update

--- Arrays/1_Two_Sum.py
+++ Arrays/1_Two_Sum.py
@@ -6,7 +6,8 @@ import unittest2 as unittest
 
 def two_sum_v1(nums, target):
     """ Brute force approach. Not the best performance.
-        Loop through each element x and find if there is another value that equals to (target - x)
+         Loop through each element x and find if there is another value that equals to (target - x)
+
     Time complexity: O(N^2)
     Space complexity: O(1)
     """
@@ -19,24 +20,29 @@ def two_sum_v1(nums, target):
 
 
 def two_sum_v2(nums, target):
-    """ Using a hash table and 2 array passes. Trading space for time complexity.
+    """ Using a hash table and 2 passes, trading space for time complexity.
         In the first iteration, we add each element's value and its index to the table. Then, in the second iteration,
-        we check if each element's complement (target - nums[i]) exists in the table. Beware that the complement must
-        not be nums[i] itself.
-    Time complexity: O(N), for 2 array passes
+        we check if each element's complement (target - nums[i]) exists in the table.
+        Beware that the complement must not be nums[i] itself.
+
+    Time complexity: O(N), for 2 hash table passes
     Space complexity: O(N)
     """
-    indices = {num: i for i, num in enumerate(nums)}  # Add each element value and its index to the table
+    indices = {}
     for i, num in enumerate(nums):
-        if target - num in indices and indices[target-num] != i:  # The complement (target - num) found in O(1) lookup
-            return [i, indices[target-num]]
+        indices[num] = i # Add each element value and its index to the table
+    for i, num in enumerate(nums):
+        complement = target - nums[i]
+        if complement in indices and indices[complement] != i:  # The complement (target - num) found in O(1) lookup
+            return [i, indices[complement]]
 
 
 def two_sum_v3(nums, target):
     """ We can do it in one pass. While we iterate and insert elements into the hash map, we also look back to check
-        if current element's complement already exists in the hash map. If it exists, we have found a solution and
-        return immediately.
-    Time complexity: O(N), for one array pass
+         if current element's complement already exists in the hash map. If it exists, we have found a solution and
+         return immediately.
+
+    Time complexity: O(N), for one hash table pass
     Space complexity: O(N)
     """
     indices = {}
