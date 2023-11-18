8145503c7a1acfbc7f458bd5601d04947a88d63a
Anouer <hermassi.anouer@gmail.com>
2023-11-18 14:35:12 -0500

    Valid parentheses - v1 update

--- Stack/20_Valid_Parentheses.py
+++ Stack/20_Valid_Parentheses.py
@@ -9,7 +9,36 @@ import unittest2 as unittest
 
 
 def is_valid_v1(s):
-    """ An interesting property about a valid parenthesis expression is that a sub-expression of a valid expression
+    """ Before looking at how we can check if a given expression consisting of these parentheses is valid or not, let
+         us look at a simpler version of the problem that consists of just one type of parenthesis.
+
+         We process the expression one bracket at a time starting from the left.
+         Suppose we encounter an opening bracket i.e. (, it may or may not be an invalid expression because there can
+         be a matching ending bracket somewhere in the remaining part of the expression. Here, we simply increment the
+         counter keeping track of left parenthesis till now: left += 1
+         If we encounter a closing bracket, this has two meanings:
+            - One, there was no matching opening bracket for this closing bracket and in that case we have an invalid
+               expression. This is the case when left == 0 i.e. when there are no unmatched left brackets available.
+            - We had some unmatched opening bracket available to match this closing bracket. This is the case when
+               left > 0 i.e. we have unmatched left brackets available.
+        If we encounter a closing bracket i.e. ) when left == 0, then we have an invalid expression on our hands.
+        Else, we decrement left thus reducing the number of unmatched left parenthesis available.
+        Continue processing the string until all parenthesis have been processed.
+        If in the end we still have unmatched left parenthesis available, this implies an invalid expression.
+
+        If we try and follow the same approach for our original problem, then it simply won't work. The reason a
+        simple counter based approach works above is because all the parenthesis are of the same type. So when we
+        encounter a closing bracket, we simply assume a corresponding opening matching bracket to be available i.e.
+        if left > 0. But, in our problem, if we encounter say ], we don't really know if there is a corresponding
+        opening [ available or not.
+
+        Why not maintain a separate counter for the different types of parenthesis?
+        This doesn't work because the relative placement of the parenthesis also matters here. e.g.: [{]
+        If we simply keep counters here, then as soon as we encounter the closing square bracket ], we would know
+        there is an unmatched opening square bracket available as well. But, the closest unmatched opening bracket
+        available is a curly bracket { and not a square bracket [ and hence the counting approach breaks here.
+
+        An interesting property about a valid parenthesis expression is that a sub-expression of a valid expression
         should also be a valid expression. (Not every sub-expression)
 
         What if whenever we encounter a matching pair of parenthesis in the expression, we simply remove it from the
@@ -29,7 +58,8 @@ def is_valid_v1(s):
         In the end, if we are left with a stack still having elements, then this implies an invalid expression.
 
     Time complexity: O(N)
-    Space complexity: O(N)
+    Space complexity: O(N), we push all opening brackets onto the stack and in the worst case we will end up pushing
+    all the brackets onto the stack. e.g. ((((((((((
     """
     brackets = {'(': ')', '{': '}', '[': ']'}
     stack = []
@@ -38,7 +68,7 @@ def is_valid_v1(s):
             stack.append(c)
         elif not stack or brackets[stack.pop()] != c:
             return False
-    return True
+    return not stack
 
 
 def is_valid_v2(s):
