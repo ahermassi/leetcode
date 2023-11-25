4e9071b4fde9ebf6a1b0f23cc671c91da6246dcc
Anouer <hermassi.anouer@gmail.com>
2023-11-25 13:48:32 -0500

    Validate binary search tree - v4 update

--- Tree/98_Validate_Binary_Search_Tree.py
+++ Tree/98_Validate_Binary_Search_Tree.py
@@ -83,33 +83,34 @@ def is_valid_bst_v3(root):
 
 def is_valid_bst_v4(root):
     """ All the previous approaches explore the left subtree first. Therefore, even if the BST property does not hold
-        at a node which is close to the root (e.g., the key stored at the right child is less than the key stored at
-        the root), their time complexity is still O(N).
+         at a node which is close to the root (e.g., the key stored at the right child is less than the key stored at
+         the root), their time complexity is still O(N).
 
-        We can search for violations of the BST property in a BFS manner, thereby reducing the time complexity when the
-        property is violated at a node whose depth is small.
+         We can search for violations of the BST property in a BFS manner, thereby reducing the time complexity when the
+         property is violated at a node whose depth is small.
 
-        Specifically, we use a queue, where each queue entry contains a node, as well as an upper and a lower bound on
-        the keys stored at the subtree rooted at that node. The queue is initialized to the root, with lower bound -∞
-        and upper bound +∞.
+         Specifically, we use a queue, where each queue entry contains a node, as well as an upper and a lower bound on
+         the keys stored at the subtree rooted at that node. The queue is initialized to the root, with lower bound -∞
+         and upper bound +∞.
 
-        We iteratively check the constraint on each node. If it violates the constraint we stop: The BST property has
-        been violated. Otherwise, we add its children along with the corresponding constraint.
+         We iteratively check the constraint on each node. If it violates the constraint we stop: The BST property has
+         been violated. Otherwise, we add its children along with the corresponding constraint.
 
     Time complexity: O(N)
-    Space complexity: O(N)
+    Space complexity: O(N), in the worst case scenario, we have a completely balanced tree. In such case, the maximum
+    space consumption will occur at the last level (at the leaves) where we have N/2 nodes in the queue
     """
     queue = deque([(root, float('-inf'), float('inf'))])
     while queue:
         n = len(queue)
         for _ in range(n):
             node, lower, upper = queue.popleft()
-            if not node:
-                continue
             if not lower < node.val < upper:
                 return False
-            queue.append((node.left, lower, node.val))
-            queue.append((node.right, node.val, upper))
+            if node.left:
+                queue.append((node.left, lower, node.val))
+            if node.right:
+                queue.append((node.right, node.val, upper))
     return True
 
 
@@ -130,6 +131,8 @@ class Test(unittest.TestCase):
         self.assertFalse(is_valid_bst_v2(self.root2))
         self.assertTrue(is_valid_bst_v3(self.root1))
         self.assertFalse(is_valid_bst_v3(self.root2))
+        self.assertTrue(is_valid_bst_v4(self.root1))
+        self.assertFalse(is_valid_bst_v4(self.root2))
 
 
 if __name__ == '__main__':
