71b2317aef7cb898c47c8d1dbb86abdf8c267a44
Anouer <hermassi.anouer@gmail.com>
2023-12-05 11:05:44 -0500

    Spiral matrix - v3 update

--- Arrays/54_Spiral_Matrix.py
+++ Arrays/54_Spiral_Matrix.py
@@ -52,20 +52,25 @@ def spiral_order_v2(matrix):
                 res.append(row.pop(0))
     return res
 
+
 # Video explanation: https://www.youtube.com/watch?v=BJnMZNwUk1M
-
-
 def spiral_order_v3(matrix):
     """ We simulate peeling off the layers as in the previous solution, but without modifying the input matrix.
 
-        We process entries in the sequence (Ro,Co), (R0,C1), …  (R0,Cn-1), i.e., we are moving to the right. Then we
-        process entries (Ro,Cn-1), (R1,Cn-1), …, (Rn-1,Cn-.1)., i.e., we are moving down. Then we process entries
-        (Rn-1,Cn-2), (Rn-1,Cn-3), …  (Rn-1,C0), i.e., we are moving to the left. Then we process entries
-        (Rn-2,C0), (Rn-3,C0), …, (R1,C0), i.e., we are moving up.
+        We process entries in the sequence (Ro,Co), (R0,C1), …  (R0,Cn-1), i.e., we are moving to the right.
+        Then we process entries (Ro,Cn-1), (R1,Cn-1), …, (Rn-1,Cn-.1)., i.e., we are moving down.
+        Then we process entries (Rn-1,Cn-2), (Rn-1,Cn-3), …  (Rn-1,C0), i.e., we are moving to the left.
+        Then we process entries (Rn-2,C0), (Rn-3,C0), …, (R1,C0), i.e., we are moving up.
         This method is applied until all elements are processed. Conceptually, we are processing the array in ‘shells'
         from the outside moving to the center.
 
-        We can achieve moving in different directions by modifying row and column indices.
+        We can achieve moving in different directions by modifying row and column indices. Given that we are at
+        (row, col), where row is the row index, and col is the column index, we have:
+
+                move right: (row, col + 1)
+                move downwards: (row + 1, col)
+                move left: (row, col - 1)
+                move upwards: (row - 1, col)
 
         When shall we change our direction? We need to turn when we either reach the matrix boundaries, or we reach the
         cells in the array that we have visited before. Matrix boundaries are fixed and provided already, but how could
@@ -81,37 +86,35 @@ def spiral_order_v3(matrix):
 
         Before we traverse from right to left, we need to make sure that we are not on a row that has already been
         traversed. If we are not, then we can traverse from right to left.
-        Similarly, before we traverse from top to bottom, we need to make sure that we are not on a column that has
-        already been traversed. Then we can traverse from down to up.
+        Similarly, before we traverse upwards, we need to make sure that we are not on a column that has already been
+        traversed. Then we can traverse from down to up.
 
-        So these previous conditions avoid repeating the right-to-left or down-to-up scan if there is only 1 row or
-        column in the matrix.
-        First, (row_begin <= row_end) will return False if there is only 1 row, so no need to scan right-to-left.
-        Then, (col_begin <= col_end) will return False if there is only 1 column, so no need to scan down-to-up.
+        These checks avoid repeating the right-to-left or down-to-up scan if there is only 1 row or column in the
+        matrix.
+        First, (row_begin <= row_end) will return false if there is only 1 row, so no need to scan right-to-left.
+        Then, (col_begin <= col_end) will return false if there is only 1 column, so no need to scan down-to-up.
 
     Time complexity: O(N * M)
     Space complexity: O(1)
     """
-    if not matrix:
-        return None
     row_begin, row_end = 0, len(matrix) - 1
     col_begin, col_end = 0, len(matrix[0]) - 1
     res = []
     while row_begin <= row_end and col_begin <= col_end:
-        # Traverse Right
+        # Traverse from left to right
         for j in range(col_begin, col_end + 1):
             res.append(matrix[row_begin][j])
         row_begin += 1
-        # Traverse Down
+        # Traverse downwards
         for i in range(row_begin, row_end + 1):
             res.append(matrix[i][col_end])
         col_end -= 1
-        # Traverse Left
+        # Traverse from right to left
         if row_begin <= row_end:
             for j in reversed(range(col_begin, col_end + 1)):
                 res.append(matrix[row_end][j])
         row_end -= 1
-        # Traverse Up
+        # Traverse upwards
         if col_begin <= col_end:
             for i in reversed(range(row_begin, row_end + 1)):
                 res.append(matrix[i][col_begin])
@@ -166,7 +169,7 @@ class Test(unittest.TestCase):
     def test_spiral_order(self):
         for test_array, result in self.data:
             self.assertEqual(result, spiral_order_v1(test_array))
-            self.assertEqual(result, spiral_order_v3(test_array))
+            self.assertEqual(result, spiral_order_v3([test_array]))
 
 
 if __name__ == '__main__':
