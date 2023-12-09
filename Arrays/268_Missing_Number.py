36b4081f8daa70f58f7658e85d6ee1e513623ae6
Anouer <hermassi.anouer@gmail.com>
2023-12-09 07:43:44 -0500

    Missing number - v2 update / v3

--- Arrays/268_Missing_Number.py
+++ Arrays/268_Missing_Number.py
@@ -28,6 +28,12 @@ def missing_number_v2(nums):
         Therefore, if we initialize an integer to n and XOR it with every index and value, we will be left with the
         missing number.
 
+        A better way to understand XOR solution is that if each number at an index was equal to its corresponding index,
+         i.e. nums[i] == i, then taking the XOR of all the indices and all the values would have resulted in a perfect
+         cancellation (zero out) and thus yielding the missing number as n ( the highest number in the range ). Now that
+          can give a better clue that not only all the indices and values but also the highest number in the range (n)
+          need to be XOR'd to get the missing number
+
     Time complexity: O(N), assuming that XOR is a constant-time operation
     Space complexity: O(1)
     """
@@ -38,6 +44,27 @@ def missing_number_v2(nums):
 
 
 def missing_number_v3(nums):
+    """  An easier to understand version of the XOR solution.
+
+         Think of the "missing" variable as something like a singularity: we can stuff all the expected numbers into a
+         single variable, remove the numbers that are found in the list, and get back the missing value.
+
+    Time complexity: O(N)
+    Space complexity: O(1)
+    """
+    n = len(nums)
+    missing = 0
+    for i in range(n+1):
+        # Using XOR, "add" each number in 0 ... n that "should be" in the list
+        missing ^= i
+    for num in nums:
+        # Using XOR, "subtract" each number that we actually find in the list
+        missing ^= num
+    # We've removed every number except the missing value, so this must be it
+    return missing
+
+
+def missing_number_v4(nums):
     """ A brute force method would be to simply check for the presence of each number that we expect to be present. Use
          a set to get constant time containment queries and overall linear runtime.
     Time complexity: O(N)
@@ -57,6 +84,7 @@ class Test(unittest.TestCase):
             self.assertEqual(result, missing_number_v1(test_array))
             self.assertEqual(result, missing_number_v2(test_array))
             self.assertEqual(result, missing_number_v3(test_array))
+            self.assertEqual(result, missing_number_v4(test_array))
 
 
 if __name__ == '__main__':
