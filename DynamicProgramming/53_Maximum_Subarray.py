877ced8711a8bff2f1cef86824990b8feef6c6fc
Anouer <hermassi.anouer@gmail.com>
2023-12-10 11:18:43 -0500

    Maximum subarray - v1 update

--- DynamicProgramming/53_Maximum_Subarray.py
+++ DynamicProgramming/53_Maximum_Subarray.py
@@ -4,18 +4,48 @@ sum and return its sum. """
 import unittest2 as unittest
 
 
+# Video explanation https://youtu.be/2MmGzdiKR9Y
 def maximum_subarray_v1(nums):
-    """ This is called Kadane's algorithm. Assume first element is the maximum (local and global) sum. Iterate over the
-        array, and at each step ask yourself: which is greater, the current element x or (last sum + x) ? Update the
-        local sum and global sum accordingly.
+    """ Kadane's algorithm.
+
+         Whenever a question asks for the maximum or minimum of something, consider Dynamic Programming as a
+         possibility. The difficult part of this problem is figuring out when a negative number is "worth" keeping in a
+         subarray. This question in particular is a popular problem that can be solved using an algorithm called
+         Kadane's Algorithm.
+
+         Let's focus on one important part: where the optimal subarray begins. We'll use the following example:
+         nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+        We can see that the optimal subarray couldn't possibly involve the first 3 values - the overall sum of those
+        numbers would always subtract from the total. Therefore, the subarray either starts at the first 4, or
+        somewhere further to the right.
+
+        What if we had this example though? nums = [-2, 1000000000, -3, 4, -1 ,2, 1, -5, 4]
+        We need a general way to figure out when a part of the array is worth keeping.
+
+        As expected, any subarray whose sum is positive is worth keeping. Let's start with an empty array, and iterate
+        through the input, adding numbers to our array as we go along. Whenever adding the current element to the
+        current sub array results in smaller value than the current element taken alone , we know the entire array is
+        not worth keeping, so we'll reset it back to a single element sub array made up of the current element only.
+
+        However, we don't actually need to build the subarray, we can just keep an integer variable max_ending_here and
+        add the values of each element ending at the current index. When it becomes negative, we reset it to 0
+        (an empty array).
+
+         Iterate over the array, and at each step ask the following question: which is greater, the current element x or
+         (sum thus far + x) ? If max_ending_here becomes negative, we know it isn't worth keeping, so throw it away.
+
+         Judging if a subarray is "worth keeping" happens every time we reach a new number. Even if all the numbers are
+         negative, it just means that it is never worth keeping more than 1 element.
+
     Time complexity: O(N)
     Space complexity: O(1)
     """
     n = len(nums)
     max_so_far = max_ending_here = nums[0]
     for i in range(1, n):
-        max_ending_here = max(nums[i], max_ending_here + nums[i])  # What's the maximum sub array ending here ? Either
-        # [current element] or [previous sub array, current element]
+        # What's the maximum sub array ending here ? Either [current element] or [previous sub array, current element]
+        max_ending_here = max(nums[i], max_ending_here + nums[i])
         max_so_far = max(max_so_far, max_ending_here)  # Update global max
     return max_so_far
 
