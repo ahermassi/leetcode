4b7ac3028c8402964918f62d2bf61f0ae804ce3a
Anouer <hermassi.anouer@gmail.com>
2023-12-12 11:38:02 -0500

    House robber II - v1 update

--- DynamicProgramming/213_House_Robber_II.py
+++ DynamicProgramming/213_House_Robber_II.py
@@ -9,46 +9,59 @@ money you can rob tonight without alerting the police.
 import unittest2 as unittest
 
 
+# Video explanation: https://youtu.be/rWAJCfYYOvM
 def rob_v1(nums):
-    """ In the original question, 198- House Robber, whether to rob nums[0] is entirely our choice. But, it is now
-        constrained by whether num[-1] is robbed. However, since we already have a nice solution to the simpler
-        problem, we do not want to throw it away. Then, it becomes how can we reduce this problem to the simpler one.
-        This problem can be simply decomposed into two House Robber problems.
+    """ This problem is a minor extension to the original 198- House Robber Problem. The only difference is that the
+         first and the last houses are adjacent to each other and therefore, if the thief has robbed the first house,
+         they cannot steal the last house and vice versa.
+
+         Therefore, the problem becomes to rob either nums[0]...nums[n-2] or nums[1]...nums[n-1], depending on which
+         choice offers more money. Now the problem has degenerated to the original house robber.
+
+         Assume we have nums = [7,4,1,9,3,8,6,5].
+         Since the start house and last house are adjacent to each other, if the thief decides to rob the start house 7,
+         they cannot rob the last house 5. Similarly, if they select last house 5, they have to start from a house with
+         value 4. Therefore, the final solution that we are looking for is to take the maximum value the thief can rob
+         between houses of list [7,4,1,9,3,8,6] and [4,1,9,3,8,6,5]. For each of the lists, all we need to do is to
+         figure out the maximum value the thief can get using the approach in the original house robber problem.
+
         Suppose there are n houses. Since house 0 and n - 1 are now neighbors, we cannot rob them together, and thus
         the solution is now the maximum of:
+
             1- Rob houses 0 to n - 2
             2- Rob houses 1 to n - 1
-        If we rob the first, then we cannot rob the last, so nums[:-1]
-        If we do not rob the first, then we can rob the last, so nums[1:]
+
         You want the first, leave the last. You want the last, leave the first.
-        Let us try to prove it. For nums[0..n-1], 0 and n-1 are neighboring each other. Basically, there are only three
+
+        Let us try to prove it. For nums[0...n-1], 0 and n-1 are neighboring each other. Basically, there are only three
         possible cases:
+
             1- Rob 0, but leave n-1 untouched
             2- Leave 0 untouched, rob n-1
-            3- Leave both 0 and n-1 untouched. Obviously, this case can be covered by case 1 or case 2 in the simple
+            3- Leave both 0 and n-1 untouched. Obviously, this case can be covered by case 1 or case 2 in the original
                House Robber problem.
-        Hence, the above solution covers all the possible cases.
+
+        Hence, this solution covers all the possible cases.
+
     Time complexity: O(N)
     Space complexity: O(N)
     """
 
-    def helper(nums):
-        if not nums:
+    def rob_houses(houses):
+        if not houses:
             return 0
-        n = len(nums)
+        n = len(houses)
         if n == 1:
-            return nums[0]
+            return houses[0]
         dp = [0] * n
-        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        dp[0], dp[1] = houses[0], max(houses[0], houses[1])
         for i in range(2, n):
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+            dp[i] = max(houses[i] + dp[i - 2], dp[i - 1])
         return dp[-1]
 
-    if not nums:
-        return 0
     if len(nums) == 1:
         return nums[0]
-    return max(helper(nums[:-1]), helper(nums[1:]))
+    return max(rob_houses(nums[1:]), rob_houses(nums[:-1]))
 
 
 def rob_v2(nums):
