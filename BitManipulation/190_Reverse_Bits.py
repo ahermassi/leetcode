9bb1c50caf22fd221f03fb3fff323f634d3478a3
Anouer <hermassi.anouer@gmail.com>
2023-12-13 09:50:52 -0500

    Reverse bits - update

--- BitManipulation/190_Reverse_Bits.py
+++ BitManipulation/190_Reverse_Bits.py
@@ -2,20 +2,39 @@
 
 
 def reverse_bits(n):
-    """ Similar to 7- Reverse Integer. However, instead of using modulo and division by 10, we use bit manipulation.
-        Shift 1 bit to left == multiply by 2
-        Shift 1 bit to right == divide by 2
-        Bit & 1 determines if a bit is 0 or 1. Similarly, x & 1 gets the value of the least significant bit of x.
-        Let's work on an example. Say, n = 19, which is 00000000000000000000000000010011 in binary, so in the output we
-        should get 11001000000000000000000000000000 in binary, which is 3355443200.
-        We initialize 'answer' to 0, so in binary it's 32 zeroes. We loop over 32 times, since every integer is gonna
-        have 32 possible 0/1.
-        1st line in the loop: n & 1, we check if last bit of n is set, is it 1 or 0; ans << 1 shifts all bits that we
+    """ Similar to 7- Reverse Integer, but instead of using modulo and division by 10, we use bit manipulation.
+
+         One of the most intuitive solutions is to reverse the bits one by one.
+
+         To retrieve the right-most bit in an integer n, we could apply the bit AND operation (i.e. n & 1). In order to
+         combine the results of reversed bits, we could use the bit OR operation.
+
+         Shift 1 bit to left == multiply by 2
+         Shift 1 bit to right == divide by 2
+         Bit & 1 determines if a bit is 0 or 1. Similarly, x & 1 gets the value of the least significant bit of x.
+
+         The key idea is that for a bit that is situated at the index i, after being reversed, its position should be
+         31-i (note: the index starts from zero).
+
+            - We iterate through the bit string of the input integer, from right to left (i.e. n = n >> 1). To retrieve
+               the right-most bit, we apply the bit AND operation (n & 1).
+            - For each bit, we reverse it to the correct position, then we accumulate this reversed bit to the final
+               result.
+
+        Let's work on an example. Say, n = 19, which is 00000000000000000000000000010011 in binary, so in the
+        output we should get 11001000000000000000000000000000 in binary, which is 3355443200.
+
+        We initialize 'answer' to 0, so in binary it's 32 zeroes. We loop over 32 times, since every integer
+        is going to have 32 possible 0/1's.
+
+        1st line in the loop: n & 1, we check if last bit of n is set, is it 1 or 0; answer << 1 shifts all bits that we
         already have in our answer to the left, so after this shifting the bit on the right is 0; by using | we set
         the last bit in the answer to the value that we got in n & 1.
+
         2nd line in the loop: we shift bits of our initial number n to the right, since we've already checked the least
         significant bit of n, so we just move on to the next bit.
-        So, in our example, we're gonna work on only the first 5 right bits, since other bits are 0.
+
+        So, in our example, we're going to work on only the first 5 right bits, since other bits are 0.
 
         answer = 0, in binary: 00000000000000000000000000000000
         answer << 1 = 00000000000000000000000000000000, n & 1 = 00000000000000000000000000000001,
@@ -37,13 +56,21 @@ def reverse_bits(n):
         answer << 1 = 00000000000000000000000000011000, n & 1 = 00000000000000000000000000000001
         after | operation, answer = 00000000000000000000000000011001
 
-        And after that in our example, we'll just shift 00000000000000000000000000011001 all the way to the left, which
-        leads to 11001000000000000000000000000000.
+        And after that, we'll just shift 00000000000000000000000000011001 all the way to the left, which results in
+        11001000000000000000000000000000
+
+        We are asked to reverse bits in our number. What is the most logical way to do it? Create number 'answer',
+        process the original number bit by bit starting with the Least Significant Bit, and add this bit to the end of
+        'answer' number, and that is all!
+
+            - answer = (answer << 1) | (n & 1) adds last bit of n to 'answer'
+            - n = n >> 1 removes last bit from n
+
     Time complexity: O(1)
     Space complexity: O(1)
     """
-    res = 0
+    reversed = 0
     for _ in range(32):
-        res = (res << 1) | (n & 1)
+        reversed = (reversed << 1) | (n & 1)
         n = n >> 1
-    return res
+    return reversed
