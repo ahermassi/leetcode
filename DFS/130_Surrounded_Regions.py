154df15c1dc5d116f4a4791dc825f6ab46909766
Anouer <hermassi.anouer@gmail.com>
2024-01-06 10:57:48 -0500

    Surrounded regions - v1 update

--- DFS/130_Surrounded_Regions.py
+++ DFS/130_Surrounded_Regions.py
@@ -4,9 +4,8 @@ from collections import deque
 
 import unittest2 as unittest
 
+
 # Video explanation: https://www.youtube.com/watch?v=9z2BunfoZ5Y
-
-
 def solve_v1(board):
     """ If we are asked to summarize the algorithm in one sentence, it would be that we enumerate all those candidate
          cells (i.e. the ones filled with O), and check one by one if they are captured or not, i.e. we start with a
@@ -16,39 +15,45 @@ def solve_v1(board):
         that the inverse is simpler is that if an 'O' is adjacent to an 'O that can reach the border, then the first
         'O' can reach it too.
 
-        Surrounded regions shouldn't be on the border, which means that any 'O' on the border of the board is not
-        flipped to 'X'.
+        Surrounded regions shouldn't be on the border, which means that:
 
-        Any 'O' that is not on the border but is connected to an 'O' on the border will not be flipped to 'X'.
-        Any 'O' that is not on the border and is not connected to an 'O' on the border will be flipped to 'X'.
+                Any 'O' on the border of the board is not flipped to 'X'.
+                Any 'O' that is not on the border but is connected to an 'O' on the border will not be flipped to 'X'.
+                Any 'O' that is not on the border and is not connected to an 'O' on the border will be flipped to 'X'.
 
         The algorithm consists of three steps:
 
             1- We select all the cells that are located on the borders of the board.
 
-            2- Start from each of the above selected cell, we then perform the DFS traversal.
-                 If a cell on the border happens to be O, then we know that this cell is alive, together with the other
-                 O cells that are connected to this border cell. Two cells are connected, if there exists a path
-                 consisting of only O letter that bridges between the two cells.
-                Based on the above conclusion, the goal of our DFS traversal would be to mark out all those connected O
-                cells that is originated from the border, with any distinguished letter such as T
+            2- Starting from each of the above selected cells, we then perform a DFS traversal.
+
+                 If a cell on the border happens to be 'O', then we know that this cell is alive, together with the
+                 other 'O' cells that are connected to this border cell. Two cells are connected if there exists a path
+                 consisting of only 'O' letter that bridges between the two cells.
+
+                Based on the above conclusion, the goal of the DFS traversal would be to mark all those connected 'O'
+                cells that originated from the border with any distinguished letter such as 'T'.
 
             3- Once we iterate through all border cells, we would then obtain three types of cells:
-                    - The one with the X letter: The cell that we could consider as the wall.
-                    - The one with the O letter: The cells that are spared in our DFS traversal, i.e. these cells have
-                       no connection to the border, therefore they are captured. We then should replace
-                       these cells with X letter.
-                    - The one with the T letter: These are the cells that are marked during our DFS traversal, i.e.
-                       these are the cells that have at least one connection to the borders, therefore they are not
-                       captured. As a result, we would revert the cell to its original letter O.
+
+                    - The ones with letter 'X': The cells that we could consider as the wall.
+
+                    - The ones with letter 'O': The cells that are spared during the DFS traversal, i.e. these cells
+                       have no connection to the border, therefore they are captured. We then should replace
+                       these cells with 'X'.
+
+                    - The ones with letter 'T': These are the cells that are marked during the DFS traversal, i.e.
+                       these are the cells that have at least one connection to the border, therefore they are not
+                       captured. As a result, we would revert these cells back to 'O'.
 
         Rather than using a 'visited' set to keep track of the visited cells, we simply mark visited cells in place.
 
-    Time complexity: O(N * M), in the worst case where it contains only the O cells on the board, we would traverse each
-    cell twice: once during the DFS traversal and the other time during the cell reversion in the last step.
+    Time complexity: O(N * M), in the worst case where it contains only the O cells on the board (cells that are not at
+    the border), we would traverse each cell twice: once during the DFS traversal and a second time to capture the
+    regions.
     Space complexity: O(N * M), during the recursive calls of DFS() function we would consume some space in the function
     call stack, i.e. the call stack will pile up along with the depth of recursive calls. The maximum depth of recursive
-    calls would be N*M as in the worst scenario mentioned in the time complexity.
+    calls would be N * M as in the worst scenario mentioned in the time complexity.
     """
 
     def dfs(i, j):
