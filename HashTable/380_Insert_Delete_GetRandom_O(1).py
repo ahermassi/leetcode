7f77d134e8c0172d6d05dc75beaf6a3bf30e75a6
Anouer <hermassi.anouer@gmail.com>
2024-01-16 18:03:57 -0500

    Insert delete getRandom O(1) - update

--- HashTable/380_Insert_Delete_GetRandom_O(1).py
+++ HashTable/380_Insert_Delete_GetRandom_O(1).py
@@ -8,23 +8,32 @@ import random
 import unittest2 as unittest
 
 
+# Video explanation: https://youtu.be/j4KwhBziOp
 class RandomizedSet(object):
-    """ When we store everything in a dictionary, it's fine when we insert or remove.
-        But if we want to achieve O(1) on getRandom(), it's impossible. We have to turn it into a list first, which
-        is O(N). The idea of getRandom is to choose a random index and then to retrieve an element with that index.
-        There are no indices in hash map, and hence to get a truly random value, we have to first convert hash map keys
-        into a list, and that would take a linear time.
-        For this reason, we use a dictionary to just keep track of the indices of the added elements, so when we remove
-        them, we copy the last one into it. An array/list holds the inserted values.
-        To delete a value at arbitrary index takes linear time. The solution here is to always delete the last value:
+    """ Let's figure out how to implement such a structure. Starting from the Insert, we immediately have two good
+         candidates with O(1) average insert time: hashmap (or hash set, the implementation is very similar), and array.
+
+         Hashmap provides insert and delete in average constant time, but it has problems with get random.
+         The idea of get random is to choose a random index and then retrieve an element with that index. There are no
+         indexes in the hashmap, and hence to get a true random value, we have first to convert the hashmap keys to a
+         list, which would take linear time. The solution here is to build a list of keys aside and use this list to
+         compute get random in constant time.
+
+        Arrays have indexes and could provide insert and get random in average constant time, but have problems with
+        delete. Deleting a value at an arbitrary index takes linear time. The solution here is to always delete the
+        last value:
+
             - Swap the element to delete with the last one
-            - Pop the last element out
-        For that, we have to compute the index of each element in constant time, and hence we need a hash map which
-        stores element -> index.
+            - Pop the last element
+
+        To do that, we have to compute the index of each element in constant time and hence we use a dictionary to
+        keep track of the indices of the added elements.
+
         Both ways converge into the same combination of data structures:
             - Hash map element -> index
             - List of elements
-        This way, we achieve average O(1) for insert, remove, and getRandom.
+
+        This way, we achieve average O(1) for insert, remove, and get random.
     """
 
     def __init__(self):
@@ -36,24 +45,24 @@ class RandomizedSet(object):
         if val in self.indices:
             return False
         self.nums.append(val)
-        self.indices[val] = len(self.nums) - 1  # Insert value along with its index in nums list
+        self.indices[val] = len(self.nums) - 1  # Insert the value along with its index in nums list
         return True
 
     def remove(self, val):
         """ Removes a value from the set. Returns true if the set contained the specified element.
             Retrieve the index of element to delete from the hash map.
-            Move the last element to the place of the element to delete, O(1) time.
-            Pop the last element out, O(1) time.
+            Move the last element to the place of the element to delete, in O(1) time.
+            Pop the last element, in O(1) time.
         """
         if val not in self.indices:
             return False
-        # Essentially, we're going to move the last element in the list into the location of the element we want to
+        # Essentially, we're going to move the last element in the list to the location of the element we want to
         # remove. This is a significantly more efficient operation than the obvious solution of removing the item and
-        # shifting the values of every item iin the dictionary to match their new position in the list
-        index = self.indices[val]  # Get val index in list
+        # shifting the values of every item iin the dictionary to match their new position in the list.
+        index = self.indices[val]  # Get val index in the list
         last = self.nums[-1]  # Get the last added element
-        self.nums[index] = last  # Overwrite val index with last element
-        self.nums.pop()  # Get rid of the last element from its original spot. It now has a new home elsewhere.
+        self.nums[index] = last  # Overwrite val index with the last element
+        self.nums.pop()  # Pop the last element. It now has a new home elsewhere.
         self.indices[last] = index  # Update the last element's index to its new spot
         del self.indices[val]  # Delete value along with its index from the dictionary
         return True
