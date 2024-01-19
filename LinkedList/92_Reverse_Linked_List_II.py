326cfb936bbc3250a7f8759bfefd5a565a8f8d26
Anouer <hermassi.anouer@gmail.com>
2024-01-19 11:04:30 -0500

    Reverse linked list II - v1 update

--- LinkedList/92_Reverse_Linked_List_II.py
+++ LinkedList/92_Reverse_Linked_List_II.py
@@ -12,44 +12,60 @@ class ListNode(object):
         self.next = None
 
 
-# Checkout the illustration on this article: https://leetcode.com/articles/reverse-linked-list-ii/
+# Check out the illustration on this article: https://leetcode.com/articles/reverse-linked-list-ii/
+# Video explanation: https://youtu.be/RF_M9tX4Eag
+def reverse_between_v1(head, left, right):
+    """ Starting from the node at 'left' position and all the way up to 'right', we reverse the next pointers for all
+         the nodes in between.
 
-def reverse_between_v1(head, m, n):
-    """ Starting from the node at position m and all the way up to n, we reverse the next pointers for all the nodes in
-        between.
-        We need two pointers, prev and cur. The prev pointer should be initialized to None initially while cur is
-        initialized to the head of the linked list.
-        We progress the cur pointer one step at a time and the prev pointer follows it.
-        We keep progressing the two pointers in this way until the cur pointer reaches the mth node from the beginning
-        of the list. This is the point from where we start reversing our linked list.
-        An important thing to note here is the usage of two additional pointers which we will call as tail and con.
-        The tail pointer points to the mth node from the beginning of the linked list and we call it a tail pointer
-        since this node becomes the tail of the reverse sublist. The con points to the node one before mth node and
-        this connects to the new head of the reversed sublist.
-        The tail and the con pointers are set once initially and then used in the end to finish the linked list
-        reversal.
-        Once we reach the mth node, we iteratively reverse the links as explained before using the two pointers. We
-        keep on doing this until we are done reversing the link (next pointer) for the nth node. At that point, the
-        prev pointer would point to the nth node.
-        We use the con pointer to attach to the prev pointer since the node now pointed to by the prev pointer (the nth
-        node from the beginning) will come in place of the mth node due after the reversal. Similarly, we will make use
-        of the tail pointer to connect to the node next to the prev node i.e. (n+1)th node from the beginning.
+            - We need two pointers, prev and cur. The prev pointer should be initialized to None initially while cur is
+               initialized to the head of the linked list.
+
+            - We progress cur pointer one step at a time and the prev pointer follows it.
+
+            - Keep progressing the two pointers in this way until cur pointer reaches the node at 'left' position.
+               This is the point from where we start reversing the linked list.
+
+            - Create two additional pointers called tail and connector. The tail pointer points to the node at 'left'
+               position from the beginning of the linked list, and we call it a tail pointer since this node becomes the
+               tail of the reversed sublist. The connector points to the node before 'left' node and connects to the
+               head of the reversed sublist.
+
+            - The tail and the connector pointers are set once initially and then used at the end to finish the linked
+               list reversal.
+
+            - Once we reach the 'left' node, we iteratively reverse the links. We keep doing this until we are done
+               reversing the link (next pointer) for the 'right' node. At that point, the prev pointer points to the
+               'right' node.
+
+            - Use the connector pointer to attach to the prev pointer since the node now pointed at by the prev pointer
+               (the 'right' node) will come in place of the 'left' node after the reversal. Similarly, we make use of
+               the tail pointer to connect to the node next to the prev node i.e. (right+1)th node.
+
+        To summarize:
+
+            1- Walk (left - 1) steps to reach the first node of the range we want to reverse
+            2- Reverse the range [left ... right]
+            3- Fix the connections for the start and the end of the reversed list
+
     Time complexity: O(N)
     Space complexity: O(1)
     """
     pre, cur = None, head
-    for _ in range(m - 1):  # Move the two pointers until they reach the proper starting point in the list
+    for _ in range(left - 1):
+        # Move the two pointers until they reach the proper starting point in the list
         pre = cur
         cur = cur.next
-    con, tail = pre, cur  # The two pointers that will fix the final connections
-    for _ in range(n - m + 1):  # Iteratively reverse the nodes
+    connector, tail = pre, cur  # The two pointers that will fix the final connections
+    for _ in range(right - left + 1):
+        # Iteratively reverse the nodes
         node_to_reverse = cur.next
         cur.next = pre
         pre = cur
         cur = node_to_reverse
     # Adjust the final connections
-    if con:
-        con.next = pre
+    if connector:
+        connector.next = pre
     else:
         head = pre
     tail.next = cur
