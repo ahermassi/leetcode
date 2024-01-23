4955a4baab42279e1ed2230a001f86a35ab74e26
Anouer <hermassi.anouer@gmail.com>
2024-01-23 18:10:49 -0500

    Sum root to leaf numbers - v1 update

--- Tree/129_Sum_Root_to_Leaf_Numbers.py
+++ Tree/129_Sum_Root_to_Leaf_Numbers.py
@@ -3,23 +3,38 @@ An example is the root-to-leaf path 1->2->3 which represents the number 123.
 Find the total sum of all root-to-leaf numbers. """
 
 
+# Video explanation: https://youtu.be/Jk16lZGFWxE
 def sum_numbers_v1(root):
-    """ Recursive pre-order traversal is extremely simple: follow Root->Left->Right direction, i.e. do all the business
-        with the node (update the current total and root-to-leaf sum), and then do the recursive calls for the left and
-        right child nodes.
-    Time complexity: O(N)
-    Space complexity: O(h), to keep the recursion stack, where h is tree height
+    """ Recursive preorder traversal.
+
+         Each time we visit a node, we compute the integer it encodes using the number for its parent. If the node is a
+         leaf, we add its integer to the total output sum. If it is not a leaf, we recursively explore the left and
+         right subtrees.
+
+            - Start at the root with current number = 0.
+
+            - Every time, the current node's digit will be appended to the number from root to leaf and we recurse for
+               left and right children.
+
+            - If we reach a leaf node, we have formed the complete root-to-leaf number. We will then add this number to
+               the overall sum.
+
+        Note that this implementation can be converted to an iterative preorder using an explicit stack and to a BFS
+        traversal using a queue.
+
+    Time complexity: O(N), each node is visited once
+    Space complexity: O(h), to keep the recursion stack, where h is the tree height
     """
 
-    def dfs(root, total):
+    def dfs(root, number):
         if not root:
             return
-        total = total * 10 + root.val
+        number = number * 10 + root.val
         if not root.left and not root.right:
-            res[0] += total
+            res[0] += number
         else:
-            dfs(root.left, total)
-            dfs(root.right, total)
+            dfs(root.left, number)
+            dfs(root.right, number)
 
     res = [0]
     dfs(root, 0)
