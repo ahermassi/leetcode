3b300a675c0ca995ef07fbd4fa5cbd5135b0d3a3
Anouer <hermassi.anouer@gmail.com>
2024-01-23 18:17:38 -0500

    Sum root to leaf numbers - v3 update

--- Tree/129_Sum_Root_to_Leaf_Numbers.py
+++ Tree/129_Sum_Root_to_Leaf_Numbers.py
@@ -77,19 +77,20 @@ def sum_numbers_v2(root):
 
 
 def sum_numbers_v3(root):
-    """ Here we implement standard iterative pre-order traversal with the stack.
+    """ Here we implement the iterative preorder traversal using an explicit stack.
+
     Time complexity: O(N)
-    Space complexity: O(h), to keep the recursion stack, where h is tree height
+    Space complexity: O(h), to keep the recursion stack, where h is the tree height
     """
     if not root:
         return 0
     res, stack = 0, [(root, 0)]
     while stack:
-        node, total = stack.pop()
-        total = total * 10 + node.val
+        node, number = stack.pop()
+        number = number * 10 + node.val
         if not node.left and not node.right:
-            res += total
+            res += number
         else:
-            stack.extend([(child, total) for child in (node.left, node.right) if child])
+            stack.extend([(child, number) for child in (node.left, node.right) if child])
     return res
 
