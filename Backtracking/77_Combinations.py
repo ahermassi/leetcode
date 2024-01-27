65f2bcaa518c0605462f3283ced96d568144e482
Anouer <hermassi.anouer@gmail.com>
2024-01-27 11:14:36 -0500

    Combinations - v1 update

--- Backtracking/77_Combinations.py
+++ Backtracking/77_Combinations.py
@@ -1,8 +1,56 @@
 """ Given two integers n and k, return all possible combinations of k numbers out of 1 ... n. """
 
 
+# Video explanation: https://youtu.be/q0s6m7AiM7o
 def combine_v1(n, k):
-    """ Here is a backtrack function which takes a first integer to add 'index' and a current combination 'path'
+    """ We are given that n <= 20. Typically, problems that ask to find all of something with low bounds can be solved
+         with backtracking.
+
+         Backtracking is a general algorithm for finding all (or some) solutions to some computational problems which
+         incrementally builds candidates to the solution and abandons a candidate ("backtracks") as soon as it
+         determines that the candidate cannot lead to a valid solution.
+         It is due to this backtracking behaviour, the backtracking algorithms are often much faster than the brute
+         force search algorithm, since it eliminates many unnecessary explorations.
+
+         Overall, the enumeration of candidates is done in two levels:
+
+            1- At the first level, the function is implemented as recursion. At each occurrence of recursion, the
+                 function is one step further to the final solution.
+            2- As the second level, within the recursion, we have an iteration that allows us to explore all the
+                 candidates that are of the same progress to the final solution.
+
+        The range of elements we are working with is [1, n]. To generate a combination one element at a time, we will
+        use an array 'path'' that represents the current combination we are building.
+
+        To start, we add the first element 1, so we have path = [1]. We are locking in this 1, and we will now find all
+        combinations that start with 1.
+
+        To find all combinations that start with 1, we start by adding the first element after 1, which is 2. We now
+        have path = [1, 2]. We are locking in this 2, and we will now find all combinations that start with [1, 2].
+
+        This continues until we reach the target length k. Let's say that we have finished finding all combinations that
+        start with [1, 2]. Now what? We BACKTRACK by removing the 2, and we have path = [1] again. Now, we add the
+        second element that comes after 1, which is 3. We have path = [1, 3], and now we need to find all combinations
+        that start with [1, 3].
+
+        Once we find all the combinations that start with [1], we backtrack by removing the 1 from path and adding the
+        next element. We have path = [2], and now we need to find all combinations that start with 2.
+
+        This process is very recursive in nature. Each time we add an element, we solve a new version of the problem
+        (find all combinations that start with path). The initial version of the problem is to find all combinations
+        that start with [], which represents all possible combinations.
+
+        The best way to think about the backtracking process is by modeling it as a tree. Imagine the solution space as
+        a tree, with each node representing a version of path. Label each node with a number that represents the last
+        number in path. Moving to a child is like adding the child's label to path.
+        To prevent duplicate combinations like [1, 2] and [2, 1], a node only has children with labels greater than its
+        own. The root node represents an empty []. From the root, every node's path represents the path taken from the
+        root. The nodes at depth k represent the answer combinations.
+
+        Solving this problem is equivalent to "traversing" this tree. The easiest way to perform the traversal is by
+        using recursion and passing path as an argument.
+
+        Here is a backtrack function which takes a first integer to add 'index' and a current combination 'path'
         as arguments.
             - If the current combination is done, add it to output
             - Iterate over the integers from 'index' to n
