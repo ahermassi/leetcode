07d95f953850457be8198f5a7e239d9fc69070be
Anouer <hermassi.anouer@gmail.com>
2024-02-03 10:37:07 -0500

    Find the index of the first occurrence in a string - v1 update

--- String/28_Implement_strStr().py
+++ String/28_Implement_strStr().py
@@ -4,17 +4,38 @@ Return the index of the first occurrence of needle in haystack, or -1 if needle 
 import unittest2 as unittest
 
 
+# Video explanation: https://www.youtube.com/watch?v=Gjkhm1gYIMw
 def str_str_v1(haystack, needle):
-    """ Standard search. Linearly scan haystack. Pay attention to the boundaries of the search range: 0 .. n - m + 1,
-        as it is useless to go beyond haystack[n - m] where needle's size is m.
-    Time complexity: O(N * M), where N is the length of haystack and M is the length of needle
+    """ The brute force approach is to traverse each possible substring of length M in the haystack and check if it is
+         equal to the needle, where M is the length of the needle.
+
+         First substring of length M will start at index 0 in the haystack and will end at index (M - 1) + 0 = M - 1.
+         The second substring of length M will start at index 1 in the haystack and will end at index (M - 1) + 1 = M.
+         The third substring of length M will start at index 2 in the haystack and will end at index (M - 1) + 2 = M + 1
+         Thus, the last substring of length M will start at index (N - M) in the haystack and will end at index N - 1.
+
+         We will create a window of size M and slide it across the haystack. We will keep track of the starting index of
+         the window in a variable i. For every i, we will iterate till (i + m). During each iteration:
+
+            - If the ith character in the window is equal to the ith character in the needle, then we will increment i.
+            - If the ith character in the window is not equal to the ith character in the needle, then we conclude that
+               the substring of length M starting from index i cannot be equal to the needle, and we will reset i to i+1.
+            - If all the ith characters in the window are equal to the ith characters of needle, then we will return i.
+
+        If we are done iterating over all values of i and none of them return a match, then return -1.
+
+    Time complexity: O(N * M), where N is the length of haystack and M is the length of needle. One example where the
+    worst case occurs is when needle is "aaaaab", while haystack is all a's (Let's say, "aaaaaaaaaa"). In this case, we
+    always have to take the last character of needle into comparison to conclude that the current M-substring is not
+    equal to needle. Thus, we will have to iterate M times for every window start.
     Space complexity: O(1)
     """
-    if not needle:
-        return 0
     n, m = len(haystack), len(needle)
     for i in range(n - m + 1):
-        if haystack[i] == needle[0] and haystack[i: i + m] == needle:
+        j = 0
+        while j < m and haystack[j + i] == needle[j]:
+            j += 1
+        if j == m:
             return i
     return -1
 
