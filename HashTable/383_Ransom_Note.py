89f71f946d08dc31a5c080b9049fa439097f581b
Anouer <hermassi.anouer@gmail.com>
2024-02-03 11:35:32 -0500

    Ransom note - v1 update

--- HashTable/383_Ransom_Note.py
+++ HashTable/383_Ransom_Note.py
@@ -3,19 +3,22 @@ function that will return true if the ransom note can be constructed from the ma
 false.
 Each letter in the magazine string can only be used once in your ransom note. """
 
-from collections import Counter
+from collections import Counter, defaultdict
 import unittest2 as unittest
 
 
 def can_construct_v1(ransomNote, magazine):
-    """ Make a single pass over the magazine, storing the character counts in a single hash map. Next, we make a pass
-        over the ransom note. When processing a character c, decrease its count in the frequency map. If at any time
-        the count of a character drops below 0, it means there are more occurrences of this character in the note than
-        in the magazine.
-    Time complexity: O(N + M)
+    """ Make a single pass over the magazine, storing the character counts in a single hashmap. Next, we make a pass
+         over the ransom note. When processing a character, decrease its count in the frequency map. If at any time the
+         count of a character drops below 0, it means there are more occurrences of this character in the note than in
+         the magazine.
+
+    Time complexity: O(N + M), where N is the length of the ransom note and M is the length of the magazine.
     Space complexity: O(1), the counter can hold at most 26 characters (or 128 characters)
     """
-    counter = Counter(magazine)
+    counter = defaultdict(int)
+    for c in magazine:
+        counter[c] += 1
     for c in ransomNote:
         counter[c] -= 1
         if counter[c] < 0:
