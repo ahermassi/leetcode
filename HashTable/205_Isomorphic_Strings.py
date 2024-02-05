6e237e863fe1e7ea81d362187e06744c6bed09eb
Anouer <hermassi.anouer@gmail.com>
2024-02-05 16:04:43 -0500

    Isomorphic strings - update

--- HashTable/205_Isomorphic_Strings.py
+++ HashTable/205_Isomorphic_Strings.py
@@ -6,27 +6,48 @@ No two characters may map to the same character but a character may map to itsel
 import unittest2 as unittest
 
 
+# Video explanation: https://youtu.be/7yF-U1hLEqQ
 def is_isomorphic(s, t):
-    """ The idea is that we need to map a character to another one, for example, 'egg' and 'add', we need to construct
-        the mapping 'e' -> 'a', 'a' -> 'e', 'g' -> 'd', and 'd' -> 'g'. When we encounter a character for the first
-        time, we map it with the corresponding current character of the other string. If we encounter that character
-        later, then we go and fetch the previous mapping and compare it to the current character of the other string.
-        If they match, then fine. Otherwise, the strings can't be isomorphic.
-    Time complexity: O(N + M), where N is the length of s and t is the length of t
+    """ Matching the order will be easy. Since we will iterate over the two strings and do some sort of comparison from
+         left to right, the task of ensuring that the character order is the same in both strings will take care of
+         itself.
+
+         We will process both of the strings from left to right. At each step, we take one character at a time from the
+         two strings and compare them. There are three cases we need to handle here:
+
+            1- If the characters don't have a mapping, we add one in the dictionary and move on.
+
+            2- The characters already have a mapping in the dictionary. If that is the case, then we're good to go.
+
+            3- The final case is when a mapping already exists for one of the characters, but it doesn't map to the
+                 other character. In this case, we can safely conclude that the given strings are not isomorphic,
+                 and we can return.
+
+        The above three cases only care about one-way-mapping i.e. mapping characters from the first string to the
+        second one only. Don't we need the mapping from the other side as well?
+
+        We will need two dictionaries instead of one since we need one-to-one mapping from the string s to string t and
+        vice versa.
+
+        For example, s= 'egg' and t = 'add', we need to construct the mapping 'e' -> 'a', 'a' -> 'e', 'g' -> 'd', and
+        'd' -> 'g'. When we encounter a character for the first time, we map it with the corresponding current character
+        of the other string. If we encounter that character later, then we go and fetch the previous mapping and compare
+        it to the current character of the other string. If they match, then fine. Otherwise, the strings can't be
+        isomorphic.
+
+    Time complexity: O(N), where N is the length of s and t
     Space complexity: O(1), the hash maps can't store more than the size of the alphabet characters
     """
-    s_chars, t_chars = {}, {}
-    for c1, c2 in zip(s, t):
-        if c1 not in s_chars and c2 not in t_chars:
-            s_chars[c1] = c2
-            t_chars[c2] = c1
-        elif s_chars.get(c1, c2) != c2 or t_chars.get(c2, c1) != c1:  # We use a default value with .get() to avoid
-            # returning False when both c1 and c2 are encountered for the first time
+    s_mapping, t_mapping = {}, {}
+    n = len(s)
+    for i in range(n):
+        s_char, t_char = s[i], t[i]
+        if s_char not in s_mapping and t_char not in t_mapping:
+            s_mapping[s_char] = t_char
+            t_mapping[t_char] = s_char
+        elif (s_char in s_mapping and s_mapping[s_char] != t_char) or \
+                (t_char in t_mapping and t_mapping[t_char] != s_char):
             return False
-        # The previous block is equivalent to:
-        # if (c1 in s_chars and s_chars[c1] != c2) or (c2 in t_chars and t_chars[c2] != c1):
-        #     return False
-        # s_chars[c1], t_chars[c2] = c2, c1
     return True
 
 
