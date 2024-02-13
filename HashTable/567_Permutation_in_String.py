a9ef0c457c43301ebcbe920cf193af1577d6554c
Anouer <hermassi.anouer@gmail.com>
2024-02-13 16:07:27 -0500

    Permutation in string - v2 update

--- HashTable/567_Permutation_in_String.py
+++ HashTable/567_Permutation_in_String.py
@@ -50,46 +50,55 @@ def check_inclusion_v1(s1, s2):
     return False
 
 
+# Video explanation: https://youtu.be/UbyhOgBN834
 def check_inclusion_v2(s1, s2):
-    """ The previous approach can be optimized. Instead of comparing all the elements of the hashmaps for every
-        updated map corresponding to every window of s2 considered, we keep track of the number of characters
-        which were already matching in the previous window and update just the count of matching characters when
-        we slide the current window.
-        We maintain a 'matches' variable which stores the number of characters that have the same occurrence frequency
-        in s1 and the current window in s2. When we slide the window, if the deduction of the leftmost character and the
-        addition of the new character leads to a new frequency match for any of the characters, we increment 'matches'
-        by 1. Otherwise, we keep 'matches' intact. However, if a character whose frequency was the same earlier
-        (prior to addition or removal) is added, it now leads to a frequency mismatch which is taken into account by
-        decrementing 'matches'. If, after sliding the window 'matches' evaluates to 26, it means all the characters
-        match in frequency totally. So, we return a True in that case immediately.
+    """ The previous approach can be optimized. Instead of comparing all the elements of the hashmaps for every updated
+         frequency map of every window of s2, we keep track of the number of characters which were already matching in
+         the previous window and only update the count of matching elements when we shift the window towards the right.
+
+         We maintain a 'matches' variable which stores the number of characters that have the same frequency of
+         occurrence in s1, and the current window in s2. When we slide the window:
+
+            - If the exclusion of the leftmost character and the inclusion of the new character lead to a new frequency
+               match for any of the characters, we increment 'matches'.
+
+            - Otherwise, we keep 'matches' intact.
+
+        However, if a character whose frequency was the same earlier (prior to addition or removal) is included, it
+        could lead to a frequency mismatch which is taken into account by decrementing 'matches'.
+
+        If after sliding the window 'matches' evaluates to 26, it means all the characters match in frequency.
+
     Time complexity: O(N + M)
     Space complexity: O(1)
     """
+    if len(s1) > len(s2):
+        return False
     n, m = len(s1), len(s2)
-    s1_counter, s2_counter = Counter(s1), Counter(s2[:n])
+    s1_counter, s2_counter = defaultdict(int), defaultdict(int)
+    for i in range(n):
+        s1_counter[s1[i]] += 1
+        s2_counter[s2[i]] += 1
     matches = 0
-    for c in string.ascii_lowercase:
-        if s1_counter[c] == s2_counter[c]:
+    for i in range(26):
+        if s1_counter[chr(i + ord('a'))] == s2_counter[chr(i + ord('a'))]:
             matches += 1
     if matches == 26:
         return True
     for i in range(n, m):
-        cur_char, prev_char = s2[i], s2[i-n]
-
+        cur_char, leftmost_char = s2[i], s2[i-n]
         s2_counter[cur_char] += 1  # Expand the window
         if s1_counter[cur_char] == s2_counter[cur_char]:
             matches += 1
         # If the addition of the current character disrupts an earlier match
-        elif s1_counter[cur_char] == s2_counter[cur_char] - 1:
+        elif s2_counter[cur_char] == s1_counter[cur_char] + 1:
             matches -= 1
-
-        s2_counter[prev_char] -= 1  # Shrink the window
-        if s1_counter[prev_char] == s2_counter[prev_char]:
+        s2_counter[leftmost_char] -= 1  # Shrink the window
+        if s1_counter[leftmost_char] == s2_counter[leftmost_char]:
             matches += 1
         # If the removal of the current character disrupts an earlier match
-        elif s1_counter[prev_char] == s2_counter[prev_char] + 1:
+        elif s1_counter[leftmost_char] == s2_counter[leftmost_char] + 1:
             matches -= 1
-
         if matches == 26:
             return True
     return False
