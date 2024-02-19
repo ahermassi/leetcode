4c3c2ff0ed37d62dbb535c5ceba8d349b8011572
Anouer <hermassi.anouer@gmail.com>
2024-02-19 10:56:36 -0500

    Last stone weight - v1 update

--- Heap/1046_Last_Stone_Weight.py
+++ Heap/1046_Last_Stone_Weight.py
@@ -12,10 +12,16 @@ import unittest2 as unittest
 
 def last_stone_weight_v1(stones):
     """ Conceptually, the simplest way we could solve this problem is to repeatedly search for the 2 largest stones in
-        the array, delete them, and then if they are not the same size, add the new stone size back in. We can repeat
-        this process until there is only one stone left.
-        To simplify the search-for-maximum process, we could maintain a sorted array. We'd need to sort the array at
-        the start, and then ensure that each time we need to add a stone back, that we're maintaining the sorted order.
+         the array, delete them, and then if they are not the same size, add the new stone size back in. We can repeat
+         this process until there is only one stone left.
+
+         To simplify the search-for-maximum process, we could maintain a sorted array. We'd need to sort the array at
+         the start, and then ensure that each time we need to add a stone back, that we're maintaining the sorted order.
+
+         Unfortunately, inserting a stone into a sorted array is an O(N) operation. While we can use binary search to
+         determine where we should put it, inserting it still ultimately requires shifting all the stones after it down
+         by one place.
+
     Time complexity: O(N logN + N^2) = O(N^2), sorting and maintaining sorted order by shuffling existing stones
     Space complexity: O(N), for the sort
     """
