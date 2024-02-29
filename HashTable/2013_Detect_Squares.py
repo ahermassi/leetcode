d3019446d3f17df204d44ea35b2f6836b8d0d09d
Anouer <hermassi.anouer@gmail.com>
2024-02-29 13:28:46 -0500

    Detect squares - v3 update

--- HashTable/2013_Detect_Squares.py
+++ HashTable/2013_Detect_Squares.py
@@ -124,17 +124,16 @@ class DetectSquaresV2:
         return res
 
 
-# Video explanation: https://www.youtube.com/watch?v=bahebearrDc
-
+# Video explanation: https://youtu.be/bahebearrDc
 class DetectSquaresV3:
     """ Similar to the first solution, we maintain a counter of points, that is how many times we have each of them, in
-        a hash map.
+        a hashmap.
 
         However, for a query point p1 = (x, y), we try all the points p2 which together with p1 form the diagonal of
-        non-empty square, i.e. abs(p1.x - p2.x) == abs(p1.y - p2.y) and p1.x != p2.x
+        a non-empty square, i.e. abs(p1.x - p2.x) == abs(p1.y - p2.y) and p1.x != p2.x
 
-        Since we now have two points p1 and p2, we can form a square by computing the positions of the two remain points
-        p3, p4:
+        Since we now have two points p1 and p2, we can form a square by calculating the positions of the two remaining
+        points, p3 and p4:
 
                     p3 = (p1.x, p2.y)
                     p4 = (p2.x, p1.y)
@@ -144,24 +143,24 @@ class DetectSquaresV3:
     """
 
     def __init__(self):
-        self.counter = defaultdict(int)
+        self.points = defaultdict(int)
 
     def add(self, point):
         x, y = point
-        self.counter[(x, y)] += 1
+        self.points[(x, y)] += 1
 
     def count(self, point):
         x, y = point
         res = 0
         # Attempting to access a non-existent key in a defaultdict will add that key (with a value of zero in this
-        # case). This causes the dictionary to mutate while we are iterating over it, which throws an error.
-        # Therefore, we have to 1) Make a copy of the list of keys before iterating, and 2) Include the diagonal
+        # case). This causes the dictionary to mutate during iteration, which throws an error.
+        # Therefore, we have to 1) make a copy of the list of keys before iterating, and 2) include the diagonal
         # point's count in the multiplication before adding to the result. This is because the call to .keys() will
         # never include duplicates, whereas iterating over a list will allow us to come across multiple copies of
         # the diagonal point.
-        keys = list(self.counter.keys())
+        keys = list(self.points.keys())
         for x1, y1 in keys:
             # Skip empty square or invalid square point. Diagonal points CAN NOT lie on the same x-axis.
-            if x != x1 and abs(x - x1) == abs(y - y1):
-                res += self.counter[(x1, y1)] * self.counter[(x, y1)] * self.counter[(x1, y)]
+            if x1 != x and abs(x1 - x) == abs(y1 - y):
+                res += self.points[(x1, y1)] * self.points[(x, y1)] * self.points[(x1, y)]
         return res
