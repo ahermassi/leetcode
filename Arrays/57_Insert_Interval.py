938e0a45bda08de1ab5dc87e0d92d29e0102ce9b
Anouer <hermassi.anouer@gmail.com>
2024-03-23 09:27:15 -0400

    Insert interval - v2 update

--- Arrays/57_Insert_Interval.py
+++ Arrays/57_Insert_Interval.py
@@ -60,22 +60,22 @@ def insert_v2(intervals, new_interval):
          Finally, we add all the intervals that are to the right of newInterval.
 
          Example: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
-         Interval = [1,2] is before [4,8], i.e. interval.end < newInterval.start, so we just add it to output.
+         Interval = [1,2] ends before [4,8] starts, so we just add it to the output.
          Interval = [3,5], overlap, newInterval = [min(3, 4), max(5, 8) = [3,8]
          Interval = [6,7], overlap, newInterval = [min(3, 6), max(8, 7] = [3,8]
          Interval = [8,10], overlap, newInterval = [min(3, 8), max(8, 10)] = [3,10]
          Interval = [12,16], starts after newInterval ends, so no overlap.
-         Outside the loop we combine merged = [1,2], newInterval = [3,10], and intervals[4:] = [12,16]
-         --> merged = [[1,2], [3,10], [12,16]]
+         Outside the loop we combine res = [1,2], newInterval = [3,10], and intervals[4:] = [12,16]
+         --> res = [[1,2], [3,10], [12,16]]
 
     Time complexity: O(N)
     Space complexity: O(N)
     """
-    n, merged = len(intervals), []
+    n, res = len(intervals), []
     start, end = new_interval
     i = 0
     while i < n and intervals[i][1] < start:
-        merged.append(intervals[i])
+        res.append(intervals[i])
         i += 1
     while i < n and intervals[i][0] <= end:
         # Merge overlapping intervals. We're mutating newInterval to represent the overall merged interval of
@@ -83,10 +83,11 @@ def insert_v2(intervals, new_interval):
         start = min(start, intervals[i][0])
         end = max(end, intervals[i][1])
         i += 1
-    merged.append([start, end])  # Add the union of previous intervals that overlapped with newInterval. Notice that if
-    # no interval overlaps with newInterval, [start, end] is the same as newInterval
-    merged.extend(intervals[i:])  # Add all the remaining intervals
-    return merged
+    # Add the union of previous intervals that overlapped with newInterval. Notice that if no interval overlaps with
+    # newInterval, [start, end] is the same as newInterval.
+    res.append([start, end])
+    res.extend(intervals[i:])  # Add all the remaining intervals
+    return res
 
 
 def insert_v3(intervals, new_interval):
