89d2f1b0696c4b51d607eb2fa150fce057a88c1a
Anouer <hermassi.anouer@gmail.com>
2024-03-24 10:23:20 -0400

    Snakes and ladders - update

--- BFS/909_Snakes_and_Ladders.py
+++ BFS/909_Snakes_and_Ladders.py
@@ -9,13 +9,13 @@ def snakes_and_ladders(board):
     """ We can model the grid as a graph. Each square is a node. There are edges between squares within 6 of each other,
          and the snakes and ladders add new edges.
 
-         The problem is asking us for the minimum number of moves, which suggests this is a shortest-path problem:
+         The problem is asking for the minimum number of moves, which suggests this is a shortest-path problem:
 
                 Given an unweighted directed graph, the shortest path problem is the problem of finding a path from one
                 vertex to another, such that the number of edges is the minimum possible.
 
         We can consider the input as an unweighted directed graph. The edges are moves corresponding to the results of
-        a 6-sided die roll.
+        a 6-sided dice roll.
 
         Breadth-first search is an algorithm for finding the shortest path in unweighted graphs (directed or
         undirected). It maintains a queue of vertices (nodes). It starts with only the starting vertex (cell 1 in this
@@ -28,39 +28,40 @@ def snakes_and_ladders(board):
                first cell to the queue.
 
             - While the queue is not empty:
-                    * Pop a cell from the queue. Let's say its label is cur. For each square next_square with a label in
-                       the range (cur+1) to min(curr+6,n^2), if next_square has a snake or a ladder, set next_square to
-                       the destination of that snake or ladder.
+
+                    * Pop a cell from the queue, cur. For each square next_square in range [(cur+1), min(curr+6,n^2)],
+                       if next_square has a snake or a ladder, set next_square to the destination of that snake or ladder.
+
                     * If next_square has not been visited yet, increment the number of moves and push it along
-                       next_square on to the queue.
+                       next_square to the queue.
 
             - Return the distance to cell n^2 if it is reachable, otherwise return −1.
 
     Time complexity: O(N^2), we run BFS on a graph whose vertices are the board cells, and the edges are moves between
     them. There are N^2 vertices and no more than 6 * N^2=O(N^2) edges. The time complexity of BFS is O(∣V∣+∣E∣).
-    We have ∣V∣= N^2 and ∣E∣ <6 * N^2, thus the total time complexity for BFS is O(7 * N^2)=O(N^2).
+    We have ∣V∣= N^2 and ∣E∣ < 6 * N^2, thus the total time complexity for BFS is O(7 * N^2) = O(N^2).
     Space complexity: O(N)
     """
 
-    def get_coordinates(num):
-        quot, rem = divmod(num - 1, n)
-        row = n - quot - 1
-        col = rem if quot % 2 == 0 else n - 1 - rem
+    def get_coordinates(square):
+        a, b = (square - 1)//n, (square - 1) % n
+        row = n - a - 1
+        col = b if a % 2 == 0 else n - 1 - b
         return row, col
 
     n, m = len(board), len(board[0])
     queue = deque([(1, 0)])
     visited = set()
     while queue:
-        square, steps = queue.popleft()
+        square, moves = queue.popleft()
         for next_square in range(square + 1, min(square + 6, n * n) + 1):
             x, y = get_coordinates(next_square)
             if board[x][y] != -1:
                 next_square = board[x][y]
             if next_square == n * n:
-                return steps + 1
+                return moves + 1
             if next_square not in visited:
-                queue.append((next_square, steps + 1))
+                queue.append((next_square, moves + 1))
                 visited.add(next_square)
     return -1
 
