5d0b99dc56ed618c48512516e51ccc026d9a3f68
Anouer <hermassi.anouer@gmail.com>
2024-03-30 09:04:27 -0400

    Minimum window substring - v1 update

--- HashTable/76_Minimum_Window_Substring.py
+++ HashTable/76_Minimum_Window_Substring.py
@@ -12,85 +12,89 @@ def min_window_v1(s, t):
         We can use a simple sliding window approach to solve this problem. Let us call a window desirable if it has all
         the characters from T.
 
-        In any sliding window based problem we have two pointers. One right pointer whose job is to expand the current
-        window and one left pointer whose job is to contract a given window. At any point in time, only one of these
+        In any sliding window based problem we have two pointers: one right pointer whose job is to expand the current
+        window, and one left pointer whose job is to contract the window. At any point in time, only one of these
         pointers moves and the other one remains fixed.
 
-        The solution is pretty intuitive: We keep expanding the window by moving the right pointer. When the window has
-        all the desired characters, we contract (if possible) and save the smallest window till now. The answer is the
-        smallest desirable window seen so far.
+        The solution is pretty intuitive: keep expanding the window by moving the right pointer. When the window has
+        all the desired characters, we contract (if possible) and save the smallest window so far. The answer is the
+        smallest desirable window.
 
-            - We start with two pointers, 'left' and 'right', initially pointing to the first element of the string S.
+            - Start with two pointers, 'left' and 'right', initially pointing to the first element of the string S.
 
-            - We use the right pointer to expand the window until we get a desirable window i.e. a window that
-               contains all the characters of T.
+            - Use the right pointer to expand the window until we get a desirable window i.e. a window that contains all
+               the characters of T.
 
             - Once we have a window with all the characters, we can move the left pointer ahead one by one. If the
-               window is still a desirable one, we keep on updating the minimum window size. If the window is not
-               desirable anymore, we repeat step 2 onwards.
+               window is still desirable, we keep on updating the minimum window size. If the window is no longer
+               desirable, we repeat step 2 onwards.
 
-        To check if a window is valid/desirable, we use a map 'counter' to store (char, count) of characters
-        in T (even though the map evolves to include characters in S but with a negative count). We also use a counter
+        To check if a window is valid/desirable, we use a map 'counter' to store (char, count) of characters in T (even
+        though the map evolves to include characters in S but with a negative count). We also use a count variable
         'matched_characters' for the number of chars of T so far matched in S.
 
-        The key part is counter[cur_char] -= 1: We decrease the count of EVERY character in S. If it does not exist in
+        The key part is counter[cur_char] -= 1: we decrease the count of EVERY character in S. If it does not exist in
         T, the count will drop below zero. When contracting the window, we make sure that the active window always
         contains all the characters in T. In this case, every time the window expands, only the new character is
         checked.
 
         The idea is we use a variable-length sliding window which is gradually applied across the string. We use two
-        pointers 'left' and 'right' to mark the sliding window. We start by fixing the left pointer and moving the
-        right pointer to the right. The way we determine whether the current window is valid is by checking if all the
-        target characters are present in the current window. If we are in a valid sliding window:
+        pointers 'left' and 'right' to mark the sliding window. We start by fixing the left pointer and moving the right
+        pointer to the right. The way we determine whether the current window is valid is by checking if all the target
+        characters are present in the current window. If we are in a valid sliding window:
 
-            - We first make note of the sliding window of the most minimum length we have seen so far.
+            - We first take note of the sliding window of the minimum length we have seen so far.
+
             - Next, we try to contract the sliding window by moving the left pointer.
+
             - If the sliding window continues to be valid, we note the new minimum length.
-            - If it becomes invalid (at least one character of the target string have been dropped), we break out of
+               If it becomes invalid (at least one character of the target string have been dropped), we break out of
                the inner loop and go back to expanding the window.
 
         The idea is a general solution for substring (longest/shortest/non-duplicate, etc):
 
-            - Create a hash map tracking occurrences of any specific characters in the target substring.
-            - Use two pointers, left=0 and right=0, to track current width/distance/range of the active window.
-            - Create a counter tracking if current width/distance/range is a valid substring range.
-            - As long as the counter indicates it's a valid range, shrink/expand the window until range's validity
-               changes (by altering counter accordingly), record the valid width and let it compare with the last value
-               (using min()/max() accordingly).
+            - Create a hashmap to track the occurrences of any specific characters in the target substring.
+
+            - Use two pointers, left and right, to track the current width/distance/range of the active window.
+
+            - Create a counter to check if the current width/distance/range is a valid substring range.
+
+            - As long as the counter indicates it's a valid range, shrink/expand the window until the range's validity
+               changes (by altering the counter accordingly), record the valid width, and let it compare with the last
+               value (using min()/max() accordingly).
 
     Time complexity: O(N + M), both pointers scan the string s once
     Space complexity: O(N + M), or O(1)
     """
     n = len(s)
     counter = Counter(t)
-    # characters_to_match is the total number of characters yet to be matched in the current window to be
-    # desirable/valid
+    # characters_to_match is the total number of characters yet to be matched in the current window to make it valid
     characters_to_match = len(t)
     left = right = 0
-    min_left, min_length = 0, float('inf')  # min_left is the left boundary of the minimum window found so far
+    min_left, min_length = 0, float('inf')  # min_left is the left boundary of the minimum valid window found so far
     while right < n:
         cur_char = s[right]
-        # Always decrement the counter of the current character. If the character is not in t, the counter drops below
-        # zero. Interestingly, the counter for non-target characters never gets positive because the upper loop
-        # decrements and lower loop increments the same elements
+        # Always decrement the count of the current character. If the character is not in t, the count drops below zero.
+        # Interestingly, the count for non-target characters never gets positive because the upper loop decrements and
+        # the lower loop increments the same elements.
         counter[cur_char] -= 1
         if counter[cur_char] >= 0:
-            # If the counter of the current character doesn't drop below zero, it means the character exists in t, so
-            # decrement the counter of characters to match
+            # If the count of the current character doesn't drop below zero, it means the character exists in t, so
+            # decrement the count of characters to match.
             characters_to_match -= 1
         while characters_to_match == 0:
-            # Try and contract the window till it ceases to be desirable/valid
-            cur_length = right - left + 1
-            if cur_length < min_length:
-                min_left, min_length = left, cur_length  # Save the smallest window
-            # Increment the counter of the leftmost character in the window. We increment this for all characters, but
-            # only the target characters have a chance to have positive counters.
+            # Try to contract the window until it ceases to be desirable/valid
+            window_length = right - left + 1
+            if window_length < min_length:
+                min_left, min_length = left, window_length  # Save the smallest valid window
+            # Increment the count of the leftmost character in the window. We increment this for all characters, but
+            # only the target characters have a chance to have positive counts.
             counter[s[left]] += 1
             if counter[s[left]] > 0:
                 # If the character we just discarded was part of t, then it would have a counter equal to 0 AT LEAST,
                 # and if it's the case it would be > 0 after being incremented.
                 # For every character that is not in s, we do one increment and one decrement. So, it totals up to 0,
-                # which will never pass the if condition.
+                # which will never satisfy the if condition.
                 characters_to_match += 1
             left += 1
         right += 1
