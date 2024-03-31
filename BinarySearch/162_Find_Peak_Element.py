a0d0e99e528f95fa3b4d811e369335fb077d46e5
Anouer <hermassi.anouer@gmail.com>
2024-03-31 09:50:58 -0400

    Find peak element - v2 update

--- BinarySearch/162_Find_Peak_Element.py
+++ BinarySearch/162_Find_Peak_Element.py
@@ -43,38 +43,40 @@ def find_peak_element_v1(nums):
 
 def find_peak_element_v2(nums):
     """ We can view any given sequence in nums array as alternating ascending and descending sequences. By making
-         use of this, and the fact that we can return any peak as the result, we can make use of binary search to find
-         the peak element.
+         use of this property and the fact that we can return any peak, we can use binary search to find the peak
+         element.
 
-         In the case of simple binary search, we work on a sorted sequence of numbers and try to find out the required
-         number by reducing the search space at every step. In this case, we use a modification of this simple binary
-         search to our advantage.
+         In the case of a regular binary search, we work on a sorted sequence of numbers and try to find the required
+         number by reducing the search space at every step. For this problem, we use a modified binary search.
 
-         We start off by finding the middle element 'mid' from the given nums array.
+         We start off by finding the middle element 'mid'.
 
-            - If mid happens to be lying in a descending sequence of numbers, or a local falling slope (found by
-               comparing nums[i] to its left neighbor), it means that the peak will always lie towards the left of mid.
-               Thus, we reduce the search space to the left of mid (including itself) and perform the same process on
-               the left subarray.
+            - If mid happens to be in a descending sequence of numbers, or a local falling slope (found by comparing
+               nums[i] to its left neighbor), it means that the peak will always be towards the left of mid. Thus, we
+               reduce the search space to the left of mid (including itself) and perform the same process on the left
+               subarray.
 
-            - If mid lies in an ascending sequence of numbers, or a rising slope (found by comparing nums[i] to its
-               right neighbor), it obviously implies that the peak lies towards the right of this element. Thus, we
-               reduce the search space to the right of mid and perform the same process on the right subarray.
+            - If mid is in an ascending sequence of numbers, or a rising slope (found by comparing nums[i] to its
+               right neighbor), it obviously implies that the peak is towards the right of this element. Thus, we reduce
+               the search space to the right of mid and perform the same process on the right subarray.
 
-         In this way, we keep on reducing the search space till we eventually reach a state where only one element is
+         In this way, we keep on reducing the search space until we eventually reach a state where only one element is
          remaining in the search space. This single element is the peak element.
 
-         Binary search works here because we need to return any local peak, not necessarily the global peak. If the
-         number to its right is higher than the middle value, then somewhere on the right there must be a peak - either
-         the numbers ascend and then descend, in which case there would be a peak where the change from ascent to
-         descent happens, or the numbers continue to ascend until the end of the array, in which case the last value in
-         the array would be a local peak (because nums[n] = -∞).
-         The same with the other way. If the value to the left of the middle element is bigger than the middle value,
-         then it must be that either the middle value itself is a peak or that there is definitely a peak on the left
-         side of the middle value. This is because if the number on the left is bigger than the middle value, there are
-         two options: either the numbers continue ascending in the left direction until the end, in which case the first
-         element of the array would be a peak (because nums[0] = -∞), or the values increase to the left until a point
-         at which they start decreasing, and that point would be a peak.
+         Binary search works here because we need to return any local peak, not necessarily the global peak.
+
+            - If the number to the right is bigger than the middle value, then somewhere on the right there must be a
+               peak - either the numbers ascend and then descend, in which case there would be a peak where the change
+               from ascent to descent happens, or the numbers continue to ascend until the end of the array, in which
+               case the last value in the array would be a local peak (because nums[n] = -∞).
+
+
+            - If the value to the left is bigger than the middle value, then it must be that either the middle value
+               itself is a peak or that there is definitely a peak on the left side of the middle value. This is because
+               if the number on the left is bigger than the middle value, there are two options: either the numbers
+               continue ascending in the left direction until the beginning of the array, in which case the first
+               element of the array would be a peak (because nums[0] = -∞), or the values increase to the left until a
+               point at which they start decreasing, and that point would be a peak.
 
          So binary search works because as long as we follow the slopes upward, we will for sure encounter a peak.
          Yes, we may miss some peaks during binary cuts, but there is at least 1 that will be encountered since beyond 0
@@ -90,53 +92,56 @@ def find_peak_element_v2(nums):
          -∞ | 3,2,1,0|-∞
          So all we need to do is find the end of any increasing slope in the input array.
 
-         Let's say we have a mid at index x, nums[x]. If nums[x] < nums[x+1], that means a peak element HAS TO exist at
+         Let's say we have a mid at index i, nums[i]. If nums[i] < nums[i+1], that means a peak element HAS TO exist in
          the right half of the array, because (since every number is unique):
 
             - If the numbers keep increasing on the right side, then the peak will be the last element.
-            - If the numbers stop increasing and there is a 'dip', or there exists somewhere a number such that
-               nums[y] < nums[y-1], it means number[x] is a peak element.
+            - If the numbers stop increasing and there is a 'dip', or there exists somewhere an index j such that
+               nums[j] < nums[j-1], it means number[i] is a peak element.
 
-         This same logic can be applied to the left-hand side (nums[x] < nums[x-1]).
+         This same logic can be applied to the left-hand side (nums[i] < nums[i-1]).
 
          Example:
         | 1 | 2 | 3 | 4 | 5 | 4 | 3 | 2 | 1 |
         |---|---|---|---|---|---|---|
-        | l | _ | _ | _ | m | _ | _ | _ | r |
-        nums[mid] > nums[mid+1] -> r=mid (Not mid-1 since mid can be the answer)
+        | l | _ | _ | _ | m | _ | _ | _ | r    |
+        nums[mid] > nums[mid+1] -> r=mid (not mid-1 since mid can be the answer)
 
         | 1 | 2 | 3 | 4 | 5 | 4 | 3 | 2 | 1 |
         |---|---|---|---|---|---|---|
-        | l | _ | m | _ | r | X | X | X | X |
-        nums[mid] < nums[m+1] -> l = mid+1 (Since nums[mid] < nums[mid+1], mid can't  be the answer)
+        | l | _ | m | _ | r | X | X | X | X   |
+        nums[mid] < nums[m+1] -> l = mid+1 (since nums[mid] < nums[mid+1], mid can't be the answer)
 
         | 1 | 2 | 3 | 4 | 5 | 4 | 3 | 2 | 1 |
         |---|---|---|---|---|---|---|
-        | X | X | X |l,m | r | X | X | X | X |
-        nums[mid] < nums[m+1] -> l = mid+1 (Since nums[mid] < nums[mid+1], mid can't be the answer)
+        | X | X | X |l,m | r | X | X | X | X|
+        nums[mid] < nums[m+1] -> l = mid+1 (since nums[mid] < nums[mid+1], mid can't be the answer)
 
         | 1 | 2 | 3 | 4 | 5   | 4 | 3 | 2 | 1 |
         |---|---|---|---|-----|---|---|
         | X | X | X | X | l,r | X | X | X | X |
-        l is the answer
+        l=5 is the answer
 
-                 5   5                           5
+                 5   5                            5
                 / \ / \                         / \
                4   4   4                       4  -∞
-              /         \                     /
-             3           3           3       3
-            /             \         / \     /
+              /          \                      /
+             3           3           3        3
+            /             \         / \      /
            2               2       2   2   2
-          /                 \     /     \ /
-        -∞                   1   1       1
-                              \ /
-                               0
-        0 1 2 3 4 5 6 7 8 910111213141516171819 indices
-        2,3,4,5,4,5,4,3,2,1,0,1,2,3,2,1,2,3,4,5 (20 nums)
-       l                 m                   r l=0, m=9, r=19
-       l       m         r                     l=0, m=4, r= 9
-                 l   m   r                     l=5, m=7, r= 9
-                 l>m r                         l=5, m=6, r= 7
+          /                 \     /      \ /
+        -∞                 1   1       1
+                               \ /
+                                0
+        0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19   (indices)
+        2, 3, 4, 5, 4, 5, 4, 3, 2, 1, 0,  1,  2,  3,  2,  1,  2,  3,  4,  5    (nums)
+        l                                   m                                              r     l=0, r=19, m=9
+        l               m                 r                                                      l=0, r=9, m=4
+                            l      m      r                                                      l=5, r=9, m=7
+                            l  m r                                                               l=5, r=7, m=6
+                      m=l  r                                                                    l=5, r=6, m=5
+                  r=m=l                                                                       l=5, r=5, m=5
+        peak = nums[l] = nums[5] = 5
 
     Time complexity: (logN)
     Space complexity: O(1)
