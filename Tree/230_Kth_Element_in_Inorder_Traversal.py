6c15050143ae1ff5dadf6ab28387884295a98bd7
Anouer <hermassi.anouer@gmail.com>
2024-04-02 07:54:47 -0400

    Kth element in inorder traversal - update

--- Tree/230_Kth_Element_in_Inorder_Traversal.py
+++ Tree/230_Kth_Element_in_Inorder_Traversal.py
@@ -10,19 +10,19 @@ class TreeNode(object):
 
 
 def find_kth_node_binary_tree(root, k):
-    """ If k is greater than the number of nodes in the left subtree, the kth node cannot lie in the left subtree.
+    """ If k is greater than the number of nodes in the left subtree, the kth node cannot exist in the left subtree.
          More precisely, if the left subtree has L nodes, then the kth node in the original tree is the (k - L - 1)th
-        node when we skip the left subtree. Conversely, if k < L, the desired node lies in the left subtree.
+         node when we skip the left subtree. Conversely, if k < L, the desired node is in the left subtree.
 
     Time complexity: O(logN) best case, O(N) worst case of a skewed tree
     Space complexity: O(1)
     """
     cur = root
     while cur:
-        left_size = cur.left.size if cur.left else 0
-        if left_size == k - 1:
+        left_subtree_size = cur.left.size if cur.left else 0
+        if left_subtree_size == k - 1:
             return cur
-        if left_size < k - 1:
+        if left_subtree_size < k - 1:
             cur = cur.right
         else:
             cur = cur.left
