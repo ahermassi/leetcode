d7ae8a628d0f162e2900033e4184f2868a32c25b
Anouer <hermassi.anouer@gmail.com>
2024-04-15 08:29:56 -0400

    Zigzag conversion - update

--- String/6_ZigZag_Conversion.py
+++ String/6_ZigZag_Conversion.py
@@ -12,27 +12,29 @@ import unittest2 as unittest
 
 
 def convert(s, num_rows):
-    """ Construct the final string line by line and then join the lines.
+    """ Construct the final string line by line and then join the lines together.
 
-         We start with variable 'row_index' with the value 0, 'direction' with the value 1. Each consequent character is
+         We start with a variable 'row_index' set to 0, 'direction' with the value 1. Each consequent character is
          added to a specific line. When we are in the first row, the direction should move down, i.e. direction = 1.
          When it's the last row, the direction should go back up, i.e. direction = -1.
 
-        Take the string "PAYPALISHIRING" for example:
+            - We start with variable 'row_index' with the value 0, 'direction' set to 1 (or -1).
 
-        We start with variable 'row_index' with the value 0, 'direction' with the value 1.
-        Each character is appended to the line pointed to by 'row_index'. If we reach the bottom row, we need to go
-        back up to the next above row, so we change 'direction' value to -1 and start moving up. We keep 'direction'
-        value unchanged until we reach the top row.
+            - Each character is appended to the line pointed to by 'row_index'.
 
-        Again, if we reach the top row, we need to reset 'direction' value to 1 and start moving down.
+            - If we reach the bottom row, we need to go back up to the row above, so we change 'direction' value to -1
+               and start moving up. We keep 'direction' value unchanged until we reach the top row.
+
+            - Again, if we reach the top row, we need to reset 'direction' value to 1 and start moving down.
 
         What we need to remember is: the zigzag pattern is just a pictorial image for us to have a better understanding.
-        The trick of algorithm is actually adding the next character of the string to different rows.
-        Don't really think how to move the cursor in the matrix. It's really a misleading way of thinking. Even it
+        The trick of the algorithm is actually adding the next character of the string to different rows.
+
+        Don't think about how to move the cursor in the matrix. It's really a misleading way of thinking. Even if it
         works, it's not efficient.
 
-        This can be better understood by looking at the contents of 'rows' list in each iteration:
+        This can be better understood by looking at the contents of 'rows' list in each iteration. Take the string
+        "PAYPALISHIRING" for example:
 
             P ['P', '', '']
             A ['P', 'A', '']
@@ -52,9 +54,9 @@ def convert(s, num_rows):
     Time complexity: O(N)
     Space complexity: O(N)
     """
-    if num_rows == 1 or num_rows >= len(s):
+    if num_rows == 1:
         return s
-    rows = [''] * num_rows # This list will hold the num_rows rows that will be joined together to form the final result
+    rows = [''] * num_rows # This list will hold num_rows rows that will be joined together to form the final result
     row_index = 0
     # 'direction' is initially set to -1 (go up) so that it's reset to 1 (go down) the first time the loop runs. If we
     # want to initialize direction = 1, we would need to have 2 checks:
@@ -62,10 +64,10 @@ def convert(s, num_rows):
     # elif row_index == num_rows - 1: direction = -1
     direction = -1
     for c in s:
-        rows[row_index] += c # Append the current character to the current line
-        if row_index == num_rows - 1 or row_index == 0:
-            # If we are at the first line, move down (append to following (+1) line in the next iteration).
-            # # If we are at the last line, move up (append to previous line in the next iteration)
+        rows[row_index] += c # Append the current character to the corresponding line
+        if row_index == 0 or row_index == num_rows - 1:
+            # If we are at the first line, move down (append to the next (+1) line in the next iteration).
+            # # If we are at the last line, move up (append to the previous line in the next iteration)
             direction *= -1
         row_index += direction # This is what determines which line to append to next: following (+1) or previous (-1)
     return ''.join(rows)
