fdd7a6da02637db003c7d59493d6773f3b771c3a
Anouer <hermassi.anouer@gmail.com>
2024-04-27 11:03:34 -0400

    Add and search word data structure - v1 update

--- Tree/211_Add_and_Search_Word_Data_structure_design.py
+++ Tree/211_Add_and_Search_Word_Data_structure_design.py
@@ -7,9 +7,8 @@ can represent any one letter. """
 from collections import defaultdict
 import unittest2 as unittest
 
+
 # Video explanation: https://www.youtube.com/watch?v=BTf05gs_8iU
-
-
 class TrieNode:
     def __init__(self):
         self.children = defaultdict(TrieNode)
@@ -18,12 +17,12 @@ class TrieNode:
 
 class WordDictionaryV1(object):
     """
-        It's quite easy to write the solution using such data structures as hashmap or balanced tree. Once the hash
-        table increases in size, there are a lot of hash collisions
+        It's quite easy to write the solution using data structures like hashmap or balanced tree. Once the hash
+        table increases in size, there are a lot of hash collisions.
 
         Trie could use less space compared to hashmap when storing many keys with the same prefix.
 
-        Use a trie to store the words, and backtracking to check each character of word to search.
+        Use a trie to store the words, and backtracking to check each character of the word to search.
     """
 
     def __init__(self):
@@ -35,9 +34,9 @@ class WordDictionaryV1(object):
             At each step, we have to verify if the child node to add is already present. If yes, just go one step down.
             If not, add it into the trie and then go one step down.
 
-        Time complexity: O(M), where M is the length of word
-        Space complexity: O(M) in the worst case where the newly inserted word doesn't share a prefix with the
-        words already inserted in the trie. We have to add M new nodes.
+        Time complexity: O(N), where N is the length of word
+        Space complexity: O(N) in the worst case where the newly inserted word doesn't share a prefix with the
+        words already inserted in the trie. We have to add N new nodes.
         """
         root = self.root
         for c in word:
@@ -46,7 +45,7 @@ class WordDictionaryV1(object):
 
     def search(self, word):
         """ Returns if the word is in the data structure. A word could contain the dot character '.' to represent
-            any one letter.
+            any letter.
 
             In the absence of '.' characters, the search would be as simple as addWord. Each key is represented in the
             trie as a path from the root to the internal node or leaf. We start from the root and go down in trie,
@@ -61,31 +60,31 @@ class WordDictionaryV1(object):
             the children of the current trie node is equal to the character at current 'index', we recursively try to
             find the rest of the string starting at (index + 1), and the exploration starts from that child.
 
-        Time complexity: O(M) if we exclude wildcards (.), worst case search will iterate through the longest word
-        (i.e. linear to the maximum number of characters in a word). With wildcards, it will iterate through the whole
-        tree in the worst case (i.e. linear to the total number of characters), which is O(26^M) when we have an
-        undefined word '.....' whose length is M, as the branching factor is 26 and the depth is M. Each key could
-        have 26 chars, and for M length of searched word we need to search 26^M with all keys.
-        Suppose the word is '...z'; It would be O(26^M) where M is the length of the word; At each node due to dot
-        we search in 26 more branches until we find the match.
+        Time complexity: O(N) if we exclude wildcards (.), worst case search iterates through the longest word (i.e.
+        linear to the maximum number of characters in a word). With wildcards, it iterates through the whole tree in the
+        worst case (i.e. linear to the total number of characters), which is O(26^N) when we have an undefined word
+        '.....' whose length is N, as the branching factor is 26 and the depth is N. Each key could have 26 chars, and
+        for N length of searched word we need to search 26^N with all keys.
+        Suppose the word is '...z'; It would be O(26^N) where N is the length of the word; At each node due to '.' we
+        search in 26 more branches until we find the match.
         """
 
-        def dfs(node, index):
+        def dfs(index, node):
             if index == n:
                 return node.end_of_word
             c = word[index]
             if c != '.':
                 if c not in node.children:
                     return False
-                return dfs(node.children[c], index + 1)  # Start of the prefix was found, so keep following that path
+                return dfs(index + 1, node.children[c])  # Start of the prefix was found, so keep following that path
             # Can we find the REST of the string in any of the children?
             for child in node.children.values():
-                if dfs(child, index + 1):
+                if dfs(index + 1, child):
                     return True
             return False
 
         n = len(word)
-        return dfs(self.root, 0)
+        return dfs(0, self.root)
 
 
 class WordDictionaryV2(object):
