48710c84c18ace36c106c615d49e6c79e4401199
Anouer <hermassi.anouer@gmail.com>
2024-04-28 11:34:05 -0400

    Redundant connection - v1 update

--- Tree/684_Redundant_Connection.py
+++ Tree/684_Redundant_Connection.py
@@ -18,35 +18,33 @@ def find_redundant_connection_v1(edges):
 
                 An undirected connected graph with no cycles is a tree
 
-         The statement of the problem guarantees that by deleting one and only one edge the graph is a tree.
-         So, we know it is connected and has exactly one cycle. Our goal is to find the edge in this cycle with the
+         The statement of the problem guarantees that by deleting one and only one edge, the graph is a tree.
+         So, we know it is connected and has exactly one cycle. The goal is to find the edge in this cycle with the
          largest index and return it.
 
-         For each edge (u, v), traverse the graph with DFS to see if we can already connect u to v. If we can, then it
+         For each edge (u, v), traverse the graph using DFS to see if we can already connect u to v. If we can, then it
          must be the duplicate edge.
 
          We're basically doing cycle prevention (and not detection). We build the graph one edge at a time. However,
-         before adding an edge between u and v, we first check if there is already a path between them, avoiding a
+         before adding an edge between u and v, we first check if a path already exists between u and v, avoiding a
          cycle.
 
         How does the algorithm handle "If there are multiple answers, return the answer that occurs last in the input" ?
 
          Since we are already iterating in the order of the input edges, and since we return the edge as soon as it
-         produces a cycle, it is a prerequisite that all the other edges that participate in forming the cycle have
-         already come before this edge. That is why adding this edge would give us a cycle. Hence, this should be the
-         last edge among the edges that participate in the cycle.
-         Adding an edge to a tree creates only one cycle (trivial to prove). Assume that returned edge isn't last. We
-         now have a cycle. When the last edge is reached, we detect another cycle and that is contradiction.
+         produces a cycle, it is a prerequisite that all the other edges that participate in forming the cycle appeared
+         before this edge. That is why adding this edge would result in a cycle. Hence, this should be the last edge
+         among the edges that participate in the cycle.
+
+         Adding an edge to a tree creates only one cycle (trivial to prove). Assume that returned edge isn't the last.
+         We now have a cycle. When the last edge is reached, we detect another cycle and that is contradiction.
 
          Since there are N vertices and N edges, there can be only one cycle. All the edges from that same cycle are
          candidates for removal, but since we are iterating the input array from left to right, there is only one edge
          that "completes" the cycle. Consider this example: edges = [[1,2],[1,3],[2,3]]. All edges are part of the cycle,
-         but it's [2,3] that actually "introduces" it.
-
-         Remember that the input graph contains a unique cycle. The main goal is to find the last edge of that cycle,
-         which is equivalent to finding the edge that will close that cycle. By adding edges one by one (in the order
-         they appear in input) we can be sure that we will stop at the last edge in the input which forms a cycle,
-         i.e., the edge that destroys the tree property of the initial graph.
+         but it's [2,3] that actually first "introduces" it. By adding edges one by one (in the order they appear in the
+         input), we can be sure that we will stop at the last edge in the input which forms a cycle, i.e., the edge that
+         destroys the tree property of the graph.
 
     Time complexity: O(N^2), where N is the number of vertices (and also the number of edges) in the graph. In the
     worst case, for every edge we include, we have to search every previously-occurring edge of the graph.
@@ -54,9 +52,9 @@ def find_redundant_connection_v1(edges):
     """
 
     def path_exists(src, target):
-        # path_exists(src, target) returns True if target node is reachable from src in the currently built graph.
+        # path_exists(src, target) returns True if target node is reachable from src in the current graph
         if src in visited:
-            # If a node is visited, we cannot find a path through it as it itself formed a cycle which we do not want.
+            # If a node was visited, we cannot find a path through it as it itself formed a cycle which we do not want.
             return False
         if src == target:
             # A path was found
@@ -69,8 +67,7 @@ def find_redundant_connection_v1(edges):
 
     graph = defaultdict(list)
     for a, b in edges:
-        # Perform DFS every time we add an edge so that we if a cycle is formed we know exactly this edge
-        # caused the cycle.
+        # Perform DFS before adding the edge so that we if a cycle is detected we know this edge would cause the cycle
         visited = set()
         if path_exists(a, b):
             return [a, b]
