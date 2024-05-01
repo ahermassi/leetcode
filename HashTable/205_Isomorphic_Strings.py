fa60ba386bdeb3309aa520a77f0522fd71e019e3
Anouer <hermassi.anouer@gmail.com>
2024-05-01 08:14:04 -0400

    Isomorphic strings - v1 update

--- HashTable/205_Isomorphic_Strings.py
+++ HashTable/205_Isomorphic_Strings.py
@@ -8,46 +8,39 @@ import unittest2 as unittest
 
 # Video explanation: https://youtu.be/7yF-U1hLEqQ
 def is_isomorphic_v1(s, t):
-    """ Matching the order will be easy. Since we will iterate over the two strings and do some sort of comparison from
-         left to right, the task of ensuring that the character order is the same in both strings will take care of
-         itself.
+    """ Matching the order is easy. Since we iterate over the two strings and do some sort of comparison from left to
+         right, the task of ensuring that the characters order is the same in both strings will take care of itself.
 
-         We will process both of the strings from left to right. At each step, we take one character at a time from the
-         two strings and compare them. There are three cases we need to handle here:
+         We process the two strings from left to right. At each step, we consider two characters at a time from each of
+         the two strings and compare them. We need two dictionaries instead of one since we need one-to-one mapping from
+         string s to string t and vice versa.
+         There are 3 cases we need to handle here:
 
-            1- If the characters don't have a mapping, we add one in the dictionary and move on.
+            1- The characters don't have mappings, so we create their mappings.
 
-            2- The characters already have a mapping in the dictionary. If that is the case, then we're good to go.
+            2- The characters already have mappings, then we're good to go.
 
-            3- The final case is when a mapping already exists for one of the characters, but it doesn't map to the
-                 other character. In this case, we can safely conclude that the given strings are not isomorphic,
-                 and we can return.
+            3- A mapping already exists for one of the characters, but it doesn't map to the other character. In this
+                 case, we can conclude that the given strings are not isomorphic.
 
-        The above three cases only care about one-way-mapping i.e. mapping characters from the first string to the
-        second one only. Don't we need the mapping from the other side as well?
+        For example, s= 'egg' and t = 'add', we need to create the mappings {'e' -> 'a', 'g' -> 'd'} and
+        {'a' -> 'e', 'd' -> 'g'}. When we encounter a character for the first time, we map it to the corresponding
+        character at the same index in the other string. If we encounter that character later again, then we check its
+        current mapping and compare it to the current character of the other string. If they don't match, then the
+        strings can't be isomorphic.
 
-        We will need two dictionaries instead of one since we need one-to-one mapping from the string s to string t and
-        vice versa.
-
-        For example, s= 'egg' and t = 'add', we need to construct the mapping 'e' -> 'a', 'a' -> 'e', 'g' -> 'd', and
-        'd' -> 'g'. When we encounter a character for the first time, we map it with the corresponding current character
-        of the other string. If we encounter that character later, then we go and fetch the previous mapping and compare
-        it to the current character of the other string. If they match, then fine. Otherwise, the strings can't be
-        isomorphic.
-
-    Time complexity: O(N), where N is the length of s and t
-    Space complexity: O(1), the hash maps can't store more than the size of the alphabet characters
+    Time complexity: O(N), where N is the length of s (and t)
+    Space complexity: O(1), the hashmaps can't store more than the size of the alphabet characters
     """
-    s_mapping, t_mapping = {}, {}
     n = len(s)
+    s_mapping, t_mapping = {}, {}
     for i in range(n):
         s_char, t_char = s[i], t[i]
-        if s_char not in s_mapping and t_char not in t_mapping:
-            s_mapping[s_char] = t_char
-            t_mapping[t_char] = s_char
-        elif (s_char in s_mapping and s_mapping[s_char] != t_char) or \
+        if (s_char in s_mapping and s_mapping[s_char] != t_char) or \
                 (t_char in t_mapping and t_mapping[t_char] != s_char):
             return False
+        s_mapping[s_char] = t_char
+        t_mapping[t_char] = s_char
     return True
 
 
