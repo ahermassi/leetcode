a1aeca0db1193c9ede4091e33d256587991c4a6d
Anouer <hermassi.anouer@gmail.com>
2024-05-11 10:56:01 -0400

    Populating next right pointers in each node II - v2 update

--- Tree/117_Populating_Next_Right_Pointers_in_Each_Node_II.py
+++ Tree/117_Populating_Next_Right_Pointers_in_Each_Node_II.py
@@ -85,9 +85,7 @@ def connect_v1(root):
 
 
 def connect_v2(root):
-    """ Same algorithm but using an extra while loop to finish the wiring of complete child level at each iteration of
-         the outer loop. The previous algorithm moves 'cur' to its next node but finishes the wiring of child level at
-         the following iteration.
+    """ Same algorithm but we establish next pointers of the complete level in each iteration.
 
     Time complexity: O(N)
     Space complexity: O(1)
