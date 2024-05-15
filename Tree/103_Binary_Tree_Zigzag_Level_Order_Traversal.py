f6b73905affccf8a595c186e9f3bfb8cf19db6ba
Anouer <hermassi.anouer@gmail.com>
2024-05-15 08:19:08 -0400

    Binary tree zigzag level order traversal - v1 update

--- Tree/103_Binary_Tree_Zigzag_Level_Order_Traversal.py
+++ Tree/103_Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -33,23 +33,17 @@ def zigzag_level_order_v1(root):
          The default ordering of BFS within a single level is from left to right. As a result, we should adjust the BFS
          algorithm a bit to generate the desired zigzag ordering.
 
-         There are several ways to implement the BFS algorithm. One way would be to run a two-level nested loop, with
-         the outer loop iterating each level on the tree, and with the inner loop iterating each node within a single
+         There are several ways to implement the BFS algorithm. One way is to run a two-level nested loop, with the
+         outer loop iterating over each level on the tree, and with the inner loop iterating each node within a single
          level.
 
-         We use a 'direction' flag to indicate whether to add the child nodes to the next level in left->right or
-         right->left order.
+         We use a 'direction' flag to indicate whether to add the child nodes of the current level to the output list in
+         left->right or right->left order.
 
-         direction==1 means process the nodes of the CURRENT level from left to right.
-         direction==-1 means process the nodes of the CURRENT level from right to left.
+         direction == 1 means add the nodes of the CURRENT level from left to right.
+         direction == -1 means add the nodes of the CURRENT level from right to left.
 
-         For the ordering of left-to-right of the NEXT level (current direction==-1), we append the new elements to the
-         tail of the queue, so that the element that comes late would get out late as well.
-
-         For the ordering of right-to-left of the NEXT level (current direction==1), we insert the new element to the
-         head of the queue, so that the element that comes late would get out first.
-
-    Time complexity: O(N), we visit each node once and only once. In addition, the insertion operation on either end of
+    Time complexity: O(N), we visit each node twice. In addition, the insertion operation on either end of
     the queue takes a constant time
     Space complexity: O(N), the queue would hold the nodes that are at most across two levels. Therefore, at most, the
     size of the queue would be no more than 2⋅L, where L is the maximum number of nodes that might reside on the
@@ -62,13 +56,13 @@ def zigzag_level_order_v1(root):
     while queue:
         n, values = len(queue), []
         for _ in range(n):
-            node = queue.pop() if direction == 1 else queue.popleft()
-            if direction == 1:
-                queue.extendleft([kid for kid in (node.left, node.right) if kid])
-            else:
-                queue.extend([kid for kid in (node.right, node.left) if kid])
+            node = queue.popleft()
             values.append(node.val)
-        res.append(values)
+            queue.extend([kid for kid in (node.left, node.right) if kid])
+        if direction == 1:
+            res.append(values)
+        else:
+            res.append(values[::-1])
         direction = -direction
     return res
 
