577e80b2edba8fe3243e48f73b1b9fbc2ae90a3e
Anouer <hermassi.anouer@gmail.com>
2024-05-21 08:24:10 -0400

    Construct quad tree - v1 update

--- Tree/427_Construct_Quad_Tree.py
+++ Tree/427_Construct_Quad_Tree.py
@@ -14,68 +14,71 @@ class Node(object):
 
 # Video explanation: https://youtu.be/UQ-1sBMV0v4
 def construct_v1(grid):
-    """ We have a square matrix of size N * N of 0's and 1's. We need to convert this matrix to a quad tree with nodes
+    """ We have a square matrix of size N*N of 0's and 1's. We need to convert this matrix into a quad tree with nodes
          having two attributes, val and isLeaf:
 
-            1- If the whole matrix has the same value (0 or 1), then isLeaf would be true and val would be the same as
+            1- If the entire matrix has the same value (0 or 1), then isLeaf would be true and val would be the same as
                  the matrix value, and we can return.
-            2- Otherwise, it's not a leaf node, so isLeaf would be false, and val will not matter. This node will have
-                 four child nodes.
-            3- Divide the current matrix into four equally sized square matrices and recurse the same process to each.
 
-        We can see in that the matrix is repeatedly divided into 4 sub-parts again and again. It suggests we use a
+            2- Otherwise, it's not a leaf node, so isLeaf would be false, and val will not matter. This node will have
+                 four children nodes.
+
+            3- Divide the current matrix into 4 equally sized square matrices and apply the same process to each.
+
+        We can see that the matrix is repeatedly divided into 4 sub-parts again and again. It suggests the use of a
         recursive approach to divide the current matrix into 4 equal parts and then again repeat the same division on
         smaller parts recursively.
 
         The most intuitive way of solving recursive problems like this is to perform what the problem description says
         and just follow the same recursive approach. Before we start the process, we need a way to define the current
-        state of the matrix to generate the following states from it.
+        state of the matrix in order to generate the following states from it.
 
         The given matrix is square; we can represent it using two coordinates such as the top-left and bottom-right.
-        Though this would be correct too, for simplicity, we will use only one top-left coordinate and another variable,
-        side_length, which will be the side of the square. Using these two pieces of information, we can find any corner
-        coordinate of the square.
+        Though this is correct too, for simplicity we can use only one top-left coordinate and another variable,
+        side_length, which represents the side of the square. Using these two pieces of information, we can find any
+        corner coordinate of the square.
 
-            - If the matrix contains only 0 or 1, then we can return. Notice that this is the base condition, we can
-               simply check all the values of the current matrix state, and if it has all the same values, we will
-               simply return the node with specified attributes.
+            - If the matrix contains only 0's or 1's, then we can return. Notice that this is the base condition, we can
+               check all the values of the current matrix state.
 
-            - If not, then we need to divide the matrix into 4 equal parts and follow the above process to each of the
-               4 separately, and then they would become the four child nodes. The four matrices would have the top-left
-               coordinates, and each of them would have the length of the side as side_length / 2. Therefore, we will
-               call the recursive function for each sub-matrix and assign the returned nodes as the child of the root
-               node we will return.
+            - Otherwise, we need to divide the matrix into 4 equal parts and apply the above process to each of the 4
+               separately, and then they would become the 4 children nodes. The four matrices would have the top-left
+               coordinates, and each of them would have the length of the side as side_length / 2. Therefore, we call
+               the recursive function for each sub-matrix and assign the returned nodes as the children of the root
+               node.
                For each recursive call at position (i,j), where i is the row index and j is the column index of the
                top-left corner of the current sub-grid, we have the following:
 
-                    * Top-left matrix has top-left coordinate as (i, j)
-                    * Top-right matrix has top-left coordinate as (i, j + side_length / 2)
-                    * Bottom-left matrix has top-left coordinate as (i + side_length / 2, j)
-                    * Bottom-right matrix has top-left coordinate as (i + side_length / 2, j + side_length / 2)
+                    * Top-left matrix has top-left coordinate (i, j)
+                    * Top-right matrix has top-left coordinate (i, j + side_length / 2)
+                    * Bottom-left matrix has top-left coordinate (i + side_length / 2, j)
+                    * Bottom-right matrix has top-left coordinate (i + side_length / 2, j + side_length / 2)
 
-    Time complexity: O(N^2 * logN), after every level of recursion the original length of matrix get reduced to half,
+    Time complexity: O(N^2 * logN), after every level of recursion the original length of matrix gets reduced to half,
     this implies that the size of matrix will be reduced down to 1 after logN iterations. At each of these logN
-    iterations, we will have some number of recursive calls for the current matrix size. For example, initially we have
-    one call for the size of matrix N * N, then we will have four recursive calls each for matrix of size ( N * N)/4 and
-    so on. At each level the total number of iterations over the matrix cells remains same at N^2. Hence, N^2 iterations
-    at each of the logN levels makes up the time complexity to be O(N^2 * logN).
+    iterations, we have a number of recursive calls for the current matrix size. For example, we initially have one call
+    for the matrix of size N*N, then we have 4 recursive calls each matrices of size (N*N)/4 and so on. At each level,
+    the total number of iterations over the matrix cells remains the same N^2. Hence, N^2 iterations at each of the logN
+    levels makes a time complexity of O(N^2 * logN).
     Space complexity: O(logN), for the recursion call stack; the maximum number of active stack calls is logN
     """
 
-    def dfs(i, j, side_length):
+    def dfs(top, left, side_length):
+        if side_length == 1:
+            return Node(grid[top][left], True, None, None, None, None)
         all_same_value = True
-        for x in range(i, i + side_length):
-            for y in range(j, j + side_length):
-                if grid[x][y] != grid[i][j]:
+        for i in range(top, top + side_length):
+            for j in range(left, left + side_length):
+                if grid[i][j] != grid[top][left]:
                     all_same_value = False
                     break
         if all_same_value:
-            return Node(grid[i][j], True, None, None, None, None)
-        topLeft = dfs(i, j, side_length // 2)
-        topRight = dfs(i, j + side_length // 2, side_length // 2)
-        bottomLeft = dfs(i + side_length // 2, j, side_length // 2)
-        bottomRight = dfs(i + side_length // 2, j + side_length // 2, side_length // 2)
-        return Node(grid[i][j], False, topLeft, topRight, bottomLeft, bottomRight)
+            return Node(grid[top][left], True, None, None, None, None)
+        top_left = dfs(top, left, side_length // 2)
+        top_right = dfs(top, left + side_length // 2, side_length // 2)
+        bottom_left = dfs(top + side_length // 2, left, side_length // 2)
+        bottom_right = dfs(top + side_length // 2, left + side_length // 2, side_length // 2)
+        return Node(grid[top][left], False, top_left, top_right, bottom_left, bottom_right)
 
     return dfs(0, 0, len(grid))
 
