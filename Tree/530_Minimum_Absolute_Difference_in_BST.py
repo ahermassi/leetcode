8256604f6dba2c6701b1085d262154bc94054f5a
Anouer <hermassi.anouer@gmail.com>
2024-06-15 16:41:39 -0400

    Minimum absolute difference in BST - v2

--- Tree/530_Minimum_Absolute_Difference_in_BST.py
+++ Tree/530_Minimum_Absolute_Difference_in_BST.py
@@ -10,7 +10,7 @@ class TreeNode(object):
         self.right = None
 
 
-def get_minimum_difference(root):
+def get_minimum_difference_v1(root):
     """ Let's try to solve a simpler problem first. Given a sorted array of integers, find the minimum difference
         between any two integers in the array. To solve this problem, we don't need to check every pair of integers.
         Instead, checking the difference between every two consecutive integers would work. This is because the array
@@ -41,3 +41,34 @@ def get_minimum_difference(root):
     for i in range(1, n):
         res = min(res, values[i] - values[i - 1])
     return res
+
+
+def get_minimum_difference_v2(root):
+    """ As we can notice in the previous approach, we only need the immediate inorder predecessor of any node to
+        calculate the minimum difference. The rest of the nodes will not be needed and are stored unnecessarily in the
+        list.
+
+        Thus, we can avoid storing elements in a list if we can find the difference between consecutive nodes on the fly
+        during inorder traversal. For each node in the tree, we need the previous node we have handled, and then we can
+        find the difference. This can be done using another variable prev that will store the value of the node we
+        processed previously in the inorder traversal. This way, we don't have to store the elements in an array and at
+        the same time, don't have to re-iterate over the nodes again.
+
+
+    Time complexity: O(N), we visit every node exactly once
+    Space complexity: O(N), for the call stack. The maximum number of active stack calls at a
+    time would be the tree's height, which in the worst case would be O(N) when the tree is skewed.
+    """
+
+    def inorder(root):
+        if not root:
+            return
+        inorder(root.left)
+        res[0] = min(res[0], root.val - prev[0])
+        prev[0] = root.val
+        inorder(root.right)
+
+    res, values = [float('inf')], []
+    prev = [float('-inf')]
+    inorder(root)
+    return res[0]
