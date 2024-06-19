9564869886bb4959479d8fb69e26b95238b52878
Anouer <hermassi.anouer@gmail.com>
2024-06-19 07:44:33 -0400

    Valid palindrome II - update

--- String/680_Valid_Palindrome_II.py
+++ String/680_Valid_Palindrome_II.py
@@ -4,20 +4,47 @@ import unittest2 as unittest
 
 
 def valid_palindrome(s):
-    """ We can use the standard two-pointer approach that starts at the left and right of the string and move
-        inwards. Whenever there is a mismatch, we can either exclude the character at the left or the right pointer. We
-        then check if either substring is a palindrome.
-        In other words: check from left and right at the same time until the first different pair. Now we have
-        something like a****b, where a and b are different. We need to delete either a or b to make it a palindrome.
-    Time complexity: O(N)
-    Space complexity: O(N) for the creation of reversed strings using [::-1] notation
+    """ An important thing to notice is that once we verify two characters match at positions i and j, we only care
+        about the indices between i and j.
+
+        For example, with s = 'racecar', after verifying that s[0] and s[6] are the same character, we only care about
+        indices 1 through 5, which represent the substring 'aceca'. If 'aceca' is a palindrome, then 'racecar' is a
+        palindrome as well.
+
+        For our purposes, we can basically pretend that matched characters no longer exist. For example, after verifying
+        that the first and last characters of 'racecar' match, we can reframe the problem as checking if 'aceca' can be
+        a palindrome with at most one deletion.
+
+        If s can be a palindrome after one deletion, the deletion must be of one of the mismatched characters.
+        This leaves us two scenarios:
+
+            1- s is a palindrome - great, we can just return true.
+
+            2- Somewhere in s, there will be a pair of mismatched characters. We must use the allowed deletion on one of
+                 these characters. Try both options - if neither results in a palindrome, then return false. Otherwise,
+                 return true. We can "delete" the character at i by moving the bounds to (i + 1, j). Likewise, we can
+                 "delete" the character at j by moving the bounds to (i, j - 1).
+
+    Time complexity: O(N), each character is visited at most once
+    Space complexity: O(1)
     """
+
+    def is_palindrome(left, right):
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
     left, right = 0, len(s) - 1
-    while left < right and s[left] == s[right]:
+    while left < right:
+        if s[left] != s[right]:
+            # Found a mismatched pair - try both deletions
+            return is_palindrome(left + 1, right) or is_palindrome(left, right - 1)
         left += 1
         right -= 1
-    left_subs, right_subs = s[left:right], s[left + 1:right + 1]  # Deleting right and left characters respectively
-    return left_subs == left_subs[::-1] or right_subs == right_subs[::-1]
+    return True
 
 
 class Test(unittest.TestCase):
