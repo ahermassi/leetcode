f8e41d972c2a3bdfe9b5a232d52afeaee6770da3
Anouer <hermassi.anouer@gmail.com>
2024-06-21 14:45:26 -0400

    Spiral matrix - v1 update

--- Arrays/54_Spiral_Matrix.py
+++ Arrays/54_Spiral_Matrix.py
@@ -3,29 +3,29 @@
 import unittest2 as unittest
 
 
-def spiral_order_v1(matrix):
-    """ Take the first row plus the spiral order of the rotated remaining matrix.
-
-        Here's how the matrix changes by always extracting the first row and rotating the remaining matrix
-        counter-clockwise:
-
-        |1 2 3|         |6 9|         |8 7|         |4|  =>  |5|  =>  ||
-        |4 5 6|  =>  |5 8|  =>  |5 4|  =>  |5|
-        |7 8 9|         |4 7|
-
-        Now look at the first rows we extracted:
-
-        |1 2 3|      |6 9|      |8 7|      |4|      |5|
-
-        Those concatenated are the desired result.
-    Time complexity: O(N * M)
-    Space complexity: O(N * M)
-    """
-    res = []
-    while matrix:
-        res.extend(matrix.pop(0))
-        matrix = list(zip(*matrix))[::-1]
-    return res
+# def spiral_order_v1(matrix):
+#     """ Take the first row plus the spiral order of the rotated remaining matrix.
+#
+#         Here's how the matrix changes by always extracting the first row and rotating the remaining matrix
+#         counter-clockwise:
+#
+#         |1 2 3|         |6 9|         |8 7|         |4|  =>  |5|  =>  ||
+#         |4 5 6|  =>  |5 8|  =>  |5 4|  =>  |5|
+#         |7 8 9|         |4 7|
+#
+#         Now look at the first rows we extracted:
+#
+#         |1 2 3|      |6 9|      |8 7|      |4|      |5|
+#
+#         Those concatenated are the desired result.
+#     Time complexity: O(N * M)
+#     Space complexity: O(N * M)
+#     """
+#     res = []
+#     while matrix:
+#         res.extend(matrix.pop(0))
+#         matrix = list(zip(*matrix))[::-1]
+#     return res
 
 
 def spiral_order_v2(matrix):
