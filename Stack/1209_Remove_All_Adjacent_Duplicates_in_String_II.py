c5aa9549c4c0eff185724001acb27ec285e2e0d9
Anouer <hermassi.anouer@gmail.com>
2024-07-06 11:42:47 -0400

    Remove all adjacent duplicates in string II - v2 update

--- Stack/1209_Remove_All_Adjacent_Duplicates_in_String_II.py
+++ Stack/1209_Remove_All_Adjacent_Duplicates_in_String_II.py
@@ -38,68 +38,72 @@ def remove_duplicates_v1(s, k):
 
 def remove_duplicates_v2(s, k):
     """ We can also use an in-place stack approach for the char array 'chars' formed from the input string, rather than
-        using a separate stack. To do so, we can use a two-pointer system in which one pointer 'left' keeps track of
-        the end of the in-place "stack", while the other pointer 'right' iterates through 'chars'.
+        using a separate stack.
+
+        To do so, we use a two-pointer system in which one pointer 'left' keeps track of the end of the in-place
+        "stack", while the 'right' pointer iterates over 'chars'.
+
         As we move 'right' through 'chars', we write to the stack 'chars' by overwriting chars[left] with chars[right].
-        When we want to remove k elements from 'chars', we just move 'left' back by k. Then, once we're done, we can
-        return the result string, which is the first part of 'chars' through 'left'.
-        Example: S = 'aabbbcdddcc', k = 3
+        When we want to remove k elements from 'chars', we move 'left' back by k. Then, once we're done, we return the
+        result string, which is the first part of 'chars' through 'left'.
 
-              i,j                                   // i, j start at 0
+        Example: S = 'aabbbcdddcc', k = 3 (another example in the png file):
+
+                 i,j                                            // i, j start at 0
         S  = [ a, a, b, b, b, c, d, d, d, c, c ]    // S[j] overwrites S[i]
-        stack = [ 1 ]                               // S[i] == S[i-1], stack.push(1)
+        stack = [ 1 ]                                    // i == 0, stack.push(1)
 
-                 i,j                                // i, j move up 1
+                    i,j                                          // i, j move up 1
         S  = [ a, a, b, b, b, c, d, d, d, c, c ]    // S[j] overwrites S[i]
-        stack = [ 2 ]                               // S[i] == S[i-1], increment top of stack
+        stack = [ 2 ]                                    // S[i] == S[i-1], increment top of stack
 
-                   ->i,j                            // i, j move up 1
+                    ->i,j                                     // i, j move up 1
         S  = [ a, a, b, b, b, c, d, d, d, c, c ]    // S[j] overwrites S[i]
-        stack = [ 2, 1 ]                            // S[i] != S[i-1], stack.push(1)
+        stack = [ 2, 1 ]                                // S[i] != S[i-1], stack.push(1)
 
-                      ->i,j                         // i, j move up 1
+                        ->i,j                                 // i, j move up 1
         S  = [ a, a, b, b, b, c, d, d, d, c, c ]    // S[j] overwrites S[i]
-        stack = [ 2, 2 ]                            // S[i] == S[i-1], increment top of stack
+        stack = [ 2, 2 ]                                // S[i] == S[i-1], increment top of stack
 
-                         ->i,j                      // i, j move up 1
+                            ->i,j                              // i, j move up 1
         S  = [ a, a, b, b, b, c, d, d, d, c, c ]    // S[j] overwrites S[i]
-        stack = [ 2, 3 ]                            // S[i] == S[i-1], increment top of stack
+        stack = [ 2, 3 ]                                // S[i] == S[i-1], increment top of stack
 
-                  i<-------j                        // i moves back because...
+                     i<-------j                             // i moves back 3 steps because...
         S  = [ a, a, b--b--b, c, d, d, d, c, c ]    // ...3 b's found, so...
-        stack = [ 2 ]                               //  stack.pop()
+        stack = [ 2 ]                                       //  stack.pop()
 
-                     ->i      ->j                   // i, j move up 1
+                     ->i            ->j                          // i, j move up 1
         S  = [ a, a, c<-------c, d, d, d, c, c ]    // S[j] overwrites S[i]
-        stack = [ 2, 1 ]                            // S[i] != S[i-1], stack.push(1)
+        stack = [ 2, 1 ]                                     // S[i] != S[i-1], stack.push(1)
 
-                        ->i      ->j                // i, j move up 1
+                        ->i             ->j                      // i, j move up 1
         S  = [ a, a, c, d<-------d, d, d, c, c ]    // S[j] overwrites S[i]
-        stack = [ 2, 1, 1 ]                         // S[i] != S[i-1], stack.push(1)
+        stack = [ 2, 1, 1 ]                                 // S[i] != S[i-1], stack.push(1)
 
-                           ->i      ->j             // i, j move up 1
+                            ->i                 ->j              // i, j move up 1
         S  = [ a, a, c, d, d<-------d, d, c, c ]    // S[j] overwrites S[i]
-        stack = [ 2, 1, 2 ]                         // S[i] == S[i-1], increment top of stack
+        stack = [ 2, 1, 2 ]                                 // S[i] == S[i-1], increment top of stack
 
-                              ->i      ->j          // i, j move up 1
+                                ->i             ->j               // i, j move up 1
         S  = [ a, a, c, d, d, d<-------d, c, c ]    // S[j] overwrites S[i]
-        stack = [ 2, 1, 3 ]                         // S[i] == S[i-1], increment top of stack
+        stack = [ 2, 1, 3 ]                                 // S[i] == S[i-1], increment top of stack
 
-                     i<--------        j            // i moves back because...
+                         i<--------        j             // i moves back because...
         S  = [ a, a, c, d--d--D,  ,  ,  , c, c ]    // ...3 d's found, so...
-        stack = [ 2, 1 ]                            // stack.pop()
+        stack = [ 2, 1 ]                                 // stack.pop()
 
-                       ->i               ->j        // i, j move up 1
+                        ->i                             ->j           // i, j move up 1
         S  = [ a, a, c, c<----------------c, c ]    // S[j] overwrites S[i]
-        stack = [ 2, 2 ]                            // S[i] == S[i-1], increment top of stack
+        stack = [ 2, 2 ]                                          // S[i] == S[i-1], increment top of stack
 
-                          ->i               ->j     // i, j move up 1
+                            ->i                            ->j        // i, j move up 1
         S  = [ a, a, c, c, c<----------------c ]    // S[j] overwrites S[i]
-        stack = [ 2, 3 ]                            // S[i] == S[i-1], increment top of stack
+        stack = [ 2, 3 ]                                         // S[i] == S[i-1], increment top of stack
 
-                  i<--------                 j      // i moves back because...
-        S  = [ a, a, c--c--c,  ,  ,  ,  ,  ,   ]    // ...3 c's found, so...
-        stack = [ 2 ]                               // stack.pop()
+                     i<--------                 j      // i moves back because...
+        S  = [ a, a, c--c--c,  ,  ,  ,  ,  ,   ]     // ...3 c's found, so...
+        stack = [ 2 ]                                  // stack.pop()
 
         S  = [ a, a ]                               // Only keep S up to i ..
         = "aa"                                      // then join to a string
@@ -111,11 +115,13 @@ def remove_duplicates_v2(s, k):
     left = right = 0
     while right < n:
         chars[left] = chars[right]
-        if left == 0 or chars[left] != chars[left - 1]:  # For first character and when the previous character is not
-            # equal to the current one, we start keeping count for the current character
+        if left == 0 or chars[left] != chars[left - 1]:
+            # For the first character and when the current and previous character are not equal, we start keeping count
+            # of the current character
             stack.append(1)
-        else:  # Another occurrence of the previous character is encountered
-            stack[-1] = stack[-1] + 1
+        else:
+            # Another occurrence of the previous character is encountered
+            stack[-1] += 1
             if stack[-1] == k:
                 stack.pop()
                 left -= k
