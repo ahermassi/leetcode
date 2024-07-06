b82adcf7634cfb35aabd75e71ba1cd598b9072cc
Anouer <hermassi.anouer@gmail.com>
2024-07-06 14:36:36 -0400

    Minimum remove to make valid parentheses - v1 update

--- Stack/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
+++ Stack/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -7,47 +7,68 @@ import unittest2 as unittest
 
 def min_remove_to_make_valid_v1(s):
     """ The parentheses in a string are balanced if and only if these 2 conditions are met:
+
             1- There is the same number of '(' and ')' in the string
             2- Scanning through the string from left to right and counting how many '(' and ')' there are so far,
-               there should never be a time where there are more ')' than '('
+               there should never be a time when there are more ')' than '('
+
         We call count('(') - count(')') the balance of the string.
+
+        For starters, we know we'll need to remove any ')' that we encountered when balance was already 0. It would be
+        impossible to remove less ')', because there are not enough '(' before them.
+
         So, remembering that each ')' was paired with the closest '(' that isn't already paired, how could we do this
-        in code? We need to know the indexes of the problematic '('.
+        in code? We need to know the indices of the problematic '('s.
+
         We can use a stack. Each time we see a '(', we add its index to the stack. Each time we see a ')', we remove
         an index from the stack because the ')' will match with whatever '(' was at the top of the stack. The length
         of the stack is equivalent to the balance.
+
         We will need to:
-            1- Remove a ')' if it is encountered when stack was already empty (prevent negative balance)
+
+            1- Mark a ')' as invalid/unmatched if it is encountered when stack was already empty (prevent negative
+                 balance)
             2- Remove a '(' if it is left on stack at end (prevent non-zero final balance)
+
         After removing invalid ')', the number of '(' we remove is the minimum needed to ensure that
         count('(') == count(')'), or that the balance is equal to 0.
-        If we put the indexes of the '(' on the stack, then we'll know that all the indices on the stack at the end
-        are the indices of the unmatched '('. We should also use a set to keep track of the unmatched ')' we come
-        across. Then, we can remove the character at each of those indices and then return the edited string.
+
+        If we put the indices of the '(' on the stack, then we'll know that all the indices on the stack at the end
+        are the indices of the unmatched '('. We should also use a hash set (or a second stack) to keep track of the
+        unmatched ')' we've come across. Then, we can remove the character at each of those indices and then return the
+        edited string.
+
+        If all the indices we need to remove are in a set, then we can iterate through each index in the string, check
+        if the current index is in the set, and if it is not, then add the character at that index to the string builder.
+
         In fewer words:
         To make the string valid with minimum removals, we need to get rid of all parentheses that do not have a
         matching pair.
+
             1- Push char index into the stack when we see '('
             2- Pop from the stack when we see ')'
                 - If the stack is empty, then we have ')' without the pair, and it needs to be removed
-            3- In the end, the stack will contain indexes of '(' without the pair, if any. We need to remove all of
+            3- In the end, the stack will contain indices of '(' without the pair, if any. We need to remove all of
                them too.
+
     Time complexity: O(N)
     Space complexity: O(N)
     """
-    stack, invalid_indices = [], set()
+    unmatched_left, unmatched_right = [], []
     for i, c in enumerate(s):
         if c == '(':
-            stack.append(i)
+            unmatched_left.append(i)
         elif c == ')':
-            if stack:
-                stack.pop()
+            if unmatched_left:
+                unmatched_left.pop()
             else:
                 # Meet closing parentheses but no opening parenthesis -> remove closing parenthesis
-                invalid_indices.add(i)
-    while stack:
-        invalid_indices.add(stack.pop())  # Remove remaining open parenthesis
-    return ''.join([c for i, c in enumerate(s) if i not in invalid_indices])
+                unmatched_right.append(i)
+    while unmatched_left:
+        # Collect all the unmatched parentheses into a single holder
+        unmatched_right.append(unmatched_left.pop())
+    unmatched_brackets = set(unmatched_right)
+    return ''.join([c for i, c in enumerate(s) if i not in unmatched_brackets])
 
 
 def min_remove_to_make_valid_v2(s):
