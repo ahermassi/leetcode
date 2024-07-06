6894eac07f1b5e1b1964843f225f6221369a08cd
Anouer <hermassi.anouer@gmail.com>
2024-07-06 16:15:14 -0400

    Diagonal traverse - v3 update

--- HashTable/1424_Diagonal_Traverse_II.py
+++ HashTable/1424_Diagonal_Traverse_II.py
@@ -57,22 +57,48 @@ def find_diagonal_order_v2(nums):
 
 
 def find_diagonal_order_v3(nums):
-    """ We can think of the given matrix as a tree and use BFS to solve this problem.
-        The top-left number, nums[0][0], is the root node. nums[1][0] is its left child, and nums[0][1] is its right
+    """ In the previous approaches, we require two passes. The first pass populates the hashmap, and the second pass
+         populates the output list. Can we do better, perhaps solving the problem in one pass?
+
+        We can think about the grid as a graph. Each square is a node, and we can imagine each node having an edge to
+        the squares below and to the right (if they exist).
+
+        A node with identifier x has edges to nodes with identifier x+1. If we consider the top-left square (0,0) as a
+        "source" node, then each square's identifier is exactly equal to its distance from the source. This allows us to
+        visit the diagonals in order using BFS.
+
+        The top-left cell, nums[0][0], is the root node. nums[1][0] is its left child, and nums[0][1] is its right
         child. Same analogy applies to all nodes nums[i][j].
-        Note that nums[i][j] is both the left child of nums[i-1][j] and the right child of nums[i][j-1]. To avoid
-        double counting, we only consider a number's left child when we are at the leftmost column (j == 0).
-    Time complexity: O(N * M)
-    Space complexity: O(N * M)
+
+        We start a BFS from (0,0). At each node (row, col), we first push (row+1, col) to the queue and then
+        (row, col+1). Note that we only add a square to the queue if it both exists and has not been visited yet.
+
+        How do we know if a square has been visited yet? We could use a hash set to keep track of visited squares, but
+        there is a simpler way. We only need to consider the square (row+1, col) (down) if we are at the start of a
+        diagonal. Otherwise, for every other square on the diagonal, the square below it has already been visited by the
+        right edge of the previous square.
+
+        The level-wise nature of BFS will ensure that we visit all squares in a diagonal with identifier x before we
+        visit any square in a diagonal with identifier x+1. This means we visit the diagonals in the correct order.
+        Because we add the square (row+1, col) before (row, col+1), we also traverse over each diagonal in the correct
+        order as well.
+
+        In other words, nums[row][col] is both the left child of nums[row-1][col] and the right child of nums[col][row-1].
+        To avoid double counting, we only consider a number's left child when we are at the leftmost column (col == 0).
+
+    Time complexity: O(N), where N is the number of integers in the grid
+    Space complexity: O(sqrt(N)), the largest size queue will be is proportional to the size of the largest diagonal
     """
     n, res = len(nums), []
     queue = deque([(0, 0)])
     while queue:
         row, col = queue.popleft()
         res.append(nums[row][col])
-        if col == 0 and row < n - 1:  # We only add the number at the bottom (left child) if we are at column 0. This
-            # is because this node couldn't have been added by a parent node to its left as a right child
+        if col == 0 and row + 1 < n:
+            # We only add the number at the bottom (left child) if we are at column 0. This is because this node
+            # couldn't have been added by a parent node to its left as a right child
             queue.append((row + 1, col))
-        if col < len(nums[row]) - 1:  # Add the number on the right (right child)
+        if col + 1 < len(nums[row]):
+            # Add the number on the right (right child)
             queue.append((row, col + 1))
     return res
