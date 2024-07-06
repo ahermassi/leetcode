afa3544072620e745b194aa7f48e0a837ddb33ee
Anouer <hermassi.anouer@gmail.com>
2024-07-06 17:23:19 -0400

    Dot product of two sparse vectors - v3

--- HashTable/1570_Dot_Product_of_Two_Sparse_Vectors.py
+++ HashTable/1570_Dot_Product_of_Two_Sparse_Vectors.py
@@ -99,3 +99,62 @@ class SparseVectorV2:
             else:
                 j += 1
         return res
+
+
+class SparseVectorV3:
+    def __init__(self, nums):
+        """ Hashing/lookups, while on surface look efficient, for large sparse vectors, hashing function takes up bulk
+             of the computation. Technically, a hashmap uses an array of nodes internally, but to get an element we hash
+             the key, and then access this array based on the hashcode. Depending on the hashing function, can result in
+             collisions. Also, the hashmap resizes when it gets to 75% (by default) capacity, but then it will rehash
+             everything. And this is likely to happen a lot as size grows. So between the resizing and the collisions,
+             the hashmap approach isn't going to scale well to a very large vector.
+
+             We can represent the elements of a sparse vector as a list of <index, value> pairs. We then use two
+             pointers to iterate through the two vectors to calculate the dot product.
+
+        Time complexity: O(N), for creating the <index, value> pair for non-zero values, O(L1 + L2) for calculating the
+        dot product, where L1 is the number of non-zero values in the first vector and L2 is the number of non-zero
+        values in the second vector
+        Space complexity: O(L), for creating the <index, value> pairs for non-zero values, O(1) for calculating the dot
+        product
+        """
+        self.non_zeros = []
+        for i, num in enumerate(nums):
+            if num:
+                self.non_zeros.append((i, num))
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec):
+        """ What if only one vector is sparse and the other is full of non-zero values?
+
+             If the sizes of the two index-pair vectors are n and m, then we iterate the smaller vector (sparse) and
+             binary search the larger one. So the effective runtime is O(min(n, m) * Log(max(n, m)))
+
+             Let's say, m is few orders of magnitude bigger than n, like n=5 and m = 1024. nLog(m) = 5*Log(1024) = 50,
+             whereas n + m = 5 + 1024 = 1029, so it does make a difference.
+
+        :type vec: 'SparseVector'
+        :rtype: int
+        """
+        def binary_search(index, pairs):
+            left, right = 0, len(pairs) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if pairs[mid][0] == index:
+                    return pairs[mid][1]
+                if pairs[mid][0] < index:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return float('inf')
+
+        self_non_zeros, vec_non_zeros = self.non_zeros, vec.non_zeros
+        if len(vec_non_zeros) < len(self_non_zeros):
+            return vec.dotProduct(self)
+        res = 0
+        for i, num1 in enumerate(self_non_zeros):
+            num2 = binary_search(i, vec_non_zeros)
+            if num2 != float('inf'):
+                res += num1 * num2
+        return res
