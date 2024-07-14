1331508a849d1b0d36c5fdfd69267a609b145115
Anouer <hermassi.anouer@gmail.com>
2024-07-14 11:10:49 -0400

    Check completeness of a binary tree - v3 update

--- Tree/958_Check_Completeness_of_a_Binary_Tree.py
+++ Tree/958_Check_Completeness_of_a_Binary_Tree.py
@@ -79,18 +79,34 @@ def is_complete_tree_v3(root):
            2   3
           / \  /
          4  5 6
-         This solution relies on is two properties of a heap. First, a heap is always a complete binary tree. So if a
-         tree can be viewed as a heap, it is complete. Second, inside a heap (which is an array), a parent with index
-         i has two children with index (i * 2) and (i * 2 + 1), if the parent has two children in the first place.
-         With these two properties known, it is easy to understand this solution. Suppose we have an array which
-         represents a heap of size 'node_count'. The algorithm tries to verify whether the tree with 'node_count' nodes
-         can fit into the array. The tree can fit into the array if and only if all nodes' indices are within the
-         boundary of the array, that is, 'node_count'. The dfs() essentially checks whether the index of a node exceeds
-         the boundary of a heap with size 'node_count'. If index > node_count, there must be an empty slot in the
-         array, which means the tree cannot fit into the array so dfs() returns false. If idx <= total, dfs()
-         recursively checks the children of root.
-    Time complexity:
-    Space complexity:
+
+         A complete binary tree has an interesting property that we can use to find the children and parents of any
+         node.
+
+         A complete binary tree can be represented with an array. If the index of a node in the array is i, the element
+         at index 2i + 1 will be its left child and the element at index 2i + 2 will be its right child. If there are a
+         total of n nodes in a complete binary tree, it can be represented with an array where the nodes are ordered
+         level by level, left to right. As we saw in the previous approach, there will be no null node between two
+         non-null nodes.
+
+         Suppose we have an array which represents a heap of size n. The algorithm tries to verify whether the tree with
+         n nodes can fit into the array.
+
+         Starting with the root node and assigning it an index of 0, we can use the above property to assign indices to
+         all the other nodes in the tree. The assigned index of every node must be smaller than or equal to n for the
+         given tree to form a complete binary tree. The tree can fit into the array if and only if all nodes' indices
+         are within the boundary of the array, that is, n.
+
+         If the index of a node is greater than or equal to n, it means a node is missing from the first n indices.
+         Therefore, the tree is not a complete binary tree. The array representation of such a binary tree will have at
+         least one null node in between non-null nodes. (check the attached diagram)
+
+         The DFS essentially checks whether the index of a node exceeds the boundary of a heap of size n. If index > n,
+         there must be an empty slot in the array, which means the tree cannot fit into the array. Otherwise, it
+         recursively checks the children of the root.
+
+    Time complexity: O(N), where N is the number of nodes in the tree. Each node is visited once.
+    Space complexity: O(h), for call stack
     """
 
     def count_nodes(root):
@@ -101,12 +117,12 @@ def is_complete_tree_v3(root):
     def dfs(root, index, node_count):
         if not root:
             return True
-        if index > node_count:
+        if index >= node_count:
             return False
-        return dfs(root.left, index * 2, node_count) and dfs(root.right, index * 2 + 1, node_count)
+        return dfs(root.left, 2 * index + 1, node_count) and dfs(root.right, 2 * index + 2, node_count)
 
     node_count = count_nodes(root)
-    return dfs(root, 1, node_count)
+    return dfs(root, 0, node_count)
 
 
 class Test(unittest.TestCase):
