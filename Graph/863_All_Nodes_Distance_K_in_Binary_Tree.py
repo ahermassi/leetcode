f4f003e14adcaeef5996eeb18349bf4718a55cba
Anouer <hermassi.anouer@gmail.com>
2024-07-25 15:53:36 -0400

    All nodes distance K in binary tree - v1 update

--- Graph/863_All_Nodes_Distance_K_in_Binary_Tree.py
+++ Graph/863_All_Nodes_Distance_K_in_Binary_Tree.py
@@ -5,37 +5,52 @@ order. """
 from collections import deque, defaultdict
 
 
-def distance_k_v1(root, target, K):
-    """ If we know the parent of every node x, we know all nodes that are distance 1 from x. We can then perform a
-        breadth first search from the target node to find the answer.
-        We first do a depth first search where we annotate every node with information about its parent. Therefore, we
-        need a set to keep track of nodes we have visited so that we do not go back and revisit what has already been
-        processed and cause an infinite cycle.
-        After, we do a BFS to find all nodes a distance K from the target.
-    Time complexity: O(N), where N is the number of nodes in the given tree
+def distance_k_v1(root, target, k):
+    """ In the given tree, each node only has pointers to its left and right child nodes, making the typical tree
+         traversal approach only applicable to the subtree rooted at the target node. We cannot access other parts of
+         the tree beyond the subtree as we can't access the parent, which implies the need to establish additional
+         connections beyond the child node pointers.
+
+         We can implement a parent pointer to each node (apart from the root node) recursively.
+         If we know the parent of every node x, we know all nodes that are distance 1 from x. We can then perform a BFS
+         from the target node to find the answer.
+
+
+        We use a hash set to keep track of all the visited nodes. Whenever we find an unvisited neighbor node, we add
+        it to the hash set, so it won't be visited anymore.
+
+        The BFS processes the nodes in layers, where the entirety of nodes of each level are popped at each iteration,
+        and their unvisited children and parent are added to the queue. Done k times, this leaves us with a queue that
+        contains all the nodes k distance from target node.
+
+    Time complexity: O(N), where N is the number of nodes in the given tree. In a typical BFS search, the time
+    complexity is O(V+E) where V is the number of vertices and E is the number of edges. There are N nodes and
+    N-1 edges because the graph is a tree. Each node is added to the queue and popped from the queue once, and it takes
+    O(N) to handle all nodes.
     Space complexity: O(N)
     """
 
-    def annotate(node, par=None):
+    def annotate(node, par):
         if node:
             node.par = par
             annotate(node.left, node)
             annotate(node.right, node)
 
-    annotate(root)
-    queue = deque([(target, 0)])  # When our BFS starts, we are standing at layer 0
-    visited = {target}
-    while queue:
-        if queue[0][1] == K:  # Is this the layer we want ? If so, extract and return it
-            return [node.val for node, depth in queue]
-        node, depth = queue.popleft()  # Pull a node from the search queue. We are going to basically use our current
-        # layer to populate the next layer of nodes that we need to search in the next while loop iteration
-        for n in (node.left, node.right, node.par):  # Let's process all nodes in the layer. This is BFS.
-            if n and n not in visited:  # Every node is gonna add its children as well as its parent. We risk
-                # adding a (parent) node that's been already processed
-                queue.append((n, depth + 1))
-                visited.add(n)
-    return []
+    annotate(root, None)
+    queue = deque([target])  # When BFS starts, we are standing at layer 0
+    visited = set()
+    while k:
+        size = len(queue)
+        # Let's process all nodes in the layer
+        for _ in range(size):
+            # Pull a node from the search queue. We are going to basically use the current layer to populate the next
+            # layer of nodes that we need to search in the next while loop iteration.
+            node = queue.popleft()
+            visited.add(node)
+            # Every node is going to add its children as well as its parent if they haven't been already processed
+            queue.extend(neighbor for neighbor in (node.left, node.right, node.par) if neighbor and neighbor not in visited)
+        k -= 1
+    return [node.val for node in queue]
 
 # Watch: https://www.youtube.com/watch?v=nPtARJ2cYrg
 
