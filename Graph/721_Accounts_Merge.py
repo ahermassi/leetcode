8dcda7b40aab5d45d7da2133200c834f9d9af7a2
Anouer <hermassi.anouer@gmail.com>
2024-08-08 16:00:26 -0400

    Accounts merge - v1 update

--- Graph/721_Accounts_Merge.py
+++ Graph/721_Accounts_Merge.py
@@ -14,47 +14,99 @@ import unittest2 as unittest
 
 
 def accounts_merge_v1(accounts):
-    """ For every pair of emails in the same account, draw an edge between those emails. The problem is about
-        enumerating the connected components of this graph.
-        Notice that each accounts[i] tells us some edges. What we have to do is as follows:
-        Use these edges to build some components. Common email addresses are like the intersections that connect each
-        single component for each account.
-        Because each component represents a merged account, do DFS search for each component and add it into a list.
-        Before adding the name to this list, sort the emails. Then add name string into it.
-        Example: Assume we have three accounts, we connect them like this in order to use DFS.
-            {Name, 1, 2, 3} => Name -- 1 -- 2 -- 3
-            {Name, 2, 4, 5} => Name -- 2 -- 4 -- 5 (The same graph node 2 appears)
-            {Name, 6, 7, 8} => Name -- 6 -- 7 -- 8
-            (Where numbers represent email addresses)
-    Time complexity: O(sum(a_i log(a_i)), where a_i is the length of accounts[i]. Without the log factor, this is the
-    complexity of building the graph and searching for each component. The log factor is for sorting each component at
-    the end.
-    Space complexity: O(sum(a_i))
-    """
-    def build_graph():
-        for account in accounts:
-            n = len(account)
-            for i in range(1, n - 1):
-                graph[account[i]].append(account[i + 1])
-                graph[account[i + 1]].append(account[i])
+    """ The goal is, for each person, to identify all the emails that belong to that person. Therefore, every time we
+         find two accounts with an email in common, we merge the two accounts into one.
 
-    def dfs(vertex, emails):
-        if vertex not in visited:  # Each vertex/node is an email address
-            emails.append(vertex)
-            visited.add(vertex)
-            for neighbor in graph[vertex]:
-                dfs(neighbor, emails)
+         Whenever we must work with a set of elements (emails) that are connected (belong to the same user), we should
+         always consider visualizing the input as a graph. In this problem, converting the input into a graph will
+         facilitate the process of "merging" two accounts.
+
+         We can see that the set of emails of each account form a single group belonging to a person. If we visualize
+         these as a graph, we can think of them as various connected components of a graph. More specifically, the
+         emails belonging to a person form the node of a connected component and all these connected components make up
+         the whole graph.
+
+         Each email from one account will have an edge with one another email of the same account. But if an email is
+         found in multiple accounts, they will have edge with other emails from all these multiple accounts as well,
+         effectively merging them into one connected component.
+
+         Therefore, for every pair of emails in the same account, draw an edge between those emails. The problem is
+         about enumerating the connected components of this graph.
+
+         Suppose an account has K emails, and we want to connect these emails. We can create an acyclic graph using K−1
+         edges. Recall that K−1 is the minimum number of edges required to connect K nodes. So we connect emails in an
+         account in a "star" manner with the first email as the central node of the star and all other emails as the
+         leaves.
+
+         The beauty of connecting the emails in each account in this manner is that after connecting an email to a
+         second account, that email will have one edge going to an email in the first account and one edge going to an
+         email in the second account. Thereby automatically merging the two accounts.
+
+         !!! IMPORTANT !!!
+         We only need to connect each email of an account to the first email of that account and vice versa because that
+         will construct the same set of connected components for the vertices, even though the graph isn't truly
+         complete.
+
+         Note that we can also connect every email at index i with the email at index i+1.
+
+         After iterating over each account and connecting the emails as described above, we will have one or more
+         connected components. Each connected component represents one person, and the nodes in the connected component
+         are the person's emails. Now the task is to explore each connected component to find all the emails that belong
+         to each person.
+
+         Since a DFS is guaranteed to explore every node in a connected component, we perform a DFS on each connected
+         component (person) to find all the connected emails.
+
+        Start with each account, take an email from that account (let's say 1st email) and traverse over the component
+        of that email. Each email traversed belongs to the person of this account (since they form a connected
+        component). We can use DFS for traversing the component. We sort the list of emails found in this traversal and
+        add it to the final list of merged accounts along with the person name at the beginning.
+        We also mark each of these email as seen so we don't iterate over them again and form a duplicate.
+
+        An example is attached as .img file.
+
+        Example: suppose we have three accounts, we can connect them like this in order to use DFS, where numbers
+        represent email addresses:
+            {Name, 1, 2, 3} => Name -- 1 -- 2 -- 3
+            {Name, 2, 4, 5} => Name -- 2 -- 4 -- 5 (node 2 is common)
+            {Name, 6, 7, 8} => Name -- 6 -- 7 -- 8
+
+    Time complexity: O(NK logNK), where N is the number of accounts and K is the maximum length of an account. In the
+    worst case, all the emails will end up belonging to a single person. The total number of emails will be NK, and we
+    need to sort these emails. DFS traversal will take NK operations as no email will be traversed more than once.
+    Space complexity: O(NK), building the adjacency list takes O(NK) space. In the end, visited will contain
+    all the emails hence it will use O(NK) space. Also, the call stack for DFS will use O(NK) space in the worst case.
+    """
+
+    def dfs(vertex):
+        # Each vertex/node is an email address
+        component.append(vertex)
+        visited.add(vertex)
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                dfs(neighbor)
 
     graph = defaultdict(list)
-    build_graph()
-    res, visited, = [], set()
     for account in accounts:
+        name, first_email = account[0], account[1]
+        n = len(account)
+        for i in range(2, n):
+            graph[account[i]].append(first_email)
+            graph[first_email].append(account[i])
+    res = []
+    visited = set()
+    for account in accounts:
+        name = account[0]
         n = len(account)
         for i in range(1, n):
-            if account[i] not in visited:  # This vertex hasn't been explored yet. Each account[i] is an email address
-                user_name, emails = account[0], []
-                dfs(account[i], emails)  # Collect the vertices/emails that belong to the same connected component
-                res.append([user_name] + sorted(emails))
+            email = account[i]
+            if email not in visited:
+                # This vertex hasn't been explored yet. visited set ensures that DFS gets called for every component and
+                # NOT on every node.
+                component = []
+                # Collect the vertices/emails that belong to the same connected component
+                dfs(email)
+                res.append([name] + sorted(component))
     return res
 
 
