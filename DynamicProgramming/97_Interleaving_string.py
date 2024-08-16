1bf5a2d7e72451906b531454b52384e029cd0645
Anouer <hermassi.anouer@gmail.com>
2024-08-16 17:19:57 -0400

    Interleaving string - v5 update

--- DynamicProgramming/97_Interleaving_string.py
+++ DynamicProgramming/97_Interleaving_string.py
@@ -203,3 +203,14 @@ def is_interleave_v5(s1, s2, s3):
                 use_s2 = dp[j - 1] and s2[j - 1] == s3[i + j - 1]
                 dp[j] = use_s1 or use_s2
     return dp[m]
+    # Alternatively:
+    # dp[0] = True
+    # for j in range(1, m + 1):
+    #     dp[j] = dp[j - 1] and s2[j - 1] == s3[j - 1]
+    # for i in range(1, n + 1):
+    #     dp[0] = dp[0] and s1[i - 1] == s3[i - 1]
+    #     for j in range(1, m + 1):
+    #         use_s1 = dp[j] and s1[i - 1] == s3[i + j - 1]
+    #         use_s2 = dp[j - 1] and s2[j - 1] == s3[i + j - 1]
+    #         dp[j] = use_s1 or use_s2
+    # return dp[m]
