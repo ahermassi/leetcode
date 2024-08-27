25d34580c6a3b31374b035cd9ed8597c4ebd0827
Anouer <hermassi.anouer@gmail.com>
2024-08-27 08:12:10 -0400

    Making a large island - v1 update

--- DFS/827_Making_A_Large_Island.py
+++ DFS/827_Making_A_Large_Island.py
@@ -9,27 +9,33 @@ from collections import defaultdict
 
 def largest_island_v1(grid):
     """ Brute force. TLE.
-        For each 0 in the grid, let's temporarily change it to a 1, then count the size of the group from that square.
-        For each 0, change it to a 1, then do a depth first search to find the size of that component. The answer is
-        the maximum size component found. Of course, if there is no 0 in the grid, then the answer is the size of the
-        whole grid.
+
+         For each 0 in the grid, temporarily change it to a 1, then count the size of the group from that square.
+         From each flipped 0, we perform a DFS to find the size of that component. The answer is the maximum size
+         component found.
+
+         If there is no 0 in the grid, then the answer is the size of the entire grid.
+
     Time complexity: O(N^4)
     Space complexity: O(N^2)
     """
 
-    def dfs(i, j, visited):
+    def dfs(i, j):
         if not 0 <= i < n or not 0 <= j < n or not grid[i][j] or (i, j) in visited:
             return 0
         visited.add((i, j))
-        return 1 + dfs(i - 1, j, visited) + dfs(i + 1, j, visited) + dfs(i, j - 1, visited) + dfs(i, j + 1, visited)
+        area = 1
+        for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+            area += dfs(x, y)
+        return area
 
-    n = len(grid)
-    res = 0
+    n, res = len(grid), 0
     for i in range(n):
         for j in range(n):
             if not grid[i][j]:
                 grid[i][j] = 1
-                res = max(res, dfs(i, j, set()))  # A new 'visited' set for every dfs exploration from a 0
+                visited = set()  # A new visited set for every DFS exploration from a 0
+                res = max(res, dfs(i, j))
                 grid[i][j] = 0
     return res if res != 0 else n * n
 
