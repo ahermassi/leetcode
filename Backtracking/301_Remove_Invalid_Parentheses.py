5f7ca73897aa5898fbac6ae1c0510a0a45a56fd3
Anouer <hermassi.anouer@gmail.com>
2024-09-02 10:40:33 -0400

    Remove invalid parentheses - v1 update

--- Backtracking/301_Remove_Invalid_Parentheses.py
+++ Backtracking/301_Remove_Invalid_Parentheses.py
@@ -7,91 +7,109 @@ from collections import deque
 
 
 def remove_invalid_parentheses_v1(s):
-    """ Since we don't know which of the brackets can possibly be removed, we try out all the options!
-        For every bracket we have two choices:
-            - Either it can be considered a part of the final expression, OR
-            - It can be ignored i.e. we can delete it from our final expression.
-        Such kind of problems where we have multiple options and we have no strategy or metric of deciding greedily
-        which option to take and we try out all of the options and see which ones lead to an answer are perfect
-        candidates for the programming paradigm, Backtracking.
+    """ Since we don't know which of the brackets can possibly be removed, we try all the options. The only condition
+         is that we should be removing the minimum number of brackets to make an invalid expression, valid. If this
+         condition was not present, we could potentially remove most of the brackets and come down to say 2 brackets in
+         the end which form () and that would be a valid expression.
 
-        The state of the recursion is defined by the index which we are currently processing in the original
-        expression. Also, we have a second variable 'balance' that represents the number of left and right parentheses
-        we have added to our expression till now, incremented when we add a left bracket and decremented when we add a
-        closing bracket, so that a valid expression should have a balance value of 0.
+         For every bracket we have two choices:
 
-        If the current character i.e. s[index] is neither a closing or an opening parenthesis, then we simply add this
-        character to our final solution string for the current recursion.
-        However, if the current character is either of the two brackets i.e. s[index] == '(' or s[index] == ')', then
-        we have two options: We can either discard this character by marking it an invalid character, or we can
-        consider this bracket to be a part of the final expression.
+             - Either it can be considered a part of the final expression, OR
+             - It can be ignored i.e. we can delete it from our final expression.
 
-        When all of the parentheses in the original expression have been processed, we simply check if the expression
-        represented by 'path' i.e. the expression formed till now is valid. The way we check if the final expression
-        is valid is by looking at the value of 'balance'. For an expression to be valid balance == 0. If it is indeed
-        valid, then it could be one of our possible solutions.
+         Such kind of problems where we have multiple options and have no strategy or metric of deciding greedily
+         which option to take and try all the options and see which ones lead to an answer are perfect candidates for
+         backtracking.
 
-        Even though we have a valid expression, we also need to keep track of the number of removals we did to get
-        this expression. This is done by another DERIVED variable called 'removal_count'. Once recursion finishes, we
-        check if the current value of 'removal_count' is <= the least number of steps we took to form a valid
-        expression till now i.e. the global minima. If this is not the case, we don't record the new expression, else
-        we record it.
+         The state of the recursion is defined by the index which we are currently processing in the original
+         expression. Also, we have a second variable 'balance' that represents the number of left and right parentheses
+         we have added to the expression till now, incremented when we add a left bracket and decremented when we add a
+         closing bracket, so that a valid expression should have a balance value of 0.
 
-        One small optimization that we can do from an implementation perspective is introducing some sort of pruning
-        to our algorithm. Right now we simply go till the very end i.e. process all of the parentheses and when we are
-        done processing all of them, we check if the expression we have can be considered or not. We have to wait till
-        the very end to decide if the expression formed in recursion is a valid expression. Is there a way for us to
-        cutoff some of the recursion paths early on because they wouldn't lead to a solution? The answer to this is Yes!
-        The optimization is based on the following idea.
+            - If the current character i.e. s[index] is neither a closing nor an opening parenthesis, then we simply
+               add this character to the final solution string for the current recursion.
 
-        For a left bracket encountered during recursion, if we decide to consider it, then it may or may not lead to an
-        invalid final expression. It may lead to an invalid expression eventually if there are no matching closing
-        bracket available afterwards. But, we don't know for sure if this will happen or not.
+            - However, if the current character is either of the two brackets, then we have two options: either
+               discard this character by marking it an invalid character, or consider it part of the final expression.
 
-        However, for a closing bracket, if we decide to keep it as a part of our final expression (remember for every
-        bracket we have two options, either to keep it or to remove it and recurse further) and there is no
-        corresponding opening bracket to match it in the expression till now, then it will definitely lead to an
-        invalid expression no matter what we do afterwards.
+            - When all the parentheses in the original expression have been processed, we simply check if the expression
+               represented by the current path, i.e. the expression formed till now, is valid. The way we check if the
+               final expression is valid is by looking at the value of the balance. If it is indeed valid, then it could
+               be one of the possible solutions.
 
-        That is why, whenever we encounter a right parenthesis, we check if balance > 0 (it also means
-        left_parentheses > right_parentheses) . If this is the case, then only we consider that right parenthesis and
-        recurse further. Otherwise, we don't as we know it will make the expression invalid. This simple optimization
-        saves a lot of runtime.
-    Time complexity: O(2^N), since in the worst case we will have only left parentheses in the expression and for every
-    bracket we will have two options i.e. whether to remove it or consider it
+         Even though we have a valid expression, we also need to keep track of the number of removals we did to get
+         this expression. This is done by another DERIVED variable called removal_count. Once recursion finishes, we
+         check if the current value of removal_count is <= the least number of steps we took to form a valid
+         expression till now i.e. the global minima. If this is not the case, we don't record the new expression, else
+         we record it.
+
+         One small optimization that we can do from an implementation perspective is introducing some sort of pruning
+         to the algorithm. Right now we process all the parentheses and finally check if the expression we have can be
+         considered or not. We have to wait till the very end to decide if the expression formed in recursion is a valid
+         expression. Is there a way to cut down on the recursion paths early because they wouldn't lead to a solution?
+         The optimization is based on the following idea.
+
+         For a left bracket encountered during recursion, if we decide to consider it, then it may or may not lead to an
+         invalid final expression. It may lead to an invalid expression eventually if there is no matching closing
+         bracket available afterwards. But, we don't know for sure if this will happen or not.
+
+         However, for a closing bracket, if we decide to keep it in the final expression (remember for every bracket we
+         have two options, either to keep it or to remove it and recurse further) and there is no corresponding opening
+         bracket to match it in the expression so far, then it will definitely lead to an invalid expression no matter
+         what we do afterwards.
+
+         Therefore, whenever we encounter a right parenthesis, we check if balance > 0 (it also means
+         left parentheses > right parentheses) . If this is the case, then only we consider that right parenthesis and
+         recurse further. Otherwise, we don't as we know it will make the expression invalid.
+
+         That is why, in addition to having the index in the original string/expression which we are currently
+         processing and the expression string formed si far, we also keep track of the number of left and right
+         parentheses.
+
+         !!! IMPORTANT !!!
+
+         An important thing to note is that there are multiple ways of reaching the same solution. Suppose the optimal
+         number of parentheses to be removed to make the original expression valid is K. We can remove multiple
+         different sets of K brackets that will eventually give us the same final expression. But, each valid expression
+         should be recorded only once. We have to take care of this in the solution by using a hash set to record the
+         valid expressions.
+
+    Time complexity: O(N * 2^N), in the worst case we have only left parentheses in the expression and for every bracket we
+    have two options i.e. whether to remove it or leave it. We also need O(N) to create the output expression from the
+    path.
     Space complexity: O(N), the maximum depth of recursion decides the stack space used. Since we process one character
-    at a time and the base case for the recursion is when we have processed all of the characters of the expression
-    string, the size of the stack would be O(N)
+    at a time and the base case for the recursion is when we have processed all the characters of the expression string,
+    the size of the stack would be O(N).
     """
 
-    def dfs(index, balance, path):
+    def dfs(index, left, right, path):
         if index == n:
-            if balance == 0:
+            if left == right:
                 removal_count = len(s) - len(path)
-                # If the current count of brackets removed <= current minimum, ignore update the current minimum count
+                # If the current count of brackets removed <= current minimum, don't update the global minimum count
                 if removal_count <= min_removals[0]:
-                    res.add(path)
+                    res.add(''.join(path))
                     min_removals[0] = removal_count
-                    return
             return
         c = s[index]
         if c not in '()':
-            dfs(index + 1, balance, path + c)
+            dfs(index + 1, left, right, path + [c])
         elif c == '(':
-            dfs(index + 1, balance + 1, path + c)  # If the current parenthesis is an opening bracket, we consider it,
-            # increment the balance, and  move forward
-            dfs(index + 1, balance, path)  # Ignore the bracket
+            # If the current parenthesis is an opening bracket, we consider it
+            dfs(index + 1, left + 1, right, path + [c])
+            # Ignore the bracket
+            dfs(index + 1, left, right, path)
         else:
-            # If the current parenthesis is a closing bracket, we consider it only if we have more number of opening
-            # brackets, i.e. balance > 0
-            if balance > 0:
-                dfs(index + 1, balance - 1, path + c)
-            dfs(index + 1, balance, path)
+            if left > right:
+                # If the current parenthesis is a closing bracket, we consider it only if we have more opening brackets
+                dfs(index + 1, left, right + 1, path + [c])
+            # Ignore the bracket
+            dfs(index + 1, left, right, path)
 
     n, res = len(s), set()
     min_removals = [float('inf')]
-    dfs(0, 0, '')
-    return res if res else [""]
+    dfs(0, 0, 0, [])
+    return res
 
 
 def remove_invalid_parentheses_v2(s):
