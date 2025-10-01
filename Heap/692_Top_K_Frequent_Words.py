30ac6a70a8deee19116813c5d9742a93bf12515e
anwar <hermassi.anouer@gmail.com>
2025-10-01 13:28:29 -0400

    Top k frequent words - v1 update

--- Heap/692_Top_K_Frequent_Words.py
+++ Heap/692_Top_K_Frequent_Words.py
@@ -7,18 +7,19 @@ from heapq import heappush, heappop
 import unittest2 as unittest
 
 
-class Element:
+class HeapElement:
     def __init__(self, count, word):
         self.count = count
         self.word = word
 
     def __lt__(self, other):
         if self.count == other.count:
-            return self.word > other.word  # Pay attention to this ! Because the result will be reversed, we reverse
-            # the __lt__ logic here. Example: heap = [(3, 'x'), (4, 'a')], and we want to push (4, 'b').
-            # If we allow the default lexicographical order, the heap will be: [(3, 'x'), (4, 'a'), (4, 'b')]
+            return self.word > other.word
+            # Pay attention to this ! Because the result will be reversed, we reverse the __lt__ logic here.
+            # Example: heap = [(3, 'x'), (4, 'a')], and we want to push (4, 'b').
+            # If we allow the default lexicographical order, the heap will be: [(3, 'x'), (4, 'a'), (4, 'b')].
             # After we pop the elements and reverse the result: res = ['b', 'a', 'x'], which is NOT the desired output.
-            # For this reason, we reverse the lexicographical order of elements which have same count.
+            # For this reason, we reverse the lexicographical order of elements with the same count.
             # --> heap = [(3, 'x'), (4, 'b'), (4, 'a')], in which we considered (4, 'b') 'less' than (4, 'a')
             # --> res = ['a', 'b', 'x']
         return self.count < other.count
@@ -28,24 +29,27 @@ class Element:
 
 
 def top_k_frequent_v1(words, k):
-    """ Count the frequency of each word, then add it to heap that stores the best k candidates. Each time a new word
-        is encountered, the front element of heap (min frequency) is popped. We end up with a heap containing the k
+    """ Count the frequency of each word, then add it to a min heap that stores the best k candidates. Each time a new
+        word is added, the front element of heap (min frequency) is popped. We end up with a heap containing the k
         most frequent words.
+
     Time complexity: O(N + N logK) = O(N logK); O(N) to build frequency map, and O(N logK) to build the heap of size k
     Space complexity: O(N)
     """
     heap, res = [], []
     counter = Counter(words)
-    for key, value in counter.items():
-        heappush(heap, Element(value, key))
+    for word, count in counter.items():
+        heappush(heap, HeapElement(count, word))
         if len(heap) > k:
             heappop(heap)
-    while heap:  # return [e.word for e in heap] is WRONG. Successive heappop() is what gives correct results
-        # because heap structure (array) is such that heap[k] <= heap[2k] and heap[k] <= heap[2k+1], not that the heap
-        # stores its elements in sorted order. The only exception is heap[0] which ALWAYS contains the min value.
+    while heap:
+        # return [e.word for e in heap] is WRONG. Successive heappop() is what gives correct results because heap
+        # structure (array) is such that heap[k] <= heap[2k] and heap[k] <= heap[2k+1], not that the heap stores the
+        # elements in sorted order. The only exception is heap[0] which ALWAYS contains the min value.
         res.append(heappop(heap).word)
-    return res[::-1]  # Because we want the words sorted by frequency from highest to lowest, we reverse the result
-    # as the heap is a min heap
+    # Because we want the words sorted by frequency from highest to lowest, we reverse the result because the heap is
+    # a min heap
+    return res[::-1]
 
 
 def top_k_frequent_v2(words, k):
